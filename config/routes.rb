f88d5a2209180105debcb0cc6a2ff96b6223945a
Sunny Ripert <negatif@gmail.com>
2016-07-27 12:25:56 +0200

    Replace controllers with one component controller (#29)

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,6 @@ Kitten::Engine.routes.draw do
   if Rails.env.development?
     get '/sassdoc', to: redirect('/sassdoc/index.html')
 
-    namespace :atoms do
-      get '/typography/:name' => 'typography#show'
-      get '/buttons/:name' => 'buttons#show'
-      get '/form/:name' => 'form#show'
-    end
-
-    namespace :molecules do
-      get '/form/:name' => 'form#show'
-      get '/simulators/:name' => 'simulators#show'
-    end
-
-    namespace :organisms do
-      get '/simulators/:name' => 'simulators#show'
-    end
+    get ':type/:group/:name' => 'components#show'
   end
 end
