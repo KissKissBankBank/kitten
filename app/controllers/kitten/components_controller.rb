97aa65bd2a0f2401384db27678d58a7282d7e39b
Camille Delbove <camille.delbove@gmail.com>
2016-07-29 18:57:01 +0200

    Style guide base

--- app/controllers/kitten/components_controller.rb
+++ app/controllers/kitten/components_controller.rb
@@ -1,20 +1,8 @@
 module Kitten
   class ComponentsController < Kitten::ApplicationController
-    TYPES = %w(atoms molecules organisms)
-    GROUPS_MATCH = /\A[a-z-]+\Z/
-    NAMES_MATCH = /\A[a-z-]+\Z/
-
     def show
-      type = params[:type]
-      group = params[:group]
-      name = params[:name]
-
-      # Protect from people accessing other templates
-      raise "Unavailable type #{type.inspect}" unless TYPES.include?(type)
-      raise "Unavailable group #{group.inspect}" unless group =~ GROUPS_MATCH
-      raise "Unavailable name #{group.inspect}" unless name =~ NAMES_MATCH
-
-      render template: "kitten/#{type}/#{group}/#{name}"
+      render template: "layouts/kitten/components",
+             layout: "kitten/application"
     end
   end
 end
