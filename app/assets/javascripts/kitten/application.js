292d04736cb92f11fc5d9e4e46d92ae4df1a43ae
Sunny Ripert <sunny@sunfox.org>
2016-08-01 20:34:07 +0200

    First version of a simulator with in-browser babel

---
+++ app/assets/javascripts/kitten/application.js
@@ -0,0 +1,6 @@
+//= require kitten/helpers/dom/classes
+//= require kitten/helpers/events/keyboard
+//= require kitten/components/form/slider
+//= require kitten/components/icons/grabber_icon
+//= require kitten/components/simulators/loan_simulator
+//= require kitten/components/simulators/simulator_result
