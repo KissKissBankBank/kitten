fab0d81c33fbf23b79fcf34203f390114356d184
Fanny Cheung <fanny@ynote.hk>
2016-08-04 13:18:01 +0200

    Update npm exports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 var path = require('path');
+var getLoadPaths = function() {
+  var kittenPath = require.resolve('kitten');
+  var sassyMapsPath = require.resolve('sassy-maps');
+  var modularscalePath = require.resolve('modularscale-sass');
+
+  return [
+    path.join(kittenPath, '..', '..', 'assets', 'stylesheets'),
+    path.join(sassyMapsPath, '..'),
+    path.join(modularscalePath, '..')
+  ];
+}
 
 module.exports = {
-  loadPaths: function() {
-    var kittenPath = require.resolve('kitten');
-    var sassyMapsPath = require.resolve('sassy-maps');
-    var modularscalePath = require.resolve('modularscale-sass');
-
-    return [
-      path.join(kittenPath, '..', '..', 'assets', 'stylesheets'),
-      path.join(sassyMapsPath, '..'),
-      path.join(modularscalePath, '..')
-    ];
-  }
+  loadPaths: getLoadPaths()
 };
