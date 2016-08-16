cf61e682fd0d0efae36049ad2e96f2241c34273a
Fanny Cheung <fanny@ynote.hk>
2016-08-16 16:05:02 +0200

    Add external rich link molecule and React component

--- app/assets/javascripts/kitten/application.js
+++ app/assets/javascripts/kitten/application.js
@@ -4,3 +4,4 @@
 //= require kitten/components/icons/grabber-icon
 //= require kitten/components/simulators/loan-simulator
 //= require kitten/components/simulators/simulator-result
+//= require kitten/components/links/external-rich-link
