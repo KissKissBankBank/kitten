cc50e25eef43aaa82247dd9b2b8dda627bb87d13
Sunny Ripert <negatif@gmail.com>
2016-08-17 12:33:20 +0200

    Simulator fixes (#65)

--- app/assets/javascripts/kitten/application.js
+++ app/assets/javascripts/kitten/application.js
@@ -1,6 +1,6 @@
 //= require kitten/helpers/events/keyboard
-//= require kitten/helpers/numbers/is_number
+//= require kitten/helpers/numbers/is-number
 //= require kitten/components/form/slider
-//= require kitten/components/icons/grabber_icon
-//= require kitten/components/simulators/loan_simulator
-//= require kitten/components/simulators/simulator_result
+//= require kitten/components/icons/grabber-icon
+//= require kitten/components/simulators/loan-simulator
+//= require kitten/components/simulators/simulator-result
