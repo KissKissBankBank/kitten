fc60e3cc35c74d8c3a2d0a6c4de179f7388875b2
Fanny Cheung <fanny@ynote.hk>
2016-08-22 17:51:15 +0200

    Improve comments

--- app/helpers/kitten/webpack_helper.rb
+++ app/helpers/kitten/webpack_helper.rb
@@ -1,9 +1,9 @@
 module Kitten
   module WebpackHelper
     def webpack_stylesheet_tags(entry_point)
-      # webpack-rails provides a 'webpack_asset_paths' helper. This latter only
+      # webpack-rails provides a `webpack_asset_paths` helper. This latter only
       # outputs a relative path when we are on production.
-      # As we serve assets through Cloudfront cdn, we need to append the cdn url
+      # As we serve assets through Cloudfront CDN, we need to append the CDN URL
       # in the stylesheet asset paths.
       host = served_by_asset_host? ? "#{Kitten.configuration.asset_host}" : ''
 
