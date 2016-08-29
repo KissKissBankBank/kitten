b6048c7295fb9f31eeaf07c03f34734554eea895
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2016-08-29 14:48:17 +0200

    Responsive and more fixes

---
+++ assets/javascripts/kitten/components/buttons/button-image-with-text-and-badge.js
@@ -0,0 +1,68 @@
+window.ButtonImageWithTextAndBadge = React.createClass({
+  propTypes: {
+    children: React.PropTypes.string,
+    onClick: React.PropTypes.func,
+    isExpanded: React.PropTypes.bool,
+    text: React.PropTypes.string,
+    title: React.PropTypes.string,
+    srcImg: React.PropTypes.string,
+    widthImg: React.PropTypes.number,
+    heightImg: React.PropTypes.number,
+    altImg: React.PropTypes.string,
+    notifications: React.PropTypes.number,
+  },
+  // Lifecycle
+  getDefaultProps: function() {
+    return {
+      children: 'Toggle button',
+      onClick: () => {},
+    }
+  },
+
+  render: function() {
+    const { className, isExpanded, text, title, classNameText,
+            srcImg, widthImg, heightImg, altImg, notifications,
+            ...rest } = this.props
+
+    let buttonClassName = classNames('k-ButtonImageWithText', className)
+    let textClassName = classNames('k-ButtonImageWithText__text', classNameText)
+
+    if (notifications > 0) {
+      buttonClassName = classNames('k-ButtonWithBadge', buttonClassName)
+    }
+
+    return(
+      <button className={ buttonClassName }
+              aria-haspopup="true"
+              aria-expanded={ isExpanded }
+              title={ title }
+              { ...rest }>
+        <span className="k-ButtonWithBadge">
+          <span ref="buttonImage" className="k-ButtonImage">
+            <img className="k-ButtonImage__img"
+                 src={ srcImg }
+                 width={ widthImg }
+                 height={ heightImg }
+                 alt={ altImg } />
+          </span>
+          <span className="k-ButtonWithBadge__badge k-Badge">
+            { notifications }
+          </span>
+        </span>
+        <p className={ textClassName }>{ text }</p>
+      </button>
+    )
+  }
+})
+
+// <button class="k-ButtonWithBadge">
+//       <span class="k-ButtonImage k-ButtonImage--tiny">
+//         <img class="k-ButtonImage__img"
+//              src="https://placekitten.com/g/40/40"
+//              width="40"
+//              height="40"
+//              alt="" />
+//       </span>
+//
+//       <span class="k-ButtonWithBadge__badge k-Badge">4</span>
+//     </button>
