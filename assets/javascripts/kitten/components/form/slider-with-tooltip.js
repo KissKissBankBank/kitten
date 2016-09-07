250df41670bee14a4384bbc7ad8c53505642aa95
Sunny Ripert <sunny@sunfox.org>
2016-09-07 12:19:05 +0200

    Spaces inside {}

--- assets/javascripts/kitten/components/form/slider-with-tooltip.js
+++ assets/javascripts/kitten/components/form/slider-with-tooltip.js
@@ -4,17 +4,17 @@ window.SliderWithTooltip = React.createClass({
     const leftStyle = { left: percentage }
 
     return (
-      <div className={classNames('k-SliderTooltip', tooltipClass)}>
+      <div className={ classNames('k-SliderTooltip', tooltipClass) }>
         <div className="k-SliderTooltip__tip">
-          <div className="k-SliderTooltip__tip__content" style={leftStyle}>
-            {this.props.children}
+          <div className="k-SliderTooltip__tip__content" style={ leftStyle }>
+            { this.props.children }
           </div>
         </div>
         <div className="k-SliderTooltip__arrow">
           <div className="k-SliderTooltip__arrow__content"
-               style={leftStyle}></div>
+               style={ leftStyle }></div>
         </div>
-        <Slider {...sliderProps} />
+        <Slider { ...sliderProps } />
       </div>
     )
   },
