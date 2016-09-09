77e34ea98ce4c193900892f3d46cca40da1cc4e2
Camille Delbove <camilledel@users.noreply.github.com>
2016-09-09 15:20:08 +0200

    Update styleguide (#88)

--- app/assets/javascripts/kitten/dropdown.js
+++ app/assets/javascripts/kitten/dropdown.js
@@ -1,13 +1,24 @@
-var openItems = $('.karl-Menu__listItem').each(function(key, element){
-  $(element).on("click", function(){
-    $(element).toggleClass("is-opened");
-    $(element).siblings(".karl-Menu__subList").toggleClass('is-opened');
-  })
-});
+var subItems = $("[data-karl-menu] a")
 
-var openSubItems = $('.karl-Menu__subListItem').each(function(key, element){
-  $(element).on("click", function(){
-    $(element).toggleClass("is-opened");
-    $(element).siblings(".karl-Menu__subSubList").toggleClass('is-opened');
-  })
+$(document).ready(function(){
+  subItems.each(function(){
+    var $handle  = $(this)
+
+    // Parents
+    if ($handle.next('ul').find('li').length > 0) {
+      $handle.addClass('with-arrow');
+
+      $handle.on("click", function(){
+        $handle.toggleClass("is-opened");
+        $handle.siblings("ul").toggleClass('is-opened');
+      });
+    }
+    // Child
+    else {
+      $handle.on("click", function(){
+        $('.is-selected').removeClass('is-selected');
+        $handle.addClass('is-selected');
+      });
+    }
+  });
 });
