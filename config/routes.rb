336e46b8b8a3dc9778263bea6f2eeba62151b128
Camille Delbove <camilledel@users.noreply.github.com>
2016-09-09 15:36:40 +0200

    Add branding section (#95)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Kitten::Engine.routes.draw do
   get '/sassdoc', to: redirect('/sassdoc/index.html')
 
-  get '/', to: redirect('components')
+  get '/', to: redirect('branding')
+  get 'branding' => 'branding#index'
   get 'components' => 'components#index'
   get 'components/:type/:group/:name' => 'components#show'
   get 'playground' => 'playgrounds#show'
