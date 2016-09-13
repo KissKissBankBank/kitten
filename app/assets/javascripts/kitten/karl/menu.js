bc38069e3e2fe68ea64e2f6287123e8a903209a1
Camille Delbove <camille.delbove@gmail.com>
2016-09-13 13:01:26 +0200

    Migration dropdown.js to menu.js

--- app/assets/javascripts/kitten/karl/menu.js
+++ app/assets/javascripts/kitten/karl/menu.js
@@ -7,3 +7,30 @@ window.karl = (function(karl) {
 
   return karl
 })(window.karl || {})
+
+
+// Dropdown list
+var subItems = $("[data-karl-menu] a")
+
+$(document).ready(function(){
+  subItems.each(function(){
+    var $handle  = $(this)
+
+    // Parents
+    if ($handle.next('ul').find('li').length > 0) {
+      $handle.addClass('with-arrow');
+
+      $handle.on("click", function(){
+        $handle.toggleClass("is-opened");
+        $handle.siblings("ul").toggleClass('is-opened');
+      });
+
+    // Child
+    } else {
+      $handle.on("click", function(){
+        $('.is-selected').removeClass('is-selected');
+        $handle.addClass('is-selected');
+      });
+    }
+  });
+});
