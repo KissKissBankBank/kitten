b41ff5f7c3c35d13b2d49ad3eb2fe32ffcd05933
Sunny Ripert <sunny@sunfox.org>
2016-10-10 11:38:28 +0200

    Remember overgrid on page reload (#144)

--- app/assets/javascripts/kitten/karl.js
+++ app/assets/javascripts/kitten/karl.js
@@ -1,9 +1,11 @@
 //= require kitten/karl/init-toggle-class-listener
 //= require kitten/karl/display-shortcut
+//= require kitten/karl/overgrid
 //= require kitten/karl/menu
 
 $(document).ready(function() {
   window.karl.initToggleClassListener()
   window.karl.displayShortcut()
   window.karl.initMenu()
+  window.karl.initOvergrid()
 })
