e0c77dfbc33f459a9c78b55a5ec32b94c370bb7e
Fanny Cheung <fanny@ynote.hk>
2016-10-31 14:44:18 +0100

    Remove useless hoc

--- assets/javascripts/kitten/karl/molecules/search.js
+++
@@ -1,9 +0,0 @@
-import defaultProps from 'kitten/hoc/default-props'
-import React from 'react'
-import Search from 'kitten/components/form/search'
-
-// KarlSearch
-const KarlSearch = defaultProps(Search, {
-})
-
-export { KarlSearch }
