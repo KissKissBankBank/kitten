3e7063bcf30c5fbb266ba864cba5d20dbae131cf
Sunny Ripert <sunny@sunfox.org>
2016-12-08 15:06:34 +0100

    Path fixes

--- src/index.js
+++ src/index.js
@@ -1,43 +1,42 @@
 var path = require('path');
-var kittenPath = require.resolve('kitten');
 
 var getScssPaths = function() {
   var kittenStylesheetsPath = path.join(
-    kittenPath,
-    '../../assets/stylesheets'
+    __dirname,
+    '../assets/stylesheets'
   )
   var karlStylesheetsPath = path.join(
-    kittenPath,
-    '../../app/assets/stylesheets'
+    __dirname,
+    '../app/assets/stylesheets'
   )
   var vendorStylesheetsPath = path.join(
-    kittenPath,
-    '../../vendor/assets/stylesheets'
+    __dirname,
+    '../vendor/assets/stylesheets'
   )
-  var modularscalePath = require.resolve('modularscale-sass');
+  var modularscalePath = require.resolve('modularscale-sass')
 
   return [
     kittenStylesheetsPath,
     karlStylesheetsPath,
     vendorStylesheetsPath,
     path.join(modularscalePath, '..'),
-  ];
+  ]
 }
 
 var getJsPaths = function() {
   return [
-    path.join(kittenPath, '..', '..', 'assets/javascripts'),
-  ];
+    path.join(__dirname, '../assets/javascripts'),
+  ]
 }
 
 var getImagesPaths = function() {
   return [
-    path.join(kittenPath, '..', '..', 'assets/images'),
-  ];
+    path.join(__dirname, '../assets/images'),
+  ]
 }
 
 module.exports = {
   scssPaths: getScssPaths(),
   jsPaths: getJsPaths(),
   imagesPaths: getImagesPaths(),
-};
+}
