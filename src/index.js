a020170b119fd79e2c475d9f8d83948d7619e5da
Sunny Ripert <sunny@sunfox.org>
2016-12-08 16:18:49 +0100

    Path fixes (#275)

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
-var path = require('path');
-var kittenPath = require.resolve('kitten');
+var path = require('path')
 
 var getScssPaths = function() {
   var kittenStylesheetsPath = path.join(
-    kittenPath,
-    '../../assets/stylesheets'
+    __dirname,
+    '../assets/stylesheets'
   )
   var karlStylesheetsPath = path.join(
-    kittenPath,
-    '../../app/assets/stylesheets'
+    __dirname,
+    '../app/assets/stylesheets'
   )
   var vendorStylesheetsPath = path.join(
-    kittenPath,
-    '../../vendor/assets/stylesheets'
+    __dirname,
+    '../vendor/assets/stylesheets'
   )
-  var modularscalePath = require.resolve('modularscale-sass');
+
+  var sassyMapsStylesheetsPath = path.join(
+    __dirname,
+    '../vendor/assets/stylesheets/kitten/sassy-maps/' +
+    'node_modules/sassy-maps/sass'
+  )
+  var modularscalePath = require.resolve('modularscale-sass')
 
   return [
     kittenStylesheetsPath,
     karlStylesheetsPath,
     vendorStylesheetsPath,
+    sassyMapsStylesheetsPath,
     path.join(modularscalePath, '..'),
-  ];
+  ]
 }
 
 var getJsPaths = function() {
   return [
-    path.join(kittenPath, '..', '..', 'assets/javascripts'),
-  ];
+    path.join(__dirname, '../assets/javascripts'),
+  ]
 }
 
 var getImagesPaths = function() {
   return [
-    path.join(kittenPath, '..', '..', 'assets/images'),
-  ];
+    path.join(__dirname, '../assets/images'),
+  ]
 }
 
 module.exports = {
   scssPaths: getScssPaths(),
   jsPaths: getJsPaths(),
   imagesPaths: getImagesPaths(),
-};
+}
