29ef40f7dca0a69f00f286d21470e24a55270e83
Camille Delbove <camille.delbove@gmail.com>
2016-12-15 19:07:59 +0100

    Add function and change name option

--- assets/javascripts/kitten/components/form/form-amount-and-currency.js
+++ assets/javascripts/kitten/components/form/form-amount-and-currency.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import SelectWithState from 'kitten/components/form/select-with-state'
 
-class FormAmountAndCurrency extends React.Component {
-  render() {
-    return (
-      <div className="k-FormComposer">
-        <div className="k-FormComposer__element">
-          <input className="k-TextInput k-TextInput--tiny"
-                 type="text"
-                 placeholder="Lorem ipsum dolor sit amet" />
-        </div>
-
-        <div className="k-FormComposer__element k-FormComposer__element--main">
-          <SelectWithState name="currency"
-                           options={ this.props.currencyOptions }
-                           value={ this.props.value } />
-        </div>
+function FormAmountAndCurrency(props) {
+  return (
+    <div className="k-FormComposer">
+      <div className="k-FormComposer__element">
+        <input className="k-TextInput k-TextInput--tiny"
+               type="text"
+               placeholder={ props.placeholder } />
       </div>
-    )
-  }
+
+      <div className="k-FormComposer__element k-FormComposer__element--main">
+        <SelectWithState name="currency"
+                         options={ props.currencyOptions }
+                         value={ props.value } />
+      </div>
+    </div>
+  )
 }
 
 export default FormAmountAndCurrency
--- assets/javascripts/kitten/components/form/form-phone-number.js
+++ assets/javascripts/kitten/components/form/form-phone-number.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import SelectWithState from 'kitten/components/form/select-with-state'
 
-class FormPhoneNumber extends React.Component {
-  render() {
-    return (
-      <div className="k-FormComposer">
-        <div className="k-FormComposer__element k-FormComposer__element--main">
-          <SelectWithState name="currency"
-                           options={ this.props.currencyOptions }
-                           value={ this.props.value } />
-        </div>
-
-        <div className="k-FormComposer__element">
-          <input className="k-TextInput k-TextInput--tiny"
-                 type="text"
-                 placeholder="Lorem ipsum dolor sit amet" />
-        </div>
+function FormPhoneNumber(props) {
+  return (
+    <div className="k-FormComposer">
+      <div className="k-FormComposer__element k-FormComposer__element--main">
+        <SelectWithState name="currency"
+                         options={ props.areaZoneOptions }
+                         value={ props.value } />
       </div>
-    )
-  }
+
+      <div className="k-FormComposer__element">
+        <input className="k-TextInput k-TextInput--tiny"
+               type="text"
+               placeholder={ props.placeholder } />
+      </div>
+    </div>
+  )
 }
 
 export default FormPhoneNumber
