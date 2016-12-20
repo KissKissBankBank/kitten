59b2a4001648a9ec42f8f39344319418e0da520c
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2016-12-20 12:24:31 +0100

    Add DocLinkBox testing

---
+++ assets/javascripts/kitten/components/box/doc-link-box.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import DocLinkBox from '../../components/box/doc-link-box'
+import LinkBox from '../../components/box/link-box'
+
+describe('<DocLinkBox />', () => {
+  const component = shallow(<DocLinkBox />)
+
+  it('renders a <LinkBox />', () => {
+    expect(component).to.have.type(LinkBox)
+  })
+
+  it('renders a svg icon', () => {
+    expect(component).to.have.prop('displayIcon', true)
+    expect(component.find('.k-LinkBox__icon--svg')).to.have.length(1)
+  })
+})
