4bd8ce527afe4d6c1d3d8976f631cc1e16c64687
Fanny Cheung <fanny@ynote.hk>
2016-12-20 20:00:45 +0100

    Update spec/dummy components files architecture

--- app/controllers/kitten/components_controller.rb
+++ app/controllers/kitten/components_controller.rb
@@ -1,6 +1,5 @@
 module Kitten
   class ComponentsController < Kitten::ApplicationController
-    TYPES = %w(atoms molecules organisms).freeze
     NAME_MATCH = /\A[a-z-]+\Z/.freeze
 
     def index
@@ -9,16 +8,15 @@ module Kitten
     end
 
     def show
-      type = params[:type]
       group = params[:group]
       name = params[:name]
 
       # Protect from people accessing other templates
-      unless TYPES.include?(type) && group =~ NAME_MATCH && name =~ NAME_MATCH
+      unless group =~ NAME_MATCH && name =~ NAME_MATCH
         return render status: :not_found, plain: 'Impossible component'
       end
 
-      render template: "kitten/components/#{type}/#{group}/#{name}",
+      render template: "kitten/components/#{group}/#{name}",
              layout: 'kitten/application'
     end
   end
