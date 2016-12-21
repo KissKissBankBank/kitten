b030cb098a7dcf9781012f442089f10964791f59
Karine Herbaut <karine.herbaut@kisskissbankbank.com>
2016-12-21 14:35:04 +0100

    display utilities examples

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Kitten::Engine.routes.draw do
   get '/', to: redirect('branding')
   get 'branding' => 'branding#index'
   get 'components' => 'components#index'
+  get 'utilities' => 'utilities#index'
   get 'components/:type/:group/:name' => 'components#show'
   get 'pages/:name' => 'pages#show'
   get 'playground' => 'playgrounds#show'
