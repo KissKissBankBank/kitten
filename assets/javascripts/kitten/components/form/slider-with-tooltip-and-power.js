686f50297e3dbb7ff724f6b6dd893b2c94b710ec
Sunny Ripert <sunny@sunfox.org>
2016-12-22 19:33:34 +0100

    Remove handleKeyDown from SliderWithTooltipAndPower

--- assets/javascripts/kitten/components/form/slider-with-tooltip-and-power.js
+++ assets/javascripts/kitten/components/form/slider-with-tooltip-and-power.js
@@ -7,41 +7,12 @@ class BaseSliderWithTooltipAndPower extends React.Component {
   constructor(props) {
     super(props)
 
-    this.handleKeyDown = this.handleKeyDown.bind(this)
     this.handleMove = this.handleMove.bind(this)
   }
 
   // Allow other components to focus
   focus() {
     this.refs.focus()
-  }
-
-  handleKeyDown(e) {
-    // Make bigger steps when alt or shift key is being held
-    const { min, max } = this.props
-
-    // make larger steps if alt or shift is entered,
-    // change direction if we go to high to low
-    let step = this.props.step
-    if (e.altKey)
-      step *= 10
-    if (e.shiftKey)
-      step *= 10
-    if (min > max)
-      step *= -1
-
-    switch (e.keyCode) {
-      case domEvents.keyboard.right:
-      case domEvents.keyboard.up:
-        return this.move(this.props.value + step)
-      case domEvents.keyboard.left:
-      case domEvents.keyboard.down:
-        return this.move(this.props.value - step)
-      case domEvents.keyboard.home:
-        return this.move(min)
-      case domEvents.keyboard.end:
-        return this.move(max)
-    }
   }
 
   handleMove(ratio) {
