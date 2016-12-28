020761331998f7ad1906a32cb55feb3f86476940
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2016-12-28 13:39:46 +0100

    Refactor components with function version

--- assets/javascripts/kitten/components/steppers/stepper-icon.js
+++ assets/javascripts/kitten/components/steppers/stepper-icon.js
@@ -3,54 +3,44 @@ import classNames from 'classnames'
 import { ArrowIcon } from 'kitten/components/icons/arrow-icon'
 import { CheckedIcon } from 'kitten/components/icons/checked-icon'
 
-export class StepperIcon extends React.Component {
-  render() {
-    const iconClassNames = classNames(
-      'k-ButtonIcon',
-      'k-ButtonIcon--micro',
-      'k-ButtonIcon--hydrogen',
-      'k-ButtonIcon--withoutHover',
-      'k-Stepper__icon',
-      this.props.className,
-    )
+const StepperIcon = ({ className, children }) => {
+  const iconClassNames = classNames(
+    'k-ButtonIcon',
+    'k-ButtonIcon--micro',
+    'k-ButtonIcon--hydrogen',
+    'k-ButtonIcon--withoutHover',
+    'k-Stepper__icon',
+    className,
+  )
 
-    return (
-      <span className={ iconClassNames }>
-        { this.props.children }
-      </span>
-    )
-  }
+  return (
+    <span className={ iconClassNames }>{ children }</span>
+  )
 }
 
-export class StepperIconDefault extends React.Component {
-  render() {
-    return (
-      <StepperIcon className="k-ButtonIcon--empty--micro" />
-    )
-  }
+export const StepperIconDefault = () => {
+  return (
+    <StepperIcon className="k-ButtonIcon--empty--micro" />
+  )
 }
 
-export class StepperIconInProgress extends React.Component {
-  render() {
-    return (
-      <StepperIcon className="k-Stepper__icon--inProgress">
-        <ArrowIcon className="k-ButtonIcon__svg k-Stepper__svg" />
-      </StepperIcon>
-    )
-  }
+export const StepperIconInProgress = () => {
+  return (
+    <StepperIcon className="k-Stepper__icon--inProgress">
+      <ArrowIcon className="k-ButtonIcon__svg k-Stepper__svg" />
+    </StepperIcon>
+  )
 }
 
-export class StepperIconValidated extends React.Component {
-  render() {
-    const validatedClassNames = classNames(
-      'k-Stepper__icon--validated',
-      'k-ButtonIcon--checked--micro',
-    )
+export const StepperIconValidated = () => {
+  const validatedClassNames = classNames(
+    'k-Stepper__icon--validated',
+    'k-ButtonIcon--checked--micro',
+  )
 
-    return (
-      <StepperIcon className={ validatedClassNames }>
-        <CheckedIcon className="k-ButtonIcon__svg k-Stepper__svg" />
-      </StepperIcon>
-    )
-  }
+  return (
+    <StepperIcon className={ validatedClassNames }>
+      <CheckedIcon className="k-ButtonIcon__svg k-Stepper__svg" />
+    </StepperIcon>
+  )
 }
