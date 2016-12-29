d8afb9076c87e2a85f366d2b4ea83892ce9624c8
Sunny Ripert <sunny@sunfox.org>
2016-12-29 11:28:33 +0100

    Add multi-level option support for Select

--- assets/javascripts/kitten/components/form/select-with-state.js
+++ assets/javascripts/kitten/components/form/select-with-state.js
@@ -25,15 +25,51 @@ class SelectWithState extends React.Component {
     const { value, onChange, className, ...other } = this.props
 
     return (
-      <div className={ classNames("k-Select", className) }>
-        <Select clearable={ false }
-                searchable={ false }
-                multi={ false }
-                value={ this.state.value }
-                onChange={ this.handleChange }
-                { ...other } />
+      <div className={ classNames('k-Select', className) }>
+        <SelectWithMultiLevel clearable={ false }
+                              searchable={ false }
+                              multi={ false }
+                              value={ this.state.value }
+                              onChange={ this.handleChange }
+                              { ...other } />
       </div>
     )
+  }
+}
+
+class SelectWithMultiLevel extends React.Component {
+  optionRenderer({ level, label }) {
+    if (!level)
+      return label
+
+    return (
+      <span className={ 'k-Select__option--level' + level }>
+        { label }
+      </span>
+    )
+  }
+
+  flattenedOptions() {
+    const options = []
+    this.props.options.map((option) => {
+      if (option.children) {
+        option.level = 1
+        options.push(option)
+        option.children.map((opt) => {
+          opt.level = 2
+          options.push(opt)
+        })
+      } else {
+        options.push(option)
+      }
+    })
+    return options
+  }
+
+  render() {
+    return <Select optionRenderer={ this.optionRenderer }
+                   { ...this.props }
+                   options={ this.flattenedOptions() } />
   }
 }
 
