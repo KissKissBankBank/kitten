c8df338eccc5ba4c9e7dca8d2fc8635efeaa0b02
Camille Delbove <camille.delbove@gmail.com>
2016-12-30 18:04:28 +0100

    Update

--- assets/javascripts/kitten/components/tooltips/tooltip.js
+++ assets/javascripts/kitten/components/tooltips/tooltip.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import ReactTooltip from 'react-tooltip'
-import ButtonTooltipIcon from '../../components/buttons/button-tooltip-icon'
+import ButtonTooltipIcon from 'kitten/components/buttons/button-tooltip-icon'
 
 export default class Tooltip extends React.Component {
   render() {
     return (
-      <div>
+      <div className="k-Tooltip">
         <ButtonTooltipIcon data-tip
-                           data-for={ this.props.data_for }
+                           data-for={ this.props.data_id }
                            data-event="click"
-                           data-dismiss={ this.props.data_for } />
+                           data-dismiss={ this.props.data_id }
+                           aria-describedby={ this.props.data_id } />
 
         <ReactTooltip id={ this.props.data_id }
-                      class="k-Tooltip"
-                      role={ this.props.data_role }
-                      aria-describedby={ this.props.data_id }
+                      class="k-Tooltip__content"
+                      role="tooltip"
                       effect="solid"
                       place={ this.props.place }
-                      globalEventOff="click" >
+                      globalEventOff="click">
           { this.props.children }
         </ReactTooltip>
       </div>
