5fba36c49988bf4a71d300f9073a5a67fc76d04c
Camille Delbove <camille.delbove@gmail.com>
2017-01-09 19:27:45 +0100

    Add test

---
+++ assets/javascripts/kitten/components/buttons/button-icon-words.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import Button from 'kitten/components/buttons/button'
+import ButtonIconWords from 'kitten/components/buttons/button-icon-words'
+
+describe('<ButtonIconWords />', () => {
+  const component = shallow(<ButtonIconWords />)
+
+  it('renders a <Button />', () => {
+    expect(component).to.have.type(Button)
+  })
+
+  it('renders an svg icon', () => {
+    expect(component).to.have.className('k-Button--icon')
+    expect(component.find('.k-Button__icon')).to.have.length(1)
+  })
+
+})
---
+++ assets/javascripts/kitten/components/buttons/button.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import Button from 'kitten/components/buttons/button'
+
+describe('<Button />', () => {
+  const component = shallow(
+    <Button className="k-Button"
+            name="hydrogen"
+            disabled="true"
+            text="Button" />
+  )
+
+  it('has a <Button class="k-Button" />', () => {
+    expect(component).to.have.tagName('button')
+    expect(component).to.have.className('k-Button')
+    expect(component).to.have.className('k-Button--hydrogen')
+    expect(component).to.not.have.className('k-Button--helium')
+  })
+
+  describe('name', () => {
+    it('accepts helium', () => {
+      const component = shallow(<Button name="helium" />)
+      expect(component).to.have.className('k-Button--helium')
+    })
+  })
+
+  describe('size', () => {
+    it('accepts tiny', () => {
+      const component = shallow(<Button size="tiny" />)
+      expect(component).to.have.className('k-Button--tiny')
+    })
+
+    it('accepts big', () => {
+      const component = shallow(<Button size="big" />)
+      expect(component).to.have.className('k-Button--big')
+    })
+  })
+
+  describe('with iconRight svg', () => {
+    const componentWithIconRight = shallow(
+      <Button iconOnRight="true" />
+    )
+
+    it('has an icon right', () => {
+      expect(componentWithIconRight).to.have.className('k-Button--iconRight')
+    })
+  })
+})
