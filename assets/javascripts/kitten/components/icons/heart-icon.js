fec44d8e4515867c3a874997753aa694fd5faf37
Camille Delbove <camille.delbove@gmail.com>
2017-01-16 18:53:51 +0100

    Change className to props

--- assets/javascripts/kitten/components/icons/heart-icon.js
+++ assets/javascripts/kitten/components/icons/heart-icon.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const HeartIcon = ({ className }) => {
+export const HeartIcon = props => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg"
          viewBox="0 0 14 12"
