47812834b0824b330024422fb4887e42ff1b377c
Camille Delbove <camilledel@users.noreply.github.com>
2017-01-17 10:44:04 +0100

    Add `RadioButton` React component (#351)

---
+++ assets/javascripts/kitten/components/form/radio-button.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export class RadioButton extends React.Component {
+  renderContent() {
+    if (!this.props.content)
+      return
+
+    const labelContentsClassNames = classNames(
+      'k-RadioButton__labelContents',
+      { 'k-RadioButton__labelContents--large': this.props.largeContent }
+    )
+    return (
+      <div className={ labelContentsClassNames }>
+        <p>{ this.props.content }</p>
+      </div>
+    )
+  }
+
+  render() {
+    const { id, text, large, ...others } = this.props
+
+    let radioButtonClassNames = classNames (
+      'k-RadioButton__label',
+      { 'k-RadioButton__label--large': large },
+    )
+
+    return (
+      <div className="k-RadioButton">
+        <input id={ id }
+               type="radio"
+               className="k-RadioButton__input"
+               { ...others } />
+
+        <label htmlFor={ id }
+               className={ radioButtonClassNames } >
+          { text }
+        </label>
+        { this.renderContent() }
+      </div>
+    )
+  }
+}
+
+RadioButton.defaultProps = {
+  text: 'Default',
+  large: false,
+  largeContent: false,
+  content: '',
+}
