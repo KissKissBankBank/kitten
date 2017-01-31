f63e187bcc51d4f7a569d139e2f72cf788afff88
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2017-01-31 13:09:46 +0100

    Add TabBar component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,8 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [unreleased]
 
-- Breaking change: Change `k-Tabbar` html structure to fix spacing.
+- Feature: `TabBar` React component with new html structure.
+- Feature: Add `react-markdown` and `json-loader` to manage HTML props.
 
 ## [5.6.0] - 2017-01-30
 
--- spec/dummy/client/entries/app-kitten.js
+++ spec/dummy/client/entries/app-kitten.js
@@ -43,6 +43,9 @@ import ExternalRichLink from 'kitten/components/links/external-rich-link'
 // Lists
 import NavList from 'kitten/components/lists/nav-list'
 
+// Navigation
+import { TabBar } from 'kitten/components/navigation/tab-bar'
+
 // Simulators
 import LoanSimulator from 'kitten/components/simulators/loan-simulator'
 import SimulatorResult from 'kitten/components/simulators/simulator-result'
@@ -149,6 +152,9 @@ ReactOnRails.register({
 
   // Lists
   NavList,
+
+  // Navigation
+  TabBar,
 
   // Simulators
   LoanSimulator,
