ef0b657a70ac99dd60f7d1cc06ab0bf145b09976
Camille Delbove <camille.delbove@gmail.com>
2017-02-01 18:45:47 +0100

    Props:

--- assets/javascripts/kitten/components/meters/progress.js
+++ assets/javascripts/kitten/components/meters/progress.js
@@ -4,18 +4,28 @@ import classNames from 'classnames'
 export class Progress extends React.Component {
   render() {
     const { className,
+            name,
             children,
             value,
+            max,
+            text,
             ...others } = this.props
 
+    const progressClassNames = classNames(
+      'k-Progress',
+      className,
+    )
+
     return (
-      <div className={ className } { ...others }>
+      <progress className={ progressClassNames } { ...others }>
         { children }
-      </div>
+      </progress>
     )
   }
 }
 
-// Progress.defaultProps = {
-
-// }
+Progress.defaultProps = {
+  text: '50%',
+  value: "50",
+  max: "100",
+}
