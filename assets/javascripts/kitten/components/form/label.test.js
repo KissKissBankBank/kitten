f4a2136c888f29137956559f3e2a42bc68c0efd9
Bastien Maillard <bastien.maillard@gmail.com>
2017-02-02 11:49:31 +0100

    `Label` React component (#411)

---
+++ assets/javascripts/kitten/components/form/label.test.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { expect } from 'chai'
+import { mount, shallow } from 'enzyme'
+import { Label } from 'kitten/components/form/label'
+
+describe('<Label />', () => {
+  const component = shallow(
+    <Label />
+  )
+
+  it('renders a <label class="k-Label" />', () => {
+    expect(component).to.have.tagName('label')
+    expect(component).to.have.className('k-Label')
+  })
+
+  it('renders default children', () => {
+    expect(component).to.have.text('Label')
+  })
+
+  describe('with custom props', () => {
+    const component = shallow(
+      <Label tag="span"
+             className="custom-class"
+             focusId="custom-id"
+             size="tiny">
+        <span>Custom label</span>
+      </Label>
+    )
+
+    it('renders a span with options', () => {
+      expect(component).to.have.tagName('span')
+      expect(component).to.have.className('custom-class')
+      expect(component).to.have.className('k-Label--tiny')
+      expect(component).not.to.have.attr('for', 'custom-id')
+    })
+  })
+
+  describe('with `focusId` prop', () => {
+    const wrapper = mount(
+      <div>
+        <Label tag="span" focusId="focus" children="Label" />
+        <input type="text" id="focus" />
+      </div>,
+      { attachTo: document.body }
+    )
+
+    it('simulates click event on label', () => {
+      const input = wrapper.find('#focus').at(0).node
+      const label = wrapper.find('.k-Label')
+
+      label.simulate('click')
+
+      expect(input).to.be.equal(document.activeElement)
+    })
+  })
+})
