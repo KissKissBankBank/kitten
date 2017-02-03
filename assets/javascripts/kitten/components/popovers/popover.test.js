ec6b76fcab55717bd772f9e8412b335e3bbfd67a
Camille Delbove <camille.delbove@gmail.com>
2017-02-03 18:28:27 +0100

    Update test popover

--- assets/javascripts/kitten/components/popovers/popover.test.js
+++ assets/javascripts/kitten/components/popovers/popover.test.js
@@ -4,72 +4,48 @@ import { shallow } from 'enzyme'
 import { Popover } from 'kitten/components/popovers/popover'
 import { Button } from 'kitten/components/buttons/button'
 
-describe('Popover', () => {
+describe('<Popover />', () => {
   const defaultComponent = shallow(
     <Popover />)
 
-  it('renders <div class="k-Popover">', () => {
-    expect(defaultComponent.find('.k-Popover')).to.have.length(1)
+  describe('with default props', () => {
+    it('renders <div class="k-Popover">', () => {
+      expect(defaultComponent.find('.k-Popover')).to.have.length(1)
+    })
+
+    it('renders aria attributes', () => {
+      const popoverComponent = defaultComponent.find('.k-Popover')
+
+      expect(popoverComponent).to.have.attr('role', 'dialog')
+      expect(popoverComponent).to.have.attr('aria-hidden', 'true')
+      expect(popoverComponent).to.have.attr('aria-labelledby', 'dialogtitle')
+    })
+
+    it('renders close button', () => {
+      const buttonIconComponent = defaultComponent.find('.k-ButtonIcon')
+
+      expect(buttonIconComponent).to.have.length(1)
+    })
   })
-})
 
-describe('<Popover />', () => {
-
-  describe('basic props', () => {
+  describe('with basic props', () => {
     const component = shallow(
-      <Popover className="k-Popover k-Popover--custom"/>
-    )
+      <Popover popoverClassName="k-Popover--custom"
+               containerClassName="k-Popover__container--custom"
+               closeButtonLabel="Fermer" />)
 
-    it('renders a <div class="k-Popover" />', () => {
-      expect(component).to.have.className('k-Popover')
-      expect(component).to.have.className('k-Popover--custom')
+    it('renders <div className="k-Popover--custom" />', () => {
+      expect(component.find('.k-Popover--custom')).to.have.length(1)
+     })
+
+    it('renders <div className="k-Popover__container--custom" />', () => {
+      expect(component.find('.k-Popover__container--custom')).to.have.length(1)
     })
 
-    describe('children', () => {
-      const component = shallow(
-        <div class="k-Popover__content">
-          <div class="k-Popover--custom" />
-        </div>)
+    it('renders close button with a specific label', () => {
+      const closeButtonComponent = component.find('.k-ButtonIcon')
 
-      it('has only text', () => {
-        expect(component.find('.k-Popover__text')).to.have.className('k-Popover__content')
-      })
-
-      it('has a children', () => {
-        expect(component.children()).to.have.text('')
-      })
+      expect(closeButtonComponent).to.have.attr('title', 'Fermer')
     })
   })
 })
-
-describe('with close button', () => {
-
-  describe('<button />', () => {
-
-    describe('basic props', () => {
-      const componentWithCloseButton = shallow(
-        <Button title="close"
-                aria-label="close"
-                onClick=""
-                className="k-ButtonIcon k-ButtonIcon--custom" />)
-
-      it('renders a <Button className="k-ButtonIcon">', () => {
-        expect(componentWithCloseButton).to.have.className('k-ButtonIcon')
-        expect(componentWithCloseButton).to.have.className('k-ButtonIcon--custom')
-      })
-
-      describe('children', () => {
-        const componentWithCloseButton = shallow(
-          <Button>
-            <svg />
-          </Button>)
-
-        it('has children', () => {
-          expect(componentWithCloseButton.children()).to.have.tagName('svg')
-        })
-      })
-    })
-  })
-})
-
-
