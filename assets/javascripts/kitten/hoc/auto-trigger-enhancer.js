00b8ed80bbc631b4b2366b2c279ce2cee94dcb02
Fanny Cheung <fanny@ynote.hk>
2017-02-06 17:40:15 +0100

    Rename prop

--- assets/javascripts/kitten/hoc/auto-trigger-enhancer.js
+++ assets/javascripts/kitten/hoc/auto-trigger-enhancer.js
@@ -44,7 +44,7 @@ export const autoTriggerEnhancer = (WrappedComponent, wrappedComponentProps) => 
 
     shouldStart() {
       if (!domElementHelper.canUseDom()) { return false }
-      if (this.props.development) { return true }
+      if (this.props.verifyStorageOnStart) { return true }
 
       return !this.hasPlayed()
     }
@@ -89,7 +89,7 @@ export const autoTriggerEnhancer = (WrappedComponent, wrappedComponentProps) => 
 
   AutoTriggerWrapper.defaultProps = {
     storeName: 'kitten.AutoTrigger',
-    development: false,
+    verifyStorageOnStart: false,
     stopEventName: 'k:auto-trigger:stop',
     stopHandlerName: null,
   }
