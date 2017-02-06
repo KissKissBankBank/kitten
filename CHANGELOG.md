5f82e8de331969433cf764e890f72f3968c90f53
Camille Delbove <camilledel@users.noreply.github.com>
2017-02-06 14:50:53 +0100

    Add new `TextInput` React component. (#421)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [unreleased]
 
+- Feature: Add new `TextInput` React component.
+
 ## [5.8.0] - 2017-02-06
 
 Features:
---
+++ assets/javascripts/kitten/components/form/text-input.test.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import { TextInput } from 'kitten/components/form/text-input'
+
+describe('Text-input with default props', () => {
+  const defaultComponent = shallow(
+    <TextInput />)
+
+  it('has a default text-input attributes', () => {
+    expect(defaultComponent.find('.k-TextInput')).to.have.length(1)
+  })
+
+  it('has a default tag', () => {
+    expect(defaultComponent).to.have.tagName('input')
+  })
+
+  it('does not contain a is-valid element', () => {
+    expect(defaultComponent.find('.is-valid')).to.have.length(0)
+  })
+
+  it('does not contain a is-error element', () => {
+    expect(defaultComponent.find('.is-error')).to.have.length(0)
+  })
+
+  it('does not contain a tiny element', () => {
+    expect(defaultComponent.find('.k-TextInput--tiny')).to.have.length(0)
+  })
+
+  describe('<TextInput />', () => {
+    const component = shallow(
+      <TextInput className="k-TextInput--custom" />)
+
+    it('renders a <TextInput class="k-TextInput" />', () => {
+      expect(component).to.have.tagName('input')
+      expect(component).to.have.className('k-TextInput--custom')
+    })
+
+    describe('text-input with valid class', () => {
+      const componentWithValidClass = shallow(
+        <TextInput valid={ true } />
+      )
+
+      it('has a valid class', () => {
+        expect(componentWithValidClass).to.have.className('is-valid')
+      })
+    })
+
+    describe('text-input with error class', () => {
+      const componentWithErrorClass = shallow(
+        <TextInput error={ true } />
+      )
+
+      it('has a error class', () => {
+        expect(componentWithErrorClass).to.have.className('is-error')
+      })
+    })
+
+    describe('text-input with tiny class', () => {
+      const componentWithTinyClass = shallow(
+        <TextInput tiny={ true } />
+      )
+
+      it('has a tiny class', () => {
+        expect(componentWithTinyClass).to.have.className('k-TextInput--tiny')
+      })
+    })
+  })
+})
