38428977981722ab301be4988d763c96eb1dbf75
Fanny Cheung <fanny@ynote.hk>
2017-02-08 17:58:23 +0100

    Update tests setup

--- assets/javascripts/kitten/config/test-setup.js
+++ assets/javascripts/kitten/config/test-setup.js
@@ -1,11 +1,11 @@
-require('babel-register')()
-
-const jsdom = require('jsdom').jsdom
-const chai = require('chai')
-const chaiEnzyme = require('chai-enzyme')
-const fetch = require('isomorphic-fetch')
+import { jsdom } from 'jsdom'
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import fetch from 'isomorphic-fetch'
+import 'mock-local-storage'
 
 global.document = jsdom('')
 global.window = document.defaultView
+global.navigator = window.navigator
 
 chai.use(chaiEnzyme())
