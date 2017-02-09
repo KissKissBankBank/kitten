d329b5bfcae7326a2847c27e96291b0650be9b39
Camille Delbove <camille.delbove@gmail.com>
2017-02-09 20:44:34 +0100

    Add TextInputWithAutocomplete React component

---
+++ assets/javascripts/kitten/components/form/text-input-with-autocomplete.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export class TextInputWithAutocomplete extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+TextInputWithAutocomplete.defaultProps = {
+
+}
--- spec/dummy/client/entries/app-kitten.js
+++ spec/dummy/client/entries/app-kitten.js
@@ -31,6 +31,7 @@ import SelectWithState from 'kitten/components/form/select-with-state'
 import Slider from 'kitten/components/form/slider'
 import Switch from 'kitten/components/form/switch'
 import { TextInput } from 'kitten/components/form/text-input'
+import { TextInputWithAutocomplete } from 'kitten/components/form/text-input-with-autocomplete'
 
 // Grid
 import { Grid, GridCol } from 'kitten/components/grid/grid'
@@ -159,6 +160,7 @@ ReactOnRails.register({
   Slider,
   Switch,
   TextInput,
+  TextInputWithAutocomplete,
 
   // Icons
   GrabberIcon,
