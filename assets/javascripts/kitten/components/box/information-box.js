baa53853610b9ca8f9c553f22dc53abee249af68
Camille Delbove <camilledel@users.noreply.github.com>
2017-02-16 19:27:19 +0100

    Add `InformationBox` react component (#433)

---
+++ assets/javascripts/kitten/components/box/information-box.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export class InformationBox extends React.Component {
+  render() {
+    const {
+      title,
+      children,
+      ...other
+    } = this.props
+
+    return (
+      <div className="k-InformationBox" { ...other }>
+        <div className="k-InformationBox__container">
+          <p className="k-InformationBox__title">{ title }</p>
+          <div className="k-InformationBox__content">
+            { children }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+InformationBox.defaultProps = {
+  title: null,
+  children: null,
+}
---
+++ assets/javascripts/kitten/components/box/information-box.test.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import { InformationBox } from 'kitten/components/box/information-box'
+
+describe('<InformationBox />', () => {
+  describe('by default', () => {
+    const defaultComponent = shallow(<InformationBox />)
+
+    it('renders .k-InformationBox', () => {
+      expect(defaultComponent.find('.k-InformationBox')).to.have.length(1)
+    })
+
+    it('has a default title', () => {
+      expect(defaultComponent.find('.k-InformationBox__title')).to.have.length(1)
+    })
+
+    it('has a default children', () => {
+      expect(defaultComponent.find('.k-InformationBox__content')).to.have.length(1)
+    })
+  })
+
+  describe('other props', () => {
+    const component = shallow(
+      <InformationBox className="k-InformationBox__custom" />)
+
+    it('renders a <InformationBox class="k-InformationBox" />', () => {
+      expect(component).to.have.className('k-InformationBox__custom')
+    })
+  })
+
+  describe('title prop', () => {
+    const component = shallow(
+      <InformationBox title="Lorem ipsum" />
+    )
+
+    it('adds a title element', () => {
+      const title = component.find('.k-InformationBox__title')
+      expect(title).to.have.length(1)
+      expect(title).to.have.text('Lorem ipsum')
+    })
+  })
+
+  describe('children prop', () => {
+    const component = shallow(
+      <InformationBox>Example content</InformationBox>
+    )
+
+    it('adds a children element', () => {
+      const children = component.find('.k-InformationBox__content')
+      expect(children).to.have.length(1)
+      expect(children).to.have.text('Example content')
+    })
+  })
+})
