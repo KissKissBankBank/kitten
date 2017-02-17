9d1261274d21dfd5ad43946cde400082d4646fa9
Fanny Cheung <fanny@ynote.hk>
2017-02-17 16:33:42 +0100

    Revert "Add TextInputWithUnit React component [WIP]"
    This reverts commit 565d60d397327547e87195032786ae82ba0ba71c.

--- spec/dummy/client/entries/app-kitten.js
+++ spec/dummy/client/entries/app-kitten.js
@@ -33,7 +33,6 @@ import Slider from 'kitten/components/form/slider'
 import Switch from 'kitten/components/form/switch'
 import { TextInput } from 'kitten/components/form/text-input'
 import { TextInputWithLimit } from 'kitten/components/form/text-input-with-limit'
-import { TextInputWithUnit } from 'kitten/components/form/text-input-with-unit'
 
 // Grid
 import { Grid, GridCol } from 'kitten/components/grid/grid'
@@ -168,7 +167,6 @@ ReactOnRails.register({
   Switch,
   TextInput,
   TextInputWithLimit,
-  TextInputWithUnit,
 
   // Icons
   GrabberIcon,
