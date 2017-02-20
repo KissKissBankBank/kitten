803ce64ff9fd7ffefeba4f074271eda393c2ab0a
Camille Delbove <camille.delbove@gmail.com>
2017-02-20 13:44:34 +0100

    Test component

---
+++ assets/javascripts/kitten/components/form/location-input.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+import { LocationInput } from 'kitten/components/form/location-input'
+import { LocationIcon } from 'kitten/components/icons/location-icon'
+
+describe('<LocationInput />', () => {
+  describe('by default', () => {
+    const component = shallow(<LocationInput />)
+
+    it('render a .k-LocationInput', () => {
+      expect(component.find('.k-LocationInput')).to.have.length(1)
+    })
+
+    it('render a .k-LocationInput__label', () => {
+      expect(component.find('.k-LocationInput__label')).to.have.length(1)
+    })
+
+    it('render a .k-LocationInput__icon', () => {
+      expect(component.find('.k-LocationInput__icon')).to.have.length(1)
+    })
+
+    it('renders a PlacesAutocomplete', () => {
+      expect(component.find('PlacesAutocomplete')).to.have.length(1)
+    })
+  })
+
+  describe('label prop', () => {
+    const component = shallow(
+      <LocationInput label="Lorem ipsum" />
+    )
+
+    it('add a label element', () => {
+      const label = component.find('.k-LocationInput__label')
+      expect(label).to.have.length(1)
+      expect(label).to.have.text('Lorem ipsum')
+    })
+  })
+})
