0bfbe8a63ce058054830d197139d09e331edc319
Sunny Ripert <sunny@sunfox.org>
2017-02-24 10:34:51 +0100

    Add keyboard accessibility to `Button`s with `a` tags and no `href` (#477)

--- assets/javascripts/kitten/components/buttons/button.test.js
+++ assets/javascripts/kitten/components/buttons/button.test.js
@@ -4,47 +4,69 @@ import { shallow } from 'enzyme'
 import { Button } from 'kitten/components/buttons/button'
 
 describe('Button with default props', () => {
-  const defaultComponent = shallow(
-    <Button />)
+  const component = shallow(<Button />)
 
-  it('has a default button attributes', () => {
-    expect(defaultComponent.find('.k-Button')).to.have.length(1)
-  })
-
-  it('has a default tag', () => {
-    expect(defaultComponent).to.have.tagName('button')
-  })
-
-  it('has an iconRight svg', () => {
-    expect(defaultComponent.find('k-Button--iconRight')).to.have.length(0)
-  })
-
-  describe('<Button />', () => {
-    const component = shallow(
-      <Button className="k-Button--custom" />
-    )
-
-    it('renders a <Button class="k-Button" />', () => {
+  describe('by default', () => {
+    it('is a <button>', () => {
       expect(component).to.have.tagName('button')
-      expect(component).to.have.className('k-Button--custom')
+    })
+
+    it('has a k-Button class', () => {
+      expect(component).to.have.className('k-Button')
+    })
+
+    it('has the hydrogen modifier class', () => {
       expect(component).to.have.className('k-Button--hydrogen')
     })
 
-    describe('modifier', () => {
-      it('accepts helium', () => {
-        const component = shallow(<Button modifier="helium" />)
-        expect(component).to.have.className('k-Button--helium')
-      })
+    it('does not have the iconRight modifier class', () => {
+      expect(component).not.to.have.className('k-Button--iconRight')
     })
 
-    describe('button with iconRight svg', () => {
-      const componentWithIconRight = shallow(
-        <Button iconOnRight={ true } />
-      )
+    it('does not have a tabindex attribute', () => {
+      expect(component).not.to.have.attr('tabindex')
+    })
+  })
 
-      it('has an icon right', () => {
-        expect(componentWithIconRight).to.have.className('k-Button--iconRight')
-      })
+  describe('className prop', () => {
+    it('adds the class to the element', () => {
+      const component = shallow(<Button className="meaow" />)
+      expect(component).to.have.className('meaow')
+    })
+  })
+
+  describe('modifier prop', () => {
+    it('adds a class to the component', () => {
+      const component = shallow(<Button modifier="helium" />)
+      expect(component).to.have.className('k-Button--helium')
+    })
+  })
+
+  describe('iconRight prop', () => {
+    it('adds the iconRight modifier class', () => {
+      const component = shallow(<Button iconOnRight />)
+      expect(component).to.have.className('k-Button--iconRight')
+    })
+  })
+
+  describe('tag prop', () => {
+    it('changes the tag', () => {
+      const component = shallow(<Button tag="a" />)
+      expect(component).to.have.tagName('a')
+    })
+  })
+
+  describe('with tag="a" and no href', () => {
+    it('adds a tabindex', () => {
+      const component = shallow(<Button tag="a" />)
+      expect(component).to.have.attr('tabindex', '0')
+    })
+  })
+
+  describe('with tag="a" href="foo"', () => {
+    it('does not add a tabindex', () => {
+      const component = shallow(<Button tag="a" href="foo" />)
+      expect(component).not.to.have.attr('tabindex')
     })
   })
 })
