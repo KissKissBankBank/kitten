c957d262c6b6c7e267c381e580491a3d079356ba
Camille Delbove <camille.delbove@gmail.com>
2017-02-28 15:55:00 +0100

    Clean line.js

--- assets/javascripts/kitten/karl/layout/line.js
+++ assets/javascripts/kitten/karl/layout/line.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Line } from 'kitten/components/layout/line'
-import Tooltip from 'kitten/components/tooltips/tooltip'
+import { Tooltip } from 'kitten/components/tooltips/tooltip'
 import { Label } from 'kitten/components/form/label'
 import { TextInput } from 'kitten/components/form/text-input'
 import { HeartIcon } from 'kitten/components/icons/heart-icon'
 
-export const KarlLineWithTooltip = (props) => {
+export const KarlLineWithTooltip = props => {
   return (
     <Line>
       <Line.Item>
@@ -21,14 +21,14 @@ export const KarlLineWithTooltip = (props) => {
   )
 }
 
-export const KarlLineWithBigHeart = (props) => {
+export const KarlLineWithBigHeart = props => {
   return (
     <Line>
       <Line.Item>
         Lorem ipsum dolor sit amet.
       </Line.Item>
       <Line.Item>
-        <button className="k-buttonIcon k-ButtonIcon--hydrogen k-ButtonIcon--big">
+        <button className="k-ButtonIcon k-ButtonIcon--hydrogen k-ButtonIcon--big">
           <HeartIcon className="k-ButtonIcon__svg" />
         </button>
       </Line.Item>
