861c62261e275b09c41f24f167e256276b944b26
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2017-03-16 18:43:57 +0100

    Add LegoGrid component

---
+++ assets/javascripts/kitten/components/grid/lego-grid.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import classNames from 'classnames'
+import Masonry from 'react-masonry-component'
+
+export const LegoGrid = props => {
+  const { className, masonryProps, children } = props
+  const gridClassName = classNames('k-LegoGrid', className)
+
+  return (
+    <div className={ gridClassName } { ...props }>
+      <Masonry { ...masonryProps }>
+        { children }
+      </Masonry>
+    </div>
+  )
+}
+
+LegoGrid.Item = props => {
+  const itemClassName = classNames('k-LegoGrid__item', props.className)
+
+  return (
+    <div { ...props } className={ itemClassName } />
+  )
+}
+
+LegoGrid.defaultProps = {
+  className: null,
+  masonryProps: {},
+}
+
+LegoGrid.Item.defaultProps = {
+  className: null,
+}
---
+++ assets/javascripts/kitten/karl/grid/lego-grid.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Grid, GridCol } from 'kitten/components/grid/grid'
+import { LegoGrid } from 'kitten/components/grid/lego-grid'
+
+const randomColorHexa = () => {
+  return "#000000".replace(/0/g, () => {
+    return (~~(Math.random() * 16)).toString(16)
+  })
+}
+
+const addCard = () => {
+  const randWidth = Math.random() * 300 + 100
+  const styles = {
+    border: '1px solid #eee',
+    height: `${randWidth}px`,
+    background: randomColorHexa(),
+    'box-shadow': '0 5px 10px #ccc',
+    'margin': '0 10px 20px',
+  }
+
+  return (
+    <div style={ styles } />
+  )
+}
+
+export const KarlLegoGrid = () => {
+  return (
+    <div className="k-DevGrid__container" style={{ outline: "none" }}>
+      <Grid>
+        <GridCol col="12">
+          <LegoGrid>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+            <LegoGrid.Item>{ addCard() }</LegoGrid.Item>
+          </LegoGrid>
+        </GridCol>
+      </Grid>
+    </div>
+  )
+}
