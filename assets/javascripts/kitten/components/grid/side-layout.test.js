127168c6e6b356ef9031f72364e9c0d042c133b2
Morgan JARRY <morgan.jarry@gmail.com>
2017-03-20 17:07:35 +0100

    Add test to `SideLayout` component

---
+++ assets/javascripts/kitten/components/grid/side-layout.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+import { SideLayout } from 'kitten/components/grid/side-layout'
+import { InformationBox } from 'kitten/components/box/information-box'
+import { Paragraph } from 'kitten/components/typography/paragraph'
+
+describe('<SideLayout />', () => {
+  const informationBox = shallow(
+    <InformationBox title="InformationBox title here">
+      Lorem ipsum dolor…
+    </InformationBox>
+  )
+
+  const component = mount(
+    <SideLayout className="k-VerticalGrid__fluid"
+                title="SideLayout title here"
+                sidebarContent={ informationBox }>
+      <Paragraph>
+        Lorem ipsum dolor…
+      </Paragraph>
+    </SideLayout>
+  )
+
+  const mainContentTitle = component.find('.k-Title')
+  const mainContentText = component.find('Paragraph')
+  const informationBoxTitle = component.find('.k-InformationBox__title')
+  const informationBoxText = component.find('.k-InformationBox__content')
+
+  it('renders a <div class="k-VerticalGrid__fluid" />', () => {
+    expect(component).to.have.className('k-VerticalGrid__fluid')
+  })
+
+  it('has a title', () => {
+    expect(mainContentTitle).to.have.text('SideLayout title here')
+  })
+
+  it('has a text', () => {
+    expect(mainContentText).to.have.text('Lorem ipsum dolor…')
+  })
+
+  it('has an aside title', () => {
+    expect(informationBoxTitle).to.have.text('InformationBox title here')
+  })
+
+  it('has an aside text', () => {
+    expect(informationBoxText).to.have.text('Lorem ipsum dolor…')
+  })
+})
