6ff04aff848d06c2fc0a07a43a57d49dd9482f9d
Morgan JARRY <morgan.jarry@gmail.com>
2017-03-21 17:35:26 +0100

    Remove console log + unneeded import

--- assets/javascripts/kitten/components/layout/side-layout.test.js
+++ assets/javascripts/kitten/components/layout/side-layout.test.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { expect } from 'chai'
 import { shallow, mount } from 'enzyme'
 import { SideLayout } from 'kitten/components/layout/side-layout'
-import { InformationBox } from 'kitten/components/box/information-box'
-import { Paragraph } from 'kitten/components/typography/paragraph'
-import { Title } from 'kitten/components/typography/title'
 
 describe('<SideLayout />', () => {
   describe('by default', () => {
@@ -12,8 +9,6 @@ describe('<SideLayout />', () => {
       <SideLayout className="myAwesomeClass">
       </SideLayout>
     )
-
-    console.log(component.html())
 
     it('renders a <div class="myAwesomeClass" />', () => {
       expect(component).to.have.className('myAwesomeClass')
