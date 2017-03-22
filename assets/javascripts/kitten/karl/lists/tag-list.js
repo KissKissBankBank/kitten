e69265a9dde02ff997092b2e8899e05c72b57e0d
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2017-03-22 10:33:08 +0100

    Modify exemple

--- assets/javascripts/kitten/karl/lists/tag-list.js
+++ assets/javascripts/kitten/karl/lists/tag-list.js
@@ -3,8 +3,9 @@ import { TagList } from 'kitten/components/lists/tag-list'
 
 export const KarlTagList = props => {
   const items = [
-    "TPE / PME",
-    "Green",
+    "Tag 1",
+    "Tag 2",
+    "Tag 3",
   ]
 
   return (
