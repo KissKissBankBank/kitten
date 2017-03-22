6b5d66ca22fe82470d533b773bb35b83af51715b
Morgan JARRY <morgan.jarry@gmail.com>
2017-03-22 16:45:09 +0100

    Fix typo

--- assets/javascripts/kitten/components/layout/side-layout.js
+++ assets/javascripts/kitten/components/layout/side-layout.js
@@ -56,7 +56,7 @@ export class SideLayout extends React.Component {
 
 SideLayout.defaultProps = {
   children: 'Children',
-  // sidebar: renderOfhatYouWant() // Content for aside grid
+  // sidebar: renderOfWhatYouWant() // Content for aside grid
   // E.g: see assets/javascripts/kitten/karl/layout/side-layout.js
   sidebar: null,
 }
