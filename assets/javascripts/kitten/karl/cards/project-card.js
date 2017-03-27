5e9f5577eb1a2c2ba659bed928a36d162bbe26fc
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2017-03-27 17:32:45 +0200

    Update ProjectCard with new components

--- assets/javascripts/kitten/karl/cards/project-card.js
+++ assets/javascripts/kitten/karl/cards/project-card.js
@@ -5,53 +5,83 @@ import { Paragraph } from 'kitten/components/typography/paragraph'
 import { Progress } from 'kitten/components/meters/progress'
 import { Button } from 'kitten/components/buttons/button'
 import { ButtonIcon } from 'kitten/components/buttons/button-icon'
+import { IconBadge } from 'kitten/components/notifications/icon-badge'
+import { TagList } from 'kitten/components/lists/tag-list'
 
 export const KarlProjectCard = () => {
+  const tagItems = [
+    { key: 'tpe-pme', item: 'TPE / PME' },
+  ]
+
   return (
     <div className="k-ProjectCard">
-      <div className="k-ProjectCard__content k-ProjectCard__borderBottom">
-        <div className="k-u-margin-top-double">
-          <Grid>
-            <GridCol col="10">
-              <ButtonIcon size="big" />
-              <Paragraph modifier="quaternary">
-                par :<br />
-                <strong>JCD Diffusion</strong>
-              </Paragraph>
-            </GridCol>
-            <GridCol col="2">
-              <span className="k-Badge">B</span>
-            </GridCol>
-          </Grid>
+      <div className="k-ProjectCard__content">
+        <div className="k-ProjectCard__grid">
+          <ButtonIcon size="big" />
+
+          <Paragraph modifier="quaternary"
+                     className="k-ProjectCard__grid__fluid k-u-margin-left-single">
+            par :<br />
+            <strong>JCD Diffusion</strong>
+          </Paragraph>
+
+          <IconBadge>A</IconBadge>
         </div>
 
         <Title modifier="quaternary">Location de materiel de reception</Title>
-        <Paragraph modifier="quaternary"><strong>TPE / PME</strong></Paragraph>
+
+        <TagList items={ tagItems } />
+
         <Paragraph modifier="tertiary">
           Achat de matériel (vaisselle, tables, chaises, …) dans le cadre de
           son activité de location de matériel de réception
         </Paragraph>
 
+        <div className="k-ProjectCard__grid k-ProjectCard__grid--spaceBetween">
+          <Paragraph modifier="quaternary" className="k-u-align-center">
+            Taux d’intérêt <br />
+            …
+          </Paragraph>
+
+          <Paragraph modifier="quaternary" className="k-u-align-center">
+            Taux d’intérêt <br />
+            …
+          </Paragraph>
+
+          <Paragraph modifier="quaternary" className="k-u-align-center">
+            Taux d’intérêt <br />
+            …
+          </Paragraph>
+        </div>
+
         <img className="k-ProjectCard__img"
              src="https://placekitten.com/600/340" />
       </div>
 
-      <div className="k-ProjectCard__content k-ProjectCard__progress k-ProjectCard__borderBottom">
-        <Grid>
-          <GridCol col="11">
-            <Progress tiny />
-          </GridCol>
-          <GridCol col="1">
-            <span>23 %</span>
-          </GridCol>
-        </Grid>
+      <div className="k-ProjectCard__progress k-ProjectCard__grid">
+        <Progress className="k-ProjectCard__grid__fluid k-u-margin-right-single" />
+        <Paragraph modifier="quaternary"><strong>23 %</strong></Paragraph>
       </div>
 
-      <div className="k-ProjectCard__content k-u-centered">
-        <Button modifier="helium" className="k-u-margin-top-double k-u-margin-bottom-double">
-          Prêter
-        </Button>
+      <div className="k-ProjectCard__content">
+        <div className="k-ProjectCard__grid k-ProjectCard__grid--spaceBetween">
+          <Paragraph modifier="quaternary" className="k-u-align-center">
+            812 <br />
+            preteurs
+          </Paragraph>
+
+          <Paragraph modifier="quaternary" className="k-u-align-center">
+            71 000 € <br />
+            collectés
+          </Paragraph>
+
+          <Paragraph modifier="quaternary" className="k-u-align-center">
+            12 j <br />
+            restants
+          </Paragraph>
+        </div>
       </div>
+
     </div>
   )
 }
