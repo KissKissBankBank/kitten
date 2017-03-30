ccad89a72ab5bfbc36a69c0729af6f4c90814d3c
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2017-03-30 16:28:39 +0200

    Add new components to compose ProjectCard

--- assets/javascripts/kitten/karl/cards/project-card.js
+++ assets/javascripts/kitten/karl/cards/project-card.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import classNames from 'classnames'
 import { Grid, GridCol } from 'kitten/components/grid/grid'
+import { Marger } from 'kitten/components/layout/marger'
 import { Title } from 'kitten/components/typography/title'
 import { Paragraph } from 'kitten/components/typography/paragraph'
 import { Progress } from 'kitten/components/meters/progress'
 import { Button } from 'kitten/components/buttons/button'
-import { ButtonIcon } from 'kitten/components/buttons/button-icon'
+import { ButtonImage } from 'kitten/components/buttons/button-image'
 import { IconBadge } from 'kitten/components/notifications/icon-badge'
 import { TagList } from 'kitten/components/lists/tag-list'
 
@@ -15,84 +17,100 @@ export const KarlProjectCard = () => {
 
   return (
     <div className="k-ProjectCard">
-
-
-
-
-
-      <div className="k-ProjectCard__content">
-        <div className="k-ProjectCard__grid">
-          <ButtonIcon size="big" />
-
-          <Paragraph modifier="quaternary"
-                     className="k-ProjectCard__grid__fluid k-u-margin-left-single">
-            par :<br />
-            <strong>JCD Diffusion</strong>
+      <div className="k-ProjectCard__grid">
+        <Marger top="2" bottom="1" className="k-ProjectCard__flex">
+          <ButtonImage img={ { src: 'https://placehold.it/40x40/4a84ff/4a84ff' } } />
+          <Paragraph className={ classNames('k-ProjectCard__flex__fluid',
+                                            'k-u-margin-left-single',
+                                            'k-u-margin-right-single') }
+                     margin={ false }
+                     modifier="quaternary">
+            par&nbsp;:<br />
+            <span className="k-u-strong">JCD Diffusion</span>
           </Paragraph>
-
           <IconBadge>A</IconBadge>
-        </div>
+        </Marger>
 
-        <Title modifier="quaternary" className="k-u-margin-top-single">
-          Location de materiel de reception
-        </Title>
+        <Marger top="1" bottom=".5">
+          <Title margin={ false } modifier="quaternary">
+            Location de materiel de reception
+          </Title>
+        </Marger>
 
-        <TagList items={ tagItems } />
+        <Marger top=".5" bottom="1">
+          <TagList items={ tagItems } />
+        </Marger>
 
-        <Paragraph modifier="tertiary">
-          Achat de matériel (vaisselle, tables, chaises, …) dans le cadre de
-          son activité de location de matériel de réception
-        </Paragraph>
+        <Marger top=".5" bottom="1.5">
+          <Paragraph margin={ false } modifier="tertiary">
+            Achat de matériel (vaisselle, tables, chaises, …) dans le cadre de
+            son activité de location de matériel de réception
+          </Paragraph>
+        </Marger>
 
-        <div className="k-ProjectCard__grid k-ProjectCard__grid--spaceBetween">
-          <Paragraph modifier="quaternary" className="k-u-align-center">
-            Taux d’intérêt <br />
-            …
+        <Marger top="1.5"
+                bottom="2"
+                className="k-ProjectCard__flex k-ProjectCard__flex--spaceBetween">
+          <Paragraph margin={ false } modifier="quaternary" className="k-u-align-center">
+            Taux d’intérêt<br />
+            <span className="k-u-strong">8,8 %</span>
           </Paragraph>
 
-          <Paragraph modifier="quaternary" className="k-u-align-center">
-            Taux d’intérêt <br />
-            …
+          <Paragraph margin={ false } modifier="quaternary" className="k-u-align-center">
+            Objectif<br />
+            <span className="k-u-strong">70 000 €</span>
           </Paragraph>
 
-          <Paragraph modifier="quaternary" className="k-u-align-center">
-            Taux d’intérêt <br />
-            …
+          <Paragraph margin={ false } modifier="quaternary" className="k-u-align-center">
+            Durée<br />
+            <span className="k-u-strong">48 mois</span>
           </Paragraph>
-        </div>
+        </Marger>
 
-        <img className="k-ProjectCard__img"
-             src="https://placekitten.com/600/340" />
+        <Marger top="2" bottom="2">
+          <img className="k-u-align-center k-ProjectCard__img"
+               src="https://placehold.it/310x175/4a84ff/4a84ff" />
+        </Marger>
       </div>
 
-      <div className="k-ProjectCard__progress k-ProjectCard__grid">
-        <Progress className="k-ProjectCard__grid__fluid k-u-margin-right-single" />
-        <Paragraph modifier="quaternary">
-          <strong className="k-u-color-primary1">23 %</strong>
-        </Paragraph>
+      <div className="k-ProjectCard__grid k-ProjectCard__borderTop">
+        <Marger top="2" bottom="2" className="k-ProjectCard__flex">
+          <Progress className="k-ProjectCard__flex__fluid" />
+          <Paragraph margin={ false }
+                     modifier="quaternary"
+                     className="k-u-margin-left-single k-u-color-primary1 k-u-strong">
+            23 %
+          </Paragraph>
+        </Marger>
       </div>
 
-      <div className="k-ProjectCard__content">
-        <div className="k-ProjectCard__grid k-ProjectCard__grid--spaceBetween">
-          <Paragraph modifier="quaternary" className="k-u-align-center">
-            <span className="k-u-color-primary1">812</span> <br />
+      <div className="k-ProjectCard__grid k-ProjectCard__borderTop">
+        <Marger top="1.5"
+                bottom="1.5"
+                className="k-ProjectCard__flex k-ProjectCard__flex--spaceBetween">
+          <Paragraph margin={ false } modifier="quaternary" className="k-u-align-center">
+            <span className="k-u-strong k-u-color-primary1">495</span><br />
             preteurs
           </Paragraph>
 
-          <Paragraph modifier="quaternary" className="k-u-align-center">
-            <span className="k-u-color-primary1">71 000 €</span> <br />
+          <Paragraph margin={ false } modifier="quaternary" className="k-u-align-center">
+            <span className="k-u-strong k-u-color-primary1">71 000 €</span><br />
             collectés
           </Paragraph>
 
-          <Paragraph modifier="quaternary" className="k-u-align-center">
-            <span className="k-u-color-primary1">12 j</span> <br />
+          <Paragraph margin={ false } modifier="quaternary" className="k-u-align-center">
+            <span className="k-u-strong k-u-color-primary1">12 j</span><br />
             restants
           </Paragraph>
-        </div>
+        </Marger>
       </div>
 
-      <div className="k-ProjectCard__status">
-        Projet à l'étude
+      <div className="k-ProjectCard__grid k-ProjectCard__borderTop">
+        <Marger top="1" bottom="1">
+          <div className="k-ProjectCard__status">
+            Projet à l'étude
+          </div>
+        </Marger>
       </div>
     </div>
   )
