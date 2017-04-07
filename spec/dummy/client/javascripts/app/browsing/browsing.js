d1b57bdb87ed75921d1cd46e0f1f2fbd74fca456
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2017-04-07 16:53:22 +0200

    Update browsing example

--- spec/dummy/client/javascripts/app/browsing/browsing.js
+++ spec/dummy/client/javascripts/app/browsing/browsing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames'
 import { Grid, GridCol } from 'kitten/components/grid/grid'
 import { LegoGrid } from 'kitten/components/grid/lego-grid'
 import { Title } from 'kitten/components/typography/title'
@@ -9,7 +10,7 @@ import { KarlProjectCard } from 'kitten/karl/cards/project-card'
 import { Separator } from 'kitten/components/layout/separator'
 import { Progress } from 'kitten/components/meters/progress'
 
-const randomLogo = (key) => {
+const randomAvatar = (key) => {
   return `http://lorempixel.com/40/40/abstract/${ key }`
 }
 
@@ -71,31 +72,192 @@ export class KarlBrowsing extends React.Component {
     }
 
     const projectCards = [
-      { key: 'project-4', status: 'succeed', score: 'B', image: false, dataLocked: true, logo: randomLogo(4) },
-      { key: 'project-5', status: 'studing', score: 'B+', image: false, logo: randomLogo(5) },
-      { key: 'project-6', status: 'defaulted', score: 'C', image: randomImage(3), logo: randomLogo(6) },
-      { key: 'project-7', status: 'retarded', score: 'A', image: randomImage(4), logo: randomLogo(7) },
-      { key: 'project-8', status: 'fraudulent', score: 'B+', image: false, logo: randomLogo(8) },
-      { key: 'project-9', status: 'failed', score: 'A+', image: randomImage(5), logo: randomLogo(9) },
-      { key: 'project-9', status: 'failed', score: 'C', image: false, logo: randomLogo(10) },
-      { key: 'project-9', status: 'failed', score: 'B+', image: randomImage(6), logo: randomLogo(1) },
+      {
+        href: "#",
+        id: 'project-4',
+        status: 'succeed',
+        score: 'A',
+        image: false,
+        avatar: randomAvatar(4),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt', lockedValue: true },
+        info2: { value: '48 mois', text: 'Durée', lockedValue: true },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-5',
+        status: 'studing',
+        score: 'B+',
+        image: false,
+        avatar: randomAvatar(5),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-6',
+        status: 'defaulted',
+        score: 'C',
+        image: randomImage(3),
+        avatar: randomAvatar(6),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-7',
+        status: 'retarded',
+        score: 'A',
+        image: randomImage(4),
+        avatar: randomAvatar(7),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-8',
+        status: 'fraudulent',
+        score: 'B+',
+        image: false,
+        avatar: randomAvatar(8),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-9',
+        status: 'failed',
+        score: 'A+',
+        image: randomImage(5),
+        avatar: randomAvatar(9),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-10',
+        status: 'failed',
+        score: 'C',
+        image: randomImage(6),
+        avatar: randomAvatar(10),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-11',
+        status: 'failed',
+        score: 'B+',
+        image: false,
+        avatar: randomAvatar(1),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
     ]
 
     const projectCollectingCards = [
-      { key: 'project-1', status: 'collecting', score: 'A', image: false, logo: randomLogo(1) },
-      { key: 'project-2', status: 'closing', score: 'C', image: randomImage(1), logo: randomLogo(2) },
-      { key: 'project-3', status: 'collecting', disabled: true, score: 'A+', image: randomImage(2), logo: randomLogo(3) },
+      {
+        href: "#",
+        id: 'project-1',
+        status: 'collecting',
+        score: 'A',
+        image: false,
+        avatar: randomAvatar(1),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-2',
+        status: 'closing',
+        score: 'C',
+        image: randomImage(1),
+        avatar: randomAvatar(2),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+      },
+      {
+        href: "#",
+        id: 'project-3',
+        status: 'collecting',
+        score: 'A+',
+        image: randomImage(2),
+        avatar: randomAvatar(3),
+        name: 'Léa & Léo Groupe',
+        tags: 'TPE / PME',
+        title: 'Reprise d\'une micro crèche',
+        progress: 23,
+        info1: { value: '8,8 %', text: 'Taux d\'intérêt' },
+        info2: { value: '48 mois', text: 'Durée' },
+        info3: { value: '135 000 €', text: 'Objectif' },
+        disabled: true,
+      },
     ]
 
     return (
       <div>
         <Marger top="1.5" bottom="1.5">
-          <Title margin={ false } tag="h2" modifier="secondary">Collectes du moment</Title>
+          <Title margin={ false } tag="h2" modifier="secondary">
+            Collectes du moment
+          </Title>
         </Marger>
 
         <LegoGrid masonryProps={ masonryProps }>
           <LegoGrid.Item key="special-card">
-            <div className="k-ProjectCard">
+            <div className={ classNames('k-ProjectCard',
+                                        'k-Card',
+                                        'k-Card--light',
+                                        'k-Card--withBorder',
+                                        'k-Card--withShadow') }>
               <div className="k-ProjectCard__grid">
                 <Title modifier="quaternary">Projets recommandés</Title>
                 <Paragraph modifier="tertiary">
@@ -133,7 +295,9 @@ export class KarlBrowsing extends React.Component {
         </Marger>
 
         <Marger top="3" bottom="1.5">
-          <Title margin={ false } tag="h2" modifier="secondary">Collectes terminées</Title>
+          <Title margin={ false } tag="h2" modifier="secondary">
+            Collectes terminées
+          </Title>
         </Marger>
 
         <LegoGrid masonryProps={ masonryProps }>
@@ -149,7 +313,8 @@ export class KarlBrowsing extends React.Component {
 
   render() {
     return (
-      <div className="k-DevGrid__container k-DevGrid__container--withoutOutline">
+      <div className={ classNames('k-DevGrid__container',
+                                  'k-DevGrid__container--withoutOutline') }>
         <Marger top="3" bottom="3">
           <Grid>
             <GridCol col-l="10" col="12">
@@ -159,18 +324,37 @@ export class KarlBrowsing extends React.Component {
 
             <GridCol col="2" className="k-u-hidden@m-down">
               <Marger top="1.5" bottom="1.5">
-                <Title margin={ false } tag="p" modifier="quaternary">Statuts</Title>
+                <Title margin={ false } tag="p" modifier="quaternary">
+                  Statuts
+                </Title>
               </Marger>
 
               <Marger top="1.5" bottom="3">
-                <RadioButton id="status-option-0" name="status" defaultChecked text="Tous les statuts" />
-                <RadioButton id="status-option-1" name="status" text="En cours" />
-                <RadioButton id="status-option-2" name="status" text="En remboursement" />
-                <RadioButton id="status-option-3" name="status" text="En défaut de paiement" />
-                <RadioButton id="status-option-4" name="status" text="En retard de paiement" />
-                <RadioButton id="status-option-5" name="status" text="Frauduleux" />
-                <RadioButton id="status-option-6" name="status" text="Échouées" />
-                <RadioButton id="status-option-7" name="status" text="Annulées" />
+                <RadioButton id="status-option-0"
+                             name="status"
+                             text="Tous les statuts"
+                             defaultChecked />
+                <RadioButton id="status-option-1"
+                             name="status"
+                             text="En cours" />
+                <RadioButton id="status-option-2"
+                             name="status"
+                             text="En remboursement" />
+                <RadioButton id="status-option-3"
+                             name="status"
+                             text="En défaut de paiement" />
+                <RadioButton id="status-option-4"
+                             name="status"
+                             text="En retard de paiement" />
+                <RadioButton id="status-option-5"
+                             name="status"
+                             text="Frauduleux" />
+                <RadioButton id="status-option-6"
+                             name="status"
+                             text="Échouées" />
+                <RadioButton id="status-option-7"
+                             name="status"
+                             text="Annulées" />
               </Marger>
 
               <Marger top="3" bottom="2">
@@ -178,20 +362,43 @@ export class KarlBrowsing extends React.Component {
               </Marger>
 
               <Marger top="2" bottom="1.5">
-                <Title margin={ false } tag="p" modifier="quaternary">Domaines</Title>
+                <Title margin={ false } tag="p" modifier="quaternary">
+                  Domaines
+                </Title>
               </Marger>
 
               <Marger top="1.5" bottom="1.5">
-                <RadioButton id="domaine-option-0" name="domaines" defaultChecked text="Tous les domaines" />
-                <RadioButton id="domaine-option-1" name="domaines" text="Agriculture" />
-                <RadioButton id="domaine-option-2" name="domaines" text="Industrie et agroalimentaire" />
-                <RadioButton id="domaine-option-3" name="domaines" text="Construction" />
-                <RadioButton id="domaine-option-4" name="domaines" text="Transport" />
-                <RadioButton id="domaine-option-5" name="domaines" text="Hotellerie et restauration" />
-                <RadioButton id="domaine-option-6" name="domaines" text="Services aux entreprises" />
-                <RadioButton id="domaine-option-7" name="domaines" text="Édition, informatique, communication et conseil" />
-                <RadioButton id="domaine-option-8" name="domaines" text="Santé" />
-                <RadioButton id="domaine-option-9" name="domaines" text="Autres domaines" />
+                <RadioButton id="domaine-option-0"
+                             name="domaines"
+                             text="Tous les domaines"
+                             defaultChecked />
+                <RadioButton id="domaine-option-1"
+                             name="domaines"
+                             text="Agriculture" />
+                <RadioButton id="domaine-option-2"
+                             name="domaines"
+                             text="Industrie et agroalimentaire" />
+                <RadioButton id="domaine-option-3"
+                             name="domaines"
+                             text="Construction" />
+                <RadioButton id="domaine-option-4"
+                             name="domaines"
+                             text="Transport" />
+                <RadioButton id="domaine-option-5"
+                             name="domaines"
+                             text="Hotellerie et restauration" />
+                <RadioButton id="domaine-option-6"
+                             name="domaines"
+                             text="Services aux entreprises" />
+                <RadioButton id="domaine-option-7"
+                             name="domaines"
+                             text="Édition, informatique, communication et conseil" />
+                <RadioButton id="domaine-option-8"
+                             name="domaines"
+                             text="Santé" />
+                <RadioButton id="domaine-option-9"
+                             name="domaines"
+                             text="Autres domaines" />
               </Marger>
 
               <Marger top="3" bottom="2">
@@ -199,17 +406,34 @@ export class KarlBrowsing extends React.Component {
               </Marger>
 
               <Marger top="2" bottom="1.5">
-                <Title margin={ false } tag="p" modifier="quaternary">Localisation</Title>
+                <Title margin={ false } tag="p" modifier="quaternary">
+                  Localisation
+                </Title>
               </Marger>
 
               <Marger top="1.5" bottom="1.5">
-                <RadioButton id="localisation-option-0" name="localisation" defaultChecked text="Toutes les localisations" />
-                <RadioButton id="localisation-option-1" name="localisation" text="France" />
-                <RadioButton id="localisation-option-2" name="localisation" text="Île-de-France" />
-                <RadioButton id="localisation-option-3" name="localisation" text="Nord-Ouest" />
-                <RadioButton id="localisation-option-4" name="localisation" text="Nord-Est" />
-                <RadioButton id="localisation-option-5" name="localisation" text="Sud-Ouest" />
-                <RadioButton id="localisation-option-5" name="localisation" text="Sud-Est" />
+                <RadioButton id="localisation-option-0"
+                             name="localisation"
+                             text="Toutes les localisations"
+                             defaultChecked />
+                <RadioButton id="localisation-option-1"
+                             name="localisation"
+                             text="France" />
+                <RadioButton id="localisation-option-2"
+                             name="localisation"
+                             text="Île-de-France" />
+                <RadioButton id="localisation-option-3"
+                             name="localisation"
+                             text="Nord-Ouest" />
+                <RadioButton id="localisation-option-4"
+                             name="localisation"
+                             text="Nord-Est" />
+                <RadioButton id="localisation-option-5"
+                             name="localisation"
+                             text="Sud-Ouest" />
+                <RadioButton id="localisation-option-5"
+                             name="localisation"
+                             text="Sud-Est" />
               </Marger>
 
               <Marger top="3" bottom="2">
@@ -217,11 +441,16 @@ export class KarlBrowsing extends React.Component {
               </Marger>
 
               <Marger top="2" bottom="1.5">
-                <Title margin={ false } tag="p" modifier="quaternary">Note</Title>
+                <Title margin={ false } tag="p" modifier="quaternary">
+                  Note
+                </Title>
               </Marger>
 
               <Marger top="1.5" bottom="1.5">
-                <RadioButton id="note-option-0" name="score" defaultChecked text="Toutes les notes" />
+                <RadioButton id="note-option-0"
+                             name="score"
+                             text="Toutes les notes"
+                             defaultChecked />
                 <RadioButton id="note-option-1" name="score" text="A+" />
                 <RadioButton id="note-option-2" name="score" text="A" />
                 <RadioButton id="note-option-4" name="score" text="B+" />
