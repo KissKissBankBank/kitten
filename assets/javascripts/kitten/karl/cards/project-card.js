61b1b40a5ac97c7d272f6376197b3af49f155b0a
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2017-04-07 16:50:36 +0200

    Add KarlProjectCard

--- assets/javascripts/kitten/karl/cards/project-card.js
+++ assets/javascripts/kitten/karl/cards/project-card.js
@@ -1,43 +1,12 @@
 import React from 'react'
 import classNames from 'classnames'
-import { Grid, GridCol } from 'kitten/components/grid/grid'
-import { Marger } from 'kitten/components/layout/marger'
-import { Title } from 'kitten/components/typography/title'
-import { Paragraph } from 'kitten/components/typography/paragraph'
-import { Progress } from 'kitten/components/meters/progress'
-import { Button } from 'kitten/components/buttons/button'
-import { ButtonImage } from 'kitten/components/buttons/button-image'
-import { IconBadge } from 'kitten/components/notifications/icon-badge'
-import { TagList } from 'kitten/components/lists/tag-list'
+import { ProjectCard } from 'kitten/components/cards/project-card'
 import { CheckedCircleIcon } from 'kitten/components/icons/checked-circle-icon'
 import { WarningCircleIcon } from 'kitten/components/icons/warning-circle-icon'
 import { CrossCircleIcon } from 'kitten/components/icons/cross-circle-icon'
 import { ClockCircleIcon } from 'kitten/components/icons/clock-circle-icon'
-import { LockIcon } from 'kitten/components/icons/lock-icon'
 
 export class KarlProjectCard extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = this.initialState(props)
-  }
-
-  initialState(props) {
-    const isCollectingOrClosing = props.status == 'collecting' || props.status == 'closing'
-
-    const showProgress = isCollectingOrClosing
-    const showResults = props.status != 'studing'
-    const showCurrentlyResults = isCollectingOrClosing && !props.disabled
-    const showStatus = props.status != 'collecting'
-
-    return {
-      showProgress: showProgress,
-      showResults: showResults,
-      showCurrentlyResults: showCurrentlyResults,
-      showStatus: showStatus,
-    }
-  }
-
   getHexaByScore() {
     const hexaByScore = {
       'A+': '#00696E',
@@ -47,305 +16,136 @@ export class KarlProjectCard extends React.Component {
       'C': '#8CC31E',
     }
 
-    return hexaByScore[this.props.score]
+    return this.props.score ? hexaByScore[this.props.score] : null
+  }
+
+  getScoreData() {
+    return {
+      scoreValue: this.props.score,
+      scoreBackgroundColor: this.getHexaByScore(),
+    }
   }
 
   getStatusData() {
     const iconClassName = 'k-ProjectCard__status__svg'
-    let label = null
-    let modifier = null
+    let data = {}
 
     switch(this.props.status) {
+      case 'collecting':
+        data = {
+          infosValuesColored: true,
+        }
+        break
       case 'closing':
-        label = [
-          <CheckedCircleIcon key={ `closing-icon-${this.props.key}` }
-                             className={ iconClassName }
-                             circleColor="#68ffa0" />,
-          "Collecte en closing !"
-        ]
-        break;
+        data = {
+          statusContent: [
+            <CheckedCircleIcon key={ `closing-icon-${ this.props.id }` }
+                               className={ iconClassName }
+                               circleColor="#68ffa0" />,
+            this.props.labelClosing
+          ],
+          infosValuesColored: true,
+        }
+        break
       case 'succeed':
-        modifier = 'k-ProjectCard__status--succeed'
-        label = [
-          <CheckedCircleIcon key={ `succeed-icon-${this.props.key}` }
-                             className={ iconClassName } />,
-          "Collecte réussie !"
-        ]
-        break;
+        data = {
+          statusModifier: 'k-ProjectCard__status--succeed',
+          statusContent: [
+            <CheckedCircleIcon key={ `succeed-icon-${ this.props.id }` }
+                               className={ iconClassName } />,
+            this.props.labelSucceed
+          ],
+          progress: false,
+        }
+        break
       case 'defaulted':
-        modifier = 'k-ProjectCard__status--defaulted'
-        label = [
-          <WarningCircleIcon key={ `defaulted-icon-${this.props.key}` }
-                             warningColor="#ff4146"
-                             className={ iconClassName } />,
-          "Projet en défaut de paiement"
-        ]
-        break;
+        data = {
+          statusModifier: 'k-ProjectCard__status--defaulted',
+          statusContent: [
+            <WarningCircleIcon key={ `defaulted-icon-${ this.props.id }` }
+                               warningColor="#ff4146"
+                               className={ iconClassName } />,
+            this.props.labelDefaulted
+          ],
+          progress: false,
+        }
+        break
       case 'retarded':
-        modifier = 'k-ProjectCard__status--retarded'
-        label = [
-          <WarningCircleIcon key={ `retarded-icon-${this.props.key}` }
-                             warningColor="#ff4146"
-                             className={ iconClassName } />,
-          "Projet en retard de paiement"
-        ]
-        break;
+        data = {
+          statusModifier: 'k-ProjectCard__status--retarded',
+          statusContent: [
+            <WarningCircleIcon key={ `retarded-icon-${ this.props.id }` }
+                               warningColor="#ff4146"
+                               className={ iconClassName } />,
+            this.props.labelRetarded
+          ],
+          progress: false,
+        }
+        break
       case 'fraudulent':
-        modifier = 'k-ProjectCard__status--retarded'
-        label = [
-          <WarningCircleIcon key={ `fraudulent-icon-${this.props.key}` }
-                             warningColor="#ff4146"
-                             className={ iconClassName } />,
-          "Projet frauduleux"
-        ]
-        break;
+        data = {
+          statusModifier: 'k-ProjectCard__status--retarded',
+          statusContent: [
+            <WarningCircleIcon key={ `fraudulent-icon-${ this.props.id }` }
+                               warningColor="#ff4146"
+                               className={ iconClassName } />,
+            this.props.labelFraudulent
+          ],
+          progress: false,
+        }
+        break
       case 'failed':
-        modifier = 'k-ProjectCard__status--failed'
-        label = [
-          <CrossCircleIcon key={ `failed-icon-${this.props.key}` }
-                           crossColor="#d8d8d8"
-                           className={ iconClassName } />,
-          "Collecte échouée"
-        ]
+        data = {
+          statusModifier: 'k-ProjectCard__status--failed',
+          statusContent: [
+            <CrossCircleIcon key={ `failed-icon-${ this.props.id }` }
+                             crossColor="#d8d8d8"
+                             className={ iconClassName } />,
+            this.props.labelFailed
+          ],
+          progress: false,
+        }
         break;
       case 'studing':
-        modifier = 'k-ProjectCard__status--studing'
-        label = [
-          <ClockCircleIcon key={ `studing-icon-${this.props.key}` }
-                           clockColor="#197ff2"
-                           className={ iconClassName } />,
-          "Projet à l'étude"
-        ]
-        break;
+        data = {
+          statusModifier: 'k-ProjectCard__status--studing',
+          statusContent: [
+            <ClockCircleIcon key={ `studing-icon-${ this.props.id }` }
+                             clockColor="#197ff2"
+                             className={ iconClassName } />,
+            this.props.labelStuding
+          ],
+          progress: false,
+        }
+        break
       default:
-        label = ""
+        data = {}
     }
 
-    return { label: label, modifier: modifier }
-  }
-
-  renderCompany() {
-    const scoreStyles = {
-      backgroundColor: this.getHexaByScore(),
-    }
-
-    return (
-      <div className="k-ProjectCard__grid">
-        <Marger top="2" bottom="1" className="k-ProjectCard__flex">
-          <ButtonImage tag="span" img={ { src: this.props.logo } } />
-          <Paragraph className={ classNames('k-ProjectCard__flex__fluid',
-                                            'k-u-margin-left-single',
-                                            'k-u-margin-right-single') }
-                     margin={ false }
-                     normalLineHeight
-                     modifier="quaternary">
-            par&nbsp;:<br />
-            <span className="k-u-strong">{ this.props.company }</span>
-          </Paragraph>
-          <IconBadge style={ scoreStyles }>{ this.props.score }</IconBadge>
-        </Marger>
-
-        <Marger top="1" bottom=".5">
-          <Title margin={ false } modifier="quaternary">
-            { this.props.title }
-          </Title>
-        </Marger>
-
-        <Marger top=".5" bottom="1">
-          <TagList items={ this.props.tags } />
-        </Marger>
-
-        <Marger top=".5" bottom="0.5">
-          <Paragraph margin={ false } modifier="tertiary">
-            { this.props.description }
-          </Paragraph>
-        </Marger>
-
-        <Marger top=".5" bottom="1.5">
-          <Paragraph margin={ false } modifier="quaternary">
-            {/* TODO: Add `Paragraph.Link` component. */}
-            <span className="k-Paragraph__link k-u-hidden@s-up">
-              Lire la suite…
-            </span>
-          </Paragraph>
-        </Marger>
-      </div>
-    )
-  }
-
-  renderGoals() {
-    return (
-      <div className="k-ProjectCard__grid">
-        <Marger top="1.5"
-                bottom="2"
-                className={ classNames('k-ProjectCard__flex',
-                                       'k-ProjectCard__flex--spaceBetween') }>
-          <span className="k-u-align-center k-ProjectCard__data">
-            Taux d’intérêt<br />
-            <span className="k-ProjectCard__data__value">
-              { this.props.dataLocked ? <LockIcon width="12" /> : this.props.interestRate }
-            </span>
-          </span>
-
-          <span className="k-u-align-center k-ProjectCard__data">
-            Objectif<br />
-            <span className="k-ProjectCard__data__value">
-              { this.props.goal }
-            </span>
-          </span>
-
-          <span className="k-u-align-center k-ProjectCard__data">
-            Durée<br />
-            <span className="k-ProjectCard__data__value">
-              { this.props.dataLocked ? <LockIcon width="12" /> : this.props.duration }
-            </span>
-          </span>
-        </Marger>
-      </div>
-    )
-  }
-
-  renderImage() {
-    if (!this.props.image) return
-
-    return (
-      <div className="k-ProjectCard__grid">
-        <Marger top="2" bottom="2">
-          <img className="k-u-align-center k-ProjectCard__img"
-               src={ this.props.image } />
-        </Marger>
-      </div>
-    )
-  }
-
-  renderProgress() {
-    if (!this.state.showProgress) return
-
-    return (
-      <div className="k-ProjectCard__grid k-ProjectCard__borderTop">
-        <Marger top="1.5" bottom="1.5" className="k-ProjectCard__flex">
-          <Progress className="k-ProjectCard__flex__fluid"
-                    value={ this.props.progress } />
-          <Paragraph margin={ false }
-                     modifier="quaternary"
-                     className={ classNames('k-u-margin-left-double',
-                                            'k-u-color-primary1',
-                                            'k-u-strong') }>
-            { this.props.progress }&nbsp;%
-          </Paragraph>
-        </Marger>
-      </div>
-    )
-  }
-
-  renderResults() {
-    if (!this.state.showResults) return
-
-    const dataClassName = classNames(
-      'k-ProjectCard__data__value',
-      { 'k-u-color-primary1': this.state.showCurrentlyResults },
-    )
-
-    return (
-      <div className="k-ProjectCard__grid k-ProjectCard__borderTop">
-        <Marger top="1.5"
-                bottom="1.5"
-                className="k-ProjectCard__flex k-ProjectCard__flex--spaceBetween">
-          <span className="k-u-align-center k-ProjectCard__data">
-            <span className={ dataClassName }>
-              { this.props.lenders }
-            </span><br />
-            preteurs
-          </span>
-
-          <span className="k-u-align-center k-ProjectCard__data">
-            <span className={ dataClassName }>
-              { this.props.collected }
-            </span><br />
-            collectés
-          </span>
-
-          <span className="k-u-align-center k-ProjectCard__data">
-            <span className={ dataClassName }>
-              {
-                this.state.showCurrentlyResults
-                ? this.props.remaining
-                : this.props.endAt
-              }
-            </span><br />
-            {
-              this.state.showCurrentlyResults
-              ? 'restants'
-              : 'date de fin'
-            }
-          </span>
-        </Marger>
-      </div>
-    )
-  }
-
-  renderStatus() {
-    if (!this.state.showStatus) return
-
-    const data = this.getStatusData()
-
-    const statusClassName = classNames(
-      'k-ProjectCard__grid',
-      { 'k-ProjectCard__borderTop': this.props.status == 'closing' },
-      'k-ProjectCard__status',
-      data.modifier,
-    )
-
-    return (
-      <div className={ statusClassName }>
-        <Marger top="1" bottom="1" className="k-ProjectCard__status__content">
-          { data.label }
-        </Marger>
-      </div>
-    )
+    return data
   }
 
   render() {
-    const projectCardClassName = classNames(
-      'k-ProjectCard',
-      {
-        'k-ProjectCard--fullWidth': this.props.fullWidth,
-        'is-disabled': this.props.disabled,
-        'is-failed': this.props.status == 'failed'
-      }
-    )
-
     return (
-      <div key={ this.props.key }  className={ projectCardClassName }>
-        { this.renderCompany() }
-        { this.renderGoals() }
-        { this.renderImage() }
-        { this.renderProgress() }
-        { this.renderResults() }
-        { this.renderStatus() }
-      </div>
+      <ProjectCard { ...this.props }
+                   { ...this.getStatusData() }
+                   { ...this.getScoreData() } />
     )
   }
 }
 
+// Status possibilities: collecting, closing, succeed, defaulted,
+//                       retarded, fraudulent, failed, studing.
+
 KarlProjectCard.defaultProps = {
-  key: '1',
-  disabled: false,
-  dataLocked: false,
-  logo: 'https://placehold.it/40x40/4a84ff/4a84ff',
-  company: 'JCD Diffusion',
-  score: 'A',
-  title: 'Location de materiel de reception',
-  tags: [{ key: 'tpe-pme', item: 'TPE / PME' }],
-  description: 'Achat de matériel (vaisselle, tables, chaises, …) dans le cadre de son activité de location de matériel de réception',
-  interestRate: '8,8 %',
-  goal: '70 000 €',
-  duration: '48 mois',
-  image: 'https://placehold.it/310x175/4a84ff/4a84ff',
-  progress: 28,
-  lenders: 495,
-  collected: '71 000 €',
-  remaining: '12 j',
-  endAt: '12/04/2017',
-  status: 'collecting' // collecting, closing, succeed, defaulted, retarded, fraudulent, failed, studing
+  score: null,
+  status: null,
+
+  labelClosing: "Collecte en closing !",
+  labelSucceed: "Collecte réussie !",
+  labelDefaulted: "Projet en défaut de paiement",
+  labelRetarded: "Projet en retard de paiement",
+  labelFraudulent: "Projet frauduleux",
+  labelFailed: "Collecte échouée",
+  labelStuding: "Projet à l'étude",
 }
