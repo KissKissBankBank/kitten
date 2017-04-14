6bede7d3eea09444c1c9f72522e33c54c7852cbd
Camille Delbove <camille.delbove@gmail.com>
2017-04-14 17:05:30 +0200

    Correct `,` ...

--- lib/kitten/configuration.rb
+++ lib/kitten/configuration.rb
@@ -31,29 +31,29 @@ module Kitten
           components: [
             {
               path: 'components/grid/grid',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/grid/vertical-grid',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/grid/side-grid',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/grid/row',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/grid/single-column',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/grid/lego-grid',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
-          ]
+          ],
         },
         {
           id: 'typography',
@@ -61,43 +61,43 @@ module Kitten
           components: [
             {
               path: 'components/typography/title',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/typography/paragraph',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
-          ]
+          ],
         },
         {
           id: 'buttons',
           label: 'Buttons',
           components: [
             {
-              path: 'components/buttons/button'
+              path: 'components/buttons/button',
             },
             {
-              path: 'components/buttons/button-icon'
+              path: 'components/buttons/button-icon',
             },
             {
-              path: 'components/buttons/button-with-icon-words'
+              path: 'components/buttons/button-with-icon-words',
             },
             {
-              path: 'components/buttons/button-image'
+              path: 'components/buttons/button-image',
             },
             {
-              path: 'components/buttons/button-image-with-text'
+              path: 'components/buttons/button-image-with-text',
             },
             {
-              path: 'components/buttons/tag-button'
+              path: 'components/buttons/tag-button',
             },
             {
-              path: 'components/buttons/payment-button'
+              path: 'components/buttons/payment-button',
             },
             {
-              path: 'components/buttons/button-with-badge'
+              path: 'components/buttons/button-with-badge',
             },
-          ]
+          ],
         },
         {
           id: 'form-elements',
@@ -105,99 +105,99 @@ module Kitten
           components: [
             {
               path: 'components/form/checkbox',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/radio-button',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/text-input',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/text-input-with-button',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/text-input-with-limit',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/text-input-with-unit',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/location-input',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/select',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
-              path: 'components/form/switch'
+              path: 'components/form/switch',
             },
             {
               path: 'components/form/slider',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
-              path: 'components/form/search-input'
+              path: 'components/form/search-input',
             },
             {
               path: 'components/form/form-actions',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/form-row',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/form-info',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/form-amount-and-currency',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/form-phone-number',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
-          ]
+          ],
         },
         {
           id: 'navigation',
           label: 'Navigation',
           components: [
             {
-              path: 'components/navigation/horizontal-nav'
+              path: 'components/navigation/horizontal-nav',
             },
             {
               path: 'components/navigation/tab-bar',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/navigation/user-menu',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/navigation/platform-switch',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/headers/header',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/steppers/stepper',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
-              path: 'components/dropdowns/dropdown'
+              path: 'components/dropdowns/dropdown',
             },
-          ]
+          ],
         },
         {
           id: 'notifications',
@@ -205,18 +205,18 @@ module Kitten
           components: [
             {
               path: 'components/notifications/alert',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
-              path: 'components/notifications/badge'
+              path: 'components/notifications/badge',
             },
             {
-              path: 'components/notifications/icon-badge'
+              path: 'components/notifications/icon-badge',
             },
             {
-              path: 'components/tours/tour'
+              path: 'components/tours/tour',
             },
-          ]
+          ],
         },
         {
           id: 'crowdfunding',
@@ -224,24 +224,24 @@ module Kitten
           components: [
             {
               path: 'components/meters/progress',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/simulators/loan-simulator',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
-              path: 'components/simulators/simulator-result'
+              path: 'components/simulators/simulator-result',
             },
             {
               path: 'components/teaser/teaser',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/popovers/popover',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
-          ]
+          ],
         },
         {
           id: 'presentation-elements',
@@ -249,82 +249,82 @@ module Kitten
           components: [
             {
               path: 'components/cards/card',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/heroes/hero',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
-              path: 'components/press/press-quotes'
+              path: 'components/press/press-quotes',
             },
             {
-              path: 'components/testimonies/testimony'
+              path: 'components/testimonies/testimony',
             },
             {
               path: 'components/testimonies/testimony-list',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/titles/title-with-actions',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
-              path: 'components/links/external-rich-link'
+              path: 'components/links/external-rich-link',
             },
             {
-              path: 'components/links/link-list'
+              path: 'components/links/link-list',
             },
             {
-              path: 'components/lists/bullet-list'
+              path: 'components/lists/bullet-list',
             },
             {
-              path: 'components/lists/tag-list'
+              path: 'components/lists/tag-list',
             },
-          ]
+          ],
         },
         {
           id: 'contextual-content',
           label: 'Contextual content',
           components: [
             {
-              path: 'components/box/information-box'
+              path: 'components/box/information-box',
             },
             {
-              path: 'components/box/link-box'
+              path: 'components/box/link-box',
             },
             {
-              path: 'components/box/link-box-with-icon'
+              path: 'components/box/link-box-with-icon',
             },
             {
-              path: 'components/tooltips/tooltip'
+              path: 'components/tooltips/tooltip',
             },
             {
               path: 'components/tooltips/static-tooltip',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
-          ]
+          ],
         },
         {
           id: 'layout',
           label: 'Layout',
           components: [
             {
-              path: 'components/layout/line'
+              path: 'components/layout/line',
             },
             {
               path: 'components/form/form-actions',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/form/form-row',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
             {
               path: 'components/layout/separator',
-              examples_display: :vertical
+              examples_display: :vertical,
             },
-          ]
+          ],
         }
       ]
     end
