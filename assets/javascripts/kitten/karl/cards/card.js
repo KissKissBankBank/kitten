91bcbfb6326a296545d0f01cc18742c8ac694c7e
Camille Delbove <camille.delbove@gmail.com>
2017-04-14 15:09:46 +0200

    Delete class `karl-SizeClassName`

--- assets/javascripts/kitten/karl/cards/card.js
+++ assets/javascripts/kitten/karl/cards/card.js
@@ -2,18 +2,8 @@ import React from 'react'
 import classNames from 'classnames'
 import { card } from 'kitten/hoc/card'
 
-const KarlCardComponent = ({ className, ...props }) => {
-   const cardClassName = classNames(
-      'karl-SizeCard',
-      className,
-    )
-
-   const styles = { height: '100px' }
-
-  return (
-    <div className={ cardClassName } styles={ styles } { ...props } />
-  )
-}
+const KarlCardComponent = props =>
+  <div style={ { height: '100px' } } { ...props } />
 
 export const KarlCard = card(KarlCardComponent, {
   light: true,
