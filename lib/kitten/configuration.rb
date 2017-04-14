ce655c96302ede5b686bb0bac6fc5e87f97053d3
Camille Delbove <camille.delbove@gmail.com>
2017-04-14 17:33:33 +0200

    Remove `path:` objet.

--- lib/kitten/configuration.rb
+++ lib/kitten/configuration.rb
@@ -29,261 +29,127 @@ module Kitten
           id: 'grid-system',
           label: 'Grid system',
           components: [
-            {
-              path: 'components/grid/grid',
-            },
-            {
-              path: 'components/grid/vertical-grid',
-            },
-            {
-              path: 'components/grid/side-grid',
-            },
-            {
-              path: 'components/grid/row',
-            },
-            {
-              path: 'components/grid/single-column',
-            },
-            {
-              path: 'components/grid/lego-grid',
-            },
+            'components/grid/grid',
+            'components/grid/vertical-grid',
+            'components/grid/side-grid',
+            'components/grid/row',
+            'components/grid/single-column',
+            'components/grid/lego-grid',
           ],
         },
         {
           id: 'typography',
           label: 'Typography',
           components: [
-            {
-              path: 'components/typography/title',
-            },
-            {
-              path: 'components/typography/paragraph',
-            },
+            'components/typography/title',
+            'components/typography/paragraph',
           ],
         },
         {
           id: 'buttons',
           label: 'Buttons',
           components: [
-            {
-              path: 'components/buttons/button',
-            },
-            {
-              path: 'components/buttons/button-icon',
-            },
-            {
-              path: 'components/buttons/button-with-icon-words',
-            },
-            {
-              path: 'components/buttons/button-image',
-            },
-            {
-              path: 'components/buttons/button-image-with-text',
-            },
-            {
-              path: 'components/buttons/tag-button',
-            },
-            {
-              path: 'components/buttons/payment-button',
-            },
-            {
-              path: 'components/buttons/button-with-badge',
-            },
+            'components/buttons/button',
+            'components/buttons/button-icon',
+            'components/buttons/button-with-icon-words',
+            'components/buttons/button-image',
+            'components/buttons/button-image-with-text',
+            'components/buttons/tag-button',
+            'components/buttons/payment-button',
+            'components/buttons/button-with-badge',
           ],
         },
         {
           id: 'form-elements',
           label: 'Form elements',
           components: [
-            {
-              path: 'components/form/checkbox',
-            },
-            {
-              path: 'components/form/radio-button',
-            },
-            {
-              path: 'components/form/text-input',
-            },
-            {
-              path: 'components/form/text-input-with-button',
-            },
-            {
-              path: 'components/form/text-input-with-limit',
-            },
-            {
-              path: 'components/form/text-input-with-unit',
-            },
-            {
-              path: 'components/form/location-input',
-            },
-            {
-              path: 'components/form/select',
-            },
-            {
-              path: 'components/form/switch',
-            },
-            {
-              path: 'components/form/slider',
-            },
-            {
-              path: 'components/form/search-input',
-            },
-            {
-              path: 'components/form/form-actions',
-            },
-            {
-              path: 'components/form/form-row',
-            },
-            {
-              path: 'components/form/form-info',
-            },
-            {
-              path: 'components/form/form-amount-and-currency',
-            },
-            {
-              path: 'components/form/form-phone-number',
-            },
+            'components/form/checkbox',
+            'components/form/radio-button',
+            'components/form/text-input',
+            'components/form/text-input-with-button',
+            'components/form/text-input-with-limit',
+            'components/form/text-input-with-unit',
+            'components/form/location-input',
+            'components/form/select',
+            'components/form/switch',
+            'components/form/slider',
+            'components/form/search-input',
+            'components/form/form-actions',
+            'components/form/form-row',
+            'components/form/form-info',
+            'components/form/form-amount-and-currency',
+            'components/form/form-phone-number',
           ],
         },
         {
           id: 'navigation',
           label: 'Navigation',
           components: [
-            {
-              path: 'components/navigation/horizontal-nav',
-            },
-            {
-              path: 'components/navigation/tab-bar',
-            },
-            {
-              path: 'components/navigation/user-menu',
-            },
-            {
-              path: 'components/navigation/platform-switch',
-            },
-            {
-              path: 'components/headers/header',
-            },
-            {
-              path: 'components/steppers/stepper',
-            },
-            {
-              path: 'components/dropdowns/dropdown',
-            },
+            'components/navigation/horizontal-nav',
+            'components/navigation/tab-bar',
+            'components/navigation/user-menu',
+            'components/navigation/platform-switch',
+            'components/headers/header',
+            'components/steppers/stepper',
+            'components/dropdowns/dropdown',
           ],
         },
         {
           id: 'notifications',
           label: 'Notification',
           components: [
-            {
-              path: 'components/notifications/alert',
-            },
-            {
-              path: 'components/notifications/badge',
-            },
-            {
-              path: 'components/notifications/icon-badge',
-            },
-            {
-              path: 'components/tours/tour',
-            },
+            'components/notifications/alert',
+            'components/notifications/badge',
+            'components/notifications/icon-badge',
+            'components/tours/tour',
           ],
         },
         {
           id: 'crowdfunding',
           label: 'Crowdfunding',
           components: [
-            {
-              path: 'components/meters/progress',
-            },
-            {
-              path: 'components/simulators/loan-simulator',
-            },
-            {
-              path: 'components/simulators/simulator-result',
-            },
-            {
-              path: 'components/teaser/teaser',
-            },
-            {
-              path: 'components/popovers/popover',
-            },
+            'components/meters/progress',
+            'components/simulators/loan-simulator',
+            'components/simulators/simulator-result',
+            'components/teaser/teaser',
+            'components/popovers/popover',
           ],
         },
         {
           id: 'presentation-elements',
           label: 'Presentation elements',
           components: [
-            {
-              path: 'components/cards/card',
-            },
-            {
-              path: 'components/heroes/hero',
-            },
-            {
-              path: 'components/press/press-quotes',
-            },
-            {
-              path: 'components/testimonies/testimony',
-            },
-            {
-              path: 'components/testimonies/testimony-list',
-            },
-            {
-              path: 'components/titles/title-with-actions',
-            },
-            {
-              path: 'components/links/external-rich-link',
-            },
-            {
-              path: 'components/links/link-list',
-            },
-            {
-              path: 'components/lists/bullet-list',
-            },
-            {
-              path: 'components/lists/tag-list',
-            },
+            'components/cards/card',
+            'components/heroes/hero',
+            'components/press/press-quotes',
+            'components/testimonies/testimony',
+            'components/testimonies/testimony-list',
+            'components/titles/title-with-actions',
+            'components/links/external-rich-link',
+            'components/links/link-list',
+            'components/lists/bullet-list',
+            'components/lists/tag-list',
           ],
         },
         {
           id: 'contextual-content',
           label: 'Contextual content',
           components: [
-            {
-              path: 'components/box/information-box',
-            },
-            {
-              path: 'components/box/link-box',
-            },
-            {
-              path: 'components/box/link-box-with-icon',
-            },
-            {
-              path: 'components/tooltips/tooltip',
-            },
-            {
-              path: 'components/tooltips/static-tooltip',
-            },
+            'components/box/information-box',
+            'components/box/link-box',
+            'components/box/link-box-with-icon',
+            'components/tooltips/tooltip',
+            'components/tooltips/static-tooltip',
           ],
         },
         {
           id: 'layout',
           label: 'Layout',
           components: [
-            {
-              path: 'components/layout/line',
-            },
-            {
-              path: 'components/form/form-actions',
-            },
-            {
-              path: 'components/form/form-row',
-            },
-            {
-              path: 'components/layout/separator',
-            },
+            'components/layout/line',
+            'components/form/form-actions',
+            'components/form/form-row',
+            'components/layout/separator',
           ],
         }
       ]
