1a7b0eff278aa147e91df25c8b8e9ef85da91007
Little Cheung <f.cheung.hk@gmail.com>
2017-04-18 15:03:19 +0200

    [Karl] - New components view (#606)

--- lib/kitten/configuration.rb
+++ lib/kitten/configuration.rb
@@ -17,9 +17,142 @@ module Kitten
     # This option is used to pass an array of output bundles different from
     # `application-bundle` for hot reloading.
     attr_accessor :webpack_output_bundles
+    attr_accessor :default_categories
 
     def initialize
       self.webpack_output_bundles = ['application-bundle.js']
+    end
+
+    def default_categories
+      [
+        {
+          id: 'grid-system',
+          label: 'Grid system',
+          components: [
+            'components/grid/grid',
+            'components/grid/vertical-grid',
+            'components/grid/side-grid',
+            'components/grid/row',
+            'components/grid/single-column',
+            'components/grid/lego-grid',
+          ],
+        },
+        {
+          id: 'typography',
+          label: 'Typography',
+          components: [
+            'components/typography/title',
+            'components/typography/paragraph',
+          ],
+        },
+        {
+          id: 'buttons',
+          label: 'Buttons',
+          components: [
+            'components/buttons/button',
+            'components/buttons/button-icon',
+            'components/buttons/button-with-icon-words',
+            'components/buttons/button-image',
+            'components/buttons/button-image-with-text',
+            'components/buttons/tag-button',
+            'components/buttons/payment-button',
+            'components/buttons/button-with-badge',
+          ],
+        },
+        {
+          id: 'form-elements',
+          label: 'Form elements',
+          components: [
+            'components/form/checkbox',
+            'components/form/radio-button',
+            'components/form/text-input',
+            'components/form/text-input-with-button',
+            'components/form/text-input-with-limit',
+            'components/form/text-input-with-unit',
+            'components/form/location-input',
+            'components/form/select',
+            'components/form/switch',
+            'components/form/slider',
+            'components/form/search-input',
+            'components/form/form-actions',
+            'components/form/form-row',
+            'components/form/form-info',
+            'components/form/form-amount-and-currency',
+            'components/form/form-phone-number',
+          ],
+        },
+        {
+          id: 'navigation',
+          label: 'Navigation',
+          components: [
+            'components/navigation/horizontal-nav',
+            'components/navigation/tab-bar',
+            'components/navigation/user-menu',
+            'components/navigation/platform-switch',
+            'components/headers/header',
+            'components/steppers/stepper',
+            'components/dropdowns/dropdown',
+          ],
+        },
+        {
+          id: 'notifications',
+          label: 'Notification',
+          components: [
+            'components/notifications/alert',
+            'components/notifications/badge',
+            'components/notifications/icon-badge',
+            'components/tours/tour',
+          ],
+        },
+        {
+          id: 'crowdfunding',
+          label: 'Crowdfunding',
+          components: [
+            'components/meters/progress',
+            'components/simulators/loan-simulator',
+            'components/simulators/simulator-result',
+            'components/teaser/teaser',
+            'components/popovers/popover',
+          ],
+        },
+        {
+          id: 'presentation-elements',
+          label: 'Presentation elements',
+          components: [
+            'components/cards/card',
+            'components/heroes/hero',
+            'components/press/press-quotes',
+            'components/testimonies/testimony',
+            'components/testimonies/testimony-list',
+            'components/titles/title-with-actions',
+            'components/links/external-rich-link',
+            'components/links/link-list',
+            'components/lists/bullet-list',
+            'components/lists/tag-list',
+          ],
+        },
+        {
+          id: 'contextual-content',
+          label: 'Contextual content',
+          components: [
+            'components/box/information-box',
+            'components/box/link-box',
+            'components/box/link-box-with-icon',
+            'components/tooltips/tooltip',
+            'components/tooltips/static-tooltip',
+          ],
+        },
+        {
+          id: 'layout',
+          label: 'Layout',
+          components: [
+            'components/layout/line',
+            'components/form/form-actions',
+            'components/form/form-row',
+            'components/layout/separator',
+          ],
+        }
+      ]
     end
   end
 end
