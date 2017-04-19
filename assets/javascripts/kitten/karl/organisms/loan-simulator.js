8750ad31f485c030514b44b726c0e3605e7bf5f1
Sunny Ripert <sunny@sunfox.org>
2017-04-19 19:41:58 +0200

    Update english examples

--- assets/javascripts/kitten/karl/organisms/loan-simulator.js
+++ assets/javascripts/kitten/karl/organisms/loan-simulator.js
@@ -8,34 +8,20 @@ const KarlLoanSimulator = defaultProps(LoanSimulator, {
   amountMin: 200,
   amountMax: 10000,
   amountName: 'amount',
-  initialAmount: null,
 
-  amountEmptyError: 'Please select an amount',
   amountOutOfBoundsError: 'Please choose an amount between $200 and $10,000',
 
-  installmentLabel: 'I can reimburse',
   installmentName: 'installment',
 
   durationName: 'duration',
 
   sliderPlaceholder: 'Slide to choose',
 
-  durationText: 'therefore',
-  durationMin: 1,
-  durationMax: 36,
-  durationSymbol: 'month',
-  durationSymbolPlural: 'months',
-
-  currencySymbol: '$',
-  installmentSymbol: '$/month',
-  locale: 'fr',
-
-  actionLabel: 'Go!',
+  actionLabel: 'OK',
 })
 
 const KarlLoanSimulatorWithCommission = defaultProps(KarlLoanSimulator, {
   displayCommission: true,
-  commissionLabel: 'Commission:',
 
   commissionRules: [
     { durationMax: 9, rate: 0.03 },
@@ -53,9 +39,6 @@ const KarlLoanSimulatorWithCommission = defaultProps(KarlLoanSimulator, {
 const KarlLoanSimulatorWithfeesExemption = defaultProps(KarlLoanSimulator, {
   displayCommission: true,
   feesExemption: true,
-  feesExemptionLabel: `instead of`,
-  commissionLabel: `Commission:`,
-
   commissionRules: [
     { durationMax: 9, rate: 0.03 },
     { durationMax: 18, rate: 0.04 },
