9de3b61c9f24c5bd9b4a6e6dd1ff8d2de0327468
robertanisko <robert.anisko@gmail.com>
2017-05-09 19:42:10 +0200

    Duplicate select component to create an async version

---
+++ assets/javascripts/kitten/components/form/select-with-state-async.js
@@ -0,0 +1,134 @@
+// Select that uses https://github.com/JedWatson/react-select
+// with default states and handles the current value as a local state.
+//
+// Also handles multi-level options. You can give `children: []` to each option
+// and fill it with options to represent a hierarchy.
+//
+// This version of the component uses asynchronous loading of options
+// instead of a predefined list of values.
+//
+//
+// E.g:
+//    <SelectWithStateAsync loadOptions={} />
+//
+import React from 'react'
+import classNames from 'classnames'
+import Select from 'react-select'
+
+class SelectWithStateAsync extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: props.value,
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(val) {
+    this.setState({ value: val })
+    this.props.onChange(val)
+    this.props.onInputChange({ value: val, name: this.props.name })
+  }
+
+  renderLabel() {
+    if (!this.props.labelText) return ''
+
+    return (
+      <label className="k-Select__label" id={ this.props.id }>
+        { this.props.labelText }
+      </label>
+    )
+  }
+
+  render() {
+    const { value, onChange, className, tiny, error, valid, disabled, ...other } = this.props
+    const selectClassName = classNames(
+      'k-Select',
+      className,
+      {
+        'k-Select--tiny': tiny,
+        'is-error': error,
+        'is-valid': valid,
+        'is-disabled': disabled,
+      }
+    )
+
+    return (
+      <div className={ selectClassName }>
+        { this.renderLabel() }
+        <SelectWithMultiLevelAsync value={ this.state.value }
+                                   onChange={ this.handleChange }
+                                   disabled={ disabled }
+                                   { ...other } />
+      </div>
+    )
+  }
+}
+
+class SelectWithMultiLevelAsync extends React.Component {
+  // Turns a hierarchy of options with `children` into a flat array
+  // of options with a `level` of 1, 2 or null.
+  // TODO: Where should we post-process the list of returned options?
+  flattenedOptions() {
+    const options = []
+
+    this.props.options.map(option => {
+      if (option.children) {
+        option.level = 1
+        options.push(option)
+        option.children.map(opt => {
+          opt.level = 2
+          options.push(opt)
+        })
+      } else {
+        options.push(option)
+      }
+    })
+
+    return options
+  }
+
+  // React-Select allows changing the way options are rendered.
+  optionRenderer({ level, label }) {
+    if (!level) return label
+
+    return (
+      <span className={ 'k-Select__option--level' + level }>
+        { label }
+      </span>
+    )
+  }
+
+  render() {
+    let inputProps = this.props.inputProps
+
+    if (this.props.labelText && !inputProps['aria-labelledby'])
+      inputProps['aria-labelledby'] = this.props.id
+
+    return <Select.Async optionRenderer={ this.optionRenderer }
+                         { ...this.props }
+                         inputProps={ inputProps }
+                         options={ this.props.options }
+                         loadOptions={ this.props.loadOptions } />
+  }
+}
+
+SelectWithStateAsync.defaultProps = {
+  onChange: function() {},
+  onInputChange: function() {},
+  clearable: false,
+  searchable: false,
+  deleteRemoves: false,
+  multi: false,
+  labelText: null,
+  error: false,
+  valid: false,
+  disabled: false,
+  tiny: false,
+  name: null,
+  inputProps: {},
+}
+
+export default SelectWithStateAsync
