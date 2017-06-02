f93086d995083ba39e8dfccecc0e068f973ad1d0
Morgan JARRY <morgan.jarry@gmail.com>
2017-06-02 16:59:43 +0200

    Prevent className to be override

--- assets/javascripts/kitten/components/layout/responsive-video-iframe-container.js
+++ assets/javascripts/kitten/components/layout/responsive-video-iframe-container.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import classNames from 'classnames'
 
-export const ResponsiveVideoIframeContainer = ({ ratio, ...props }) => {
-  return (
-    <div className='k-ResponsiveVideoIframeContainer'
-         style={ { paddingBottom: `${ratio}%` } }
-         { ...props } />
-  )
-}
+export const ResponsiveVideoIframeContainer =
+  ({ ratio, className, ...props }) => {
+    const componentClassName = classNames(
+      'k-ResponsiveVideoIframeContainer',
+      className
+    )
+
+    return (
+      <div className={ componentClassName }
+           style={ { paddingBottom: `${ratio}%` } }
+           { ...props } />
+    )
+  }
 
 ResponsiveVideoIframeContainer.defaultProps = {
   ratio: '67.5',
