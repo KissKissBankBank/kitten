313459dccdeb758103198997af0491b934956653
Camille Delbove <camille.delbove@gmail.com>
2017-06-02 19:57:44 +0200

    Center form-action component

--- assets/javascripts/kitten/karl/form/form-actions.js
+++ assets/javascripts/kitten/karl/form/form-actions.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { FormActions } from 'kitten/components/form/form-actions'
+import { Button } from 'kitten/components/buttons/button'
 
-export const KarlFormActions = () => {
+export const KarlFormActions = props => {
   return (
-    <FormActions>
-      <a href="#" className="k-Button k-Button--hydrogen">
+    <FormActions {...props}>
+      <a href="#" className="k-Button k-Button--hydrogen karl-FormActions__button">
         Button
       </a>
 
-      <button className="k-Button k-Button--helium">
+      <Button className="karl-FormActions__button" modifier="helium">
         Button
-      </button>
+      </Button>
 
-      <input className="k-Button k-Button--beryllium"
+      <input className="k-Button k-Button--beryllium karl-FormActions__button"
              type="submit"
              value="Button" />
     </FormActions>
