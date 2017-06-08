d79871b326fee76263dfff16d115536770f44fde
Camille Delbove <camille.delbove@gmail.com>
2017-06-08 12:01:20 +0200

    Add `Button component in `FormActions`

--- assets/javascripts/kitten/karl/form/form-actions.js
+++ assets/javascripts/kitten/karl/form/form-actions.js
@@ -5,15 +5,15 @@ import { Button } from 'kitten/components/buttons/button'
 export const KarlFormActions = props => {
   return (
     <FormActions {...props}>
-      <a href="#" className="k-Button k-Button--hydrogen karl-FormActions__button">
+      <Button tag="a" href="#" className="k-Button k-Button--hydrogen karl-FormActions__button">
         Button
-      </a>
+      </Button>
 
       <Button className="karl-FormActions__button" modifier="helium">
         Button
       </Button>
 
-      <input className="k-Button k-Button--beryllium karl-FormActions__button"
+      <Button tag="input" className="k-Button k-Button--beryllium karl-FormActions__button"
              type="submit"
              value="Button" />
     </FormActions>
