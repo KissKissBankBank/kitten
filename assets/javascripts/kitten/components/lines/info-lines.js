17bdc6b78dc310c6c1d767ac139bd77b4a6ddf92
Camille Delbove <camille.delbove@gmail.com>
2017-06-12 19:27:04 +0200

    Add new `InfoLines` component

---
+++ assets/javascripts/kitten/components/lines/info-lines.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export class InfoLines extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.renderInfo = this.renderInfo.bind(this)
+  }
+
+  renderInfos() {
+    return this.props.infos.map(this.renderInfo)
+  }
+
+  renderInfo(element) {
+    const { key, value } = element
+
+    return (
+      <div className="k-InfoLines__line">
+        <div className="k-InfoLines__line__key">
+          { key }
+        </div>
+
+        <div className="k-InfoLines__line__values">
+          { value }
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="k-InfoLines">
+        { this.renderInfos() }
+      </div>
+    )
+  }
+}
+
+InfoLines.defaultProps = {
+  items: [], // Eg: [{ key: …,value: … }]
+}
