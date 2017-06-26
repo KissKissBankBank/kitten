049b04abbe4ab0aaa5d31574b5116880c54b938e
Camille Delbove <camille.delbove@gmail.com>
2017-06-26 18:22:45 +0200

    Clean

--- assets/javascripts/kitten/components/buttons/button-info-icon.js
+++ assets/javascripts/kitten/components/buttons/button-info-icon.js
@@ -3,13 +3,10 @@ import { InfoIcon } from 'kitten/components/icons/info-icon'
 import { ButtonIcon } from 'kitten/components/buttons/button-icon'
 
 export const ButtonInfoIcon = props => (
-  <ButtonIcon modifier="beryllium"
-              size="micro"
-              { ...props }>
+  <ButtonIcon
+    modifier="beryllium"
+    size="micro"
+    { ...props }>
     <InfoIcon className="k-ButtonIcon__svg" />
   </ButtonIcon>
 )
-
-ButtonInfoIcon.defaultProps = {
-  type: 'button',
-}
