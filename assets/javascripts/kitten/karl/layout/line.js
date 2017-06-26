fac905f558f0140bee75fd5828b75e53d510dc85
Camille Delbove <camille.delbove@gmail.com>
2017-06-26 17:35:51 +0200

    Remove Tooltip of `Line` component

--- assets/javascripts/kitten/karl/layout/line.js
+++ assets/javascripts/kitten/karl/layout/line.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import classNames from 'classnames'
 import { Line } from 'kitten/components/layout/line'
-import { withTooltip } from 'kitten/hoc/with-tooltip'
-import { ButtonInfoIcon } from 'kitten/components/buttons/button-info-icon'
-import { HeartIcon } from 'kitten/components/icons/heart-icon'
-// DEPRECATED
 import Tooltip from 'kitten/components/tooltips/tooltip'
 import { Label } from 'kitten/components/form/label'
 import { TextInput } from 'kitten/components/form/text-input'
-
-const InfoWithTooltipButtonIcon = withTooltip(ButtonInfoIcon, {})
+import { HeartIcon } from 'kitten/components/icons/heart-icon'
 
 export const KarlLineWithTooltip = props => {
   return (
@@ -17,11 +11,11 @@ export const KarlLineWithTooltip = props => {
       <Line.Item>
         { props.text }
       </Line.Item>
-      <Line.Item >
-        <InfoWithTooltipButtonIcon id="tooltip-2">
-          Lorem ipsum, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua
-        </InfoWithTooltipButtonIcon>
+      <Line.Item>
+        <Tooltip id={ props.id }
+                 place={ props.place }>
+          { props.children }
+        </Tooltip>
       </Line.Item>
     </Line>
   )
@@ -34,28 +28,9 @@ export const KarlLineWithBigHeart = props => {
         Lorem ipsum dolor sit amet.
       </Line.Item>
       <Line.Item>
-        <button className={ classNames('k-ButtonIcon',
-                                       'k-ButtonIcon--hydrogen',
-                                       'k-ButtonIcon--big') }>
+        <button className="k-ButtonIcon k-ButtonIcon--hydrogen k-ButtonIcon--big">
           <HeartIcon className="k-ButtonIcon__svg" />
         </button>
-      </Line.Item>
-    </Line>
-  )
-}
-
-//DEPRECATED
-export const KarlLineWithTooltip = props => {
-  return (
-    <Line>
-      <Line.Item>
-        { props.text }
-      </Line.Item>
-      <Line.Item >
-        <Tooltip id={ props.id }
-                 place={ props.place }>
-          { children }
-        </Tooltip>
       </Line.Item>
     </Line>
   )
