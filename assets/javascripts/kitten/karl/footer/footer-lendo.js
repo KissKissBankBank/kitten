55fbf3045108e4246efa8a60f3044bd042759b88
Camille Delbove <camille.delbove@gmail.com>
2017-07-24 18:42:02 +0200

    Add `renderQuote()` and `renderList()

--- assets/javascripts/kitten/karl/footer/footer-lendo.js
+++ assets/javascripts/kitten/karl/footer/footer-lendo.js
@@ -6,53 +6,130 @@ import { TextInputWithButton } from 'kitten/components/form/text-input-with-butt
 import { Paragraph } from 'kitten/components/typography/paragraph'
 import { Title } from 'kitten/components/typography/title'
 import { ButtonIcon } from 'kitten/components/buttons/button-icon'
-import { FacebookIcon } from 'kitten/components/icons/facebook-icon'
-import { TwitterIcon } from 'kitten/components/icons/twitter-icon'
-import { LinkedinIcon } from 'kitten/components/icons/linkedin-icon'
+import { FacebookButtonIcon,
+         TwitterButtonIcon,
+         LinkedinButtonIcon } from 'kitten/components/buttons/social-button-icon'
 import { Separator } from 'kitten/components/layout/separator'
 import { LinkList } from 'kitten/components/links/link-list'
+import { Row } from 'kitten/components/grid/row'
 
 export class KarlFooterLendo extends Component {
-  renderFollow() {
+  renderNetwork() {
     return (
-      <div>
-        <Paragraph modifier="tertiary">
-          Suivez nous
-        </Paragraph>
-        <ButtonIcon
-          modifier="facebook">
-          <FacebookIcon
-            className="k-ButtonIcon__svg" />
-        </ButtonIcon>
-        <ButtonIcon
-          modifier="twitter">
-          <TwitterIcon
-            className="k-ButtonIcon__svg" />
-        </ButtonIcon>
-        <ButtonIcon
-          modifier="linkedin">
-          <LinkedinIcon
-            className="k-ButtonIcon__svg" />
-        </ButtonIcon>
-      </div>
+      <Row
+        lightTopBorder>
+        <Marger top="3" bottom="3">
+          <Grid>
+            <GridCol col="12">
+              <div className="k-FooterLendo__network">
+                <Grid>
+                  <GridCol col="2">
+                    <Paragraph
+                      modifier="tertiary"
+                      margin={ false }>
+                      Inscrivez-vous à notre Newsletter et à l'alerte nouveau projet
+                    </Paragraph>
+                  </GridCol>
+                  <GridCol col="4">
+                    <TextInputWithButton />
+                  </GridCol>
+                  <GridCol col="2" />
+                  <GridCol col="2">
+                    <Paragraph
+                      modifier="tertiary"
+                      margin={ false }
+                      className="k-u-align-right">
+                      Suivez nous
+                    </Paragraph>
+                  </GridCol>
+                  <GridCol col="2">
+                    <FacebookButtonIcon className="k-FooterLendo__svg" />
+                    <TwitterButtonIcon className="k-FooterLendo__svg" />
+                    <LinkedinButtonIcon />
+                  </GridCol>
+                </Grid>
+              </div>
+            </GridCol>
+          </Grid>
+        </Marger>
+      </Row>
     )
   }
 
-  renderNetwork() {
+  renderQuote() {
     return (
-      <Marger top="2.5" bottom="2.5">
-        <Grid>
-          <GridCol col="12" col-m="6">
-            <Paragraph
-              modifier="tertiary"
-              className="k-FooterLendo__grid">
-              Inscrivez-vous à notre Newsletter et à l'alerte nouveau projet
-            </Paragraph>
-            <TextInputWithButton />
-            { this.renderFollow() }
-          </GridCol>
-        </Grid>
-      </Marger>
+      <Row
+        lightTopBorder>
+        <Marger top="2" bottom="2">
+          <Paragraph
+            italic
+            className="k-u-align-center">
+            Attention : Investir présente un risque d’illiquidité et de perte partielle ou totale en capital.<br/>
+            Vérifiez vos capacités financières avant d’investir.
+          </Paragraph>
+        </Marger>
+      </Row>
+    )
+  }
+
+  renderList() {
+    const items = [
+      { key: 'key1', item: 'Comment emprunter', href: '#' },
+      { key: 'key2', item: 'Déposer mon projet', href: '#' },
+    ]
+
+    const items2 = [
+      { key: 'key1', item: 'Comment investir', href: '#' },
+      { key: 'key2', item: 'Fiscalité', href: '#' },
+      { key: 'key3', item: 'Découvrez les projets', href: '#' },
+      { key: 'key4', item: 'Projets recommandés', href: '#' },
+      { key: 'key5', item: 'Simulateur', href: '#' },
+    ]
+
+    const items3 = [
+      { key: 'key1', item: 'Blog', href: '#' },
+      { key: 'key2', item: 'Statistiques', href: '#' },
+      { key: 'key3', item: 'FAQ', href: '#' },
+      { key: 'key4', item: 'Mentors', href: '#' },
+    ]
+
+    const items4 = [
+      { key: 'key1', item: 'Contact', href: '#' },
+      { key: 'key2', item: 'Devenir partenaire', href: '#' },
+      { key: 'key3', item: 'Mentions légales', href: '#' },
+      { key: 'key4', item: 'Réclamations', href: '#' },
+      { key: 'key4', item: 'CGU', href: '#' },
+    ]
+
+    return (
+      <Row
+        lightTopBorder>
+        <Marger top="7.5" bottom="2">
+          <Grid>
+            <GridCol col="2">
+              <img src="lendopolis.svg" />
+            </GridCol>
+          </Grid>
+          <Grid>
+            <GridCol col="12">
+              <Grid>
+                <GridCol col="3">
+                  <LinkList items={ items } />
+                </GridCol>
+                <GridCol col="3">
+                  <LinkList items={ items2 } />
+                </GridCol>
+                <GridCol col="3">
+                  <LinkList items={ items3 } />
+                </GridCol>
+                <GridCol col="3">
+                  <LinkList items={ items4 } />
+                </GridCol>
+              </Grid>
+            </GridCol>
+          </Grid>
+        </Marger>
+      </Row>
     )
   }
 
@@ -60,6 +137,8 @@ export class KarlFooterLendo extends Component {
     return (
       <div className="k-FooterLendo">
         { this.renderNetwork() }
+        { this.renderQuote() }
+        { this.renderList() }
       </div>
     )
   }
