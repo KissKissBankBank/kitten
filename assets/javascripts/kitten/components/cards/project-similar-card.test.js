8c3e9330aa676f3407b44a60d83e97375f0e816e
Fanny Cheung <fanny@ynote.hk>
2017-07-28 13:04:45 +0200

    Update ProjectSimilarCard tests

--- assets/javascripts/kitten/components/cards/project-similar-card.test.js
+++ assets/javascripts/kitten/components/cards/project-similar-card.test.js
@@ -9,6 +9,47 @@ import { LockIcon } from 'kitten/components/icons/lock-icon'
 import { Loader } from 'kitten/components/loaders/loader'
 
 describe('<ProjectSimilarCard />', () => {
+  const project1 = {
+    imageSrc: 'image-1',
+    title: 'Title 1',
+    description: 'This is the description of project 1.',
+    tags: [],
+    infos: []
+  }
+
+  const project2 = {
+    imageSrc: 'image-2',
+    title: 'Title 2',
+    description: 'This is the description of project 2.',
+    tags: [{ key: 'tag-1', item: 'Tag 1' }, { key: 'tag-2', item: 'Tag 2' }],
+    infos: []
+  }
+
+  const project3 = {
+    imageSrc: 'image-3',
+    title: 'Title 3',
+    description: 'This is the description of project 3.',
+    tags: [],
+    infos: [
+      {
+        key: 'info-1',
+        text: 'Custom text 1',
+        value: 'Custom value 1'
+      },
+      {
+        key: 'info-2',
+        text: 'Custom text 2',
+        value: 'Custom value 2',
+        locked: true
+      },
+      {
+        key: 'info-3',
+        text: 'Custom text 3',
+        value: 'Custom value 3'
+      }
+    ]
+  }
+
   describe('by default', () => {
     const projectSimilarCard = shallow(<ProjectSimilarCard />)
 
@@ -21,89 +62,75 @@ describe('<ProjectSimilarCard />', () => {
     })
   })
 
-  describe('with imageSrc prop', () => {
-    const projectSimilarCard = mount(<ProjectSimilarCard imageSrc="test" />)
-    const image = projectSimilarCard.find('.k-ProjectSimilarCard__img')
+  describe('with projects props', () => {
+    describe('on ComponentDidMount', () => {
+      describe('with one basic project', () => {
+        const projectSimilarCard = mount(
+          <ProjectSimilarCard projects={ [project1] }/>
+        )
+        const image = projectSimilarCard.find('.k-ProjectSimilarCard__img')
+        const title = projectSimilarCard.find('.k-Title')
+        const paragraph = projectSimilarCard.find('.k-Paragraph')
 
-    it('renders an image with good href', () => {
-      expect(image).to.have.length(1)
-      expect(image).to.have.attr('src', 'test')
-    })
-  })
+        it('renders an image with good href', () => {
+          expect(image).to.have.length(1)
+          expect(image).to.have.attr('src', 'image-1')
+        })
 
-  describe('with title prop', () => {
-    const projectSimilarCard = mount(
-      <ProjectSimilarCard title="Custom title" />)
-    const title = projectSimilarCard.find('.k-Title')
+        it('renders a <Title />', () => {
+          expect(projectSimilarCard).to.have.descendants(Title)
+          expect(title.text()).to.equal('Title 1')
+        })
 
-    it('renders a <Title />', () => {
-      expect(projectSimilarCard).to.have.descendants(Title)
-      expect(title.text()).to.equal("Custom title")
-    })
-  })
+        it('renders a <Paragraph />', () => {
+          expect(projectSimilarCard).to.have.descendants(Paragraph)
+          expect(paragraph.text())
+            .to.equal('This is the description of project 1.')
+        })
+      })
 
-  describe('with paragraph prop', () => {
-    const projectSimilarCard = mount(
-      <ProjectSimilarCard paragraph="Custom paragraph" />)
-    const paragraph = projectSimilarCard.find('.k-Paragraph')
+      describe('with one project with tags', () => {
+        const projectSimilarCard = mount(
+          <ProjectSimilarCard projects={ [project2] }/>
+        )
+        const tags = projectSimilarCard.find('.k-TagList')
 
-    it('renders a <Paragraph />', () => {
-      expect(projectSimilarCard).to.have.descendants(Paragraph)
-      expect(paragraph.text()).to.equal("Custom paragraph")
-    })
-  })
+        it('renders a <TagList />', () => {
+          expect(projectSimilarCard).to.have.descendants(TagList)
+        })
 
-  describe('with tags props', () => {
-    const projectSimilarCard = mount(
-      <ProjectSimilarCard
-        tags={ [ { key: 'custom-tag', item: 'Custom tag' } ] } />
-    )
-    const tags = projectSimilarCard.find('.k-TagList')
+        it('has a first block with good item', () => {
+          const firstTag = tags.find('.k-TagList__item--first')
 
-    it('renders a <TagList />', () => {
-      expect(projectSimilarCard).to.have.descendants(TagList)
-    })
+          expect(firstTag).to.contain.text('Tag 1')
+        })
+      })
 
-    it('has a first block with good item', () => {
-      const firstTag = tags.at(0)
+      describe('with one project with infos', () => {
+        const projectSimilarCard = mount(
+          <ProjectSimilarCard projects={ [project3] }/>
+        )
+        const infos = projectSimilarCard.find('.k-ProjectSimilarCard__info')
 
-      expect(firstTag).to.contain.text('Custom tag')
-    })
-  })
+        it('renders 3 blocks', () => {
+          expect(infos).to.have.length(3)
+        })
 
-  describe('with infos props', () => {
-    const projectSimilarCard = mount(
-      <ProjectSimilarCard
-        infos={ [ { key: 'info-1',
-                    text: 'Custom text 1',
-                    value: 'Custom value 1' },
-                  { key: 'info-2',
-                    text: 'Custom text 2',
-                    value: 'Custom value 2',
-                    locked: true },
-                  { key: 'info-3',
-                    text: 'Custom text 3',
-                    value: 'Custom value 3' } ] }
-      />
-    )
-    const infos = projectSimilarCard.find('.k-ProjectSimilarCard__info')
+        it('has a first block with good values', () => {
+          const firstInfo = infos.at(0)
 
-    it('renders 3 blocks', () => {
-      expect(infos).to.have.length(3)
-    })
+          expect(firstInfo).to.contain.text('Custom value 1')
+          expect(firstInfo).to.contain.text('Custom text 1')
+        })
 
-    it('has a first block with good values', () => {
-      const firstInfo = infos.at(0)
+        it('has second block with locked value', () => {
+          const secondInfo = infos.at(1)
 
-      expect(firstInfo).to.contain.text('Custom value 1')
-      expect(firstInfo).to.contain.text('Custom text 1')
-    })
+          expect(secondInfo).to.have.descendants(LockIcon)
+          expect(secondInfo).to.contain.text('Custom text 2')
+        })
+      })
 
-    it('has second block with locked value', () => {
-      const secondInfo = infos.at(1)
-
-      expect(secondInfo).to.have.descendants(LockIcon)
-      expect(secondInfo).to.contain.text('Custom text 2')
     })
   })
 
@@ -111,9 +138,7 @@ describe('<ProjectSimilarCard />', () => {
     const projectSimilarCard = mount(
       <ProjectSimilarCard
         coloredInfosValues
-        infos={ [ { key: 'info-1',
-                    text: 'Custom text 1',
-                    value: 'Custom value 1' } ] }
+        projects={ [project3] }
       />
     )
     const info = projectSimilarCard.find('.k-ProjectSimilarCard__info').first()
@@ -121,6 +146,33 @@ describe('<ProjectSimilarCard />', () => {
 
     it('renders an info value with color modifier', () => {
       expect(value).to.have.className('k-u-color-primary1')
+    })
+  })
+
+  describe('with refresh props', () => {
+    const projectSimilarCard = mount(
+      <ProjectSimilarCard refresh="Custom refresh" />
+    )
+    const refreshLink = projectSimilarCard.find(
+      '.k-ProjectSimilarCard__refresh__link'
+    )
+
+    it('renders a refresh link with the right text', () => {
+      expect(refreshLink).to.contain.text('Custom refresh')
+    })
+  })
+
+  describe('with onRefreshClick prop', () => {
+    const handleRefreshClick = () => {}
+    const projectSimilarCard = mount(
+      <ProjectSimilarCard onRefreshClick={ handleRefreshClick } />
+    )
+    const refreshLink = projectSimilarCard.find(
+      '.k-ProjectSimilarCard__refresh__link'
+    )
+
+    it('attaches the right handler to the onClick prop', () => {
+      expect(refreshLink.props().onClick).to.equal(handleRefreshClick)
     })
   })
 
