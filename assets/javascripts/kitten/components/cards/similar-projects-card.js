42871f237874256f2cd0f1dac2fdcc63c1ea90b1
Fanny Cheung <fanny@ynote.hk>
2017-07-31 13:06:25 +0200

    Add SimilarProjectsCard

---
+++ assets/javascripts/kitten/components/cards/similar-projects-card.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import { ProjectSimilarCard } from 'kitten/components/cards/project-similar-card'
+
+export class SimilarProjectsCard extends Component {
+  constructor() {
+    super()
+
+    this.handleLeftArrowClick = this.handleLeftArrowClick.bind(this)
+    this.handleRightArrowClick = this.handleRightArrowClick.bind(this)
+
+    this.state = {
+      currentIndex: 0,
+    }
+  }
+
+  handleLeftArrowClick() {
+    const newCurrentIndex = this.state.currentIndex - 1
+
+    this.goTo(newCurrentIndex)
+    this.props.onLeftArrowClick()
+  }
+
+  handleRightArrowClick() {
+    const newCurrentIndex = this.state.currentIndex + 1
+
+    this.goTo(newCurrentIndex)
+    this.props.onRightArrowClick()
+  }
+
+  goTo(index) {
+    this.setState({ currentIndex: index })
+  }
+
+  isLastProjectCurrent() {
+    return this.state.currentIndex == this.props.projects.length - 1
+  }
+
+  isFirstProjectCurrent() {
+    return this.state.currentIndex == 0
+  }
+
+  currentProject() {
+    const projects = this.props.projects
+
+    if (projects.length == 0) return
+
+    return projects[this.state.currentIndex]
+  }
+
+  currentProjectProps() {
+    if (!this.currentProject()) return
+
+    return {
+      imageSrc: this.currentProject().imageSrc,
+      title: this.currentProject().title,
+      paragraph: this.currentProject().description,
+      tags: this.currentProject().tags,
+      infos: this.currentProject().infos,
+    }
+  }
+
+  currentStep() {
+    if (!this.currentProject()) return ''
+
+    return `${this.state.currentIndex + 1 }/${ this.props.projects.length }`
+  }
+
+  render() {
+    const {
+      projects,
+      onRefreshClick,
+      onLeftArrowClick,
+      onRightArrowClick,
+      ...others } = this.props
+
+
+    return (
+      <ProjectSimilarCard
+        { ...this.currentProjectProps() }
+        step={ this.currentStep() }
+        coloredInfosValues={ this.props.coloredInfosValues }
+        refresh={ this.props.refresh }
+        onRefreshClick={ this.props.onRefreshClick }
+        onLeftArrowClick={ this.handleLeftArrowClick }
+        onRightArrowClick={ this.handleRightArrowClick }
+        leftArrowDisabled={ this.isFirstProjectCurrent() }
+        rightArrowDisabled={ this.isLastProjectCurrent() }
+      />
+    )
+  }
+}
+
+SimilarProjectsCard.defaultProps = {
+  projects: [],
+  coloredInfosValues: false,
+  refresh: 'Refresh',
+  onRefreshClick: () => {},
+  onLeftArrowClick: () => {},
+  onRightArrowClick: () => {},
+}
---
+++ assets/javascripts/kitten/components/cards/similar-projects-card.test.js
@@ -0,0 +1,397 @@
+import React, { Component } from 'react'
+import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+import { SimilarProjectsCard } from
+  'kitten/components/cards/similar-projects-card'
+import { ProjectSimilarCard } from
+  'kitten/components/cards/project-similar-card'
+
+describe('<SimilarProjectsCard />', () => {
+  const emptyArray = []
+  const tags = [
+    {
+      key: 'tag-1',
+      item: 'Tag 1'
+    },
+    {
+      key: 'tag-2',
+      item: 'Tag 2'
+    }
+  ]
+  const infos = [
+    {
+      key: 'info-1',
+      text: 'Custom text 1',
+      value: 'Custom value 1'
+    },
+    {
+      key: 'info-2',
+      text: 'Custom text 2',
+      value: 'Custom value 2',
+      locked: true
+    },
+    {
+      key: 'info-3',
+      text: 'Custom text 3',
+      value: 'Custom value 3'
+    }
+  ]
+  const project1 = {
+    imageSrc: 'image-1',
+    title: 'Title 1',
+    description: 'This is the description of project 1.',
+    tags: emptyArray,
+    infos: emptyArray,
+  }
+
+  const project2 = {
+    imageSrc: 'image-2',
+    title: 'Title 2',
+    description: 'This is the description of project 2.',
+    tags: tags,
+    infos: emptyArray,
+  }
+
+  const project3 = {
+    imageSrc: 'image-3',
+    title: 'Title 3',
+    description: 'This is the description of project 3.',
+    tags: emptyArray,
+    infos: infos,
+  }
+
+  describe('by default', () => {
+    const similarProjectsCard = shallow(<SimilarProjectsCard />)
+    const staticSimilarProjectCard = similarProjectsCard.find(ProjectSimilarCard)
+
+    it('is renders <ProjectSimilarCard />', () => {
+      expect(staticSimilarProjectCard).to.have.length(1)
+    })
+
+    it('passes the right props', () => {
+      const expectedProps = {
+        step: '',
+        coloredInfosValues: false,
+        refresh: 'Refresh',
+        leftArrowDisabled: true,
+        rightArrowDisabled: true,
+      }
+
+      const projectProps = {
+        imageSrc: null,
+        title: '',
+        paragraph: '',
+        tags: null,
+        infos: false,
+      }
+
+      expect(staticSimilarProjectCard.props()).to.contains(expectedProps)
+      expect(staticSimilarProjectCard.props()).not.to.contains(projectProps)
+      expect(staticSimilarProjectCard.props().onRefreshClick).to.be.function
+      expect(staticSimilarProjectCard.props().onLeftArrowClick).to.be.function
+      expect(staticSimilarProjectCard.props().onRightArrowClick).to.be.function
+    })
+  })
+
+  describe('with projects props', () => {
+    describe('on ComponentDidMount', () => {
+      describe('with one basic project', () => {
+        const similarProjectsCard = shallow(
+          <SimilarProjectsCard projects={ [project1] } />
+        )
+        const staticSimilarProjectCard = similarProjectsCard
+          .find(ProjectSimilarCard)
+
+        it('passes the right props for the project', () => {
+          const projectProps = {
+            imageSrc: 'image-1',
+            title: 'Title 1',
+            paragraph: 'This is the description of project 1.',
+            tags: emptyArray,
+            infos: emptyArray,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+      })
+
+      describe('with one project with tags', () => {
+        const similarProjectsCard = shallow(
+          <SimilarProjectsCard projects={ [project2] } />
+        )
+        const staticSimilarProjectCard = similarProjectsCard
+          .find(ProjectSimilarCard)
+
+
+        it('passes the right props for the project', () => {
+          const projectProps = {
+            imageSrc: 'image-2',
+            title: 'Title 2',
+            paragraph: 'This is the description of project 2.',
+            tags: tags,
+            infos: emptyArray,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+      })
+
+      describe('with one project with infos', () => {
+        const similarProjectsCard = shallow(
+          <SimilarProjectsCard projects={ [project3] } />
+        )
+        const staticSimilarProjectCard = similarProjectsCard
+          .find(ProjectSimilarCard)
+
+
+        it('passes the right props for the project', () => {
+          const projectProps = {
+            imageSrc: 'image-3',
+            title: 'Title 3',
+            paragraph: 'This is the description of project 3.',
+            tags: emptyArray,
+            infos: infos,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+      })
+    })
+
+    describe('on left arrow clicked', () => {
+      describe('by default', () => {
+        let similarProjectsCard
+        let staticSimilarProjectCard
+        let leftArrowButton
+
+        before(() => {
+          similarProjectsCard = mount(
+            <SimilarProjectsCard
+              projects={ [project1, project2, project3] } />
+          )
+          staticSimilarProjectCard = similarProjectsCard
+            .find(ProjectSimilarCard)
+          leftArrowButton = similarProjectsCard.find('.k-ButtonIcon').first()
+
+          similarProjectsCard.setState({ currentIndex: 2 })
+          leftArrowButton.simulate('click')
+        })
+
+        it('passes the previous project props to <ProjectSimilarCard>', () => {
+          const projectProps = {
+            imageSrc: 'image-2',
+            title: 'Title 2',
+            paragraph: 'This is the description of project 2.',
+            tags: tags,
+            infos: emptyArray,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+
+        it('updates correctly the step information', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ step: '2/3' })
+        })
+      })
+
+      describe('when new current project is the first of the list', () => {
+        let similarProjectsCard
+        let staticSimilarProjectCard
+        let leftArrowButton
+
+        before(() => {
+          similarProjectsCard = mount(
+            <SimilarProjectsCard
+              projects={ [project1, project2, project3] } />
+          )
+          staticSimilarProjectCard = similarProjectsCard
+            .find(ProjectSimilarCard)
+          leftArrowButton = similarProjectsCard.find('.k-ButtonIcon').first()
+
+          similarProjectsCard.setState({ currentIndex: 1 })
+          leftArrowButton.simulate('click')
+        })
+
+        it('passes the first project props to <ProjectSimilarCard>', () => {
+          const projectProps = {
+            imageSrc: 'image-1',
+            title: 'Title 1',
+            paragraph: 'This is the description of project 1.',
+            tags: emptyArray,
+            infos: emptyArray,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+
+        it('updates correctly the step information', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ step: '1/3' })
+        })
+
+        it('disables the left arrow button', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ leftArrowDisabled: true })
+        })
+      })
+    })
+
+    describe('on left arrow clicked', () => {
+      describe('by default', () => {
+        let similarProjectsCard
+        let staticSimilarProjectCard
+        let leftArrowButton
+
+        before(() => {
+          similarProjectsCard = mount(
+            <SimilarProjectsCard
+              projects={ [project1, project2, project3] } />
+          )
+          staticSimilarProjectCard = similarProjectsCard
+            .find(ProjectSimilarCard)
+          leftArrowButton = similarProjectsCard.find('.k-ButtonIcon').first()
+
+          similarProjectsCard.setState({ currentIndex: 2 })
+          leftArrowButton.simulate('click')
+        })
+
+        it('passes the previous project props to <ProjectSimilarCard>', () => {
+          const projectProps = {
+            imageSrc: 'image-2',
+            title: 'Title 2',
+            paragraph: 'This is the description of project 2.',
+            tags: tags,
+            infos: emptyArray,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+
+        it('updates correctly the step information', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ step: '2/3' })
+        })
+      })
+
+      describe('when new current project is the first of the list', () => {
+        let similarProjectsCard
+        let staticSimilarProjectCard
+        let leftArrowButton
+
+        before(() => {
+          similarProjectsCard = mount(
+            <SimilarProjectsCard
+              projects={ [project1, project2, project3] } />
+          )
+          staticSimilarProjectCard = similarProjectsCard
+            .find(ProjectSimilarCard)
+          leftArrowButton = similarProjectsCard.find('.k-ButtonIcon').first()
+
+          similarProjectsCard.setState({ currentIndex: 1 })
+          leftArrowButton.simulate('click')
+        })
+
+        it('passes the first project props to <ProjectSimilarCard>', () => {
+          const projectProps = {
+            imageSrc: 'image-1',
+            title: 'Title 1',
+            paragraph: 'This is the description of project 1.',
+            tags: emptyArray,
+            infos: emptyArray,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+
+        it('updates correctly the step information', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ step: '1/3' })
+        })
+
+        it('disables the left arrow button', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ leftArrowDisabled: true })
+        })
+      })
+    })
+
+    describe('on right arrow clicked', () => {
+      describe('by default', () => {
+        let similarProjectsCard
+        let staticSimilarProjectCard
+        let rightArrowButton
+
+        before(() => {
+          similarProjectsCard = mount(
+            <SimilarProjectsCard
+              projects={ [project1, project2, project3] } />
+          )
+          staticSimilarProjectCard = similarProjectsCard
+            .find(ProjectSimilarCard)
+          rightArrowButton = similarProjectsCard.find('.k-ButtonIcon').last()
+
+          rightArrowButton.simulate('click')
+        })
+
+        it('passes the next project props to <ProjectSimilarCard>', () => {
+          const projectProps = {
+            imageSrc: 'image-2',
+            title: 'Title 2',
+            paragraph: 'This is the description of project 2.',
+            tags: tags,
+            infos: emptyArray,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+
+        it('updates correctly the step information', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ step: '2/3' })
+        })
+      })
+
+      describe('when new current project is the last of the list', () => {
+        let similarProjectsCard
+        let staticSimilarProjectCard
+        let rightArrowButton
+
+        before(() => {
+          similarProjectsCard = mount(
+            <SimilarProjectsCard
+              projects={ [project1, project2, project3] } />
+          )
+          staticSimilarProjectCard = similarProjectsCard
+            .find(ProjectSimilarCard)
+          rightArrowButton = similarProjectsCard.find('.k-ButtonIcon').last()
+
+          similarProjectsCard.setState({ currentIndex: 1 })
+          rightArrowButton.simulate('click')
+        })
+
+        it('passes the last project props to <ProjectSimilarCard>', () => {
+          const projectProps = {
+            imageSrc: 'image-3',
+            title: 'Title 3',
+            paragraph: 'This is the description of project 3.',
+            tags: emptyArray,
+            infos: infos,
+          }
+
+          expect(staticSimilarProjectCard.props()).to.contains(projectProps)
+        })
+
+        it('updates correctly the step information', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ step: '3/3' })
+        })
+
+        it('disables the right arrow button', () => {
+          expect(staticSimilarProjectCard.props())
+            .to.contains({ rightArrowDisabled: true })
+        })
+      })
+    })
+  })
+})
