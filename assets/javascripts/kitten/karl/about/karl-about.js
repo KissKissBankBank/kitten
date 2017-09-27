972aa9fb3dc90dc5f9ea642b03b0c19a860cbb03
Camille Delbove <camille.delbove@gmail.com>
2017-09-27 17:46:33 +0200

    indexation

--- assets/javascripts/kitten/karl/about/karl-about.js
+++ assets/javascripts/kitten/karl/about/karl-about.js
@@ -14,21 +14,28 @@ import { NetworkAboutIllustration } from 'kitten/components/illustrations/networ
 export class KarlAbout extends Component {
   renderIntro() {
     return (
-      <Paragraph
-        className="k-u-align-center@s-down"
-        modifier="tertiary"
-        margin={ false }
-      >
-        Chez KissKissBankBank, nous opérons trois services de financement participatif (KissKissBankBank, LENDOPOLIS, hellomerci) avec une seule équipe technique et produit.
-        Aujourd’hui, notre volonté d’unifier les expériences et la charte graphique sur les trois services nous a naturellement poussés vers la démarche d’une bibliothèque de composants réutilisables. Cette bibliothèque s'accompagne d'un styleguide vivant qui liste les composants existants et définit un langage commun pour les développeurs et les designers.
-          Nous avons donc imaginé Kitten, le premier Kit UI du crowdfunding, ie.
-      </Paragraph>
+      <div>
+        <Paragraph
+          className="k-u-align-center@s-down"
+          modifier="tertiary"
+          margin={ false }
+        >
+          Chez KissKissBankBank, nous opérons trois services de financement participatif (KissKissBankBank, LENDOPOLIS, hellomerci) avec une seule équipe technique et produit.
+        </Paragraph>
+        <Paragraph
+          className="k-u-align-center@s-down"
+          modifier="tertiary"
+        >
+          Aujourd‘hui, notre volonté d’unifier les expériences et la charte graphique sur les trois services nous a naturellement poussés vers la démarche d’une bibliothèque de composants réutilisables. Cette bibliothèque s‘accompagne d'un styleguide vivant qui liste les composants existants et définit un langage commun pour les développeurs et les designers.
+            Nous avons donc imaginé Kitten, le premier Kit UI du crowdfunding, ie.
+        </Paragraph>
+      </div>
     )
   }
 
   renderBloc1() {
     return (
-      <div className="karl-About__margerNone">
+      <div className="karl-About__bloc">
         <Grid className="karl-About__items">
           <GridCol col-s="12" col-m="5">
             <Grid>
@@ -97,7 +104,7 @@ export class KarlAbout extends Component {
                       modifier="quaternary"
                     >
                       Kitten nous permet de garder une cohérence d’unification lors de la création et de la modification de composants.
-                      L’unité globale de kitten nous apporte une certaine clarté, une utilité et un gain de temps.
+                      L‘unité globale de kitten nous apporte une certaine clarté, une utilité et un gain de temps.
                     </Paragraph>
                   </Marger>
                 </div>
@@ -111,7 +118,7 @@ export class KarlAbout extends Component {
 
   renderBloc2() {
     return (
-      <div className="karl-About__margerNone">
+      <div className="karl-About__bloc">
         <Grid className="karl-About__items">
           <GridCol col-s="12" col-m="5">
             <Grid>
@@ -129,7 +136,7 @@ export class KarlAbout extends Component {
                       weight="bold"
                       size="default"
                     >
-                      Pourquoi nous avons décidé d’OpenSourcer ?
+                      Pourquoi nous avons décidé de le rendre open-source ?
                     </Text>
                   </Marger>
                   <Marger
@@ -140,7 +147,7 @@ export class KarlAbout extends Component {
                       margin={ false }
                       modifier="quaternary"
                     >
-                      Il est important pour nous de contribuer à la communauté Tech en partageant notre librairie et en vous apportant la possibilité de collaborer à l\'évolution de Kitten.
+                      Il est important pour nous de contribuer à la communauté Tech en partageant notre librairie et en vous apportant la possibilité de collaborer à l‘évolution de Kitten.
                     </Paragraph>
                   </Marger>
                 </div>
@@ -175,7 +182,7 @@ export class KarlAbout extends Component {
                       margin={ false }
                       modifier="quaternary"
                     >
-                      Tous nos partenaires peuvent utiliser l\'ensemble du kit UI.
+                      Tous nos partenaires peuvent utiliser l‘ensemble du kit UI.
                     </Paragraph>
                   </Marger>
                 </div>
