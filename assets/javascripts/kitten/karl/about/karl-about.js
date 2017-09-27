6857f16593d4f18154ca1645641abdae45d6c903
Camille Delbove <camille.delbove@gmail.com>
2017-09-27 16:34:58 +0200

    Created utility `k-u`color-primary3

--- assets/javascripts/kitten/karl/about/karl-about.js
+++ assets/javascripts/kitten/karl/about/karl-about.js
@@ -44,6 +44,7 @@ export class KarlAbout extends Component {
                     className="karl-About__title"
                   >
                     <Text
+                      className="k-u-color-primary3"
                       weight="bold"
                       size="default"
                     >
@@ -80,6 +81,7 @@ export class KarlAbout extends Component {
                     className="karl-About__title"
                   >
                     <Text
+                      className="k-u-color-primary3"
                       weight="bold"
                       size="default"
                     >
@@ -123,6 +125,7 @@ export class KarlAbout extends Component {
                     className="karl-About__title"
                   >
                     <Text
+                      className="k-u-color-primary3"
                       weight="bold"
                       size="default"
                     >
@@ -137,7 +140,7 @@ export class KarlAbout extends Component {
                       margin={ false }
                       modifier="quaternary"
                     >
-                      Il est important pour nous de contribuer à la communauté Tech en partageant notre librairie et en vous apportant la possibilité de collaborer à l'évolution de Kitten.
+                      Il est important pour nous de contribuer à la communauté Tech en partageant notre librairie et en vous apportant la possibilité de collaborer à l\'évolution de Kitten.
                     </Paragraph>
                   </Marger>
                 </div>
@@ -157,6 +160,7 @@ export class KarlAbout extends Component {
                     className="karl-About__title"
                   >
                     <Text
+                      className="k-u-color-primary3"
                       weight="bold"
                       size="default"
                     >
@@ -171,7 +175,7 @@ export class KarlAbout extends Component {
                       margin={ false }
                       modifier="quaternary"
                     >
-                      Tous nos partenaires peuvent utiliser l’ensemble du kit UI.
+                      Tous nos partenaires peuvent utiliser l\'ensemble du kit UI.
                     </Paragraph>
                   </Marger>
                 </div>
