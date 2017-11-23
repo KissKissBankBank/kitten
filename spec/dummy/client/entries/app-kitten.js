355ba11666c3c50c43a51bceb2da92112709170d
Camille Delbove <camille.delbove@gmail.com>
2017-11-23 19:36:23 +0100

    Add instagramIcon component

--- spec/dummy/client/entries/app-kitten.js
+++ spec/dummy/client/entries/app-kitten.js
@@ -138,6 +138,7 @@ import { CrossIcon } from 'kitten/components/icons/cross-icon'
 import { FacebookIcon } from 'kitten/components/icons/facebook-icon'
 import { GrabberIcon } from 'kitten/components/icons/grabber-icon'
 import { HeartIcon } from 'kitten/components/icons/heart-icon'
+import { InstagramIcon } from 'kitten/components/icons/instagram-icon'
 import { InstrumentTagIcon } from 'kitten/components/icons/instrument-tag-icon'
 import { LinkedinIcon } from 'kitten/components/icons/linkedin-icon'
 import { LocationIcon } from 'kitten/components/icons/location-icon'
@@ -388,6 +389,7 @@ ReactOnRails.register({
   FacebookIcon,
   GrabberIcon,
   HeartIcon,
+  InstagramIcon,
   InstrumentTagIcon,
   LinkedinIcon,
   LocationIcon,
