fb4b8772f225f36d7f234a1d27904f6a61dad507
Camille Delbove <camille.delbove@gmail.com>
2017-11-27 16:23:52 +0100

    Add `HorizontalNav`  component.

---
+++ assets/javascripts/kitten/components/navigation/horizontal-nav.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+
+export class HorizontalNav extends Component {
+  renderItems() {
+    const { items } = this.props
+
+    return items.map((item, key) => this.renderItems(item, key))
+  }
+
+  renderItem(item, key) {
+    const { className ...linkProps } = item
+    const itemClassName = classNames(
+      "k-HorizontalNav__item",
+      className,
+      { "is-selected": selected },
+    )
+
+    return (
+      <li key={ key }>
+        <a className={ itemClassName } { }
+      </li>
+    )
+  }
+}
+
+
+
+<ul class="k-HorizontalNav" role="menubar">
+  <li class="k-HorizontalNav__element" role="menuitem">
+    <a class="k-HorizontalNav__item karl-Height" href="#">
+      Nav link
+    </a>
+  </li>
+  <li class="k-HorizontalNav__element" role="menuitem">
+    <a class="k-HorizontalNav__item karl-Height" href="#">
+      Nav link 2
+    </a>
+  </li>
+  <li class="k-HorizontalNav__element" role="menuitem">
+    <a class="k-HorizontalNav__item karl-Height" href="#">
+      Nav link 3
+    </a>
+  </li>
+  <li class="k-HorizontalNav__element" role="menuitem">
+    <a class="k-HorizontalNav__item karl-Height is-selected" href="#">
+      Nav link 4 (selected)
+    </a>
+  </li>
+</ul>
