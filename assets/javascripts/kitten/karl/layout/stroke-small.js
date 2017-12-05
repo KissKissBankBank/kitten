b98f7ee90b83df5fcce1186d53b8589bbcc24917
Camille Delbove <camille.delbove@gmail.com>
2017-12-05 19:18:34 +0100

    Update karl_changelog

--- assets/javascripts/kitten/karl/layout/stroke-small.js
+++ assets/javascripts/kitten/karl/layout/stroke-small.js
@@ -3,7 +3,5 @@ import { Marger } from 'kitten/components/layout/marger'
 import { StrokeSmall } from 'kitten/components/layout/stroke-small'
 
 export const KarlStrokeSmall = () => (
-  <Marger top="5">
-    <StrokeSmall />
-  </Marger>
+  <StrokeSmall />
 )
