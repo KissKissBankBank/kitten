20e4f57407a51e55e2383ec9d33ddfa366b24606
Camille Delbove <camille.delbove@gmail.com>
2017-12-06 10:58:29 +0100

    Create new hero

---
+++ assets/javascripts/kitten/components/heroes/hero.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+
+export class Hero extends Component {
+  render() {
+
+  }
+}
---
+++ assets/javascripts/kitten/karl/heroes/hero.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Hero } from 'kitten/components/heroes/hero'
+
+export const KarlHero = () => (
+  <Hero />
+)
