b54cde845f78b423e042573dc1010a4434ac9761
Camille Delbove <camille.delbove@gmail.com>
2017-12-06 11:02:41 +0100

    Update example

--- assets/javascripts/kitten/karl/layout/stroke-small.js
+++ assets/javascripts/kitten/karl/layout/stroke-small.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Marger } from 'kitten/components/layout/marger'
 import { StrokeSmall } from 'kitten/components/layout/stroke-small'
 
 export const KarlStrokeSmall = () => (
