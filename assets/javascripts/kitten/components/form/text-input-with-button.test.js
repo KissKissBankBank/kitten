dedc65af71fefa0aea267cdea31a8e8052f437aa
Camille Delbove <camille.delbove@gmail.com>
2017-12-08 10:55:43 +0100

    Remove `border-radius: 0px` and update changelog

--- assets/javascripts/kitten/components/form/text-input-with-button.test.js
+++ assets/javascripts/kitten/components/form/text-input-with-button.test.js
@@ -52,7 +52,12 @@ describe('<TextInputWithButton />', () => {
 
   describe('valid prop', () => {
     const component = mount(<TextInputWithButton valid />)
+    const textInput = component.find('input')
     const button = component.find('button')
+
+    it('has an <input /> with "is-valid" class', () => {
+      expect(textInput).to.have.className('is-valid')
+    })
 
     it('has an <button /> with "is-valid" class', () => {
       expect(button).to.have.className('is-valid')
@@ -61,7 +66,12 @@ describe('<TextInputWithButton />', () => {
 
   describe('error prop', () => {
     const component = mount(<TextInputWithButton error />)
+    const textInput = component.find('input')
     const button = component.find('button')
+
+    it('has an <input /> with "is-error" class', () => {
+      expect(textInput).to.have.className('is-error')
+    })
 
     it('has an <button /> with "is-error" class', () => {
       expect(button).to.have.className('is-error')
@@ -70,7 +80,12 @@ describe('<TextInputWithButton />', () => {
 
   describe('disabled prop', () => {
     const component = mount(<TextInputWithButton disabled />)
+    const textInput = component.find('input')
     const button = component.find('button')
+
+    it('has an <input /> with "is-disabled" class', () => {
+      expect(textInput).to.have.className('is-disabled')
+    })
 
     it('has an <button /> with "disabled" class', () => {
       expect(button).to.have.className('is-disabled')
