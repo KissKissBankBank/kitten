7fa6d708993318614365854c071f86bf080517ff
Camille Delbove <camille.delbove@gmail.com>
2017-12-12 15:11:03 +0100

    Update css

--- assets/javascripts/kitten/components/images/image-with-legend.js
+++ assets/javascripts/kitten/components/images/image-with-legend.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import { Text } from 'kitten/components/typography/text'
 
-export class ImagewithLegend extends Component {
-  renderLegend() {
+export class ImageWithLegend extends Component {
+  render() {
     const {
     children,
+    text,
     className,
     ...others } = this.props
 
     return (
-      <div className>
-
-      </div>
+      <figure className="k-ImageWithLegend">
+        <img src="https://placehold.it/300X400/4a84ff/4a84ff" />
+        <figcaption className="k-ImageWithLegend__legend">
+          <Text
+            size="default"
+            color="font-1"
+          >
+            { text }
+          </Text>
+        </figcaption>
+      </figure>
     )
   }
+}
 
-  render()
+ImageWithLegend.defaultProps = {
+  text: "Hello word",
 }
