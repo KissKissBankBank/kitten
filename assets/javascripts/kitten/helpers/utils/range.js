85b0fc50ec232fe989da0ef7eeed5a74cfc51f1b
freddy <freddy03h@gmail.com>
2017-12-20 13:00:52 +0100

    test for range utils

--- assets/javascripts/kitten/helpers/utils/range.js
+++ assets/javascripts/kitten/helpers/utils/range.js
@@ -1,4 +1,5 @@
 import arrayKeys from 'core-js/library/fn/array/keys'
 import arrayFrom from 'core-js/library/fn/array/from'
+import numberIsInteger from 'core-js/library/fn/number/is-integer'
 
-export const createRangeFromZeroTo = (num) => arrayFrom(arrayKeys(Array(num)))
+export const createRangeFromZeroTo = (num) => numberIsInteger(num) ? arrayFrom(arrayKeys(Array(num))) : []
---
+++ assets/javascripts/kitten/helpers/utils/range.test.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { expect } from 'chai'
+import { createRangeFromZeroTo } from 'kitten/helpers/utils/range'
+
+describe('utils range', () => {
+
+  describe('createRangeFromZeroTo range 2', () => {
+    const range = createRangeFromZeroTo(2)
+
+    it('return the array 0 to 1', () => {
+      expect(range).to.eql([0, 1])
+    })
+  })
+
+  describe('createRangeFromZeroTo range 7', () => {
+    const range = createRangeFromZeroTo(7)
+
+    it('return the array 0 to 6', () => {
+      expect(range).to.eql([0, 1, 2, 3, 4, 5, 6])
+    })
+  })
+
+  describe('createRangeFromZeroTo range 1', () => {
+    const range = createRangeFromZeroTo(1)
+
+    it('return the array 0', () => {
+      expect(range).to.eql([0])
+    })
+  })
+
+  describe('createRangeFromZeroTo range 0', () => {
+    const range = createRangeFromZeroTo(0)
+
+    it('return the array empty', () => {
+      expect(range).to.eql([])
+    })
+  })
+
+  describe('createRangeFromZeroTo range undefined', () => {
+    const range = createRangeFromZeroTo()
+
+    it('return the array empty', () => {
+      expect(range).to.eql([])
+    })
+  })
+
+  describe('createRangeFromZeroTo range string', () => {
+    const range = createRangeFromZeroTo('toto')
+
+    it('return the array empty', () => {
+      expect(range).to.eql([])
+    })
+  })
+
+})
