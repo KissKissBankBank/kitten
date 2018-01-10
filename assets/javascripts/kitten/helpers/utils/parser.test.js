1f0c1574c84af04e9b8601ed338ed2b33f4fb8a8
Morgan JARRY <morgan.jarry@gmail.com>
2018-01-10 13:20:54 +0100

    Fix parse html

--- assets/javascripts/kitten/helpers/utils/parser.test.js
+++ assets/javascripts/kitten/helpers/utils/parser.test.js
@@ -4,16 +4,16 @@ import ReactDOMServer from 'react-dom/server'
 describe('parseHtml()', () => {
   it('converts HTML to React with HTML tags', () => {
     const html = '<strong>Foo <br/>Bar</strong>'
-    const html_parsed = parseHtml(html)
+    const parsedHtml = parseHtml(html)
 
-    expect(ReactDOMServer.renderToStaticMarkup(html_parsed)).toBe(html)
+    expect(ReactDOMServer.renderToStaticMarkup(parsedHtml)).toBe(html)
   })
 
   it('converts HTML to React with &nbsp;', () => {
     const html = 'FooBar&nbsp;!'
-    const html_parsed = parseHtml(html)
+    const parsedHtml = parseHtml(html)
     const nbsp = '\xa0'
 
-    expect(html_parsed).toBe(`FooBar${nbsp}!`)
+    expect(parsedHtml).toBe(`FooBar${nbsp}!`)
   })
 })
