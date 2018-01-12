ec4f2882325060f10a000f4ce43677bf6271aee9
Morgan JARRY <morgan.jarry@gmail.com>
2018-01-12 16:15:28 +0100

    Remove comments in jest-setup

--- assets/javascripts/kitten/config/jest-setup.js
+++ assets/javascripts/kitten/config/jest-setup.js
@@ -2,12 +2,7 @@ import Enzyme, { shallow, render, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() })
-// const jsdom = require('jsdom')
-// const { JSDOM } = jsdom
-//
-// const { window } = new JSDOM('')
-// global.window = window
-// global.document = window.document
+
 // Make Enzyme functions available in all test files without importing
 global.shallow = shallow
 global.render = render
