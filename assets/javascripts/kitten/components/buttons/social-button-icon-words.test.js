333ca2d74c66589f2db33ac51875782ec24299c7
Camille Delbove <camille.delbove@gmail.com>
2018-01-25 11:30:43 +0100

    Test

---
+++ assets/javascripts/kitten/components/buttons/social-button-icon-words.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classNames from 'classnames'
+import {
+  FacebookButtonIconWords,
+  TwitterButtonIconWords,
+  LinkedinButtonIconWords,
+  InstagramButtonIconWords,
+} from 'kitten/components/buttons/social-button-icon-words'
+import { Button } from 'kitten/components/buttons/button'
+import { FacebookIcon } from 'kitten/components/icons/facebook-icon'
+import { TwitterIcon } from 'kitten/components/icons/twitter-icon'
+import { LinkedinIcon } from 'kitten/components/icons/linkedin-icon'
+import { InstagramIcon } from 'kitten/components/icons/instagram-icon'
