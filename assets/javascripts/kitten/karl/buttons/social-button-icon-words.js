4e406f602c00268a8d2774b037ca823b63684731
Camille Delbove <camille.delbove@gmail.com>
2018-01-30 15:10:41 +0100

    Add A11Y

--- assets/javascripts/kitten/karl/buttons/social-button-icon-words.js
+++ assets/javascripts/kitten/karl/buttons/social-button-icon-words.js
@@ -7,19 +7,25 @@ import {
 } from 'kitten/components/buttons/social-button-icon-words'
 
 export const KarlFacebookButtonIconWords = props => (
-  <FacebookButtonIconWords className="k-u-margin-bottom-single">
+  <FacebookButtonIconWords
+    className="k-u-margin-bottom-single"
+    onClick="Continuer avec Facebook"
+  >
     Continuer avec Facebook
   </FacebookButtonIconWords>
 )
 
 export const KarlTwitterButtonIconWords = props => (
-  <TwitterButtonIconWords className="k-u-margin-bottom-single">
+  <TwitterButtonIconWords
+    className="k-u-margin-bottom-single"
+    onClick="Continuer avec Twitter"
+  >
     Continuer avec Twitter
   </TwitterButtonIconWords>
 )
 
 export const KarlLinkedinButtonIconWords = props => (
-  <LinkedinButtonIconWords>
+  <LinkedinButtonIconWords onClick="Continuer avec LinkedIn">
     Continuer avec Linkedin
   </LinkedinButtonIconWords>
 )
