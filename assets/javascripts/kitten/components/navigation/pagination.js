37658d6b3b374ce88ba9a2abedcb819f573b440c
Camille Delbove <camille.delbove@gmail.com>
2018-02-02 15:14:05 +0100

    Add list

--- assets/javascripts/kitten/components/navigation/pagination.js
+++ assets/javascripts/kitten/components/navigation/pagination.js
@@ -3,18 +3,31 @@ import Radium, { StyleRoot } from 'radium'
 import classNames from 'classnames'
 import { Text } from 'kitten/components/typography/text'
 import { ArrowIcon as ArrowIconBase } from 'kitten/components/icons/arrow-icon'
-import { ScreenConfig,
-  SCREEN_SIZE_M,
-} from 'kitten/constants/screen-config'
+// import { createMatchMediaMax } from 'kitten/helpers/utils/media-queries'
+// import { ScreenConfig,
+//   SCREEN_SIZE_M,
+// } from 'kitten/constants/screen-config'
 import COLORS from 'kitten/constants/colors-config'
 import { parseHtml } from 'kitten/helpers/utils/parser'
 
 const ArrowIcon = Radium(ArrowIconBase)
 
-export class Pagination extends Component {
+class PaginationBase extends Component {
   render() {
+    return (
+      <StyleRoot>
+        { this.renderList() }
+      </StyleRoot>
+    )
+  }
+
+  renderList() {
     const {
       buttonIconPager,
+      buttonIconPager2,
+      buttonIconPager3,
+      buttonIconPagerPoints,
+      buttonIconPager6,
     } = this.props
 
     const styleButtonIcon = [
@@ -23,33 +36,76 @@ export class Pagination extends Component {
     ]
 
     return (
-      <StyleRoot>
-        <ul style={ styles.group }>
-          <li style={ styles.group.list } >
-            <a style={ styleButtonIcon }>
-              <ArrowIcon
-                style={ styles.group.list.buttonIcon.svg }
-                direction="left"
-              />
-            </a>
-          </li>
-          <li style={ styles.group.list } >
-            <a style={ styleButtonIcon }>
-              <div
-                style={ styles.group.list.buttonIcon.number }
-              >
-                <Text
-                  weight="regular"
-                  size="tiny"
-                  tag="p"
-                >
-                  { parseHtml(buttonIconPager) }
-                </Text>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </StyleRoot>
+      <ul style={ styles.group }>
+        <li style={ styles.group.list }>
+          <button style={ styleButtonIcon }>
+            <ArrowIcon
+              style={ styles.group.list.buttonIcon.svg }
+              direction="left"
+            />
+          </button>
+        </li>
+
+        <li style={ styles.group.list }>
+          <button style={ styleButtonIcon }>
+            <Text
+              weight="regular"
+              size="tiny"
+            >
+              { parseHtml(buttonIconPager) }
+            </Text>
+          </button>
+        </li>
+        <li style={ styles.group.list }>
+          <button style={ styleButtonIcon }>
+            <Text
+              weight="regular"
+              size="tiny"
+            >
+              { parseHtml(buttonIconPager2) }
+            </Text>
+          </button>
+        </li>
+        <li style={ styles.group.list }>
+          <button style={ styleButtonIcon }>
+            <Text
+              weight="regular"
+              size="tiny"
+            >
+              { parseHtml(buttonIconPager3) }
+            </Text>
+          </button>
+        </li>
+        <li style={ styles.group.list }>
+          <button style={ styleButtonIcon }>
+            <Text
+              weight="regular"
+              size="tiny"
+            >
+              { parseHtml(buttonIconPagerPoints) }
+            </Text>
+          </button>
+        </li>
+        <li style={ styles.group.list }>
+          <button style={ styleButtonIcon }>
+            <Text
+              weight="regular"
+              size="tiny"
+            >
+              { parseHtml(buttonIconPager6) }
+            </Text>
+          </button>
+        </li>
+
+        <li style={ styles.group.list }>
+          <button style={ styleButtonIcon }>
+            <ArrowIcon
+              style={ styles.group.list.buttonIcon.svg }
+              direction="right"
+            />
+          </button>
+        </li>
+      </ul>
     )
   }
 }
@@ -61,40 +117,32 @@ const styles = {
     list: {
       listStyleType: 'none',
       marginRight: '15px',
-      // &:lastChild: {
-      //   marginRight: 0,
-      // }
 
       buttonIcon: {
-        display: 'flex',
         boxSizing: 'border-box',
         cursor: 'pointer',
-        verticalAlign: 'middle',
         width: '40px',
         height: '40px',
         borderRadius: 0,
         borderWidth: '2px',
         borderStyle: 'solid',
+        textDecoration: 'none',
+        outline: 'none',
         backgroundColor: `${COLORS.background1}`,
         borderColor: `${COLORS.line1}`,
         color: `${COLORS.font1}`,
-        textAlign: 'center',
-
-        ':active': {
-          backgroundColor: `${COLORS.background1}`,
-          borderColor: `${COLORS.primary3}`,
-          color: `${COLORS.primary3}`,
-        },
-        ':hover': {
-          backgroundColor: `${COLORS.background1}`,
-          borderColor: `${COLORS.primary1}`,
-          color: `${COLORS.primary1}`,
-        },
-        ':focus': {
-          backgroundColor: `${COLORS.background1}`,
-          borderColor: `${COLORS.primary1}`,
-          color: `${COLORS.primary1}`,
-        },
+        // ':active': {
+        //   borderColor: `${COLORS.primary3}`,
+        //   color: `${COLORS.primary3}`,
+        // },
+        // ':hover': {
+        //   borderColor: `${COLORS.primary1}`,
+        //   color: `${COLORS.primary1}`,
+        // },
+        // ':focus': {
+        //   borderColor: `${COLORS.primary1}`,
+        //   color: `${COLORS.primary1}`,
+        // },
 
         disabled: {
           color: `${COLORS.background1}`,
@@ -109,18 +157,17 @@ const styles = {
           height: '14px',
           pointerEvents: 'none',
         },
-
-        number: {
-          margin: '0 auto',
-          alignSelf: 'center',
-        },
       },
     },
   },
 }
 
-
-Pagination.defaultProps = {
+PaginationBase.defaultProps = {
   buttonIconPager: '1',
   buttonIconPager2: '2',
+  buttonIconPager3: '3',
+  buttonIconPagerPoints: '…',
+  buttonIconPager6: '6',
 }
+
+export const Pagination = Radium(PaginationBase)
