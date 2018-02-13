44b8e97294a7eba49d05763641040339423fc528
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2018-02-13 16:09:32 +0100

    Tests listeners and props

--- assets/javascripts/kitten/hoc/media-queries.js
+++ assets/javascripts/kitten/hoc/media-queries.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {
-  createMatchMedia,
-  createMatchMediaMax,
-} from 'kitten/helpers/utils/media-queries'
+import { createMatchMediaMax } from 'kitten/helpers/utils/media-queries'
 import {
   SCREEN_SIZE_XS,
   SCREEN_SIZE_M,
--- assets/javascripts/kitten/hoc/media-queries.test.js
+++ assets/javascripts/kitten/hoc/media-queries.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import { mediaQueries } from 'kitten/hoc/media-queries'
 
 const SimpleComponent = ({
@@ -11,27 +12,43 @@ const SimpleComponent = ({
   )
 }
 
+const mockAddListener = jest.fn()
+const mockRemoveListener = jest.fn()
+
+const createMockMediaMatcher = matches => () => {
+  return {
+    matches,
+    addListener: mockAddListener,
+    removeListener: mockRemoveListener,
+  }
+}
+
 describe('mediaQueries()', () => {
-  const originalMatchMedia = window.matchMedia
+  const initialWindow = Object.assign({}, global.window)
+  const originalMatchMedia = global.window.matchMedia
   let SimpleComponentWithMediaQueries
   let component
+  let componentSnapshot
 
   beforeEach(() => {
-    window.matchMedia = jest.fn()
+    window.matchMedia = createMockMediaMatcher(false)
   })
 
   afterEach(() => {
-    window.matchMedia = originalMatchMedia
+    window.matchMedia = initialWindow.matchMedia
   })
 
   describe('by default', () => {
     beforeEach(() => {
       SimpleComponentWithMediaQueries = mediaQueries(SimpleComponent)
       component = mount(<SimpleComponentWithMediaQueries />)
+      componentSnapshot = renderer
+        .create(<SimpleComponentWithMediaQueries />)
+        .toJSON()
     })
 
     it('renders the wrapped component', () => {
-      expect(component.find(SimpleComponent)).toHaveLength(1)
+      expect(componentSnapshot).toMatchSnapshot()
     })
 
     it('push media queries props to wrapped component', () => {
@@ -39,6 +56,30 @@ describe('mediaQueries()', () => {
 
       expect(wrappedComponent.prop('viewportIsMobile')).toBeFalsy()
       expect(wrappedComponent.prop('viewportIsTabletOrLess')).toBeFalsy()
+    })
+
+    it('attaches listeners', () => {
+      expect(mockAddListener).toHaveBeenCalled()
+    })
+
+    it('removes listeners', () => {
+      component.instance().componentWillUnmount()
+      expect(mockRemoveListener).toHaveBeenCalled()
+    })
+  })
+
+  describe('with tablet or less version', () => {
+    beforeEach(() => {
+      window.matchMedia = createMockMediaMatcher(true)
+
+      component = mount(<SimpleComponentWithMediaQueries />)
+    })
+
+    it('push media queries props to wrapped component', () => {
+      const wrappedComponent = component.find(SimpleComponent).first()
+
+      expect(wrappedComponent.prop('viewportIsMobile')).toBeTruthy()
+      expect(wrappedComponent.prop('viewportIsTabletOrLess')).toBeTruthy()
     })
   })
 })
