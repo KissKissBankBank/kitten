17fb1529544483c76a3c1576170a0d04b17f2fb2
Camille Delbove <camille.delbove@gmail.com>
2018-02-21 15:14:03 +0100

    Change colors name

--- assets/javascripts/kitten/components/buttons/tag-button.js
+++ assets/javascripts/kitten/components/buttons/tag-button.js
@@ -43,7 +43,7 @@ export class TagButton extends Component {
 TagButton.defaultProps = {
   tag: 'button',
   children: 'Tag',
-  modifier: 'mercure',
+  modifier: 'helium',
   icon: false,
   tiny: false,
   big: false,
