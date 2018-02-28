68193de64bed4ad64feddcb8d834a5eefb86d92e
Sunny Ripert <sunny@sunfox.org>
2018-02-28 12:10:23 +0100

    Revert "Move StyleRoot out of Pagination"
    This reverts commit 7d680ccc605df6fecdddc22ec3be9adf976ada7b.

--- assets/javascripts/kitten/components/navigation/pagination.js
+++ assets/javascripts/kitten/components/navigation/pagination.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Radium from 'radium'
+import Radium, { StyleRoot } from 'radium'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { Text } from 'kitten/components/typography/text'
@@ -43,13 +43,15 @@ class PaginationBase extends Component {
     const pageNumbers = pages(1, totalPages, currentPage, size)
 
     return (
-      <nav role="navigation" aria-label={ this.props['aria-label'] }>
-        <ul style={ styles.group }>
-          { this.renderArrowButton('left') }
-          { pageNumbers.map(this.renderPage) }
-          { this.renderArrowButton('right') }
-        </ul>
-      </nav>
+      <StyleRoot>
+        <nav role="navigation" aria-label={ this.props['aria-label'] }>
+          <ul style={ styles.group }>
+            { this.renderArrowButton('left') }
+            { pageNumbers.map(this.renderPage) }
+            { this.renderArrowButton('right') }
+          </ul>
+        </nav>
+      </StyleRoot>
     )
   }
 
@@ -328,7 +330,7 @@ PaginationBase.defaultProps = {
 }
 
 export const Pagination = mediaQueries(
-  Radium(PaginationBase),
+  PaginationBase,
   {
     viewportIsMobile: true,
   },
