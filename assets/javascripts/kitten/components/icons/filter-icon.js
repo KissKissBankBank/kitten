2d9f49a60400e22ae276fd668bb6f1fcf4231c08
Camille Delbove <camille.delbove@gmail.com>
2018-03-05 11:52:44 +0100

    Add filter animation

--- assets/javascripts/kitten/components/icons/filter-icon.js
+++ assets/javascripts/kitten/components/icons/filter-icon.js
@@ -4,106 +4,221 @@ import PropTypes from 'prop-types'
 
 export class FilterIcon extends Component {
   render() {
+    return (
+      <StyleRoot>
+        { this.renderAnimation() }
+      </StyleRoot>
+    )
+  }
+
+  renderAnimation() {
+    const {
+      color,
+      dur,
+      type,
+      name,
+      begin,
+    } = this.props
 
     return (
       <svg
+        version="1.1"
         xmlns="http://www.w3.org/2000/svg"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
         x="0px"
         y="0px"
+        width="20px"
+        height="24px"
         viewBox="0 0 20 24"
-        style={ styles.svg }
-        // style="enable-background:new 0 0 20 24;"
+        style="enable-background:new 0 0 20 24;"
+        // xml:space="preserve"
       >
+
+// Animation 1
         <rect
           x="2"
-          fill={ color }
           width="2"
-          height="6"
-          style={ styles.svg.filter1 }
-          id="block1"
-        />
-
-        <animate
-          // xlink:href="#block1"
-          attributName="x"
-          from="2"
-          dur="5"
-          begin
-
-        />
-
+          height="10"
+          fill={ color }
+        >
+          <animate
+            attributeType={ type }
+            attributeName={ name }
+            values="-4; 0; -4"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </rect>
         <rect
           x="2"
           y="14"
-          fill={ color }
           width="2"
           height="10"
-        />
-
+          fill={ color }
+        >
+          <animate
+            attributeType={ type }
+            attributeName={ name }
+            values="14; 18; 14"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </rect>
         <path
           fill={ color }
           d="M4,9v2H2V9H4 M6,7H0v6h6V7L6,7z"
-        />
+        >
+          <animateTransform
+            attributeType={ type }
+            attributeName="transform"
+            type="translate"
+            values="0 0; 0 4; 0 0"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </path>
 
+  // Animation 2
         <rect
           x="9"
-          fill={ color }
           width="2"
           height="14"
-        />
-
+          fill={ color }
+        >
+          <animate
+            attributeType={ type }
+            attributeName={ name }
+            values="0; -12; 0"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </rect>
         <rect
           x="9"
           y="22"
-          fill={ color }
           width="2"
-          height="2"
-        />
-
+          height="14"
+          fill={ color }
+        >
+          <animate
+            attributeType={ type }
+            attributeName={ name }
+            values="22; 10; 22"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </rect>
         <path
           fill={ color }
           d="M11,17v2H9v-2H11 M13,15H7v6h6V15L13,15z"
-        />
+        >
+          <animateTransform
+            attributeType={ type }
+            attributeName="transform"
+            type="translate"
+            values="0 0; 0 -12; 0 0"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </path>
 
+  // Animation 3
         <rect
           x="16"
-          fill={ color }
           width="2"
-          height="4"
-        />
-
+          height="12"
+          fill={ color }
+        >
+          <animate
+            attributeType={ type }
+            attributeName={ name }
+            values="-8; 0; -8"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </rect>
         <rect
           x="16"
           y="12"
-          fill={ color }
           width="2"
           height="12"
-        />
-
+          fill={ color }
+        >
+          <animate
+            attributeType={ type }
+            attributeName={ name }
+            values="12; 20; 12"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </rect>
         <path
           fill={ color }
           d="M18,7v2h-2V7H18 M20,5h-6v6h6V5L20,5z"
-        />
-
+        >
+          <animateTransform
+            attributeType={ type }
+            attributeName="transform"
+            type="translate"
+            values="0 0; 0 8; 0 0"
+            begin={ begin }
+            dur={ dur }
+            repeatCount="indefinite"
+          />
+        </path>
       </svg>
     )
   }
 }
 
 const styles = {
-  svg: {
-    animationPlayState: 'paused',
-    ':hover': {
-      animationPlayState: 'running',
-    },
-    // filter1: {
-    //   animationFillMode: 'forwards',
-    //   animationTimingFunction: 'linear',
-    //   animationIterationCount: 'infinite',
-    // }
-  }
 }
 
 FilterIcon.defaultProps = {
   color: '#222',
+  dur: '1.5s',
+  type: 'xml',
+  name: 'y',
+  begin: 'begin',
 }
+
+
+// #big {
+//     transform-origin: 138px 118px;
+//     animation-duration: 30s;
+//     animation-name: rotateBig;
+//     animation-fill-mode: forwards;
+//     animation-timing-function: linear;
+//     animation-iteration-count: infinite;
+//     animation-play-state: paused;
+// }
+// #little {
+//     transform-origin: 32px 224px;
+//     animation-duration: 20s;
+//     animation-name: rotateLittle;
+//     animation-fill-mode: forwards;
+//     animation-timing-function: linear;
+//     animation-iteration-count: infinite;
+//     animation-play-state: paused;
+// }
+// #big:hover, #little:hover {
+//     animation-play-state: running;
+// }
+// @keyframes rotateBig {
+//     to {
+//         transform: rotate(360deg);
+//     }
+// }
+// @keyframes rotateLittle {
+//     to {
+//         transform: rotate(-360deg);
+//     }
+// }
