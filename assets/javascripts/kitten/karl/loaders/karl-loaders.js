dcc22016f89028bf25c08d1b664477066ce83d5c
Camille Delbove <camille.delbove@gmail.com>
2018-03-08 14:25:38 +0100

    Add { ...props }

--- assets/javascripts/kitten/karl/loaders/karl-loaders.js
+++ assets/javascripts/kitten/karl/loaders/karl-loaders.js
@@ -3,27 +3,21 @@ import { Loader } from 'kitten/components/loaders/loader'
 import { Button } from 'kitten/components/buttons/button'
 import { Marger } from 'kitten/components/layout/marger'
 
-export class KarlLoaderInButton extends Component {
-  render() {
-    const isDisabled = this.props.disabled
-    const isModifier = this.props.modifier
-
-    return (
-      <Marger
-        top="1"
-        bottom="1"
+export const KarlLoaderInButton = props => {
+  return (
+    <Marger
+      top="1"
+      bottom="1"
+    >
+      <Button
+        icon
+        iconWithMinWidth
+        aria-label="Loader button"
+        title="Loader button"
+        { ...props }
       >
-        <Button
-          icon
-          iconWithMinWidth
-          disabled={ isDisabled }
-          modifier={ isModifier }
-          aria-label="Loader button"
-          title="Loader button"
-        >
-          <Loader className="k-Button__icon" />
-        </Button>
-      </Marger>
-    )
-  }
+        <Loader className="k-Button__icon" />
+      </Button>
+    </Marger>
+  )
 }
