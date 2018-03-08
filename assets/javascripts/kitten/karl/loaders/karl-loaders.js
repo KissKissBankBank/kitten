6324c7617ac73a9d222aafc723444bd148b8bdaa
Camille Delbove <camilledel@users.noreply.github.com>
2018-03-08 16:00:56 +0100

    [Kitten] Update button colors (#1098)

--- assets/javascripts/kitten/karl/loaders/karl-loaders.js
+++ assets/javascripts/kitten/karl/loaders/karl-loaders.js
@@ -5,24 +5,19 @@ import { Marger } from 'kitten/components/layout/marger'
 
 export const KarlLoaderInButton = props => {
   return (
-    <div>
-      <Marger top="1" bottom="1">
-        <Button icon iconWithMinWidth>
-          <Loader className="k-Button__icon" />
-        </Button>
-      </Marger>
-
-      <Marger top="1" bottom="1">
-        <Button modifier="helium" icon iconWithMinWidth>
-          <Loader color="#fff" className="k-Button__icon" />
-        </Button>
-      </Marger>
-
-      <Marger top="1" bottom="1">
-        <Button icon iconWithMinWidth disabled>
-          <Loader className="k-Button__icon" />
-        </Button>
-      </Marger>
-    </div>
+    <Marger
+      top="1"
+      bottom="1"
+    >
+      <Button
+        icon
+        iconWithMinWidth
+        aria-label="Loader button"
+        title="Loader button"
+        { ...props }
+      >
+        <Loader className="k-Button__icon" />
+      </Button>
+    </Marger>
   )
 }
