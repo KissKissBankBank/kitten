5c520e41f771dd746c9de152a7b3fb1e15e685f4
Camille Delbove <camille.delbove@gmail.com>
2018-03-08 15:15:46 +0100

    Remove { Component }

--- assets/javascripts/kitten/karl/loaders/karl-loaders.js
+++ assets/javascripts/kitten/karl/loaders/karl-loaders.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Loader } from 'kitten/components/loaders/loader'
 import { Button } from 'kitten/components/buttons/button'
 import { Marger } from 'kitten/components/layout/marger'
