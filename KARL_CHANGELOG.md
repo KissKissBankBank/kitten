6b992fe010893bf70dec95c086c28238f84d145d
Camille Delbove <camille.delbove@gmail.com>
2018-03-08 13:16:11 +0100

    Update KarlLoaderInButton example

--- KARL_CHANGELOG.md
+++ KARL_CHANGELOG.md
@@ -4,6 +4,8 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [unreleased]
 
+- Feature: Update `KarlLoaderInButton` example.
+
 ## [20.0.2] - 2018-03-05
 
 No changes.
--- assets/javascripts/kitten/karl/loaders/karl-loaders.js
+++ assets/javascripts/kitten/karl/loaders/karl-loaders.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Loader } from 'kitten/components/loaders/loader'
 import { Button } from 'kitten/components/buttons/button'
 import { Marger } from 'kitten/components/layout/marger'
 
-export const KarlLoaderInButton = props => {
-  return (
-    <div>
-      <Marger top="1" bottom="1">
-        <Button icon iconWithMinWidth>
+export class KarlLoaderInButton extends Component {
+  render() {
+    const isDisabled = this.props.disabled
+    const isModifier = this.props.modifier
+
+    return (
+      <Marger
+        top="1"
+        bottom="1"
+      >
+        <Button
+          icon
+          iconWithMinWidth
+          disabled={ isDisabled }
+          modifier={ isModifier }
+          aria-label="Loader button"
+          title="Loader button"
+        >
           <Loader className="k-Button__icon" />
         </Button>
       </Marger>
-
-      <Marger top="1" bottom="1">
-        <Button modifier="helium" icon iconWithMinWidth>
-          <Loader color="#fff" className="k-Button__icon" />
-        </Button>
-      </Marger>
-
-      <Marger top="1" bottom="1">
-        <Button icon iconWithMinWidth disabled>
-          <Loader className="k-Button__icon" />
-        </Button>
-      </Marger>
-    </div>
-  )
+    )
+  }
 }
