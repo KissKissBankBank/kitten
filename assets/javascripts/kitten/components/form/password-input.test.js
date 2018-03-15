e7d24821b5e64d53258c788a0ef46b163cddfad1
Camille Delbove <camille.delbove@gmail.com>
2018-03-15 13:32:04 +0100

    Add test

---
+++ assets/javascripts/kitten/components/form/password-input.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { PasswordInput } from 'kitten/components/form/password-input'
+
+describe('<PasswordInput />', () => {
+  it('should match its empty snapshot', () => {
+    const tree = renderer
+      .create(
+        <PasswordInput />
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
