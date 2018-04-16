b106de7a034dd79b99c79b1dfcb54d5dbf1cc588
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2018-04-16 19:09:21 +0200

    Add checkA11y decorator

--- assets/javascripts/kitten/components/cards/article-card.stories.js
+++ assets/javascripts/kitten/components/cards/article-card.stories.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import { checkA11y } from '@storybook/addon-a11y'
 import { withKnobs, text } from '@storybook/addon-knobs/react'
 import { ArticleCard } from './article-card'
 
 storiesOf('Cards/ArticleCard', module)
   .addDecorator(withKnobs)
+  .addDecorator(checkA11y)
   .add(
     'default',
     withInfo('common info')(() => (
