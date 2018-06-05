4d04631a38f17baf47001d645610c40bff8d3bff
Morgan JARRY <morgan.jarry@gmail.com>
2018-06-05 15:56:23 +0200

    Fix `parseHtml` helper when no value is passed as parameter

--- assets/javascripts/kitten/helpers/utils/parser.js
+++ assets/javascripts/kitten/helpers/utils/parser.js
@@ -1,5 +1,8 @@
 import HtmlToReact from 'html-to-react'
 
 // We add a span to make parseHtml works with strings.
-export const parseHtml = value =>
-  new HtmlToReact.Parser().parse(`<span>${value}</span>`).props.children
+export const parseHtml = value => {
+  if (!value) return
+
+  return new HtmlToReact.Parser().parse(`<span>${value}</span>`).props.children
+}
