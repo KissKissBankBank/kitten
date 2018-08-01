d9ad309d5360d3abcb541e6d38553d2cf64b4ffe
Camille Delbove <camilledel@users.noreply.github.com>
2018-08-01 15:27:43 +0200

    Create `FacebookIconWithBackground` component. (#1256)

--- assets/javascripts/kitten/components/buttons/social-button-icon-words.js
+++ assets/javascripts/kitten/components/buttons/social-button-icon-words.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button } from 'kitten/components/buttons/button'
-import { FacebookIcon } from 'kitten/components/icons/facebook-icon'
+import { FacebookIconWithBackground } from 'kitten/components/icons/facebook-icon-with-background'
 import { TwitterIcon } from 'kitten/components/icons/twitter-icon'
 import { LinkedinIcon } from 'kitten/components/icons/linkedin-icon'
 
 export const FacebookButtonIconWords = ({ children, ...props }) => (
-  <Button icon size="big" modifier="facebook" {...props}>
-    <FacebookIcon className="k-Button__icon" />
+  <Button icon modifier="facebook" {...props}>
+    <FacebookIconWithBackground className="k-Button__icon k-Button__icon--facebook" />
     {children}
   </Button>
 )
