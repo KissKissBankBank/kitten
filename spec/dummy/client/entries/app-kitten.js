4eee15ce026af84d1da14a255ab294c29466a8c8
Morgan JARRY <morgan.jarry@gmail.com>
2018-09-17 18:37:23 +0200

    [EmbedPlayer] Create `EmbedPlayer` component (#1306)

--- spec/dummy/client/entries/app-kitten.js
+++ spec/dummy/client/entries/app-kitten.js
@@ -195,6 +195,9 @@ import { PhoneIllustration }
 import { KarlImageCropper } from 'kitten/karl/images/image-cropper'
 import { KarlImageWithCaption } from 'kitten/karl/images/image-with-caption'
 
+// Media
+import { EmbedPlayer } from 'kitten/components/media/embed-player'
+
 // Layout
 import { Separator } from 'kitten/components/layout/separator'
 import { KarlHorizontalStrokes } from 'kitten/karl/layout/horizontal-stroke'
@@ -491,6 +494,9 @@ ReactOnRails.register({
   // Images
   KarlImageCropper,
   KarlImageWithCaption,
+
+  //Media
+  EmbedPlayer,
 
   // Layout
   Separator,
