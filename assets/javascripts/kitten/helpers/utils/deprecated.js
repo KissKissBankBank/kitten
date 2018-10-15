c8eda6fcaccbfbf8aaff012290e9cb9fbd85727c
Fanny Cheung <fanny@ynote.hk>
2018-10-15 19:10:47 +0200

    [ProjectPage] Deprecate `ContributionCard`

---
+++ assets/javascripts/kitten/helpers/utils/deprecated.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Deprecated extends Component {
+  componentWillMount() {
+    if (process.env.NODE_ENV === 'development') {
+      const componentName = React.Children.only(this.props.children)._owner.type
+        .name
+
+      console.warn(
+        `Warning: ${componentName} is deprecated.`,
+        this.props.warningMessasge,
+      )
+    }
+  }
+
+  render() {
+    return this.props.children
+  }
+}
