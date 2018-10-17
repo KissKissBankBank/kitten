f56b74d17fcad513f5253bae678ccba618884275
Fanny Cheung <fanny@ynote.hk>
2018-10-18 00:27:32 +0200

    Add List tests

---
+++ assets/javascripts/kitten/components/lists/list.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { List } from 'kitten/components/lists/list'
+
+describe('<List>', () => {
+  describe('by default', () => {
+    it('matches with snapshot', () => {
+      const component = renderer
+        .create(
+          <List>
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+          </List>,
+        )
+        .toJSON()
+
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with <ListButtonItem>', () => {
+    it('matches with snapshot', () => {
+      const component = renderer
+        .create(
+          <List>
+            <List.ButtonItem>Alice</List.ButtonItem>
+            <List.ButtonItem>Cheshire Cat</List.ButtonItem>
+            <List.ButtonItem>Mad Hatter</List.ButtonItem>
+          </List>,
+        )
+        .toJSON()
+
+      expect(component).toMatchSnapshot()
+    })
+  })
+})
