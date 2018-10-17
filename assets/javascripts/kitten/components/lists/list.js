5d89636295fcdcc8bbf71fe30d650a083738beba
Fanny Cheung <fanny@ynote.hk>
2018-10-18 01:07:28 +0200

    Handle ButtonItem top border prop

--- assets/javascripts/kitten/components/lists/list.js
+++ assets/javascripts/kitten/components/lists/list.js
@@ -10,22 +10,29 @@ const ListButtonItem = ({
   style,
   onClick,
   onKeyPress,
+  withTopBorder,
   ...others
 }) => {
   let buttonStyles = { ...styles.buttonItem.base, ...style }
+
+  if (withTopBorder) {
+    buttonStyles = { ...buttonStyles, ...styles.buttonItem.topBorder }
+  }
 
   if (disabled) {
     buttonStyles = { ...buttonStyles, ...styles.buttonItem.disabled }
   }
 
+  const handleKeyPress = onKeyPress || onClick
+
   return (
     <StyleRoot>
       <div
+        {...others}
         role="button"
         style={buttonStyles}
         onClick={disabled ? null : onClick}
-        onKeyPress={disabled ? null : onKeyPress}
-        {...others}
+        onKeyPress={disabled ? null : handleKeyPress}
       >
         <div style={styles.buttonItem.content}>{children}</div>
         <div style={styles.buttonItem.arrow}>
@@ -37,11 +44,20 @@ const ListButtonItem = ({
 }
 
 ListButtonItem.propTypes = {
+  children: PropTypes.node.isRequired,
   disabled: PropTypes.bool,
+  style: PropTypes.object,
+  onClick: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  withTopBorder: PropTypes.bool,
 }
 
 ListButtonItem.defaultProps = {
   disabled: false,
+  style: {},
+  onClick: () => {},
+  onKeyPress: () => {},
+  withTopBorder: false,
 }
 
 export class ListBase extends Component {
@@ -52,7 +68,7 @@ export class ListBase extends Component {
 
     return (
       <ul style={styles.list}>
-        {React.Children.map(children, child => {
+        {React.Children.map(children, (child, index) => {
           if (!React.isValidElement(child)) return null
 
           return <li>{child}</li>
@@ -84,6 +100,9 @@ const styles = {
       ':hover': {
         backgroundColor: COLORS.background2,
       },
+    },
+    topBorder: {
+      borderTop: `${COLORS.line1} 0.125rem solid`,
     },
     disabled: {
       color: COLORS.line2,
