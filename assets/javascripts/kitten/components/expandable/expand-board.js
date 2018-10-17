962f0afba123f874ee6cdcaf69a386e79b554910
Fanny Cheung <fanny@ynote.hk>
2018-10-17 20:38:05 +0200

    [ProjectPage] Add ExpandBoard component

---
+++ assets/javascripts/kitten/components/expandable/expand-board.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react'
+import Radium from 'radium'
+import PropTypes from 'prop-types'
+import { Button } from 'kitten/components/buttons/button'
+import { ArrowIcon } from 'kitten/components/icons/arrow-icon'
+import COLORS from 'kitten/constants/colors-config'
+
+const ExpandBoardButton = ({
+  children,
+  expandChildren,
+  expanded,
+  style,
+  onClick,
+}) => {
+  const defaultExpandChildren = expandChildren ? expandChildren : children
+
+  return (
+    <Button
+      icon
+      iconOnRight
+      size="big"
+      modifier="helium"
+      style={style}
+      onClick={onClick}
+    >
+      {expanded ? defaultExpandChildren : children}
+      <ArrowIcon
+        direction={expanded ? 'top' : 'bottom'}
+        className="k-ButtonIcon__svg"
+      />
+    </Button>
+  )
+}
+
+ExpandBoardButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  expandChildren: PropTypes.node,
+  expanded: PropTypes.bool,
+}
+
+ExpandBoardButton.defaultProps = {
+  expandChildren: null,
+  expanded: false,
+}
+
+const ExpandBoardContent = ({ children }) => children
+
+class ExpandBoardBase extends Component {
+  static Button = ExpandBoardButton
+  static Content = ExpandBoardContent
+
+  constructor(props) {
+    super()
+    this.state = {
+      expanded: false,
+    }
+  }
+
+  isButtonComponent = component => component.type.name == 'ExpandBoardButton'
+
+  handleClick = () => {
+    const newState = { expanded: !this.state.expanded }
+    this.setState(newState)
+    this.props.onClick(newState)
+  }
+
+  render() {
+    const { children, style, disabled } = this.props
+
+    let button
+
+    const content = React.Children.map(children, child => {
+      if (!React.isValidElement(child)) return null
+
+      if (this.isButtonComponent(child)) {
+        button = React.cloneElement(child, {
+          disabled,
+          expanded: this.state.expanded,
+          onClick: this.handleClick,
+          style: styles.button,
+        })
+        return null
+      }
+
+      return child
+    })
+
+    return (
+      <div style={style}>
+        {button}
+        {this.state.expanded && <div>{content}</div>}
+      </div>
+    )
+  }
+}
+
+const styles = {
+  button: {
+    width: '100%',
+  },
+}
+
+ExpandBoardBase.propTypes = {
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+  style: PropTypes.object,
+  onClick: PropTypes.func,
+}
+
+ExpandBoardBase.defaultProps = {
+  disabled: false,
+  style: {},
+  onClick: () => {},
+}
+
+export const ExpandBoard = Radium(ExpandBoardBase)
---
+++ assets/javascripts/kitten/components/expandable/expand-board.stories.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import {
+  withKnobs,
+  text,
+  select,
+  boolean,
+  object,
+} from '@storybook/addon-knobs/react'
+import { StyleRoot } from 'radium'
+import { ExpandBoard } from 'kitten/components/expandable/expand-board'
+import { Grid, GridCol } from 'kitten/components/grid/grid'
+
+storiesOf('Expand/ExpandBoard', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withInfo('common info')(() => (
+      <StyleRoot>
+        <Grid>
+          <GridCol offset="1" col="10">
+            <ExpandBoard>
+              <ExpandBoard.Button>KissKissBankBank & Co</ExpandBoard.Button>
+              <ExpandBoard.Content>
+                <ul>
+                  <li>KissKissBankBank</li>
+                  <li>LENDOPOLIS</li>
+                </ul>
+              </ExpandBoard.Content>
+            </ExpandBoard>
+          </GridCol>
+        </Grid>
+      </StyleRoot>
+    )),
+  )
