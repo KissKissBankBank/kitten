155d5772528633b0a9056158117229c2a9e98134
Fanny Cheung <fanny@ynote.hk>
2018-10-25 16:10:38 +0200

    Migrate function components to class components

--- assets/javascripts/kitten/components/expandable/expand-board.js
+++ assets/javascripts/kitten/components/expandable/expand-board.js
@@ -5,77 +5,95 @@ import { Button } from 'kitten/components/buttons/button'
 import { ArrowIcon } from 'kitten/components/icons/arrow-icon'
 import COLORS from 'kitten/constants/colors-config'
 
-const ExpandBoardButton = ({
-  children,
-  expandChildren,
-  expanded,
-  disabled,
-  style,
-  onClick,
-  ariaId,
-}) => {
-  const defaultExpandChildren = expandChildren ? expandChildren : children
-  const buttonStyles = expanded
-    ? { ...style.base, ...style.expanded }
-    : style.base
+class ExpandBoardButton extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    ariaId: PropTypes.string.isRequired,
+    expandChildren: PropTypes.node,
+    expanded: PropTypes.bool,
+    disabled: PropTypes.bool,
+    style: PropTypes.object,
+    onClick: PropTypes.func,
+  }
 
-  return (
-    <Button
-      icon
-      iconOnRight
-      size="big"
-      modifier="helium"
-      disabled={disabled}
-      aria-expanded={expanded}
-      aria-controls={ariaId}
-      style={buttonStyles}
-      onClick={onClick}
-    >
-      {expanded ? defaultExpandChildren : children}
-      <ArrowIcon
-        direction={expanded ? 'top' : 'bottom'}
-        className="k-Button__icon"
-        fill={COLORS.background1}
-        style={style.arrow}
-      />
-    </Button>
-  )
+  static defaultProps = {
+    expandChildren: null,
+    expanded: false,
+    disabled: false,
+    style: {},
+    onClick: () => {},
+  }
+
+  render() {
+    const {
+      children,
+      expandChildren,
+      expanded,
+      disabled,
+      style,
+      onClick,
+      ariaId,
+    } = this.props
+    const defaultExpandChildren = expandChildren ? expandChildren : children
+    const buttonStyles = expanded
+      ? { ...style.base, ...style.expanded }
+      : style.base
+
+    return (
+      <Button
+        icon
+        iconOnRight
+        size="big"
+        modifier="helium"
+        disabled={disabled}
+        aria-expanded={expanded}
+        aria-controls={ariaId}
+        style={buttonStyles}
+        onClick={onClick}
+      >
+        {expanded ? defaultExpandChildren : children}
+        <ArrowIcon
+          direction={expanded ? 'top' : 'bottom'}
+          className="k-Button__icon"
+          fill={COLORS.background1}
+          style={style.arrow}
+        />
+      </Button>
+    )
+  }
 }
 
-ExpandBoardButton.propTypes = {
-  children: PropTypes.node.isRequired,
-  expandChildren: PropTypes.node,
-  expanded: PropTypes.bool,
-  disabled: PropTypes.bool,
-  style: PropTypes.object,
-  onClick: PropTypes.func,
-  ariaId: PropTypes.string.isRequired,
-}
+class ExpandBoardContent extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    ariaId: PropTypes.string.isRequired,
+  }
 
-ExpandBoardButton.defaultProps = {
-  expandChildren: null,
-  expanded: false,
-  disabled: false,
-  style: {},
-  onClick: () => {},
-}
+  render() {
+    const { children, ariaId } = this.props
 
-const ExpandBoardContent = ({ children, ariaId }) => (
-  <div id={ariaId}>{children}</div>
-)
-
-ExpandBoardContent.propTypes = {
-  children: PropTypes.node.isRequired,
-  ariaId: PropTypes.string.isRequired,
-}
-
-ExpandBoardContent.defaultProps = {
-  ariaId: 'k-ExpandBoard',
+    return <div id={ariaId}>{children}</div>
+  }
 }
 
 class ExpandBoardBase extends Component {
   static Button = ExpandBoardButton
   static Content = ExpandBoardContent
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    disabled: PropTypes.bool,
+    style: PropTypes.object,
+    onClick: PropTypes.func,
+    ariaId: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    disabled: false,
+    style: {},
+    onClick: () => {},
+    ariaId: 'k-ExpandBoard',
+  }
 
   constructor(props) {
     super()
@@ -146,21 +164,6 @@ const styles = {
       height: '0.375rem',
     },
   },
-}
-
-ExpandBoardBase.propTypes = {
-  children: PropTypes.node.isRequired,
-  disabled: PropTypes.bool,
-  style: PropTypes.object,
-  onClick: PropTypes.func,
-  ariaId: PropTypes.string.isRequired,
-}
-
-ExpandBoardBase.defaultProps = {
-  disabled: false,
-  style: {},
-  onClick: () => {},
-  ariaId: 'k-ExpandBoard',
 }
 
 export const ExpandBoard = Radium(ExpandBoardBase)
