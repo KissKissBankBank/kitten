03ad89c3019776cf59cf454aa40bd7cb580e2bb4
Bastien Maillard <bastien.maillard@gmail.com>
2018-10-25 15:08:53 +0200

    Update color prop
    Co-Authored-By: Ynote <fanny@ynote.hk>

--- assets/javascripts/kitten/components/lists/list.stories.js
+++ assets/javascripts/kitten/components/lists/list.stories.js
@@ -64,7 +64,7 @@ storiesOf('List', module)
                     <Text
                       tag="p"
                       weight="regular"
-                      color="font-1"
+                      color="font1"
                       size="tiny"
                       style={styles.buttonListItem.base}
                     >
