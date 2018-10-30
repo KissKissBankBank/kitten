5ee819a66608929b5f0dfae412546f5685847283
Bastien Maillard <bastien.maillard@gmail.com>
2018-10-30 17:27:07 +0100

    Update deprecation message
    Co-Authored-By: Ynote <fanny@ynote.hk>

--- assets/javascripts/kitten/components/cards/reward-card.js
+++ assets/javascripts/kitten/components/cards/reward-card.js
@@ -170,7 +170,7 @@ class RewardCardComponent extends Component {
     // Deprecated props
     titleDescription: deprecated(
       PropTypes.string,
-      'Use `RewardCard.Row`, `RewardCard.RowContent` and ``RewardCard.RowSide` to compose your card content instead',
+      'Use `RewardCard.Row`, `RewardCard.RowContent` and `RewardCard.RowSide` to compose your card content instead',
     ),
     textDescription: deprecated(
       PropTypes.string,
