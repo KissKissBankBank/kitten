224caa60552cdc118c1381c49a90f5409b23ee3e
Fanny Cheung <fanny@ynote.hk>
2018-10-30 19:17:09 +0100

    Add comment to explicit flex-grow use

--- assets/javascripts/kitten/components/cards/reward-card/row-content.js
+++ assets/javascripts/kitten/components/cards/reward-card/row-content.js
@@ -23,6 +23,8 @@ class RewardCardRowContentBase extends Component {
 }
 
 const styles = {
+  // We use an arbitrary huge ratio value for the `flex-grow` rule so that the
+  // `<div>` can take the maximum space left (by its siblings) in its container.
   flexGrow: 9999,
   flexBasis: pxToRem(275), // content min-width
   margin: pxToRem(15),
