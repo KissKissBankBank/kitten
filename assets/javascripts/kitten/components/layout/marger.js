291183e22fe2bbbd2896252515a38a01f2c11068
Fanny Cheung <fanny@ynote.hk>
2018-11-03 00:19:14 +0100

    Refacto Marger

--- assets/javascripts/kitten/components/layout/marger.js
+++ assets/javascripts/kitten/components/layout/marger.js
@@ -1,18 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Radium from 'radium'
+import PropTypes from 'prop-types'
+import { ScreenConfig } from 'kitten/constants/screen-config'
 import TYPOGRAPHY from 'kitten/constants/typography-config'
+import pipe from 'ramda/src/pipe'
+import isPlainNumber from 'is-plain-number'
 
-export const Marger = ({ top, bottom, style, ...props }) => {
-  const gutter = 10 / TYPOGRAPHY.root // Transform 10 to .625 for have rem unit.
-  const styles = {
-    ...style,
-    marginTop: top ? `${top * gutter}rem` : null,
-    marginBottom: bottom ? `${bottom * gutter}rem` : null,
+class MargerBase extends Component {
+  static propTypes = {
+    top: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.shape({
+        xxs: PropTypes.number,
+        xs: PropTypes.number,
+        s: PropTypes.number,
+        m: PropTypes.number,
+        l: PropTypes.number,
+        xl: PropTypes.number,
+      }),
+    ]),
+    bottom: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.shape({
+        xxs: PropTypes.number,
+        xs: PropTypes.number,
+        s: PropTypes.number,
+        m: PropTypes.number,
+        l: PropTypes.number,
+        xl: PropTypes.number,
+      }),
+    ]),
   }
 
-  return <div style={styles} {...props} />
+  static defaultProps = {
+    top: null,
+    bottom: null,
+  }
+
+  constructor(props) {
+    super(props)
+
+    // Use 10px as gutter base and transform it to have a rem unit.
+    this.gutter = 10 / TYPOGRAPHY.root
+  }
+
+  marginSize = value => `${value * this.gutter}rem`
+  valueIsNumber = value => {
+    // Retro-compatibility: this handles the case when the user enters `.5` as a
+    // value for a margin.
+    if (typeof value === 'string' && value.match(/^\./)) {
+      return isPlainNumber(`0${value}`)
+    }
+
+    return isPlainNumber(value)
+  }
+
+  topIsNumber = () => this.valueIsNumber(this.props.top)
+  bottomIsNumber = () => this.valueIsNumber(this.props.bottom)
+
+  render() {
+    const { top, bottom, style, ...others } = this.props
+
+    const styles = [
+      style,
+      this.topIsNumber() && { marginTop: this.marginSize(top) },
+      this.bottomIsNumber() && { marginBottom: this.marginSize(bottom) },
+    ]
+
+    return <div style={styles} {...others} />
+  }
 }
 
-Marger.defaultProps = {
-  top: 0,
-  bottom: 0,
-}
+export const Marger = Radium(MargerBase)
