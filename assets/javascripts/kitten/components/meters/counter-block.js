94241e6e2ec7f15e35b3819a6623f4d73e9edaf9
Morgan JARRY <morgan.jarry@gmail.com>
2018-11-06 17:36:17 +0100

    Add `HorizontalStroke` to `CounterBlock` component

--- assets/javascripts/kitten/components/meters/counter-block.js
+++ assets/javascripts/kitten/components/meters/counter-block.js
@@ -3,6 +3,7 @@ import Radium, { StyleRoot } from 'radium'
 import { Title as TitleBase } from 'kitten/components/typography/title'
 import { Text as TextBase } from 'kitten/components/typography/text'
 import { Marger } from 'kitten/components/layout/marger'
+import { HorizontalStroke } from 'kitten/components/layout/horizontal-stroke'
 import { ScreenConfig } from 'kitten/constants/screen-config'
 import { withMediaQueries } from 'kitten/hoc/media-queries'
 
@@ -25,31 +26,41 @@ class CounterBlockBase extends Component {
 
     return (
       <StyleRoot style={styles.counterBlock}>
-        <Marger bottom={margin}>
-          <Title modifier={titleSize} margin={false} tag={titleTag}>
-            {title}
-          </Title>
-        </Marger>
-        <Text
-          style={styles.counterText}
-          fontStyle="italic"
-          weight="bold"
-          tag={titleTag}
-        >
-          {counter}
-        </Text>
-        <Marger top={margin}>
-          <Text
-            style={styles.subTitle}
-            size={subTitleSize}
-            tag={subTitleTag}
-            fontStyle="italic"
-            weight="bold"
-            tag="p"
-          >
-            {subTitle}
-          </Text>
-        </Marger>
+        <div style={styles.counterContainer}>
+          <Marger bottom={margin}>
+            <Title modifier={titleSize} margin={false} tag={titleTag}>
+              {title}
+            </Title>
+          </Marger>
+          <div style={styles.flexGrid}>
+            <div style={styles.strokeContainer} className="k-u-hidden@m-down">
+              <HorizontalStroke style={styles.stroke} size="big" />
+            </div>
+            <Text
+              style={styles.counterText}
+              fontStyle="italic"
+              weight="bold"
+              tag={titleTag}
+            >
+              {counter}
+            </Text>
+            <div style={styles.strokeContainer} className="k-u-hidden@m-down">
+              <HorizontalStroke style={styles.stroke} size="big" />
+            </div>
+          </div>
+          <Marger top={margin}>
+            <Text
+              style={styles.subTitle}
+              size={subTitleSize}
+              tag={subTitleTag}
+              fontStyle="italic"
+              weight="bold"
+              tag="p"
+            >
+              {subTitle}
+            </Text>
+          </Marger>
+        </div>
       </StyleRoot>
     )
   }
@@ -68,6 +79,22 @@ const styles = {
     textAlign: 'center',
   },
 
+  flexGrid: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+
+  stroke: {
+    width: '100%',
+    height: 6,
+  },
+
+  strokeContainer: {
+    flexGrow: 2,
+  },
+
   counterText: {
     fontSize: '3rem',
     lineHeight: 0.7,
@@ -75,6 +102,8 @@ const styles = {
     display: 'block',
     paddingBottom: 4,
     margin: 0,
+    flexGrow: 1,
+    textIndent: '-1rem',
 
     [`@media (min-width: ${ScreenConfig.XS.min}px)`]: {
       fontSize: '4.125rem',
