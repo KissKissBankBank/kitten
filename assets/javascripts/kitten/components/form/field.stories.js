cf71cc3c68abbd88519d7b2a4d7f5ca062a01cbd
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2018-11-13 17:49:49 +0100

    Clean stories

--- assets/javascripts/kitten/components/form/field.stories.js
+++ assets/javascripts/kitten/components/form/field.stories.js
@@ -6,12 +6,20 @@ import { Marger } from 'kitten/components/layout/marger'
 import { Container } from 'kitten/components/grid/container'
 import { Grid, GridCol } from 'kitten/components/grid/grid'
 
-const FormGrid = ({ children }) => (
+const FieldBase = ({ children }) => (
   <Container>
     <Grid>
       <GridCol col="3">
         <Marger top="5" bottom="5">
-          {children}
+          <Field error={boolean('Error?', false)}>
+            <Field.Label htmlFor="label" tooltip={text('Tooltip', null)}>
+              {text('Label', 'Label')}
+            </Field.Label>
+
+            {children}
+
+            <Field.Error>{text('Error', 'Error message…')}</Field.Error>
+          </Field>
         </Marger>
       </GridCol>
     </Grid>
@@ -22,76 +30,52 @@ storiesOf('Form/Field', module)
   .addDecorator(withKnobs)
   .add('with input', () => {
     return (
-      <FormGrid>
-        <Field error={boolean('Error?', false)}>
-          <Field.Label htmlFor="label" tooltip={text('Tooltip', null)}>
-            {text('Label', 'Label')}
-          </Field.Label>
-
-          <Field.Input
-            id="label"
-            name="label"
-            placeholder={text('Placeholder', 'Placeholder…')}
-          />
-
-          <Field.Error>{text('Error', 'Error message…')}</Field.Error>
-        </Field>
-      </FormGrid>
+      <FieldBase>
+        <Field.Input
+          id="label"
+          name="label"
+          placeholder={text('Placeholder', 'Placeholder…')}
+        />
+      </FieldBase>
     )
   })
   .add('with radio buttons', () => {
     return (
-      <FormGrid>
-        <Field error={boolean('Error?', false)}>
-          <Field.Label htmlFor="label" tooltip={text('Tooltip', null)}>
-            {text('Label', 'Label')}
-          </Field.Label>
-
-          <Field.RadioButtonSet
-            name="option"
-            items={[
-              {
-                text: text('Option A', 'Option A'),
-                id: 'option-a',
-                defaultChecked: true,
-              },
-              {
-                text: text('Option B', 'Option B'),
-                id: 'option-b',
-              },
-              {
-                text: text('Option C', 'Option C'),
-                id: 'option-c',
-              },
-            ]}
-          />
-
-          <Field.Error>{text('Error', 'Error message…')}</Field.Error>
-        </Field>
-      </FormGrid>
+      <FieldBase>
+        <Field.RadioButtonSet
+          name="option"
+          items={[
+            {
+              text: text('Option A', 'Option A'),
+              id: 'option-a',
+              defaultChecked: true,
+            },
+            {
+              text: text('Option B', 'Option B'),
+              id: 'option-b',
+            },
+            {
+              text: text('Option C', 'Option C'),
+              id: 'option-c',
+            },
+          ]}
+        />
+      </FieldBase>
     )
   })
   .add('with select', () => {
     return (
-      <FormGrid>
-        <Field error={boolean('Error?', false)}>
-          <Field.Label htmlFor="label" tooltip={text('Tooltip', null)}>
-            {text('Label', 'Label')}
-          </Field.Label>
-
-          <Field.Select
-            id="select"
-            name="option"
-            placeholder={text('Placeholder', 'Select…')}
-            options={[
-              { value: 'a', label: text('Option A', 'Option A') },
-              { value: 'b', label: text('Option B', 'Option B') },
-              { value: 'c', label: text('Option C', 'Option C') },
-            ]}
-          />
-
-          <Field.Error>{text('Error', 'Error message…')}</Field.Error>
-        </Field>
-      </FormGrid>
+      <FieldBase>
+        <Field.Select
+          id="select"
+          name="option"
+          placeholder={text('Placeholder', 'Select…')}
+          options={[
+            { value: 'a', label: text('Option A', 'Option A') },
+            { value: 'b', label: text('Option B', 'Option B') },
+            { value: 'c', label: text('Option C', 'Option C') },
+          ]}
+        />
+      </FieldBase>
     )
   })
