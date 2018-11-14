0ddd17b14860504f97a62d73f73fc4c7efe729fd
Fanny Cheung <fanny@ynote.hk>
2018-11-14 19:38:54 +0100

    [ProjectPage] Add animation to List.ButtonItem hover

--- assets/javascripts/kitten/components/lists/list.js
+++ assets/javascripts/kitten/components/lists/list.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Radium, { StyleRoot } from 'radium'
-import { ArrowIcon } from 'kitten/components/icons/arrow-icon'
+import { ArrowIcon as ArrowIconBase } from 'kitten/components/icons/arrow-icon'
 import COLORS from 'kitten/constants/colors-config'
+import { pxToRem } from 'kitten/helpers/utils/typography'
 
-const ListButtonItem = ({
-  children,
-  disabled,
-  style,
-  onClick,
-  withTopBorder,
-  ...others
-}) => {
-  const buttonStyles = [
-    styles.buttonItem.base,
-    style,
-    withTopBorder && styles.buttonItem.topBorder,
-    disabled && styles.buttonItem.disabled,
-  ]
+const ArrowIcon = Radium(ArrowIconBase)
 
-  const handleKeyPress = event => {
+class ListButtonItem extends Component {
+  state = {
+    focus: false,
+  }
+
+  handleKeyPress = event => {
+    const { onKeyPress } = this.props
     const enterKey = 13
     const spaceKey = 32
 
@@ -28,30 +22,100 @@ const ListButtonItem = ({
       case spaceKey:
         event.preventDefault
         onClick()
+        if (onKeyPress) {
+          onKeyPress()
+        }
         break
     }
   }
 
-  return (
-    <StyleRoot>
-      <div
-        {...others}
-        role="button"
-        style={buttonStyles}
-        tabIndex="0"
-        onClick={disabled ? null : onClick}
-        onKeyPress={disabled ? null : handleKeyPress}
-      >
-        <div style={styles.buttonItem.content}>{children}</div>
-        <div style={styles.buttonItem.arrow}>
-          <ArrowIcon
-            className="k-Button__icon"
-            style={disabled ? styles.buttonItem.icon.disabled : null}
-          />
+  updateFocus = focusState => {
+    this.setState({ focus: focusState })
+  }
+
+  handleFocus = event => {
+    const { onFocus } = this.props
+
+    this.updateFocus(true)
+    if (onFocus) {
+      onFocus()
+    }
+  }
+
+  handleBlur = () => {
+    const { onBlur } = this.props
+
+    this.updateFocus(false)
+    if (onBlur) {
+      onBlur()
+    }
+  }
+
+  handleMouseEnter = () => {
+    const { onMouseEnter } = this.props
+
+    this.updateFocus(true)
+    if (onMouseEnter) {
+      onMouseEnter()
+    }
+  }
+
+  handleMouseLeave = () => {
+    const { onMouseLeave } = this.props
+
+    this.updateFocus(false)
+    if (onMouseLeave) {
+      onMouseLeave()
+    }
+  }
+
+  render() {
+    const {
+      children,
+      disabled,
+      style,
+      onClick,
+      withTopBorder,
+      ...others
+    } = this.props
+
+    const { focus } = this.state
+
+    const buttonStyles = [
+      styles.buttonItem.base,
+      style,
+      withTopBorder && styles.buttonItem.topBorder,
+      disabled && styles.buttonItem.disabled,
+    ]
+
+    const arrowStyles = [
+      styles.buttonItem.icon.base,
+      disabled && styles.buttonItem.icon.disabled,
+      !disabled && focus && styles.buttonItem.icon.focus,
+    ]
+
+    return (
+      <StyleRoot>
+        <div
+          {...others}
+          role="button"
+          style={buttonStyles}
+          tabIndex="0"
+          onClick={disabled ? null : onClick}
+          onKeyPress={disabled ? null : this.handleKeyPress}
+          onMouseEnter={this.handleMouseEnter}
+          onFocus={this.handleFocus}
+          onMouseLeave={this.handleMouseLeave}
+          onBlur={this.handleBlur}
+        >
+          <div style={styles.buttonItem.content}>{children}</div>
+          <div style={styles.buttonItem.arrow}>
+            <ArrowIcon className="k-Button__icon" style={arrowStyles} />
+          </div>
         </div>
-      </div>
-    </StyleRoot>
-  )
+      </StyleRoot>
+    )
+  }
 }
 
 ListButtonItem.propTypes = {
@@ -129,6 +193,14 @@ const styles = {
       alignItems: 'center',
     },
     icon: {
+      base: {
+        position: 'relative',
+        left: 0,
+        transition: 'left .2s',
+      },
+      focus: {
+        left: pxToRem(5),
+      },
       disabled: {
         fill: COLORS.line2,
       },
