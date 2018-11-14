9ead9959dd37194a84e7748a79124dcb641b5293
Camille Delbove <camille.delbove@gmail.com>
2018-11-14 16:44:10 +0100

    Add stories

--- assets/javascripts/kitten/components/cards/team-card.js
+++ assets/javascripts/kitten/components/cards/team-card.js
@@ -4,22 +4,32 @@ import PropTypes from 'prop-types'
 import { Marger } from 'kitten/components/layout/marger'
 import { Text } from 'kitten/components/typography/text'
 import { LinkedinButtonIcon } from 'kitten/components/buttons/social-button-icon'
-import { ButtonIcon } from 'kitten/components/buttons/button-icon'
+import { ButtonIcon as ButtonIconBase } from 'kitten/components/buttons/button-icon'
 import { PhoneIcon } from 'kitten/components/icons/phone-icon'
 import COLORS from 'kitten/constants/colors-config'
+
+const ButtonIcon = Radium(ButtonIconBase)
 
 export class TeamCard extends Component {
   static propTypes = {
     firstName: PropTypes.string.isRequired,
     lastName: PropTypes.string.isRequired,
     jobTilte: PropTypes.string.isRequired,
+    mail: PropTypes.string,
+    phoneNumber: PropTypes.string,
+    socialLink: PropTypes.string,
   }
 
-  static defaultProps = {}
+  static defaultProps = {
+    mail: 'example@kisskissbankbank.com',
+    phoneNumber: '00 00 00 00 00',
+    socialLink: ' ',
+  }
 
   render() {
     return (
       <StyleRoot>
+        <Marger bottom="2">{this.renderImage()}</Marger>
         <Marger top="2" bottom="1.4">
           {this.renderTitle()}
         </Marger>
@@ -28,11 +38,10 @@ export class TeamCard extends Component {
     )
   }
 
-  // renderImage() {
-  //   const { style, ...imageProps } = this.props.imageProps
-
-  //   return <img {...imageProps} style={{ ...style, ...styles.image }} />
-  // }
+  renderImage() {
+    const { imgTitle, image } = this.props
+    return <div style={imgBackground(image)} title={imgTitle} />
+  }
 
   renderTitle() {
     const { firstName, lastName, jobTitle } = this.props
@@ -50,41 +59,53 @@ export class TeamCard extends Component {
   }
 
   renderIcons() {
-    const {} = this.props
+    const { mail, phoneNumber, socialLink } = this.props
 
     return (
       <div>
-        <ButtonIcon
-          modifier="hydrogen"
-          aria-label=""
-          className="k-ButtonIcon--phone"
-        >
-          <PhoneIcon className="k-ButtonIcon__svg" />
-        </ButtonIcon>
+        {mail && (
+          <ButtonIcon
+            modifier="hydrogen"
+            aria-label=""
+            className="k-ButtonIcon--phone"
+            style={styles.icons}
+          >
+            <PhoneIcon className="k-ButtonIcon__svg" />
+          </ButtonIcon>
+        )}
 
-        <ButtonIcon
-          modifier="hydrogen"
-          aria-label=""
-          className="k-ButtonIcon--phone"
-        >
-          <PhoneIcon className="k-ButtonIcon__svg" />
-        </ButtonIcon>
+        {phoneNumber && (
+          <ButtonIcon
+            modifier="hydrogen"
+            aria-label=""
+            className="k-ButtonIcon--phone"
+            style={styles.icons}
+          >
+            <PhoneIcon className="k-ButtonIcon__svg" />
+          </ButtonIcon>
+        )}
 
-        <LinkedinButtonIcon />
+        {socialLink && <LinkedinButtonIcon size="tiny" />}
       </div>
     )
   }
 }
 
-// const imageHeight = 378
-// const imageWidth = 252
+const imageHeight = 378
+const imageWidth = 252
 
-// const imgBackground = image => ({
-//   backgroundImage: `url(${image})`,
-//   backgroundColor: COLORS.background1,
-//   backgroundSize: 'cover',
-//   backgroundPosition: 'center',
-//   backgroundRepeat: 'no-repeat',
-//   width: '100%',
-//   paddingBottom: (imageHeight / imageWidth) * 100 + '%',
-// })
+const imgBackground = image => ({
+  backgroundImage: `url(${image})`,
+  backgroundColor: COLORS.background1,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  width: '100%',
+  paddingBottom: imageHeight / imageWidth * 100 + '%',
+})
+
+const styles = {
+  icons: {
+    marginRight: 14,
+  },
+}
--- assets/javascripts/kitten/karl/cards/team-card.stories.js
+++ assets/javascripts/kitten/karl/cards/team-card.stories.js
@@ -7,20 +7,19 @@ import { Grid, GridCol } from 'kitten/components/grid/grid'
 import { TeamCard } from 'kitten/components/cards/team-card'
 import { Marger } from 'kitten/components/layout/marger'
 
-storiesOf('Cards/VerticalCard', module)
-  .addDecorator(withKnobs)
-  .add(
-    withInfo('common info')(() => (
-      <Marger top="3">
-        <Grid>
-          <GridCol col="3">
-            <TeamCard
-              firstName={text('First name', 'Lorem')}
-              lastName={text('Last name', 'Ipsum')}
-              jobTitle={text('Job title', 'Remote')}
-            />
-          </GridCol>
-        </Grid>
-      </Marger>
-    )),
-  )
+storiesOf('Cards', module).add(
+  'TeamCard',
+  withInfo('common info')(() => (
+    <Marger top="3">
+      <Grid>
+        <GridCol col="3">
+          <TeamCard
+            firstName={text('First name', 'Lorem')}
+            lastName={text('Last name', 'Ipsum')}
+            jobTitle={text('Job title', 'Remote')}
+          />
+        </GridCol>
+      </Grid>
+    </Marger>
+  )),
+)
