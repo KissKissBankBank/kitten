f86016c849189841c7dedadfd916ef42a172bbf5
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2018-11-21 11:46:21 +0100

    remove karl to build

--- src/components/dropdowns/user-menu.js
+++ src/components/dropdowns/user-menu.js
@@ -21,8 +21,6 @@ var _elementHelper = _interopRequireDefault(require("kitten/helpers/dom/element-
 
 var _emitter = _interopRequireDefault(require("kitten/helpers/utils/emitter"));
 
-var _assign = _interopRequireDefault(require("core-js/library/fn/object/assign"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
--- src/helpers/utils/range.js
+++ src/helpers/utils/range.js
@@ -5,16 +5,18 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.createRangeFromZeroTo = void 0;
 
-var _keys = _interopRequireDefault(require("core-js/library/fn/array/keys"));
-
-var _from = _interopRequireDefault(require("core-js/library/fn/array/from"));
-
-var _isInteger = _interopRequireDefault(require("core-js/library/fn/number/is-integer"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 var createRangeFromZeroTo = function createRangeFromZeroTo(num) {
-  return (0, _isInteger.default)(num) ? (0, _from.default)((0, _keys.default)(Array(num))) : [];
+  var array = [];
+
+  if (!Number.isInteger(num)) {
+    return array;
+  }
+
+  for (var i = 0; i < num; i++) {
+    array.push(i);
+  }
+
+  return array;
 };
 
 exports.createRangeFromZeroTo = createRangeFromZeroTo;
