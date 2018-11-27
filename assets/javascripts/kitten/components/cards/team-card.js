53a5e9bd45e69763b4f718f4dea431f3a8f228d8
Camille Delbove <camille.delbove@gmail.com>
2018-11-27 18:55:33 +0100

    Remove `<Fragment>`

--- assets/javascripts/kitten/components/cards/team-card.js
+++ assets/javascripts/kitten/components/cards/team-card.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { TeamCardTitle } from 'kitten/components/cards/team-card/title'
 import { TeamCardImage } from 'kitten/components/cards/team-card/image'
 import { TeamCardIcons } from 'kitten/components/cards/team-card/icons'
@@ -8,7 +8,5 @@ export class TeamCard extends Component {
   static Title = TeamCardTitle
   static Icons = TeamCardIcons
 
-  render() {
-    return <Fragment>{this.props.children}</Fragment>
-  }
+  render = () => this.props.children
 }
