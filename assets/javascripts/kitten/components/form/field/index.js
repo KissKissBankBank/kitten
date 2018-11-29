b1e5f1b1c00546030e2d40f72f04eca944dd82b5
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2018-11-29 18:06:11 +0100

    Remove unused import

--- assets/javascripts/kitten/components/form/field/index.js
+++ assets/javascripts/kitten/components/form/field/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import { FieldLabel } from './components/label'
 import { FieldInput } from './components/input'
 import { FieldError } from './components/error'
