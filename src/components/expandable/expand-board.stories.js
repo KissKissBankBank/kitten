9d587b8433da2bf707ed66b0f2590b528f5e4601
Florent DUVEAU <florent.duveau@gmail.com>
2018-11-30 10:54:20 +0100

    Release/v1.0.0 rc4 (#1431)
    * add built js files for 1.0.0-rc4
    
    * bump kitten version to 1.0.0-rc4

--- src/components/expandable/expand-board.stories.js
+++ src/components/expandable/expand-board.stories.js
@@ -14,87 +14,51 @@ var _radium = require("radium");
 
 var _expandBoard = require("../../components/expandable/expand-board");
 
-var _list = require("../../components/lists/list");
-
 var _grid = require("../../components/grid/grid");
 
 var _text = require("../../components/typography/text");
 
 var _colorsConfig = _interopRequireDefault(require("../../constants/colors-config"));
 
+var _typography = require("../../helpers/utils/typography");
+
 (0, _react2.storiesOf)('Expandable', module).addDecorator(_react3.withKnobs).add('ExpandBoard', (0, _addonInfo.withInfo)('The ExpandBoard is a container that can expand on click. It can accept any content.')(function () {
-  return _react.default.createElement(_radium.StyleRoot, null, _react.default.createElement(_grid.Grid, null, _react.default.createElement(_grid.GridCol, {
+  return _react.default.createElement(_radium.StyleRoot, null, _react.default.createElement("div", {
+    style: styles.container
+  }, _react.default.createElement(_grid.Grid, null, _react.default.createElement(_grid.GridCol, {
     offset: "1",
     col: "10"
-  }, _react.default.createElement(_expandBoard.ExpandBoard, null, _react.default.createElement(_expandBoard.ExpandBoard.Button, {
-    expandChildren: "Lancez votre projet"
-  }, "KissKissBankBank & Co"), _react.default.createElement(_expandBoard.ExpandBoard.Content, null, _react.default.createElement("div", {
+  }, _react.default.createElement(_expandBoard.ExpandBoard, {
+    withAnimation: (0, _react3.boolean)('withAnimation', true)
+  }, _react.default.createElement(_expandBoard.ExpandBoard.Button, {
+    expandChildren: (0, _react3.text)('Expanded button text', 'Lancez votre projet')
+  }, (0, _react3.text)('Button text', 'KissKissBankBank & Co')), _react.default.createElement(_expandBoard.ExpandBoard.Content, null, _react.default.createElement("div", {
     style: styles.expandBoard.defaultContent
-  }, "KissKissBankBank et 1 483 037 KissBankers vous aident \xE0 r\xE9aliser vos projets cr\xE9atifs, associatifs et entrepreneuriaux. Participez \xE0 la naissance de projets inspirants."))))));
-})).add('ExpandBoard with list of ButtonItem', (0, _addonInfo.withInfo)('This is an example of ExpandBoard composition with a list of buttons as content.')(function () {
-  return _react.default.createElement(_radium.StyleRoot, null, _react.default.createElement(_grid.Grid, null, _react.default.createElement(_grid.GridCol, {
+  }, (0, _react3.text)('Board content', "KissKissBankBank et 1 483 037 KissBankers vous aident \xE0\n                        r\xE9aliser vos projets cr\xE9atifs, associatifs et\n                        entrepreneuriaux. Participez \xE0 la naissance de projets\n                        inspirants."))))))));
+})).add('ExpandBoard with a List of ButtonItem', (0, _addonInfo.withInfo)('This is an example of ExpandBoard composition with a list of buttons as content.')(function () {
+  return _react.default.createElement(_radium.StyleRoot, null, _react.default.createElement("div", {
+    style: styles.container
+  }, _react.default.createElement(_grid.Grid, null, _react.default.createElement(_grid.GridCol, {
     offset: "1",
     col: "10"
-  }, _react.default.createElement(_expandBoard.ExpandBoard, null, _react.default.createElement(_expandBoard.ExpandBoard.Button, {
-    expandChildren: "Pr\xE9cisez votre choix"
-  }, "Je soutiens"), _react.default.createElement(_expandBoard.ExpandBoard.Content, null, _react.default.createElement(_list.List, null, _react.default.createElement(_list.List.ButtonItem, null, _react.default.createElement("div", {
-    style: styles.buttonListItem.wrapper
-  }, _react.default.createElement(_text.Text, {
-    tag: "p",
-    weight: "regular",
-    color: "font-1",
-    size: "tiny",
-    style: styles.buttonListItem.base
-  }, "Taille XS"), _react.default.createElement(_text.Text, {
-    tag: "small",
-    color: "font1",
-    size: "micro"
-  }, "Disponibilit\xE9: 10/30"))), _react.default.createElement(_list.List.ButtonItem, {
-    disabled: true
-  }, _react.default.createElement("div", {
-    style: styles.buttonListItem.wrapper
-  }, _react.default.createElement(_text.Text, {
-    tag: "p",
-    weight: "regular",
-    color: "font-1",
-    size: "tiny",
-    style: styles.buttonListItem.base
-  }, "Taille M"), _react.default.createElement(_text.Text, {
-    tag: "small",
-    color: "font1",
-    size: "micro"
-  }, "Disponibilit\xE9: 10/30"))), _react.default.createElement(_list.List.ButtonItem, null, _react.default.createElement("div", {
-    style: styles.buttonListItem.wrapper
-  }, _react.default.createElement(_text.Text, {
-    tag: "p",
-    weight: "regular",
-    color: "font-1",
-    size: "tiny",
-    style: styles.buttonListItem.base
-  }, "Taille XXL"), _react.default.createElement(_text.Text, {
-    tag: "small",
-    color: "font1",
-    size: "micro"
-  }, "Disponibilit\xE9: 10/30")))))))));
+  }, _react.default.createElement(ExpandBoardWithButtonItemList, {
+    expandedButtonText: (0, _react3.text)('Expanded button text', 'Précisez votre choix'),
+    buttonText: (0, _react3.text)('Button text', 'Je soutiens'),
+    withAnimation: (0, _react3.boolean)('With animation', true)
+  })))));
 }));
 var styles = {
+  container: {
+    margin: (0, _typography.pxToRem)(30),
+    paddingBottom: (0, _typography.pxToRem)(30),
+    borderBottom: "2px solid ".concat(_colorsConfig.default.line2)
+  },
   expandBoard: {
     defaultContent: {
       padding: '1.5em',
       borderLeft: "".concat(_colorsConfig.default.line1, " 0.125rem solid"),
       borderRight: "".concat(_colorsConfig.default.line1, " 0.125rem solid"),
       borderBottom: "".concat(_colorsConfig.default.line1, " 0.125rem solid")
-    }
-  },
-  buttonListItem: {
-    base: {
-      margin: 0,
-      padding: 0,
-      lineHeight: '1.2rem'
-    },
-    wrapper: {
-      margin: '1rem 0',
-      padding: 0
     }
   }
 };
