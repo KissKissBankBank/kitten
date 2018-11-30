5f19528d21ca7dae34169255111bbeb33a1fb73e
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2018-11-30 14:37:49 +0100

    ShippingAddress

---
+++ assets/javascripts/kitten/karl/pages/shipping_address/shipping_address.stories.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs/react'
+import ShippingAddress from './components/shipping_address'
+import { Marger } from '../../../components/layout/marger'
+
+storiesOf('Pages/ShippingAddress', module)
+  .addDecorator(withKnobs)
+  .add('ShippingAddress', () => {
+    return (
+      <Marger top="10" bottom="10">
+        <ShippingAddress
+          title="Vos informations <br/>de livraison"
+          gender={{
+            label: 'Civilité',
+          }}
+          firstname={{
+            label: 'Prénom',
+          }}
+          lastname={{
+            label: 'Nom',
+          }}
+          company={{
+            label: 'Entreprise (optionnel)',
+          }}
+          phone={{
+            label: 'Numéro de téléphone (optionnel)',
+          }}
+          address1={{
+            label: 'Adresse',
+          }}
+          address2={{
+            label: "Complément d'adresse (optionnel)",
+          }}
+          postalCode={{
+            label: 'Code postal',
+          }}
+          city={{
+            label: 'Ville',
+          }}
+          country={{
+            label: 'Pays',
+          }}
+          button="Valider mon total de 20 €"
+        />
+      </Marger>
+    )
+  })
