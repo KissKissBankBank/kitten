f7005afc3577db0d48fd4b4da9899d5c620ea81b
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2018-12-20 12:30:31 +0100

    create context to show content

--- assets/javascripts/kitten/components/accordeon/index.js
+++ assets/javascripts/kitten/components/accordeon/index.js
@@ -1,27 +1,44 @@
 import React, { Component, Fragment } from 'react'
 import { Marger } from '../layout/marger'
 
+const AccordeonContext = React.createContext(0)
+
 const Item = props => <div {...props} />
 const Header = props => <div {...props} />
-const Content = props => <div {...props} />
+const Content = props => {
+  return (
+    <AccordeonContext.Consumer>
+      {selectedItem => {
+        return selectedItem === props.id && <div {...props} />
+      }}
+    </AccordeonContext.Consumer>
+  )
+}
 
 export class Accordeon extends Component {
   static Item = Item
   static Header = Header
   static Content = Content
 
+  state = {
+    selectedItem: 0,
+  }
+
   render() {
+    const { selectedItem } = this.state
     const { children } = this.props
     const items = React.Children.toArray(children).filter(
       child => child.type === Accordeon.Item,
     )
 
     return (
-      <Fragment>
-        {items.map(item => (
-          <Marger bottom="1.5">{item}</Marger>
+      <AccordeonContext.Provider value={selectedItem}>
+        {items.map((item, index) => (
+          <Marger key={index} bottom={index === items.length - 1 ? 0 : 1.5}>
+            {React.cloneElement(item, { id: index })}
+          </Marger>
         ))}
-      </Fragment>
+      </AccordeonContext.Provider>
     )
   }
 }
