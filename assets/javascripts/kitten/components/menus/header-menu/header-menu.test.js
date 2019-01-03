f4053d6c4edacb0021d08307f6e6cb9c1a0040e4
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2019-01-03 12:55:27 +0100

    Tests

---
+++ assets/javascripts/kitten/components/menus/header-menu/header-menu.test.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import { HeaderMenu } from './index'
+
+const CustomComponent = () => <a href="#custom-component">Custom component</a>
+
+describe('<HeaderMenu />', () => {
+  let component
+  // let handleChange
+
+  describe('with three items', () => {
+    beforeEach(() => {
+      component = renderer
+        .create(
+          <HeaderMenu>
+            <HeaderMenu.Item href="#">Item 1</HeaderMenu.Item>
+            <HeaderMenu.Item href="#" modifier="light">
+              Item 2
+            </HeaderMenu.Item>
+            <HeaderMenu.Item href="#" borderTop>
+              Item 3
+            </HeaderMenu.Item>
+          </HeaderMenu>,
+        )
+        .toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with one selected item', () => {
+    beforeEach(() => {
+      component = renderer
+        .create(
+          <HeaderMenu>
+            <HeaderMenu.Item href="#">Item 1</HeaderMenu.Item>
+            <HeaderMenu.Item href="#" isSelected>
+              Item 2
+            </HeaderMenu.Item>
+            <HeaderMenu.Item href="#">Item 3</HeaderMenu.Item>
+          </HeaderMenu>,
+        )
+        .toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with one custom item', () => {
+    beforeEach(() => {
+      component = renderer
+        .create(
+          <HeaderMenu>
+            <HeaderMenu.Item href="#">Item 1</HeaderMenu.Item>
+            <HeaderMenu.Item href="#">Item 2</HeaderMenu.Item>
+            <HeaderMenu.Item>
+              <CustomComponent />
+            </HeaderMenu.Item>
+          </HeaderMenu>,
+        )
+        .toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with border to right', () => {
+    beforeEach(() => {
+      component = renderer
+        .create(
+          <HeaderMenu selectedBorderSide="right">
+            <HeaderMenu.Item href="#">Item 1</HeaderMenu.Item>
+            <HeaderMenu.Item href="#">Item 2</HeaderMenu.Item>
+            <HeaderMenu.Item href="#">Item 3</HeaderMenu.Item>
+          </HeaderMenu>,
+        )
+        .toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+})
