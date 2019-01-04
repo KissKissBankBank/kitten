dec7fae536ccbcd59e4f6cbb71c5cca39a50d0c7
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2019-01-04 12:16:00 +0100

    Use typography constant

--- assets/javascripts/kitten/components/menus/header-menu/components/item.js
+++ assets/javascripts/kitten/components/menus/header-menu/components/item.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import COLORS from '../../../../constants/colors-config'
+import TYPOGRAPHY from '../../../../constants/typography-config'
 import { pxToRem } from '../../../../helpers/utils/typography'
 import { Context } from './context'
 
@@ -12,8 +13,7 @@ const StyledItem = styled.div`
   border-left: 1px solid ${COLORS.line1};
   background-color: ${COLORS.background2};
 
-  font-family: "Maax", Helvetica, Arial, sans-serif;
-  font-weight: 500;
+  ${TYPOGRAPHY.fontStyles.regular}
   font-size: ${pxToRem(12)};
   line-height: 1;
   text-decoration: none;
