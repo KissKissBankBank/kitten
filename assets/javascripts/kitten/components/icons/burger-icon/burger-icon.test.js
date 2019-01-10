7742989e30c7ade2389014abec681b5c2b02b888
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-01-10 12:17:15 +0100

    Added tests

---
+++ assets/javascripts/kitten/components/icons/burger-icon/burger-icon.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { BurgerIcon } from './index'
+
+describe('<BurgerIcon />', () => {
+  describe('default', () => {
+    it('matches the snapshot', () => {
+      const component = renderer
+        .create(<BurgerIcon width="12" height="10" />)
+        .toJSON()
+
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with the prop `isActive` set to `true`', () => {
+    it('matches the snapshot', () => {
+      const component = renderer
+        .create(<BurgerIcon width="12" height="10" isActive="isActive" />)
+        .toJSON()
+
+      expect(component).toMatchSnapshot()
+    })
+  })
+})
