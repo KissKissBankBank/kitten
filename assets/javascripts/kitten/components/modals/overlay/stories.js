636b0a28f9f54a1c3f976cae96ade80ff3c722c0
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-01-15 17:06:58 +0100

    Update stories.js

--- assets/javascripts/kitten/components/modals/overlay/stories.js
+++ assets/javascripts/kitten/components/modals/overlay/stories.js
@@ -2,9 +2,21 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean } from '@storybook/addon-knobs'
 import { Overlay } from './index'
+import { Marger } from '../../layout/marger'
+import { Container } from '../../grid/container'
+import { Button } from '../../buttons/button'
+
+const CLICK_EVENT = 'event:toggle'
+
+const handleClick = () => window.dispatchEvent(new Event(CLICK_EVENT))
 
 storiesOf('Modals/Overlay', module)
   .addDecorator(withKnobs)
-  .add('default', () => {
-    return <Overlay isActive={boolean('isActive?', false)} />
-  })
+  .add('default', () => (
+    <Container>
+      <Marger top="2">
+        <Overlay toggleEvent={CLICK_EVENT} zIndex={-1} />
+        <Button onClick={handleClick}>Toggle Overlay</Button>
+      </Marger>
+    </Container>
+  ))
