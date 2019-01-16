482f5f5981c221ad2720886d5f9797aff1832c48
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2019-01-16 12:26:00 +0100

    Update SearchIcon

---
+++ assets/javascripts/kitten/components/icons/search-icon/stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import { SearchIcon } from './index'
+import { Marger } from '../../layout/marger'
+import { Container } from '../../grid/container'
+
+storiesOf('Icons/SearchIcon', module)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    return (
+      <Container>
+        <Marger top="5" bottom="5">
+          <SearchIcon width="16" color={text('Color', '#333')} />
+        </Marger>
+      </Container>
+    )
+  })
