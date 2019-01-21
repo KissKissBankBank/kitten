22e37f48d7411309aeb85365175969281bb1231d
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-01-21 17:14:10 +0100

    Added styles for specific items

--- assets/javascripts/kitten/karl/pages/contribute/thanks/thanks.stories.js
+++ assets/javascripts/kitten/karl/pages/contribute/thanks/thanks.stories.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import styled, { css } from 'styled-components'
+
+import { pxToRem } from '../../../../helpers/utils/typography'
+import { ScreenConfig } from '../../../../constants/screen-config'
+
 import { Container } from '../../../../components/grid/container'
 import { Grid, GridCol } from '../../../../components/grid/grid'
 import { Marger } from '../../../../components/layout/marger'
 
+import { RocketIllustration } from '../../../../components/illustrations/rocket-illustration'
 import { Title } from '../../../../components/typography/title'
 import { Text } from '../../../../components/typography/text'
 
@@ -16,10 +22,39 @@ import {
 } from '../../../../components/buttons/social-button-icon-words'
 import { EmailIcon } from '../../../../components/icons/email-icon'
 
-import { Carousel } from '../../../../components/carousel/carousel'
-import { CrowdfundingCard } from '../../../../components/cards/crowdfunding-card'
-
 import ProjectsCarousel from '../../common/projects-carousel'
+
+const FlexTitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  h1 {
+    margin-left: ${pxToRem(40)};
+  }
+
+  @media screen and (max-width: ${ScreenConfig.XXS.max}px) {
+    flex-direction: column;
+
+    h1 {
+      margin-left: 0;
+      text-align: center;
+    }
+  }
+`
+const FlexButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  > a {
+    margin-bottom: ${pxToRem(20)};
+  }
+
+  @media screen and (min-width: ${ScreenConfig.XS.min}px) {
+    max-width: ${pxToRem(325)};
+  }
+`
 
 storiesOf('Pages/Contribute', module).add('Thanks', () => {
   return (
@@ -34,52 +69,58 @@ storiesOf('Pages/Contribute', module).add('Thanks', () => {
             col-l="6"
             offset-l="3"
           >
-            <Marger bottom="3">
-              <Title modifier="tertiary">
+            <FlexTitleContainer>
+              <RocketIllustration />
+              <Title modifier="tertiary" tag="h1">
                 Bravo, grâce à vous <br />
                 la collecte progresse !
               </Title>
-            </Marger>
-            <Marger bottom="4">
+            </FlexTitleContainer>
+            <Marger top="3" bottom="4">
               <Text>
                 Vos contreparties seront délivrées par le créateur quand la
-                collecte sera réussie. Vous pourrez également lui demander un
-                reçu de don.
+                collecte sera réussie.
+                <br />
+                Vous pourrez également lui demander un reçu de don.
               </Text>
             </Marger>
+
             <Marger bottom="8">
-              <Title modifier="quinary">
+              <Title modifier="quinary" tag="h2">
                 Postez un message de soutien sur la page du projet
               </Title>
-              <Marger top="3" bottom="3">
+              <Marger top="3">
                 <TextInput
                   tag="textarea"
+                  rows="4"
                   placeholder="Laissez un message au porteur de projet…"
                 />
               </Marger>
-              <Button modifier="helium">Valider</Button>
+              <Marger top="3" className="k-u-align-center@s-down">
+                <Button modifier="helium" size="big">
+                  Valider
+                </Button>
+              </Marger>
             </Marger>
 
             <Marger bottom="8">
               <Marger bottom="3">
-                <Title modifier="quinary">Faites connaître le projet</Title>
+                <Title modifier="quinary" tag="h2">
+                  Faites connaître le projet
+                </Title>
               </Marger>
-              <Marger bottom="2">
-                <FacebookButtonIconWords>
+              <FlexButtonsContainer>
+                <FacebookButtonIconWords tag="a">
                   Partager sur Facebook
                 </FacebookButtonIconWords>
-              </Marger>
-              <Marger bottom="2">
-                <TwitterButtonIconWords>
+                <TwitterButtonIconWords tag="a">
                   Partager sur Twitter
                 </TwitterButtonIconWords>
-              </Marger>
-              <Marger bottom="9">
-                <Button modifier="lithium" size="big" icon={true}>
+                <Button modifier="lithium" size="big" icon={true} tag="a">
                   <EmailIcon className="k-Button__icon" />
                   Partager par email
                 </Button>
-              </Marger>
+              </FlexButtonsContainer>
             </Marger>
           </GridCol>
         </Grid>
