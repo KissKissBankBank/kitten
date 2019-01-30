6eb324aac1b84cae1c4a07ea1f2fa5abd6969b9c
hugo houyez <hugohconcordia@gmail.com>
2019-01-30 11:36:15 +0100

    replace fb bg icon with simple fb icon

--- assets/javascripts/kitten/components/buttons/social-button-icon-words.js
+++ assets/javascripts/kitten/components/buttons/social-button-icon-words.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button } from '../../components/buttons/button'
-import { FacebookIconWithBackground } from '../../components/icons/facebook-icon-with-background'
+import { FacebookIcon } from '../../components/icons/facebook-icon'
 import { TwitterIcon } from '../../components/icons/twitter-icon'
 import { LinkedinIcon } from '../../components/icons/linkedin-icon'
 
 export const FacebookButtonIconWords = ({ children, ...props }) => (
   <Button icon modifier="facebook" {...props}>
-    <FacebookIconWithBackground className="k-Button__icon k-Button__icon--facebook" />
+    <FacebookIcon className="k-Button__icon" />
     {children}
   </Button>
 )
