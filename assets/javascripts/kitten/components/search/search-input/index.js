bc84e758d9e2cb4980397e1aa92898e4c2b852a5
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-01-31 16:23:32 +0100

    Set `&::-ms-clear` to `display: none`.

--- assets/javascripts/kitten/components/search/search-input/index.js
+++ assets/javascripts/kitten/components/search/search-input/index.js
@@ -33,7 +33,8 @@ export const SearchInput = styled.input.attrs({ type: 'search' })`
   &::-webkit-search-decoration,
   &::-webkit-search-cancel-button,
   &::-webkit-search-results-button,
-  &::-webkit-search-results-decoration {
+  &::-webkit-search-results-decoration,
+  &::-ms-clear {
     display: none;
   }
 `
