b244c8f3841bc31ea026c6e642f7f04042277e38
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-02-05 16:56:36 +0100

    Better stories

--- assets/javascripts/kitten/components/grid/scrollable-container/stories.js
+++ assets/javascripts/kitten/components/grid/scrollable-container/stories.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { ScrollableContainer } from './index'
+import { Container } from '../../../components/grid/container'
+import COLORS from '../../../constants/colors-config'
 
 storiesOf('Grid/ScrollableContainer', module).add(
   'default',
   withInfo('common info')(() => (
-    <ScrollableContainer>
-      <div
-        style={{
-          height: '100px',
-          backgroundColor: '#f00',
-        }}
-      >
-        OIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOI
-      </div>
-    </ScrollableContainer>
+    <Container>
+      <ScrollableContainer>
+        <div
+          style={{
+            height: '100px',
+            backgroundColor: COLORS.line1,
+          }}
+        >
+          OIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOIOI
+        </div>
+      </ScrollableContainer>
+    </Container>
   )),
 )
