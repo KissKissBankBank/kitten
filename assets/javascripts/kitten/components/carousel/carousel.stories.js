1c3e20265734c091d4ac6728a7fcdaa53926dc5f
Morgan JARRY <morgan.jarry@gmail.com>
2019-02-13 16:54:04 +0100

    Add story

---
+++ assets/javascripts/kitten/components/carousel/carousel.stories.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs, text, number, object } from '@storybook/addon-knobs'
+import { CrowdfundingCard } from '../../components/cards/crowdfunding-card'
+import { Carousel } from '../../components/carousel/carousel'
+import { CONTAINER_PADDING } from '../../constants/grid-config'
+import { Marger } from '../../components/layout/marger'
+import { Container } from '../../components/grid/container'
+
+const StoryContainer = ({ children }) => (
+  <Container>
+    <Marger top="5" bottom="5">
+      {children}
+    </Marger>
+  </Container>
+)
+
+const data = [
+  {
+    title: 'Item A',
+    imageSrc: 'http://placekitten.com/500/300',
+    thumbSrc: 'http://placekitten.com/80/80',
+  },
+  {
+    title: 'Item B',
+    imageSrc: 'http://placekitten.com/501/301',
+    thumbSrc: 'http://placekitten.com/81/81',
+  },
+  {
+    title: 'Item C',
+    imageSrc: 'http://placekitten.com/502/302',
+    thumbSrc: 'http://placekitten.com/82/82',
+  },
+  {
+    title: 'Item D',
+    imageSrc: 'http://placekitten.com/503/303',
+    thumbSrc: 'http://placekitten.com/83/83',
+  },
+  {
+    title: 'Item E',
+    imageSrc: 'http://placekitten.com/504/304',
+    thumbSrc: 'http://placekitten.com/84/84',
+  },
+]
+
+storiesOf('Carousel/ProjectCarousel', module)
+  .addDecorator(withKnobs)
+  .add('default', () => (
+    <StoryContainer>
+      <Carousel
+        itemMinWidth={number('itemMinWidth', 280)}
+        baseItemMarginBetween={number(
+          'baseItemMarginBetween',
+          CONTAINER_PADDING,
+        )}
+        data={data}
+        renderItem={({ item }) => (
+          <CrowdfundingCard
+            href="#"
+            imageProps={{
+              src: item.imageSrc,
+              alt: 'Image alt',
+              backgroundColor: '#d8d8d8',
+              color: '#333',
+            }}
+            avatarProps={{
+              src: item.thumbSrc,
+              alt: 'Avatar alt',
+            }}
+            ownerTitle="Title"
+            ownerDescription="Custom description"
+            titleProps={{
+              tag: 'h4',
+            }}
+            cardTitle={item.title}
+            cardSubTitle="Custom subtitle"
+            titlesMinHeight
+            progress="84"
+            state="Custom state"
+          />
+        )}
+      />
+    </StoryContainer>
+  ))
