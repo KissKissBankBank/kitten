3e3f2029aa5afc781bcbbab2e75208b46c34f505
Camille Delbove <camille.delbove@gmail.com>
2019-02-27 12:01:09 +0100

    Clean `MarkdownLink`

--- assets/javascripts/kitten/components/pages/markdown/components/link.js
+++ assets/javascripts/kitten/components/pages/markdown/components/link.js
@@ -2,13 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-export const MarkdownLink = props => {
-  return (
-    <a href={props.href} className="k-Paragraph__link">
-      {props.children}
-    </a>
-  )
-}
+export const MarkdownLink = props => (
+  <a href={props.href} className="k-Paragraph__link">
+    {props.children}
+  </a>
+)
 
 MarkdownLink.propTypes = {
   href: PropTypes.string.isRequired,
