31485e44b399c0378cf48dad6f2449fbf1ec02df
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-03-04 17:51:16 +0100

    Added tests

--- assets/javascripts/kitten/components/grid/sticky-container/index.js
+++ assets/javascripts/kitten/components/grid/sticky-container/index.js
@@ -142,7 +142,9 @@ export class StickyContainer extends Component {
   }
 
   componentDidMount() {
-    let containerHeight = this.currentStickyContainer.current.clientHeight
+    let containerHeight = this.currentStickyContainer.current
+      ? this.currentStickyContainer.current.clientHeight
+      : 0
     this.setState({ containerHeight: containerHeight })
 
     if (
