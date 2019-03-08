1ff32fab7ccea588bb2784c77259b6acd4e29209
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2019-03-08 16:56:46 +0100

    Initialize index exportation

---
+++ assets/javascripts/kitten/index.js
@@ -0,0 +1,31 @@
+// Accordeon
+export { Accordeon } from './components/accordeon'
+
+// Box
+export { DocLinkBox } from './components/box/doc-link-box'
+export { InformationBox } from './components/box/information-box'
+export { LinkBox } from './components/box/link-box'
+
+// Buttons
+export { Button } from './components/buttons/button/button'
+export { ButtonIcon } from './components/buttons/button-icon'
+export {
+  ButtonImageWithTextAndBadge,
+} from './components/buttons/button-image-with-text-and-badge'
+export { ButtonImage } from './components/buttons/button-image'
+export {
+  ButtonQuestionMarkIcon,
+} from './components/buttons/button-question-mark-icon'
+export { CloseButton } from './components/buttons/close-button'
+export {
+  SocialButtonIconWords,
+} from './components/buttons/social-button-icon-words'
+export { SocialButtonIcon } from './components/buttons/social-button-icon'
+export { TagButton } from './components/buttons/tag-button'
+
+// Cards
+export { RewardCard } from './components/cards/reward-card'
+export { SimpleCard } from './components/cards/simple-card'
+export { TeamCard } from './components/cards/team-card'
+
+// To be continued…
--- src/index.js
+++ src/index.js
@@ -1,31 +1,133 @@
-var path = require('path')
+"use strict";
 
-var getScssPaths = function () {
-  var kittenStylesheetsPath = path.join(
-    __dirname,
-    '../assets/stylesheets'
-  )
-  var karlStylesheetsPath = path.join(
-    __dirname,
-    '../app/assets/stylesheets'
-  )
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Accordeon", {
+  enumerable: true,
+  get: function get() {
+    return _accordeon.Accordeon;
+  }
+});
+Object.defineProperty(exports, "DocLinkBox", {
+  enumerable: true,
+  get: function get() {
+    return _docLinkBox.DocLinkBox;
+  }
+});
+Object.defineProperty(exports, "InformationBox", {
+  enumerable: true,
+  get: function get() {
+    return _informationBox.InformationBox;
+  }
+});
+Object.defineProperty(exports, "LinkBox", {
+  enumerable: true,
+  get: function get() {
+    return _linkBox.LinkBox;
+  }
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _button.Button;
+  }
+});
+Object.defineProperty(exports, "ButtonIcon", {
+  enumerable: true,
+  get: function get() {
+    return _buttonIcon.ButtonIcon;
+  }
+});
+Object.defineProperty(exports, "ButtonImageWithTextAndBadge", {
+  enumerable: true,
+  get: function get() {
+    return _buttonImageWithTextAndBadge.ButtonImageWithTextAndBadge;
+  }
+});
+Object.defineProperty(exports, "ButtonImage", {
+  enumerable: true,
+  get: function get() {
+    return _buttonImage.ButtonImage;
+  }
+});
+Object.defineProperty(exports, "ButtonQuestionMarkIcon", {
+  enumerable: true,
+  get: function get() {
+    return _buttonQuestionMarkIcon.ButtonQuestionMarkIcon;
+  }
+});
+Object.defineProperty(exports, "CloseButton", {
+  enumerable: true,
+  get: function get() {
+    return _closeButton.CloseButton;
+  }
+});
+Object.defineProperty(exports, "SocialButtonIconWords", {
+  enumerable: true,
+  get: function get() {
+    return _socialButtonIconWords.SocialButtonIconWords;
+  }
+});
+Object.defineProperty(exports, "SocialButtonIcon", {
+  enumerable: true,
+  get: function get() {
+    return _socialButtonIcon.SocialButtonIcon;
+  }
+});
+Object.defineProperty(exports, "TagButton", {
+  enumerable: true,
+  get: function get() {
+    return _tagButton.TagButton;
+  }
+});
+Object.defineProperty(exports, "RewardCard", {
+  enumerable: true,
+  get: function get() {
+    return _rewardCard.RewardCard;
+  }
+});
+Object.defineProperty(exports, "SimpleCard", {
+  enumerable: true,
+  get: function get() {
+    return _simpleCard.SimpleCard;
+  }
+});
+Object.defineProperty(exports, "TeamCard", {
+  enumerable: true,
+  get: function get() {
+    return _teamCard.TeamCard;
+  }
+});
 
-  var modularscalePath = require.resolve('modularscale-sass')
+var _accordeon = require("./components/accordeon");
 
-  return [
-    kittenStylesheetsPath,
-    karlStylesheetsPath,
-    path.join(modularscalePath, '..'),
-  ]
-}
+var _docLinkBox = require("./components/box/doc-link-box");
 
-var getImagesPaths = function () {
-  return [
-    path.join(__dirname, '../assets/images'),
-  ]
-}
+var _informationBox = require("./components/box/information-box");
 
-module.exports = {
-  scssPaths: getScssPaths(),
-  imagesPaths: getImagesPaths(),
-}
+var _linkBox = require("./components/box/link-box");
+
+var _button = require("./components/buttons/button/button");
+
+var _buttonIcon = require("./components/buttons/button-icon");
+
+var _buttonImageWithTextAndBadge = require("./components/buttons/button-image-with-text-and-badge");
+
+var _buttonImage = require("./components/buttons/button-image");
+
+var _buttonQuestionMarkIcon = require("./components/buttons/button-question-mark-icon");
+
+var _closeButton = require("./components/buttons/close-button");
+
+var _socialButtonIconWords = require("./components/buttons/social-button-icon-words");
+
+var _socialButtonIcon = require("./components/buttons/social-button-icon");
+
+var _tagButton = require("./components/buttons/tag-button");
+
+var _rewardCard = require("./components/cards/reward-card");
+
+var _simpleCard = require("./components/cards/simple-card");
+
+var _teamCard = require("./components/cards/team-card");
