eee3de10f8aedd5d3badb7154117a657704cb1c1
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2019-03-20 12:51:33 +0100

    Add more components

--- assets/javascripts/kitten/components/carousel/tab-bar-carousel/index.js
+++ assets/javascripts/kitten/components/carousel/tab-bar-carousel/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { NavTabCarousel as NewNavTabCarousel } from 'carousel/nav-tab-carousel'
-import { Deprecated } from '../../helpers/utils/deprecated'
+import { NavTabCarousel as NewNavTabCarousel } from '../nav-tab-carousel'
+import { Deprecated } from '../../../helpers/utils/deprecated'
 
 export const NavTabCarousel = props => {
   const message = `DEPRECATED:
