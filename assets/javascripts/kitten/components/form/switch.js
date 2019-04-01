7bd955d24a1aada8b0f770f7119bc468e93dff42
olivier chuiton <olivier.chuiton@gmail.com>
2019-04-01 17:26:37 +0200

    gogo gadgeto composant-fonction!

--- assets/javascripts/kitten/components/form/switch.js
+++ assets/javascripts/kitten/components/form/switch.js
@@ -1,32 +1,28 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 
-export class Switch extends PureComponent {
-  render() {
-    const { id, isChecked, disabled, label, big, ...others } = this.props
+export const Switch = props => {
+  const { id, isChecked, disabled, label, big, ...others } = props
 
-    const switchClassNames = classNames('k-Switch__label', {
-      'k-Switch__label--big': big,
-    })
+  return (
+    <div className="k-Switch">
+      <input
+        {...others}
+        id={id}
+        type="checkbox"
+        className="k-Switch__input"
+        defaultChecked={isChecked}
+        disabled={disabled}
+      />
 
-    return (
-      <div className="k-Switch">
-        <input
-          {...others}
-          id={id}
-          type="checkbox"
-          className="k-Switch__input"
-          defaultChecked={isChecked}
-          disabled={disabled}
-        />
-
-        <label htmlFor={id} className={switchClassNames}>
-          {label}
-        </label>
-      </div>
-    )
-  }
+      <label
+        htmlFor={id}
+        className={classNames('k-Switch__label', {
+          'k-Switch__label--big': big,
+        })}
+      >
+        {label}
+      </label>
+    </div>
+  )
 }
-
-// DEPRECATED: do not use default export.
-export default Switch
--- src/components/form/switch.js
+++ src/components/form/switch.js
@@ -1,73 +1,41 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.Switch = void 0;
+exports.Switch = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
 var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
 
-var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
-
-var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
-
-var _possibleConstructorReturn2 = _interopRequireDefault(require("@babel/runtime/helpers/possibleConstructorReturn"));
-
-var _getPrototypeOf2 = _interopRequireDefault(require("@babel/runtime/helpers/getPrototypeOf"));
-
-var _inherits2 = _interopRequireDefault(require("@babel/runtime/helpers/inherits"));
-
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 
 var _classnames = _interopRequireDefault(require("classnames"));
 
-var Switch =
-/*#__PURE__*/
-function (_PureComponent) {
-  (0, _inherits2.default)(Switch, _PureComponent);
-
-  function Switch() {
-    (0, _classCallCheck2.default)(this, Switch);
-    return (0, _possibleConstructorReturn2.default)(this, (0, _getPrototypeOf2.default)(Switch).apply(this, arguments));
-  }
-
-  (0, _createClass2.default)(Switch, [{
-    key: "render",
-    value: function render() {
-      var _this$props = this.props,
-          id = _this$props.id,
-          isChecked = _this$props.isChecked,
-          disabled = _this$props.disabled,
-          label = _this$props.label,
-          big = _this$props.big,
-          others = (0, _objectWithoutProperties2.default)(_this$props, ["id", "isChecked", "disabled", "label", "big"]);
-      var switchClassNames = (0, _classnames.default)('k-Switch__label', {
-        'k-Switch__label--big': big
-      });
-      return _react.default.createElement("div", {
-        className: "k-Switch"
-      }, _react.default.createElement("input", (0, _extends2.default)({}, others, {
-        id: id,
-        type: "checkbox",
-        className: "k-Switch__input",
-        defaultChecked: isChecked,
-        disabled: disabled
-      })), _react.default.createElement("label", {
-        htmlFor: id,
-        className: switchClassNames
-      }, label));
-    }
-  }]);
-  return Switch;
-}(_react.PureComponent); // DEPRECATED: do not use default export.
-
+var Switch = function Switch(props) {
+  var id = props.id,
+      isChecked = props.isChecked,
+      disabled = props.disabled,
+      label = props.label,
+      big = props.big,
+      others = (0, _objectWithoutProperties2.default)(props, ["id", "isChecked", "disabled", "label", "big"]);
+  return _react.default.createElement("div", {
+    className: "k-Switch"
+  }, _react.default.createElement("input", (0, _extends2.default)({}, others, {
+    id: id,
+    type: "checkbox",
+    className: "k-Switch__input",
+    defaultChecked: isChecked,
+    disabled: disabled
+  })), _react.default.createElement("label", {
+    htmlFor: id,
+    className: (0, _classnames.default)('k-Switch__label', {
+      'k-Switch__label--big': big
+    })
+  }, label));
+};
 
 exports.Switch = Switch;
-var _default = Switch;
-exports.default = _default;
