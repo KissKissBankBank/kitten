49f07aaa6782ce23bb6ba179a4fd1be47d2dec92
olivier chuiton <olivier.chuiton@gmail.com>
2019-04-01 17:57:22 +0200

    indentation

--- assets/javascripts/kitten/components/form/switch.js
+++ assets/javascripts/kitten/components/form/switch.js
@@ -2,24 +2,23 @@ import React from 'react'
 import classNames from 'classnames'
 
 export const Switch = ({ id, isChecked, disabled, label, big, ...others }) => (
+  <div className="k-Switch">
+    <input
+      {...others}
+      id={id}
+      type="checkbox"
+      className="k-Switch__input"
+      defaultChecked={isChecked}
+      disabled={disabled}
+    />
 
-    <div className="k-Switch">
-      <input
-        {...others}
-        id={id}
-        type="checkbox"
-        className="k-Switch__input"
-        defaultChecked={isChecked}
-        disabled={disabled}
-      />
-
-      <label
-        htmlFor={id}
-        className={classNames('k-Switch__label', {
-          'k-Switch__label--big': big,
-        })}
-      >
-        {label}
-      </label>
-    </div>
+    <label
+      htmlFor={id}
+      className={classNames('k-Switch__label', {
+        'k-Switch__label--big': big,
+      })}
+    >
+      {label}
+    </label>
+  </div>
 )
