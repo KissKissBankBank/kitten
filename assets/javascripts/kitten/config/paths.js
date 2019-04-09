d5807addcfda8ddb778da22ca079748af241f628
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2019-04-09 16:06:12 +0200

    Add paths tests

--- assets/javascripts/kitten/config/paths.js
+++ assets/javascripts/kitten/config/paths.js
@@ -2,7 +2,6 @@ import path from 'path'
 
 export const getScssPaths = () => {
   const kittenStylesheetsPath = path.join(__dirname, '../../assets/stylesheets')
-
   const modularscalePath = require.resolve('modularscale-sass')
 
   return [kittenStylesheetsPath, path.join(modularscalePath, '..')]
