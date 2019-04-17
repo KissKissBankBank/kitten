d3b7c41001c711467660285d46be5a6a5072692c
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2019-04-17 17:25:41 +0200

    add icon feature on badge

---
+++ assets/javascripts/kitten/components/icons/cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cart = props => (
+  <svg width={17} height={16} {...props}>
+    <path
+      d="M4.5 6V4a4 4 0 1 1 8 0v2h4l-2 10h-12L.5 6h4zm2 0h4V4a2 2 0 0 0-4 0v2z"
+      fill="#FFF"
+      fillRule="evenodd"
+    />
+  </svg>
+)
+
+export default Cart
--- assets/javascripts/kitten/components/notifications/badge.js
+++ assets/javascripts/kitten/components/notifications/badge.js
@@ -1,19 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
 import classNames from 'classnames'
 
-export class Badge extends Component {
-  render() {
-    const { className, spaced, ...others } = this.props
+const StyledBadge = styled.span`
+  ${({ color }) =>
+    color &&
+    css`
+      background-color: ${color};
+    `}
+  ${({ withIcon }) =>
+    withIcon &&
+    css`
+      position: absolute;
+      margin-left: -8px;
+      margin-top: -4px;
+    `}
+`
 
-    const badgeClassName = classNames('k-Badge', className, {
-      'k-Badge--spaced': spaced,
-    })
+export const Badge = ({ className, spaced, Icon, ...others }) => {
+  return (
+    <div>
+      {Icon && <Icon />}
+      <StyledBadge
+        role="alert"
+        className={classNames('k-Badge', className, {
+          'k-Badge--spaced': spaced,
+        })}
+        withIcon={Icon !== null}
+        {...others}
+      />
+    </div>
+  )
+}
 
-    return <span role="alert" className={badgeClassName} {...others} />
-  }
+Badge.propTypes = {
+  className: PropTypes.string,
+  spaced: PropTypes.bool,
+  color: PropTypes.string,
+  Icon: PropTypes.func,
 }
 
 Badge.defaultProps = {
   className: null,
   spaced: false,
+  Icon: null,
+  color: null,
 }
