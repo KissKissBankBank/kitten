4b47cad805420e0cd5a059bf7b021f5316f8b721
Camille Delbove <camille.delbove@gmail.com>
2019-04-18 11:58:14 +0200

    Update `content` and `footer`

--- assets/javascripts/kitten/components/cards/reward-edition/components/content.js
+++ assets/javascripts/kitten/components/cards/reward-edition/components/content.js
@@ -1,4 +1,28 @@
 import React, { PureComponent } from 'react'
 import styled from 'styled-components'
+import { pxToRem } from '../../../../helpers/utils/typography'
+import COLORS from '../../../../constants/colors-config'
 
-export class Content extends PureComponent {}
+const borderWidth = pxToRem(2)
+const borderColor = COLORS.line1
+
+const StyledCard = styled.div`
+  width: 100%;
+  height: ${pxToRem(500)};
+  border-right-width: ${borderWidth};
+  border-left-width: ${borderWidth};
+  border-right-style: solid;
+  border-left-style: solid;
+  border-right-color: ${borderColor};
+  border-left-color: ${borderColor};
+`
+
+export class Content extends PureComponent {
+  render() {
+    return (
+      <>
+        <StyledCard />
+      </>
+    )
+  }
+}
--- assets/javascripts/kitten/components/cards/reward-edition/components/footer.js
+++ assets/javascripts/kitten/components/cards/reward-edition/components/footer.js
@@ -1,4 +1,25 @@
 import React, { PureComponent } from 'react'
 import styled from 'styled-components'
+import { pxToRem } from '../../../../helpers/utils/typography'
+import COLORS from '../../../../constants/colors-config'
 
-export class Footer extends PureComponent {}
+const borderWidth = pxToRem(2)
+const borderColor = COLORS.line1
+
+const StyledCard = styled.div`
+  width: 100%;
+  height: ${pxToRem(150)};
+  border-width: ${borderWidth};
+  border-style: solid;
+  border-color: ${borderColor};
+`
+
+export class Footer extends PureComponent {
+  render() {
+    return (
+      <>
+        <StyledCard />
+      </>
+    )
+  }
+}
