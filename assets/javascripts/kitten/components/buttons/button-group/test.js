758a339fda24d3b325b0234dac97d2f3fb576805
Camille Delbove <camille.delbove@gmail.com>
2019-04-24 18:00:57 +0200

    Rename files and add info in storie

--- assets/javascripts/kitten/components/buttons/button-group/test.js
+++ assets/javascripts/kitten/components/buttons/button-group/test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import 'jest-styled-components'
-import { ButtonGroup } from './button-group'
+import { ButtonGroup } from './index'
 
 describe('<ButtonGroup />', () => {
   let component
