f5d3bfb2254940d06cae86c741fe9887c9737051
Morgan JARRY <morgan.jarry@gmail.com>
2019-06-03 17:52:31 +0200

    Remove dead test

--- assets/javascripts/kitten/components/buttons/button/test.js
+++ assets/javascripts/kitten/components/buttons/button/test.js
@@ -16,16 +16,6 @@ describe('<Button />', () => {
     })
   })
 
-  describe('with `isChecked` prop', () => {
-    beforeEach(() => {
-      component = renderer.create(<Button isChecked>MyButton</Button>).toJSON()
-    })
-
-    it('matches with snapshot', () => {
-      expect(component).toMatchSnapshot()
-    })
-  })
-
   describe('with `tiny` prop', () => {
     beforeEach(() => {
       component = renderer.create(<Button tiny>MyButton</Button>).toJSON()
