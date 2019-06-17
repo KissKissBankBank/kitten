7e64e0d5b394f33759dedc5fa6df92e367a25c4b
Camille Delbove <camille.delbove@gmail.com>
2019-06-17 18:07:52 +0200

    Update snap

--- assets/javascripts/kitten/components/form/text-input/test.js
+++ assets/javascripts/kitten/components/form/text-input/test.js
@@ -1,4 +1,48 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import 'jest-styled-components'
-import { TextInput } from './text-input'
+import { TextInput } from './index'
+
+describe('<TextInput />', () => {
+  let component
+
+  describe('with `valid` prop', () => {
+    beforeEach(() => {
+      component = renderer.create(<TextInput valid />).toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with `error` prop', () => {
+    beforeEach(() => {
+      component = renderer.create(<TextInput error />).toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with `disabled` prop', () => {
+    beforeEach(() => {
+      component = renderer.create(<TextInput disabled />).toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('with `tiny` prop', () => {
+    beforeEach(() => {
+      component = renderer.create(<TextInput tiny />).toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+})
