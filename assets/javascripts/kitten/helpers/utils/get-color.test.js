2d1d4f7f072b3e32ef3aa70577f012b4c5d32b8a
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-06-21 15:03:57 +0200

    Add tests

---
+++ assets/javascripts/kitten/helpers/utils/get-color.test.js
@@ -0,0 +1,32 @@
+import { getColor } from '../../helpers/utils/get-color'
+
+describe('getColor helper', () => {
+  describe('getColor("primary3")', () => {
+    const color = getColor('primary3')
+
+    it('returns "#0496cc"', () => {
+      expect(color).toEqual('#0496cc')
+    })
+  })
+  describe('getColor("background")', () => {
+    const color = getColor('background')
+
+    it('returns "#fff"', () => {
+      expect(color).toEqual('#fff')
+    })
+  })
+  describe('getColor("greenLighter", {invert:true})', () => {
+    const color = getColor('greenLighter', { invert: true })
+
+    it('returns "#000"', () => {
+      expect(color).toEqual('#000')
+    })
+  })
+  describe('getColor("fake color name")', () => {
+    const color = getColor('fake color name')
+
+    it('returns `undefined`', () => {
+      expect(color).toEqual(undefined)
+    })
+  })
+})
