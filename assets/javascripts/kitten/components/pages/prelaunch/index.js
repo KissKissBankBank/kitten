426be09c0418d9444c072242333ee47e3d2585d2
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2019-06-24 13:58:06 +0200

    add background and responsive

--- assets/javascripts/kitten/components/pages/prelaunch/index.js
+++ assets/javascripts/kitten/components/pages/prelaunch/index.js
@@ -13,20 +13,27 @@ const User = styled(({ className }) => (
 
 const Container = styled.div`
   position: absolute;
-  background-color: #caf4fe;
+  background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjg4MCIgaGVpZ2h0PSIxODAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48ZGVmcz48cGF0aCBpZD0iYSIgZD0iTTAgLjA4MWgyODgwdjE4MDBIMHoiLz48L2RlZnM+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBmaWxsPSIjMDAwIiBkPSJNMCAwaDI4ODB2MTgwMEgweiIvPjxnIG9wYWNpdHk9Ii42Ij48bWFzayBpZD0iYiIgZmlsbD0iI2ZmZiI+PHVzZSB4bGluazpocmVmPSIjYSIvPjwvbWFzaz48dXNlIGZpbGw9IiNDQUY0RkUiIGZpbGwtcnVsZT0ibm9uemVybyIgeGxpbms6aHJlZj0iI2EiLz48ZyBzdHJva2U9IiMyMjIiIHN0cm9rZS13aWR0aD0iMjMuNDQ4IiBtYXNrPSJ1cmwoI2IpIj48cGF0aCBmaWxsPSIjMTlCNEZBIiBkPSJNLTYwNS4yNjQtMzM2Ljg5Mkw4NDMuNzg5LTcyNS4xNjRsMzg4LjI3IDE0NDkuMDQzLTE0NDkuMDUzIDM4OC4yNzN6Ii8+PHBhdGggZmlsbD0icmVkIiBkPSJNLTIwMS41MzggMTEwOC4wMUwxMjMyLjA2IDcyMy44OGw3MTQuOTM1IDI2NjguMTcxLTE0MzMuNTk3IDM4NC4xMzF6Ii8+PHBhdGggZmlsbD0iI0ZGRUJFMCIgZD0iTTg1My42MjYtNjg4LjQ1NWwxNzc5LjQzOC00NzYuOEwzMTA5Ljg1OSA2MTQuMTdsLTE3NzkuNDM4IDQ3Ni44eiIvPjwvZz48L2c+PC9nPjwvc3ZnPg==);
+  background-size: cover;
+  background-position: center;
   width: 100%;
   height: 100%;
 `
 
 const Card = styled.div`
-  width: ${pxToRem(440)};
-  background-color: #fff;
   position: fixed;
+  margin: auto;
+  background-color: #fff;
+  text-align: center;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  text-align: center;
-  padding: 0 ${pxToRem(115)} ${pxToRem(40)};
+  width: 80%;
+  padding: 0 5% ${pxToRem(40)};
+  @media (min-width: 690px) {
+    width: ${pxToRem(440)};
+    padding: 0 ${pxToRem(115)} ${pxToRem(40)};
+  }
 `
 
 const TitleSeparator = styled(Separator)`
