805417fe62b1b95f4c048196fb0359c9c1c75221
Camille Delbove <camille.delbove@gmail.com>
2019-06-25 09:56:40 +0200

    Stories

--- assets/javascripts/kitten/components/form/autocomplete/stories.js
+++ assets/javascripts/kitten/components/form/autocomplete/stories.js
@@ -4,32 +4,23 @@ import styled from 'styled-components'
 import { withKnobs } from '@storybook/addon-knobs'
 import { Marger } from '../../layout/marger'
 import { Container } from '../../grid/container'
-import { Text } from '../../../components/typography/text'
-import { ButtonGroup } from '../../../components/buttons/button-group'
-import { Button } from '../../../components/buttons/button/button'
-import { ExpandBoard } from '../../../components/expandable/expand-board'
+import { TextInput } from '../../form/text-input'
 import { Grid, GridCol } from '../../grid/grid'
 import { ReactAutocomplete } from './index'
 import { pxToRem } from '../../../helpers/utils/typography'
-
-const StoryContainer = ({ children }) => (
-  <Container>
-    <Grid>
-      <GridCol col="8">
-        <Marger top="5" bottom="5">
-          {children}
-        </Marger>
-      </GridCol>
-    </Grid>
-  </Container>
-)
 
 storiesOf('Form/Autocomplete', module)
   .addDecorator(withKnobs)
   .add('default', () => {
     return (
-      <StoryContainer>
-        <ReactAutocomplete />
-      </StoryContainer>
+      <Container>
+        <Grid>
+          <GridCol col="8">
+            <Marger top="5" bottom="5">
+              <TextInput ReactAutocomplete />
+            </Marger>
+          </GridCol>
+        </Grid>
+      </Container>
     )
   })
