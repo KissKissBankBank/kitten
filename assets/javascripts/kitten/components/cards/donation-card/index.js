2d8c54339fe0fbb65aabd22ba13f276224e60289
Camille Delbove <camille.delbove@gmail.com>
2019-06-28 17:35:10 +0200

    Styled-components

--- assets/javascripts/kitten/components/cards/donation-card/index.js
+++ assets/javascripts/kitten/components/cards/donation-card/index.js
@@ -3,10 +3,9 @@ import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 import { Marger } from '../../../components/layout/marger'
 import { Grid, GridCol } from '../../../components/grid/grid'
-import { Title } from '../../../components/typography/title'
+import { Title as KittenTitle } from '../../../components/typography/title'
 import { HorizontalStroke } from '../../../components/layout/horizontal-stroke'
 import COLORS from '../../../constants/colors-config'
-import { mediaQueries } from '../../../hoc/media-queries'
 import { TextInputWithUnitForm } from '../../../components/form/text-input-with-unit-form'
 import { pxToRem, stepToRem } from '../../../helpers/utils/typography'
 
@@ -27,25 +26,37 @@ const StyledGrid = styled(Grid)`
   padding: 0 ${pxToRem(20)};
 `
 
+const StyledTitle = styled(({ version, ...others }) => (
+  <KittenTitle {...others} />
+))`
+  color: ${COLORS.font1};
+  text-align: center;
+  line-height: 1.2;
+  font-size: ${stepToRem(5)};
+
+  ${({ version }) =>
+    version === 'tiny' &&
+    css`
+      font-size: ${stepToRem(4)};
+    `}
+`
+
+const StyledHorizontalStroke = styled(HorizontalStroke)`
+  margin: 0 auto;
+`
+
 export class DonationCard extends PureComponent {
   static propTypes = {
     title: PropTypes.string.isRequired,
     titleTag: PropTypes.string,
     donationForm: PropTypes.object.isRequired,
     version: PropTypes.oneOf(['default', 'tiny']),
-    textInputWithUnitFormProps: PropTypes.shape({
-      buttonLabel: PropTypes.string,
-      isDisabled: PropTypes.bool,
-    }),
+    isDisabled: PropTypes.bool,
   }
 
   static defaultProps = {
     titleTag: 'h2',
     version: 'default',
-    textInputWithUnitFormProps: {
-      buttonLabel: 'Choose',
-      isDisabled: false,
-    },
   }
 
   render() {
@@ -55,38 +66,33 @@ export class DonationCard extends PureComponent {
       titleTag,
       version,
       donationForm,
-      textInputWithUnitFormProps,
       ...others
     } = this.props
 
-    const isTinyVersion = version === 'tiny' || viewportIsSOrLess
-
-    const gridColProps = isTinyVersion
-      ? {}
-      : { 'col-m': 10, 'offset-m': 1, 'col-l': 8, 'offset-l': 2 }
-
     return (
       <StyledCard>
-        <Marger bottom={isTinyVersion ? 3 : 4} top={isTinyVersion ? 3 : 4}>
+        <Marger
+          bottom={version === 'tiny' ? 3 : 4}
+          top={version === 'tiny' ? 3 : 4}
+        >
           <StyledGrid>
-            <GridCol {...gridColProps}>
+            <GridCol
+              col-m={version === 'tiny' ? 0 : 10}
+              offset-m={version === 'tiny' ? 0 : 1}
+              col-l={version === 'tiny' ? 0 : 8}
+              offset-l={version === 'tiny' ? 0 : 2}
+            >
               <Marger bottom="2">
-                <Title
-                  modifier={isTinyVersion ? 'quinary' : 'quaternary'}
-                  margin={false}
-                  tag={titleTag}
-                  style={styles.text}
-                >
+                <StyledTitle margin={false} tag={titleTag}>
                   {title}
-                </Title>
+                </StyledTitle>
               </Marger>
 
               <Marger top="2" bottom="3">
-                <HorizontalStroke size="big" style={styles.horizontalStroke} />
+                <StyledHorizontalStroke size="big" />
               </Marger>
 
               <TextInputWithUnitForm
-                {...textInputWithUnitFormProps}
                 {...donationForm}
                 align="center"
                 formIsDisabled={isDisabled}
@@ -98,30 +104,4 @@ export class DonationCard extends PureComponent {
       </StyledCard>
     )
   }
-}
-
-const styles = {
-  card: {
-    borderWidth: 2,
-    borderStyle: 'solid',
-    borderColor: COLORS.line1,
-
-    content: {
-      paddingLeft: 20,
-      paddingRight: 20,
-    },
-    isDisabled: {
-      filter: 'grayscale(1) opacity(.4)',
-      cursor: 'not-allowed',
-    },
-  },
-
-  text: {
-    color: COLORS.font1,
-    textAlign: 'center',
-  },
-
-  horizontalStroke: {
-    margin: '0 auto',
-  },
 }
--- assets/javascripts/kitten/components/cards/donation-card/stories.js
+++ assets/javascripts/kitten/components/cards/donation-card/stories.js
@@ -1,34 +1,72 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import { withKnobs, text, boolean, object } from '@storybook/addon-knobs'
-import { StyleRoot } from 'radium'
+import {
+  withKnobs,
+  text,
+  boolean,
+  object,
+  select,
+} from '@storybook/addon-knobs'
 import { DonationCard } from './index'
 import { Grid, GridCol } from '../../grid/grid'
 
+const versionOptions = {
+  Default: 'default',
+  Tiny: 'tiny',
+}
+
+const info = {
+  text: `
+    #DonationCard
+    &nbsp;
+
+    ## Import
+    ~~~js
+    import { DonationCard } from '@kisskissbankbank/kitten/src/components/cards/donation-card'
+    ~~~
+
+    ## Usage
+    &nbsp;
+
+    #### Default
+    ~~~js
+    <DonationCard />
+    ~~~
+
+   #### Version
+    ~~~js
+    <DonationCard version="default" />
+    <DonationCard version="tiny" />
+    ~~~
+  `,
+}
+
 storiesOf('Cards/DonationCard', module)
   .addDecorator(withKnobs)
-  .add(
-    'default',
-    withInfo('common info')(() => (
-      <StyleRoot>
-        <Grid>
-          <GridCol offset="1" col="10">
-            <DonationCard
-              title={text(
-                'Title Amount',
-                'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.',
-              )}
-              buttonLabel={text('Button', 'Choose')}
-              isDisabled={boolean('Is Disabled', false)}
-              amountLabel={text('Label Input', 'Enter your amount')}
-              amountPlaceholder={text('Placeholder input', 'Your amount')}
-              isDisabled={boolean('Is Disabled', false)}
-              isError={boolean('Is error', false)}
-              errorTag={text('Error tag', 'lorem ipsum dolor sit amet')}
-            />
-          </GridCol>
-        </Grid>
-      </StyleRoot>
-    )),
-  )
+  .addDecorator(withInfo)
+  .add('default', () => {
+    return (
+      <Grid style={{ marginTop: '3em' }}>
+        <GridCol offset="1" col="10">
+          <DonationCard
+            title={text(
+              'Title Amount',
+              'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.',
+            )}
+            isDisabled={boolean('Is Disabled', false)}
+            amountLabel={text('Label Input', 'Enter your amount')}
+            amountPlaceholder={text('Placeholder input', 'Your amount')}
+            isDisabled={boolean('Is Disabled', false)}
+            isError={boolean('Is error', false)}
+            version={select('Version', versionOptions, 'default')}
+            errorTag={text('Error tag', 'lorem ipsum dolor sit amet')}
+            donationForm={object('donationForm', {
+              inputUnit: '€',
+              buttonLabel: 'Choose',
+            })}
+          />
+        </GridCol>
+      </Grid>
+    )
+  })
