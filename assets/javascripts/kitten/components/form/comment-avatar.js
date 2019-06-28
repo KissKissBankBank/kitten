61a396c75a075caff8fbce71feafd9de57c9779f
Camille Delbove <camille.delbove@gmail.com>
2019-06-28 10:55:26 +0200

    Fix commentaires

--- assets/javascripts/kitten/components/form/comment-avatar.js
+++ assets/javascripts/kitten/components/form/comment-avatar.js
@@ -3,7 +3,7 @@ import { CommentAvatar } from '../../components/comments/comment-avatar'
 import { Deprecated } from '../../helpers/utils/deprecated'
 
 export const CommentAvatar = props => {
-  const message = `You can use the RewardComponent instead.
+  const message = `You can use the CommentAvatar instead.
     For more information: https://github.com/KissKissBankBank/../../blob/master/assets/javascripts/../../components/comments/comment-avatar.js`
 
   return (
--- assets/javascripts/kitten/components/form/comment-form.js
+++ assets/javascripts/kitten/components/form/comment-form.js
@@ -3,7 +3,7 @@ import { CommentForm } from '../../components/comments/comment-form'
 import { Deprecated } from '../../helpers/utils/deprecated'
 
 export const CommentForm = props => {
-  const message = `You can use the RewardComponent instead.
+  const message = `You can use the CommentForm instead.
     For more information: https://github.com/KissKissBankBank/../../blob/master/assets/javascripts/../../components/comments/comment-form.js`
 
   return (
