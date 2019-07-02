05eb82b1f55d8c8c32404425113f66731ff364bd
Camille Delbove <camille.delbove@gmail.com>
2019-07-02 16:58:34 +0200

    Add `ActionLinks`

--- assets/javascripts/kitten/karl/pages/comments/comment.stories.js
+++ assets/javascripts/kitten/karl/pages/comments/comment.stories.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean } from '@storybook/addon-knobs/react'
-import Comment from './components/comment'
+import CommentPage from './components/comment'
 
 storiesOf('Pages/Comment', module)
   .addDecorator(withKnobs)
   .add('Comment', () => {
-    return (
-      <Comment
-        avatarImgProps={{
-          src: 'https://placehold.it/80x80/caf4fe/caf4fe',
-          alt: '',
-        }}
-      />
-    )
+    return <CommentPage />
   })
--- assets/javascripts/kitten/karl/pages/comments/components/comment.js
+++ assets/javascripts/kitten/karl/pages/comments/components/comment.js
@@ -1,15 +1,39 @@
 import React, { PureComponent } from 'react'
-import Styled from 'styled-components'
+import styled from 'styled-components'
 import { Container } from '../../../../components/grid/container'
 import { Comment } from '../../../../components/comments/comment'
 import { CommentForm } from '../../../../components/comments/comment-form'
 import { Grid, GridCol } from '../../../../components/grid/grid'
+import { Marger } from '../../../../components/layout/marger'
+import { ActionLinks } from './action-links'
 
 const CommentPage = props => (
   <Container>
     <Grid>
-      <GridCol col-l="8" offset-l="2">
-        <CommentForm />
+      <GridCol col-m="8" offset-m="2">
+        <Marger top="5" bottom="5">
+          <CommentForm
+            avatarImgProps={{
+              src: 'https://placehold.it/80x80/caf4fe/caf4fe',
+              alt: '',
+            }}
+            placeholder="Laisser un commentaire sur le projet"
+            commentButton="Commenter"
+          />
+        </Marger>
+
+        <Marger top="5">
+          <Comment
+            avatarImgProps={{
+              src: 'https://placehold.it/80x80/caf4fe/caf4fe',
+              alt: '',
+            }}
+            text="Youpi Youpi !!!voila les bons jus qui arrivent! Pleins de bonheur de partage et de revitalisation ! Rose"
+            ownerName="rose-jourdan"
+            commentDate="20 jours"
+            bottomNotes={<ActionLinks />}
+          />
+        </Marger>
       </GridCol>
     </Grid>
   </Container>
