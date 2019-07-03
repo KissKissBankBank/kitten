e24c50529a770556c0559b8568e2002052e72605
Camille Delbove <camille.delbove@gmail.com>
2019-07-03 17:43:15 +0200

    Update stories title

--- assets/javascripts/kitten/karl/pages/comments/comment.stories.js
+++ assets/javascripts/kitten/karl/pages/comments/comment.stories.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import CommentPage from './components/comment'
 
-storiesOf('Pages/Comment', module).add('Comment', () => {
+storiesOf('Pages/Comment', module).add('Comment page', () => {
   return <CommentPage />
 })
