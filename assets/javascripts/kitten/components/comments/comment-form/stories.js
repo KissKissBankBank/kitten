815bb6b7376f2ee530febd8ce35db6326db3578a
Camille Delbove <camille.delbove@gmail.com>
2019-07-11 14:52:44 +0200

    Add info in stories

--- assets/javascripts/kitten/components/comments/comment-form/stories.js
+++ assets/javascripts/kitten/components/comments/comment-form/stories.js
@@ -50,24 +50,31 @@ const info = {
 storiesOf('Comments/CommentForm', module)
   .addDecorator(withKnobs)
   .addDecorator(withInfo)
-  .add('Comment form', () => {
-    return (
-      <Grid style={{ marginTop: '5em' }}>
-        <GridCol offset="1" col="8">
-          <CommentForm
-            avatarImgProps={object('Src/Alt', {
-              src: 'https://placehold.it/80x80/caf4fe/caf4fe',
-              alt: '',
-            })}
-            placeholder={text('placeholder', 'Leave a comment on the project')}
-            commentButton={text('Comment button', 'Comment')}
-            error={boolean('Is error', false)}
-            errorMessage={text(
-              'Error message',
-              'Cum sociis natoque penatibus et magnis',
-            )}
-          />
-        </GridCol>
-      </Grid>
-    )
-  })
+  .add(
+    'Comment form',
+    () => {
+      return (
+        <Grid style={{ marginTop: '5em' }}>
+          <GridCol offset="1" col="8">
+            <CommentForm
+              avatarImgProps={object('Src/Alt', {
+                src: 'https://placehold.it/80x80/caf4fe/caf4fe',
+                alt: '',
+              })}
+              placeholder={text(
+                'placeholder',
+                'Leave a comment on the project',
+              )}
+              commentButton={text('Comment button', 'Comment')}
+              error={boolean('Is error', false)}
+              errorMessage={text(
+                'Error message',
+                'Cum sociis natoque penatibus et magnis',
+              )}
+            />
+          </GridCol>
+        </Grid>
+      )
+    },
+    { info },
+  )
