2a4cba1e3b78dd624381adb6a0f2c930e620d386
Camille Delbove <camille.delbove@gmail.com>
2019-07-16 16:45:32 +0200

    Remove return

--- assets/javascripts/kitten/components/form/field/components/password.js
+++ assets/javascripts/kitten/components/form/field/components/password.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react'
 import { Marger } from '../../../layout/marger'
 import { PasswordInput } from '../../password-input'
 
-export const FieldPassword = props => {
-  return (
-    <Marger top="1.5">
-      <PasswordInput {...props} />
-    </Marger>
-  )
-}
+export const FieldPassword = props => (
+  <Marger top="1.5">
+    <PasswordInput {...props} />
+  </Marger>
+)
