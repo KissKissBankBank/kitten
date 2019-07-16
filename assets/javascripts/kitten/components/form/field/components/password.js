82dd01b17abe021f6089b0b2b1058be2ad145449
Camille Delbove <camille.delbove@gmail.com>
2019-07-16 16:27:18 +0200

    Update field

--- assets/javascripts/kitten/components/form/field/components/password.js
+++ assets/javascripts/kitten/components/form/field/components/password.js
@@ -4,15 +4,10 @@ import { PasswordInput } from '../../password-input'
 
 export class FieldPassword extends Component {
   render() {
-    const { name, iconLabel, hiddenIconLabel, ...textInputProps } = this.props
+    const { name, iconLabel, hiddenIconLabel, ...others } = this.props
     return (
       <Marger top="1.5">
-        <PasswordInput
-          textInputProps={textInputProps}
-          name={name}
-          iconLabel={iconLabel}
-          hiddenIconLabel={hiddenIconLabel}
-        />
+        <PasswordInput {...this.props} />
       </Marger>
     )
   }
