5898a87718c167cdf4e2048a0ff5723e7baba6f7
Camille Delbove <camille.delbove@gmail.com>
2019-07-22 12:27:09 +0200

    Update header-board.js

--- assets/javascripts/kitten/karl/pages/kissbankers-board/components/header-board.js
+++ assets/javascripts/kitten/karl/pages/kissbankers-board/components/header-board.js
@@ -10,43 +10,20 @@ import COLORS from '../../../../constants/colors-config'
 import { pxToRem, stepToRem } from '../../../../helpers/utils/typography'
 import TYPOGRAPHY from '../../../../constants/typography-config'
 import { ScreenConfig } from '../../../../constants/screen-config'
+import { withMediaQueries } from '../../hoc/media-queries'
 
 const StyledContainer = styled.div`
   flex: 1;
   background-color: ${COLORS.font1};
-  padding: ${pxToRem(35)};
+  padding: ${pxToRem(40)};
   color: ${COLORS.background1};
   font-size: ${stepToRem(-1)};
   ${TYPOGRAPHY.fontStyles.regular};
+  text-align: center;
 
   display: none;
-  grid-template-columns: repeat(auto-fill, 100%);
+  grid-template-columns: 2% 15% 15% 10% 10% 10% 25% 12%;
   grid-template-rows: 100% auto;
-
-  &:nth-child(1) {
-    grid-template-columns: repeat(auto-fill, 10%);
-  }
-  &:nth-child(2) {
-    grid-template-columns: repeat(auto-fill, 10%);
-  }
-  &:nth-child(3) {
-    grid-template-columns: repeat(auto-fill, 10%);
-  }
-  &:nth-child(4) {
-    grid-template-columns: repeat(auto-fill, 10%);
-  }
-  &:nth-child(5) {
-    grid-template-columns: repeat(auto-fill, 10%);
-  }
-  &:nth-child(6) {
-    grid-template-columns: repeat(auto-fill, 10%);
-  }
-  &:nth-child(7) {
-    grid-template-columns: repeat(auto-fill, 20%);
-  }
-  &:nth-child(8) {
-    grid-template-columns: repeat(auto-fill, 20%);
-  }
 
   @media (min-width: ${ScreenConfig.S.min}px) {
     display: grid;
@@ -54,13 +31,18 @@ const StyledContainer = styled.div`
 `
 
 const StyledCheckbox = styled(Checkbox)`
+  display: block;
   margin: 0;
+`
+
+const List = styled.div`
+  display: block;
 `
 
 const HeaderBoard = props => (
   <Container>
     <StyledContainer role="rowgroup">
-      <StyledCheckbox id="checkbox" children="" />
+      <StyledCheckbox id="checkbox" />
       <div role="columnheader">Countribution</div>
       <div role="columnheader">Contributeur</div>
       <div role="columnheader">Message(s)</div>
@@ -69,6 +51,19 @@ const HeaderBoard = props => (
       <div role="columnheader">Mode de livraison</div>
       <div role="columnheader">Statut de livraison</div>
     </StyledContainer>
+
+    {viewportIsSOrLess && (
+      <StyledContainer role="rowgroup">
+        <StyledCheckbox id="checkbox" children="" />
+        <List role="rowgroup">Countribution</List>
+        <List role="rowgroup">Contributeur</List>
+        <List role="rowgroup">Message(s)</List>
+        <List role="rowgroup">Montant</List>
+        <List role="rowgroup">Statut de paiement</List>
+        <List role="rowgroup">Mode de livraison</List>
+        <List role="rowgroup">Statut de livraison</List>
+      </StyledContainer>
+    )}
   </Container>
 )
 
