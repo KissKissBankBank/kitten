c72cfec062ed9f0e8bd504cd2d56658c828a5761
Camille Delbove <camille.delbove@gmail.com>
2019-07-23 11:35:50 +0200

    Go const

--- assets/javascripts/kitten/karl/pages/kissbankers-page/index.js
+++ assets/javascripts/kitten/karl/pages/kissbankers-page/index.js
@@ -1,16 +1,14 @@
-import React, { PureComponent } from 'react'
-import { Board } from './components/board'
-import { Header } from './components/header'
-import { Pagination } from './components/pagination'
+import React from 'react'
+import Board from './components/board'
+import Header from './components/header'
+import Pagination from './components/pagination'
 
-export class KissbankersPage extends PureComponent {
-  render() {
-    return (
-      <>
-        <Header />
-        <Board />
-        <Pagination />
-      </>
-    )
-  }
-}
+const KissbankersPage = props => (
+  <>
+    <Header />
+    <Board />
+    <Pagination />
+  </>
+)
+
+export default KissbankersPage
--- assets/javascripts/kitten/karl/pages/kissbankers-page/stories.js
+++ assets/javascripts/kitten/karl/pages/kissbankers-page/stories.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs, boolean } from '@storybook/addon-knobs/react'
-import { KissbankersPage } from './index'
+import KissbankersPage from './index'
 
 storiesOf('Pages/KissbankersPage', module).add('Kissbankers-page', () => {
   return <KissbankersPage />
