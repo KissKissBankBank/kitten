8396bd2335184f337e4b5bd9115c00d0a6cc15f7
Camille Delbove <camille.delbove@gmail.com>
2019-07-23 10:40:46 +0200

    Fill

--- assets/javascripts/kitten/components/icons/email-icon/index.js
+++ assets/javascripts/kitten/components/icons/email-icon/index.js
@@ -7,10 +7,11 @@ export const EmailIcon = ({ color, title, ...props }) => (
     height="12"
     viewBox="0 0 19 12"
     xmlns="http://www.w3.org/2000/svg"
+    fill={color}
     {...props}
   >
     {title && <title>{title}</title>}
-    <path d="M9.5 8l9-6v10H.5V2l9 6zm0-2l-9-6h18l-9 6z" fill={color} />
+    <path d="M9.5 8l9-6v10H.5V2l9 6zm0-2l-9-6h18l-9 6z" />
   </svg>
 )
 
--- assets/javascripts/kitten/components/icons/export-icon/index.js
+++ assets/javascripts/kitten/components/icons/export-icon/index.js
@@ -7,13 +7,11 @@ export const ExportIcon = ({ color, title, ...props }) => (
     height="16"
     viewBox="0 0 14 16"
     xmlns="http://www.w3.org/2000/svg"
+    fill={color}
     {...props}
   >
     {title && <title>{title}</title>}
-    <path
-      d="M8 4.207v5.414H6V4.207L4.172 6.036 2.757 4.62 7 .38l1.414 1.414 2.829 2.828-1.415 1.415L8 4.207zm4 11.414H0v-6h2v4h10v-4h2v6h-2z"
-      fill={color}
-    />
+    <path d="M8 4.207v5.414H6V4.207L4.172 6.036 2.757 4.62 7 .38l1.414 1.414 2.829 2.828-1.415 1.415L8 4.207zm4 11.414H0v-6h2v4h10v-4h2v6h-2z" />
   </svg>
 )
 
