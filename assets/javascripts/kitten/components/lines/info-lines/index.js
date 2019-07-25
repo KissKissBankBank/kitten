57bacfad200ba8c21d3332ed1ded987dd0723897
Camille Delbove <camille.delbove@gmail.com>
2019-07-25 13:13:28 +0200

    Update stories

--- assets/javascripts/kitten/components/lines/info-lines/index.js
+++ assets/javascripts/kitten/components/lines/info-lines/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent, Fragment } from 'react'
+import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import COLORS from '../../../constants/colors-config'
 import { pxToRem, stepToRem } from '../../../helpers/utils/typography'
@@ -7,25 +8,26 @@ import { ScreenConfig } from '../../../constants/screen-config'
 const StyledInfoLines = styled.div`
   border-top: ${pxToRem(1)} solid ${COLORS.line1};
 
+  ${({ borderColor }) =>
+    borderColor &&
+    css`
+      border-color: ${borderColor};
+    `}
+
   ${({ withBorderRadius }) =>
     withBorderRadius &&
     css`
       border-radius: ${pxToRem(8)};
     `}
 
-  ${({ withLeftRightBorder }) =>
+  ${({ withLeftRightBorder, borderColor }) =>
     withLeftRightBorder &&
+    borderColor &&
     css`
       border-left: ${pxToRem(1)} solid ${COLORS.line1};
       border-right: ${pxToRem(1)} solid ${COLORS.line1};
-    `}
 
-  ${({ withoutResponsive }) =>
-    withoutResponsive &&
-    css`
-      @media (min-width: ${ScreenConfig.M.min}px) {
-        flex-direction: row;
-      }
+      border-color: ${borderColor};
     `}
 
   ${({ withoutTopBottomBorder }) =>
@@ -37,15 +39,30 @@ const StyledInfoLines = styled.div`
         border-bottom: none;
       }
     `}
+
+    ${({ withoutResponsive }) =>
+      withoutResponsive &&
+      css`
+        @media (min-width: ${ScreenConfig.M.min}px) {
+          flex-direction: row;
+        }
+      `}
 `
 
 const StyledLine = styled.div`
+  flex-direction: column;
   display: flex;
   padding: ${pxToRem(15)};
   border-bottom: ${pxToRem(1)} solid ${COLORS.line1};
 
+  ${({ borderColor }) =>
+    borderColor &&
+    css`
+      border-color: ${borderColor};
+    `}
+
   @media (min-width: ${ScreenConfig.M.min}px) {
-    flex-direction: column;
+    flex-direction: unset;
   }
 `
 
@@ -59,20 +76,17 @@ const StyledValue = styled.div`
 export class InfoLines extends PureComponent {
   render() {
     const {
-      borderColor,
+      id,
       infos,
       withBorderRadius,
       withLeftRightBorder,
-      withoutResponsive,
       withoutTopBottomBorder,
-      children,
-      id,
-      key,
-      value,
+      withoutResponsive,
+      borderColor,
       ...others
     } = this.props
 
-    const InfoList = ({ borderColor, key, value, ...others }) =>
+    const InfoList = ({ key, value, ...others }) =>
       React.Children.toArray(
         <StyledLine {...others} borderColor={borderColor} key={id}>
           <div>{key}</div>
@@ -80,31 +94,38 @@ export class InfoLines extends PureComponent {
         </StyledLine>,
       )
 
-    console.log(<InfoList />, InfoList)
-
     return (
       <StyledInfoLines
         {...others}
         borderColor={borderColor}
         withBorderRadius={withBorderRadius}
         withLeftRightBorder={withLeftRightBorder}
-        withoutResponsive={withoutResponsive}
         withoutTopBottomBorder={withoutTopBottomBorder}
+        withoutResponsive={withoutResponsive}
       >
         {infos.map(info => (
-          <InfoList id={info.id} key={info.key} value={info.value} />
+          <InfoList key={info.key} value={info.value} id={info.id} />
         ))}
       </StyledInfoLines>
     )
   }
 }
 
+InfoLines.PropTypes = {
+  borderColor: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  infos: PropTypes.array.isRequired,
+  withBorderRadius: PropTypes.bool,
+  withLeftRightBorder: PropTypes.bool,
+  withoutTopBottomBorder: PropTypes.bool,
+  withoutResponsive: PropTypes.bool,
+}
+
 InfoLines.defaultProps = {
   borderColor: null,
-  className: null,
   infos: [], // Eg: [{ key: …, value: …, id: … }]
   withBorderRadius: false,
   withLeftRightBorder: false,
-  withoutResponsive: false,
   withoutTopBottomBorder: false,
+  withoutResponsive: false,
 }
--- assets/javascripts/kitten/components/lines/info-lines/stories.js
+++ assets/javascripts/kitten/components/lines/info-lines/stories.js
@@ -2,28 +2,79 @@ import React from 'react'
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import { withKnobs, text, object, boolean } from '@storybook/addon-knobs'
+import { withKnobs, boolean, color } from '@storybook/addon-knobs'
 import { InfoLines } from './index'
 import { Grid, GridCol } from '../../../components/grid/grid'
+
+const info = {
+  text: `
+    # InfoLines
+    &nbsp;
+
+    ## Import
+    ~~~js
+    import { InfoLines } from '@kisskissbankbank/kitten/src/components/lines/info-lines'
+    ~~~
+
+    ## Usage
+    &nbsp;
+
+    #### Default
+    ~~~js
+    <InfoLines
+      id="…"
+      infos={[ key:'…', value: '…', id: '…' ]}
+    />
+    ~~~
+
+    #### with borders
+    ~~~js
+    <InfoLines
+      withBorderRadius="…"
+      withLeftRightBorder="…"
+    />
+    ~~~
+
+    #### without borders
+    ~~~js
+    <InfoLines withoutTopBottomBorder="…" />
+    ~~~
+
+    #### without responsive
+    ~~~js
+    <InfoLines withoutResponsive="…" />
+    ~~~
+  `,
+  header: false,
+  propTables: false,
+}
 
 storiesOf('Lines/Info Lines', module)
   .addDecorator(withKnobs)
   .addDecorator(withInfo)
-  .add('InfoLines', () => {
-    return (
-      <Grid style={{ marginTop: '5em' }}>
-        <GridCol offset="1" col="8">
-          <InfoLines
-            withBorderRadius={boolean('with border radius', false)}
-            withLeftRightBorder={boolean('with left right border', false)}
-            withoutResponsive={boolean('without responsive', false)}
-            withoutTopBottomBorder={boolean('without top bottom border', false)}
-            infos={[
-              { key: 'Lorem', value: 'Ipsum', id: 'item-1' },
-              { key: 'Dolor', value: 'Sit amet', id: 'item-2' },
-            ]}
-          />
-        </GridCol>
-      </Grid>
-    )
-  })
+  .add(
+    'InfoLines',
+    () => {
+      return (
+        <Grid style={{ marginTop: '5em' }}>
+          <GridCol offset="1" col="8">
+            <InfoLines
+              borderColor={color('custom border color', '#eee')}
+              withBorderRadius={boolean('with border radius', false)}
+              withLeftRightBorder={boolean('with left right border', false)}
+              withoutTopBottomBorder={boolean(
+                'without top bottom border',
+                false,
+              )}
+              withoutResponsive={boolean('without responsive', false)}
+              infos={[
+                { key: 'Lorem', value: 'Ipsum', id: 'item-1' },
+                { key: 'Dolor', value: 'Sit amet', id: 'item-2' },
+              ]}
+            />
+          </GridCol>
+        </Grid>
+      )
+    },
+    { info },
+  )
