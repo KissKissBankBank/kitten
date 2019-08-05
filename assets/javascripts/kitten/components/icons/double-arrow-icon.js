8c7a0a03d43eec73c759012fa934af2d45162586
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-08-05 16:21:47 +0200

    [Kissbankers] `DoubleArrowIcon`

---
+++ assets/javascripts/kitten/components/icons/double-arrow-icon.js
@@ -0,0 +1,16 @@
+import React, { Fragment, Component } from 'react'
+
+export const DoubleArrowIcon = props => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 8.48 15"
+      height="15"
+      width="8.48"
+      {...props}
+    >
+      <path d="M0 4.24 L4.24 0 L8.48 4.24 L7.08 5.60 L4.24 2.77 L1.4 5.6 z" />
+      <path d="M0 10.76 L4.24 15 L8.48 10.76 L7.08 9.4 L4.24 12.33 L1.4 9.4 z" />
+    </svg>
+  )
+}
