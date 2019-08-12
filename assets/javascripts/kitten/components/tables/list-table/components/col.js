dfdfedd5789f22bc2d573d58f76b2e4efff7d727
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-08-12 15:49:04 +0200

    Remove cruft

--- assets/javascripts/kitten/components/tables/list-table/components/col.js
+++ assets/javascripts/kitten/components/tables/list-table/components/col.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { ScreenConfig } from '../../../../constants/screen-config'
 import {
--- assets/javascripts/kitten/components/tables/list-table/components/header.js
+++ assets/javascripts/kitten/components/tables/list-table/components/header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import COLORS from '../../../../constants/colors-config'
 import { ScreenConfig } from '../../../../constants/screen-config'
