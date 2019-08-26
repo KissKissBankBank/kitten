a360f0fa6d0141e6910966885a694bb6dc6fbd16
Camille Delbove <camille.delbove@gmail.com>
2019-08-26 12:30:14 +0200

    [Pagination] - Update `Text` component styles on `Pagination`.

--- assets/javascripts/kitten/components/navigation/pagination/index.js
+++ assets/javascripts/kitten/components/navigation/pagination/index.js
@@ -60,9 +60,6 @@ const StyledButtonIcon = styled(Text)`
   border-radius: 0;
   border-width: 0;
   border-style: solid;
-  text-decoration: none;
-  font-weight: 500;
-  font-size: ${stepToRem(-1)};
   outline: none;
   color: ${COLORS.font1};
   border-color: ${COLORS.line1};
@@ -238,9 +235,12 @@ class PaginationBase extends PureComponent {
     return (
       <StyledList key={`page-${number}`}>
         <StyledButtonIcon
-          as={tag}
+          tag={tag}
           href={href}
           key={`link-${number}`}
+          weight="regular"
+          decoration="none"
+          size="tiny"
           isActive={isActive}
           aria-current={isActive && 'page'}
           aria-label={ariaLabel}
@@ -289,7 +289,7 @@ class PaginationBase extends PureComponent {
     return (
       <StyledArrowIconDirection direction={direction}>
         <StyledButtonIcon
-          as="a"
+          tag="a"
           href={this.props.goToPageHref(number)}
           key={`link-${direction}`}
           aria-label={buttonLabel}
