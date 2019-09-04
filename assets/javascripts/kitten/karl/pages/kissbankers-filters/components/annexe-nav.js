e98992f6e33e8ec52260bcc9d3330a118aa6556a
Camille Delbove <camille.delbove@gmail.com>
2019-09-04 18:46:10 +0200

    Responsive buttons

--- assets/javascripts/kitten/karl/pages/kissbankers-filters/components/annexe-nav.js
+++ assets/javascripts/kitten/karl/pages/kissbankers-filters/components/annexe-nav.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react'
 import styled, { css } from 'styled-components'
-import { Button } from '../../../../components/buttons/button/button'
+import { Button, ICON } from '../../../../components/buttons/button/button'
 import { TextInput } from '../../../../components/form/text-input'
 import { ExportIcon } from '../../../../components/icons/export-icon'
 import { EmailIcon } from '../../../../components/icons/email-icon'
 import { ScreenConfig } from '../../../../constants/screen-config'
+import { pxToRem } from '../../../../helpers/utils/typography'
 
 const StyledButtonContainer = styled.div`
   display: flex;
@@ -14,28 +15,56 @@ const StyledInput = styled(TextInput)`
   flex: 1;
 `
 
+const buttonMargin = css`
+  @media (max-width: ${ScreenConfig.M.max}px) {
+    ${ICON}
+
+    > : nth-child(n) {
+      margin-right: 0;
+    }
+  }
+`
+
+const StyledEmailButton = styled(Button)`
+  ${buttonMargin}
+
+  margin-right: ${pxToRem(10)};
+
+  @media (min-width: ${ScreenConfig.S.min}px) {
+    margin-right: ${pxToRem(20)};
+  }
+`
+
+const StyledExportButton = styled(Button)`
+  ${buttonMargin}
+
+  margin-right: ${pxToRem(20)};
+
+  @media (min-width: ${ScreenConfig.S.min}px) {
+    margin-right: ${pxToRem(80)};
+  }
+`
+
 const StyledButtonText = styled.span`
-  @media (max-width: ${ScreenConfig.S.max}px) {
+  @media (max-width: ${ScreenConfig.M.max}px) {
     display: none;
   }
 `
 
 class AnnexeNav extends PureComponent {
   render() {
-    const { icon } = this.props
-
     return (
       <>
         <StyledButtonContainer>
-          <Button icon={icon} modifier="helium" style={{ marginRight: 20 }}>
-            <EmailIcon width="18" height="18" />
+          <StyledEmailButton modifier="helium">
+            <EmailIcon width="19" height="12" />
             <StyledButtonText>Contacter</StyledButtonText>
-          </Button>
+          </StyledEmailButton>
 
-          <Button icon={icon} modifier="helium" style={{ marginRight: 80 }}>
-            <ExportIcon width="15" height="15" />
+          <StyledExportButton modifier="helium">
+            <ExportIcon width="14" height="16" />
             <StyledButtonText>Exporter</StyledButtonText>
-          </Button>
+          </StyledExportButton>
           <StyledInput placeholder="Via un email, un nom ou un #" />
         </StyledButtonContainer>
       </>
