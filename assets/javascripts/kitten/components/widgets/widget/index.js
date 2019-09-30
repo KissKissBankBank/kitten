ca49f700f8e12fcfce349628d1bf5032802c307c
Camille Delbove <camille.delbove@gmail.com>
2019-09-30 17:07:20 +0200

    Create file

---
+++ assets/javascripts/kitten/components/widgets/widget/index.js
@@ -0,0 +1,9 @@
+// import React, { PureComponent } from 'react'
+// import PropTypes from 'prop-types'
+// import { Modal } from '../../../components/modals/modal'
+// import { Title } from '../../../components/typography/title'
+// import { WidgetCard } from '../../..components/cards/widget-card'
+
+// const widget = () => {
+
+// export default Widget
