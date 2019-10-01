01fd357c4786b1cb995d9bfbae365c0219240e98
Joachim <joachim.robert@kisskissbankbank.com>
2019-10-01 15:45:13 +0200

    [Checkbox] Add `indeterminate` props and style (#1803)

--- assets/javascripts/kitten/components/form/checkbox.stories.js
+++ assets/javascripts/kitten/components/form/checkbox.stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleRoot } from 'radium'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { Checkbox } from './checkbox'
 import { Grid, GridCol } from '../../components/grid/grid'
@@ -18,6 +18,7 @@ storiesOf('Form/Checkbox', module)
             <Checkbox
               id={text('id', 'checkbox_id')}
               onLabelClick={action('label-click')}
+              indeterminate={boolean('indeterminate', false)}
             >
               {text('text', 'Checkbox Label')}
             </Checkbox>
