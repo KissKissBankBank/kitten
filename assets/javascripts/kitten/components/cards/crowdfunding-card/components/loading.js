a83a22a3fda4662eb2d8ca306e44cd6ad24b974c
Camille Delbove <camille.delbove@gmail.com>
2019-10-02 15:21:54 +0200

    Progress and Loading

---
+++ assets/javascripts/kitten/components/cards/crowdfunding-card/components/loading.js
@@ -0,0 +1,45 @@
+import React, { PureComponent } from 'react'
+import styled, { keyframes } from 'styled-components'
+import PropTypes from 'prop-types'
+
+const loadingKeyframes = keyframes`
+  from { transform: translateX(-100%) }
+  to { transform: translateX(100%) }
+`
+
+const StyledLoading = styled.span`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  transform: translateX(-100%);
+  background: linear-gradient(
+    90deg,
+    rgba(0, 0, 0, 0),
+    \rgba(255, 255, 255, 0.3),
+    rgba(0, 0, 0, 0)
+  );
+  animation: x 1s linear infinite;
+  animationname: ${loadingKeyframes};
+`
+
+class Loading extends PureComponent {
+  static propTypes = {
+    loading: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    loading: false,
+  }
+
+  render() {
+    const { loading } = this.props
+
+    if (!loading) return null
+
+    return <StyledLoading />
+  }
+}
+
+export default Loading
---
+++ assets/javascripts/kitten/components/cards/crowdfunding-card/components/state.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import COLORS from '../../../../constants/colors-config'
+import { Text } from '../../../../components/typography/text'
+import { pxToRem } from '../../../../helpers/utils/typography'
+
+const COMPONENT_GUTTER = pxToRem(10)
+
+const StyledState = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: inline-flex;
+  align-items: center;
+  background-color: ${COLORS.background1};
+  padding: ${COMPONENT_GUTTER};
+  line-height: 1;
+`
+
+class State extends PureComponent {
+  static propTypes = {
+    state: PropTypes.string,
+    loading: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    state: '',
+    loading: false,
+  }
+
+  render() {
+    const { state, loading } = this.props
+
+    return (
+      <>
+        {state && !loading && (
+          <StyledState>
+            <Text size="micro" lineHeight="normal" weight="regular">
+              {state}
+            </Text>
+          </StyledState>
+        )}
+      </>
+    )
+  }
+}
+
+export default State
