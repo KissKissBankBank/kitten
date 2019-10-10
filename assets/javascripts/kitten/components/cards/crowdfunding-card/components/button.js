2f0b74ac1f59db2e09f50726ebe6615e3c4a0cf0
Camille Delbove <camille.delbove@gmail.com>
2019-10-10 12:30:25 +0200

    Button styles

--- assets/javascripts/kitten/components/cards/crowdfunding-card/components/button.js
+++ assets/javascripts/kitten/components/cards/crowdfunding-card/components/button.js
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types'
 import { Button } from '../../../../components/buttons/button/button'
 import { pxToRem } from '../../../../helpers/utils/typography'
 
-const ButtonCard = ({ buttonText }) => {
-  return (
-    <Button fluid modifier="helium">
-      {buttonText}
-    </Button>
-  )
-}
+const ButtonCard = ({ buttonText }) => (
+  <Button fluid modifier="helium">
+    {buttonText}
+  </Button>
+)
 
 export default ButtonCard
+
+ButtonCard.propTypes = {
+  buttonText: PropTypes.string,
+}
+
+ButtonCard.defaultProps = {
+  buttonText: '',
+}
