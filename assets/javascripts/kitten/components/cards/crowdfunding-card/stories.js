168be285e2d26a15fe36fd7f72cec45ce77e8ee8
Camille Delbove <camille.delbove@gmail.com>
2019-10-16 17:58:02 +0200

    Update test and stories

--- assets/javascripts/kitten/components/cards/crowdfunding-card/stories.js
+++ assets/javascripts/kitten/components/cards/crowdfunding-card/stories.js
@@ -21,8 +21,11 @@ const StoryContainer = ({ children }) => (
 
 storiesOf('Cards/CrowdfundingCard', module)
   .addDecorator(withKnobs)
-  .add('Crowdfunding Card', () => {
-    return (
+  .add(
+    'Crowdfunding Card',
+    withInfo(
+      'The CrowdfundingCardWidget is a container that can expand on click. It can accept any content.',
+    )(() => (
       <StoryContainer>
         <CrowdfundingCard
           href={text('Link', '#')}
@@ -44,31 +47,29 @@ storiesOf('Cards/CrowdfundingCard', module)
           state={text('State', 'Lorem ipsum…')}
         />
       </StoryContainer>
-    )
-  })
+    )),
+  )
 
   .addDecorator(withKnobs)
-  .add('Crowdfunding Card Widget', () => {
-    return (
-      <StoryContainer>
-        <CrowdfundingCardWidget
-          href={text('Link', '#')}
-          ownerTitle={text('Owner title', 'Owner title')}
-          ownerDescription={text('Owner description', 'Owner description')}
-          loading={boolean('Loading', false)}
-          title={text(
-            'Title',
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-          )}
-          dayCounter={text('Counter Day', '27 jours')}
-          stateDay={text('State Day', 'restant')}
-          subtitle={text(
-            'Subtitle',
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma Aenean massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-          )}
-          buttonText={text('Button text', 'Contribuer dès 5 €')}
-          state={text('State', 'Sur')}
-        />
-      </StoryContainer>
-    )
-  })
+  .add('Crowdfunding Card Widget', () => (
+    <StoryContainer>
+      <CrowdfundingCardWidget
+        href={text('Link', '#')}
+        ownerTitle={text('Owner title', 'Owner title')}
+        ownerDescription={text('Owner description', 'Owner description')}
+        loading={boolean('Loading', false)}
+        title={text(
+          'Title',
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+        )}
+        dayCounter={text('Counter Day', '27 jours')}
+        stateDay={text('State Day', 'restant')}
+        subtitle={text(
+          'Subtitle',
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma Aenean massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+        )}
+        buttonText={text('Button text', 'Contribuer dès 5 €')}
+        state={text('State', 'Sur')}
+      />
+    </StoryContainer>
+  ))
