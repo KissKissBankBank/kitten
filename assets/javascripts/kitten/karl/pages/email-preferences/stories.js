00c84c5b67646639d5db416ee90f9ce0b2a55ae8
Joachim <joachim.robert@kisskissbankbank.com>
2019-10-25 14:18:33 +0200

    [Karl] Add page for Email Preferences (#1819)

---
+++ assets/javascripts/kitten/karl/pages/email-preferences/stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import EmailPreferences from './index'
+
+storiesOf('Pages/EmailPreferences', module).add('Email-preferences', () => {
+  return <EmailPreferences />
+})
