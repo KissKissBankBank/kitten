ee8f2c712255e116817184eb246307a0e0a073d4
Joachim <joachim.robert@kisskissbankbank.com>
2019-11-14 09:54:52 +0100

    Update assets/javascripts/kitten/helpers/utils/encode-svg.js
    Co-Authored-By: Bastien Maillard <bastien.maillard@kisskissbankbank.com>

--- assets/javascripts/kitten/helpers/utils/encode-svg.js
+++ assets/javascripts/kitten/helpers/utils/encode-svg.js
@@ -1,5 +1,5 @@
 import { Base64 } from 'js-base64'
 
 export const encodeSvgString = svgString => {
-  return 'data:image/svg+xml;base64,' + Base64.encode(svgString)
+  return `data:image/svg+xml;base64,${Base64.encode(svgString)}`
 }
