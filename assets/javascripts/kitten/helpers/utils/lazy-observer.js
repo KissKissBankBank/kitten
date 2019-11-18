1be4e77d2cbe9f1a807993c3f7b2c3603c67f210
Joachim Robert <joachim.robert@kisskissbankbank.com>
2019-11-18 12:17:12 +0100

    Move LazyObserver as an instance of IntersectionObserverClass

--- assets/javascripts/kitten/helpers/utils/lazy-observer.js
+++ assets/javascripts/kitten/helpers/utils/lazy-observer.js
@@ -1,45 +1,5 @@
-import { domElementHelper } from '../dom/element-helper'
+import { IntersectionObserverClass } from './intersection-observer'
 
-class LazyObserver {
-  observedComponents = new Map()
+const LazyObserver = new IntersectionObserverClass()
 
-  options = {
-    root: null,
-    rootMargin: '100px 0px',
-    threshold: 0.01,
-  }
-
-  intersectionCallback = entries => {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) return
-
-      const elt = entry.target
-      this.observedComponents.get(elt)()
-      this.unobserve(elt)
-    })
-  }
-
-  observer =
-    domElementHelper.canUseDom() && 'IntersectionObserver' in window
-      ? new IntersectionObserver(this.intersectionCallback, this.options)
-      : null
-
-  observe = (elt, callback = () => {}) => {
-    if (this.observer === null) {
-      callback()
-      return
-    }
-
-    this.observer.observe(elt)
-    this.observedComponents.set(elt, callback)
-  }
-
-  unobserve = elt => {
-    if (this.observer === null) return
-
-    this.observedComponents.delete(elt)
-    this.observer.unobserve(elt)
-  }
-}
-
-export default new LazyObserver()
+export default LazyObserver
