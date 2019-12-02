29e37af45d6ebf6f7376c62534098cd9f2486ba6
Camille Delbove <camille.delbove@gmail.com>
2019-12-02 15:51:06 +0100

    [RewardEdition] Create GiftCode on Storybook

--- assets/javascripts/kitten/components/cards/reward-edition/stories.js
+++ assets/javascripts/kitten/components/cards/reward-edition/stories.js
@@ -7,9 +7,13 @@ import { Container } from '../../grid/container'
 import { Text } from '../../../components/typography/text'
 import { ButtonGroup } from '../../../components/buttons/button-group'
 import { Button } from '../../../components/buttons/button/button'
+import { Field } from '../../../components/form/field'
+import { ArrowContainer } from '../../../components/layout/arrow-container'
+import { BulletList } from '../../../components/lists/bullet-list'
 import { ExpandBoard } from '../../../components/expandable/expand-board'
 import { Grid, GridCol } from '../../grid/grid'
 import { RewardEdition } from './index'
+import COLORS from '../../../constants/colors-config'
 import { pxToRem } from '../../../helpers/utils/typography'
 
 const StoryContainer = ({ children }) => (
@@ -66,6 +70,72 @@ storiesOf('Cards/RewardEdition', module)
                   <StyledButton>Button3</StyledButton>
                 </ExpandBoard.Content>
               </ExpandBoard>
+            </div>
+          </RewardEdition.Content>
+
+          <RewardEdition.Footer />
+        </RewardEdition>
+      </StoryContainer>
+    )
+  })
+
+  .add('Reward Edition Gift', () => {
+    return (
+      <StoryContainer>
+        <RewardEdition>
+          <RewardEdition.Header>
+            <Text weight="regular" size="default">
+              KissKiss Code
+            </Text>
+          </RewardEdition.Header>
+
+          <RewardEdition.Content>
+            <div className="k-u-hidden@s-down">
+              <Field>
+                <Field.Label labelProps={{ htmlFor: 'gift-code' }}>
+                  Code <Text weight="light">(obligatoire)</Text>
+                </Field.Label>
+                <Field.Input
+                  id="gift-code"
+                  name="gift-code"
+                  placeholder="Entrez votre code"
+                />
+                <Marger top="2" bottom="3">
+                  <ArrowContainer position="top" color={COLORS.primary5}>
+                    <BulletList
+                      items={[
+                        {
+                          key: '1',
+                          item: 'Uniquement des lettres et des chiffres',
+                        },
+                        { key: '2', item: "Pas d'espaces" },
+                        { key: '3', item: '30 caractères maximal' },
+                      ]}
+                    />
+                  </ArrowContainer>
+                </Marger>
+                <Marger top="3" bottom="3">
+                  <Field.Label
+                    labelProps={{ htmlFor: 'description-code' }}
+                    tooltipId="code-tooltip"
+                    tooltip="BlaBla"
+                  >
+                    Description du code <Text weight="light">(optionnel)</Text>
+                  </Field.Label>
+                  <Field.Input
+                    id="description-code"
+                    name="description-code"
+                    placeholder="Entrez votre description du code"
+                  />
+                </Marger>
+                <Marger top="3" bottom="3">
+                  <Field.Label labelProps={{ htmlFor: 'amount' }}>
+                    Montant minimum d'activation
+                    <Text weight="light">(optionnel)</Text>
+                  </Field.Label>
+                  <Field.Input id="amount" name="amount" placeholder="0" />
+                </Marger>
+              </Field>
             </div>
           </RewardEdition.Content>
 
