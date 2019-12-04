939670c558e6dbf82d097d261cbbac9a930e0a8c
Camille Delbove <camille.delbove@gmail.com>
2019-12-04 15:25:55 +0100

    Update test

--- assets/javascripts/kitten/components/lists/bullet-list/test.js
+++ assets/javascripts/kitten/components/lists/bullet-list/test.js
@@ -2,42 +2,33 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import { BulletList } from './index'
 
-const createMockMediaMatcher = matches => () => ({
-  matches,
-  addListener: () => {},
-  removeListener: () => {},
-})
-
 describe('<BulletList />', () => {
-  let originalMatchMedia
-  let component
+  describe('default', () => {
+    it('matches with snapshot', () => {
+      const component = renderer
+        .create(
+          <BulletList
+            items={[
+              { key: '1', item: 'Dis Manibus', id: '1' },
+              { key: '2', item: 'Calpurnia Felicla' },
+              { key: '3', item: 'Germulio coniugi' },
+            ]}
+            aria-hidden="true"
+          />,
+        )
+        .toJSON()
 
-  beforeEach(() => {
-    originalMatchMedia = window.matchMedia
+      expect(component).toMatchSnapshot()
+    })
   })
 
-  afterEach(() => {
-    window.matchMedia = originalMatchMedia
-  })
+  describe('with differents size props', () => {
+    it('matches with snapshot', () => {
+      const component = renderer
+        .create(<BulletList small large big huge />)
+        .toJSON()
 
-  it('should match its empty snapshot', () => {
-    window.matchMedia = createMockMediaMatcher(false)
-
-    const tree = renderer
-      .create(
-        <BulletList
-          small={false}
-          large={false}
-          big={false}
-          huge={false}
-          items={[
-            { key: '1', item: 'Dis Manibus' },
-            { key: '2', item: 'Calpurnia Felicla' },
-            { key: '3', item: 'Germulio coniugi' },
-          ]}
-        />,
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+      expect(component).toMatchSnapshot()
+    })
   })
 })
