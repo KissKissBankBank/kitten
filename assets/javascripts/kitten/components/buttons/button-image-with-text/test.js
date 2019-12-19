ec4eb9b54a4cf76bd36ef32dd1989ea4cbc9b5aa
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2019-12-19 15:32:06 +0100

    Use styled-components

--- assets/javascripts/kitten/components/buttons/button-image-with-text/test.js
+++ assets/javascripts/kitten/components/buttons/button-image-with-text/test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { ButtonImageWithText } from '../../components/buttons/button-image-with-text'
+import { ButtonImageWithText } from './index'
 
 describe('<ButtonImageWithText />', () => {
-  it('should match its empty snapshot', () => {
+  it('should match with snapshot', () => {
     const tree = renderer
       .create(
         <ButtonImageWithText
