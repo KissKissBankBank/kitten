62c5d1a0b9d90b03a16946cf71adbdfbe15c585c
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-01-06 15:15:48 +0100

    Update index.js

--- assets/javascripts/kitten/components/modals/modal/index.js
+++ assets/javascripts/kitten/components/modals/modal/index.js
@@ -16,8 +16,8 @@ const GlobalStyle = createGlobalStyle`
 
   .k-Modal__content {
     position: relative;
-    max-height: 100vh;
-    max-width: 100vw;
+    max-height: calc(100% - ${pxToRem(20)} * 2);
+    max-width: calc(100vw - ${pxToRem(20)} * 2);
 
     background-color: ${COLORS.background1};
     text-align: center;
@@ -28,12 +28,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     overflow: scroll;
 
-    @media (max-width: ${pxToRem(ScreenConfig.S.max)}) {
-      width: 100vw;
-      height: 100%;
-    }
     @media (min-width: ${pxToRem(ScreenConfig.M.min)}) {
-      max-height: calc(100vh - ${pxToRem(20)} * 2);
       max-width: ${pxToRem(690)};
       padding-left: ${pxToRem(110)};
       padding-right: ${pxToRem(110)};
