56c727f49c6bd272c551d1e7c39c3cf2a0728a07
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-01-13 15:43:47 +0100

    [Table] Add `DoubleEntryTable` component.

---
+++ assets/javascripts/kitten/components/tables/double-entry-table/test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { DoubleEntryTable } from './index'
+
+describe('<DoubleEntryTable />', () => {
+  let component
+
+  describe('with a complete structure and custom classes', () => {
+    beforeEach(() => {
+      component = renderer
+        .create(
+          <DoubleEntryTable id="DestBoubleEntryTable">
+            <DoubleEntryTable.Header className="customHeaderClass">
+              <DoubleEntryTable.HeaderCol>Header 1</DoubleEntryTable.HeaderCol>
+              <DoubleEntryTable.HeaderCol>Header 2</DoubleEntryTable.HeaderCol>
+              <DoubleEntryTable.HeaderCol>Header 3</DoubleEntryTable.HeaderCol>
+            </DoubleEntryTable.Header>
+
+            <DoubleEntryTable.Body>
+              <DoubleEntryTable.Row isHighlighted>
+                <DoubleEntryTable.TitleCol>Content 1</DoubleEntryTable.TitleCol>
+                <DoubleEntryTable.Col>Content 2</DoubleEntryTable.Col>
+                <DoubleEntryTable.Col>Content 3</DoubleEntryTable.Col>
+              </DoubleEntryTable.Row>
+              <DoubleEntryTable.Row className="customRowClass">
+                <DoubleEntryTable.TitleCol>Content 1</DoubleEntryTable.TitleCol>
+                <DoubleEntryTable.Col>Content 2</DoubleEntryTable.Col>
+                <DoubleEntryTable.Col>Content 3</DoubleEntryTable.Col>
+              </DoubleEntryTable.Row>
+            </DoubleEntryTable.Body>
+          </DoubleEntryTable>,
+        )
+        .toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+})
