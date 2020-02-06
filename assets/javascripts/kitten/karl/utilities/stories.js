3c881ae6f9b6704112c50f8c31ddcf265c71bdb6
Camille Delbove <camille.delbove@gmail.com>
2020-02-06 12:33:48 +0100

    [Utilities] Add documentation

---
+++ assets/javascripts/kitten/karl/utilities/stories.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { Grid, GridCol } from '../components/grid/grid'
+
+const info = {}
