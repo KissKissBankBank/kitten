c09f86e02b48471dcb649423354971305f9d1b0b
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-02-24 14:58:02 +0100

    add save and doc icons

---
+++ assets/javascripts/kitten/components/icons/doc-icon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const DocIcon = props => {
+  return (
+    <svg width={14} height={20} {...props}>
+      <title>Doc</title>
+      <path
+        d="M7 0v7h7v13H0V0h7zm4 14H3v2h8v-2zm0-4H3v2h8v-2zM9 0l5 5H9V0z"
+        fill="#222"
+        fillRule="evenodd"
+      />
+    </svg>
+  )
+}
---
+++ assets/javascripts/kitten/components/icons/save-icon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const SaveIcon = props => {
+  return (
+    <svg width={16} height={17} {...props}>
+      <title>Save</title>
+      <path
+        d="M12 0l4 4v13H0V0h12zM8 9a2.5 2.5 0 100 5 2.5 2.5 0 000-5zm4-6H4v3h8V3z"
+        fill="#222"
+        fillRule="evenodd"
+      />
+    </svg>
+  )
+}
