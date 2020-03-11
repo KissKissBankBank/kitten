43b6dc52733445c6ca53a036f183ceafa13b83b7
Morgan JARRY <morgan.jarry@gmail.com>
2020-03-11 17:02:31 +0100

    Allow white background on stories

--- assets/javascripts/kitten/components/navigation/header-nav/stories.js
+++ assets/javascripts/kitten/components/navigation/header-nav/stories.js
@@ -8,17 +8,20 @@ import { MinimalistHeaderNavStory } from './stories/minimalist'
 
 const Container = styled.div`
   height: 400vh;
-  background: linear-gradient(
-    0deg,
-    rgba(100, 100, 100, 1) 0%,
-    rgba(240, 240, 240, 1) 100%
-  );
+  background: ${({ whiteBg }) =>
+    whiteBg
+      ? 'white'
+      : `linear-gradient(
+        0deg,
+        rgba(100, 100, 100, 1) 0%,
+        rgba(240, 240, 240, 1) 100%
+      );`};
 `
 
 storiesOf('Navigation/HeaderNav', module)
   .addDecorator(withKnobs)
   .add('Lendopolis', () => (
-    <Container>
+    <Container whiteBg={boolean('White bg', false)}>
       <LendopolisHeaderNavStory
         isLogged={boolean('Is logged', false)}
         isFixed={boolean('Is fixed', false)}
@@ -27,7 +30,7 @@ storiesOf('Navigation/HeaderNav', module)
   ))
 
   .add('KissKissBankBank', () => (
-    <Container>
+    <Container whiteBg={boolean('White bg', false)}>
       <KissKissBankBankHeaderNavStory
         isLogged={boolean('Is logged', false)}
         isFixed={boolean('Is fixed', false)}
@@ -36,7 +39,7 @@ storiesOf('Navigation/HeaderNav', module)
   ))
 
   .add('Minimalist', () => (
-    <Container>
+    <Container whiteBg={boolean('White bg', false)}>
       <MinimalistHeaderNavStory
         isFixed={boolean('Is fixed', true)}
         text={text('Texte', 'Ma page mentor')}
