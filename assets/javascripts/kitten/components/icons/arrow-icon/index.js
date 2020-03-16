ea6f8cc06ec1f84a4f2b60a7175a9af5eae48fa6
Morgan JARRY <morgan.jarry@gmail.com>
2020-03-16 17:11:20 +0100

    Fix Pagination and ArrowIcon components

---
+++ assets/javascripts/kitten/components/icons/arrow-icon/index.js
@@ -0,0 +1,137 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { Deprecated } from '../../../helpers/utils/deprecated'
+import COLORS from '../../../constants/colors-config'
+
+export const DeprecatedArrowIconSvg = ({
+  className,
+  direction,
+  disabled,
+  version,
+  ...others
+}) => {
+  const arrowIconClassNames = classNames(
+    'k-ArrowIcon',
+    {
+      [`k-ArrowIcon--${direction}`]: direction,
+      'k-ArrowIcon--disabled': disabled,
+    },
+    className,
+  )
+
+  return (
+    <svg
+      {...others}
+      xmlns="http://www.w3.org/2000/svg"
+      className={arrowIconClassNames}
+      viewBox="0 0 6 6"
+    >
+      <title>Arrow</title>
+      <path d="M6 0H0v6h2V2h4z" />
+    </svg>
+  )
+}
+
+DeprecatedArrowIconSvg.propTypes = {
+  direction: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+}
+
+DeprecatedArrowIconSvg.defaultProps = {
+  direction: 'right',
+  disabled: false,
+  className: '',
+}
+
+export const DeprecatedArrowIcon = props => {
+  const warningMessage =
+    'The previous version of ArrowIcon does not handle ' +
+    'correctly the center of gravity of the arrow. Please use now the prop ' +
+    '`version` with the value `solid` to display an arrow with the right ' +
+    'center of gravity.'
+
+  return (
+    <Deprecated warningMessage={warningMessage}>
+      <DeprecatedArrowIconSvg {...props} />
+    </Deprecated>
+  )
+}
+
+DeprecatedArrowIcon.propTypes = {
+  direction: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+}
+
+DeprecatedArrowIcon.defaultProps = {
+  direction: 'right',
+  disabled: false,
+  className: '',
+}
+
+const SvgArrow = styled.svg`
+  transform: ${({ direction }) => {
+    switch (direction) {
+      case 'right':
+        return 'rotate(90deg)'
+      case 'left':
+        return 'rotate(-90deg)'
+      case 'bottom':
+        return 'rotate(180deg)'
+      default:
+        return 'none'
+    }
+  }};
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      fill: ${COLORS.background1};
+    `}
+`
+
+export const ArrowIcon = ({ version, direction, disabled, ...others }) => {
+  if (version === 'deprecated-center-of-gravity') {
+    return (
+      <DeprecatedArrowIcon
+        version={version}
+        direction={direction}
+        disabled={disabled}
+        {...others}
+      />
+    )
+  }
+
+  return (
+    <SvgArrow
+      {...others}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 8.48 5.64"
+      direction={direction}
+      disabled={disabled}
+    >
+      <path d="M0 4.24 L4.24,0 L8.48,4.24 L7.08,5.64 L4.24,2.77 L1.4,5.6 z" />
+    </SvgArrow>
+  )
+}
+
+ArrowIcon.propTypes = {
+  version: PropTypes.oneOf(['solid', 'deprecated-center-of-gravity']),
+  direction: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+}
+
+ArrowIcon.defaultProps = {
+  version: 'deprecated-center-of-gravity',
+  direction: 'right',
+  disabled: false,
+  className: '',
+  width: '8.48',
+  height: '5.64',
+}
--- assets/javascripts/kitten/components/navigation/pagination/index.js
+++ assets/javascripts/kitten/components/navigation/pagination/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { forwardRef } from 'react'
 import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 import { Text } from '../../../components/typography/text'
@@ -123,8 +123,6 @@ const StyledSvg = styled(ArrowIcon)`
   align-self: center;
   margin: 0;
   padding: 0;
-  width: ${pxToRem(6)};
-  height: ${pxToRem(6)};
   pointer-events: none;
   color: inherit;
   fill: currentColor;
@@ -178,143 +176,145 @@ export function pages(min, max, currentPage, availableSlots) {
   ]
 }
 
-class PaginationBase extends PureComponent {
-  static propTypes = {
-    prevButtonLabel: PropTypes.string,
-    nextButtonLabel: PropTypes.string,
-    goToPageLabel: PropTypes.func,
-    goToPageHref: PropTypes.func,
-    onPageClick: PropTypes.func,
-    totalPages: PropTypes.number,
-    currentPage: PropTypes.number,
-    currentPageLabel: PropTypes.func,
-    'aria-label': PropTypes.string,
-  }
-
-  static defaultProps = {
-    prevButtonLabel: 'Previous page',
-    nextButtonLabel: 'Next page',
-    goToPageLabel: n => `Go to page ${n}`,
-    goToPageHref: n => `#${n}`,
-    onPageClick: () => {},
-    currentPageLabel: n => `Page ${n}, this is the current page`,
-    currentPage: 1,
-    totalPages: 1,
-    'aria-label': 'Pagination navigation',
-  }
-
-  render() {
-    const { totalPages, currentPage } = this.props
-    const size = this.props.viewportIsTabletOrLess ? 5 : 7
+const PaginationBase = forwardRef(
+  (
+    {
+      prevButtonLabel,
+      nextButtonLabel,
+      goToPageLabel,
+      goToPageHref,
+      onPageClick,
+      currentPageLabel,
+      currentPage,
+      totalPages,
+      'aria-label': ariaLabelProp,
+      viewportIsMOrLess,
+    },
+    _ref,
+  ) => {
+    const size = viewportIsMOrLess ? 5 : 7
     const pageNumbers = pages(1, totalPages, currentPage, size)
+    const preventClickDefault = e => e.preventDefault()
 
-    return (
-      <nav role="navigation" aria-label={this.props['aria-label']}>
-        <StyledGroup>
-          {this.renderArrowButton('left')}
-          {pageNumbers.map(this.renderPage)}
-          {this.renderArrowButton('right')}
-        </StyledGroup>
-      </nav>
-    )
-  }
+    const pageClickHandler = number => event => onPageClick(number, event)
 
-  preventClickDefault = e => e.preventDefault()
+    const renderPage = (number, index) => {
+      if (!number) return renderSpacer(index)
 
-  pageClickHandler = number => event => this.props.onPageClick(number, event)
+      const isActive = number === currentPage
+      const tag = isActive ? 'span' : 'a'
+      const href = isActive ? null : goToPageHref(number)
 
-  renderPage = (number, index) => {
-    if (!number) return this.renderSpacer(index)
+      const ariaLabel = isActive
+        ? currentPageLabel(number)
+        : goToPageLabel(number)
 
-    const isActive = number === this.props.currentPage
-    const tag = isActive ? 'span' : 'a'
-    const href = isActive ? null : this.props.goToPageHref(number)
+      return (
+        <StyledList key={`page-${number}`}>
+          <StyledButtonIcon
+            tag={tag}
+            href={href}
+            key={`link-${number}`}
+            weight="regular"
+            decoration="none"
+            size="tiny"
+            isActive={isActive}
+            aria-current={isActive && 'page'}
+            aria-label={ariaLabel}
+            onClick={isActive ? null : pageClickHandler(number)}
+          >
+            {number}
+          </StyledButtonIcon>
+        </StyledList>
+      )
+    }
 
-    const ariaLabel = isActive
-      ? this.props.currentPageLabel(number)
-      : this.props.goToPageLabel(number)
-
-    return (
-      <StyledList key={`page-${number}`}>
-        <StyledButtonIcon
-          tag={tag}
-          href={href}
-          key={`link-${number}`}
-          weight="regular"
-          decoration="none"
-          size="tiny"
-          isActive={isActive}
-          aria-current={isActive && 'page'}
-          aria-label={ariaLabel}
-          onClick={isActive ? null : this.pageClickHandler(number)}
-        >
-          {number}
-        </StyledButtonIcon>
-      </StyledList>
-    )
-  }
-
-  renderSpacer(index) {
-    return (
+    const renderSpacer = index => (
       <StyledPoints key={`spacer-${index}`} aria-hidden="true">
         {'…'}
       </StyledPoints>
     )
-  }
 
-  renderArrowButton(direction) {
-    const {
-      prevButtonLabel,
-      nextButtonLabel,
-      currentPage,
-      totalPages,
-    } = this.props
+    const renderArrowButton = direction => {
+      const buttonLabel =
+        direction == 'left'
+          ? parseHtml(prevButtonLabel)
+          : parseHtml(nextButtonLabel)
 
-    const buttonLabel =
-      direction == 'left'
-        ? parseHtml(prevButtonLabel)
-        : parseHtml(nextButtonLabel)
+      const isDisabled =
+        direction == 'left' ? currentPage == 1 : currentPage == totalPages
 
-    const isDisabled =
-      direction == 'left' ? currentPage == 1 : currentPage == totalPages
+      const number =
+        direction == 'left'
+          ? currentPage == 1
+            ? 1
+            : currentPage - 1
+          : currentPage == totalPages
+          ? totalPages
+          : currentPage + 1
 
-    const number =
-      direction == 'left'
-        ? currentPage == 1
-          ? 1
-          : currentPage - 1
-        : currentPage == totalPages
-        ? totalPages
-        : currentPage + 1
+      return (
+        <StyledArrowIconDirection direction={direction}>
+          <StyledButtonIcon
+            tag="a"
+            href={goToPageHref(number)}
+            key={`link-${direction}`}
+            aria-label={buttonLabel}
+            aria-disabled={isDisabled}
+            title={buttonLabel}
+            isDisabled={isDisabled}
+            tabIndex={isDisabled ? -1 : null}
+            onClick={
+              isDisabled ? preventClickDefault : pageClickHandler(number)
+            }
+          >
+            <StyledSvg
+              direction={direction}
+              disabled={isDisabled}
+              aria-hidden="true"
+              version="solid"
+            />
+          </StyledButtonIcon>
+        </StyledArrowIconDirection>
+      )
+    }
 
     return (
-      <StyledArrowIconDirection direction={direction}>
-        <StyledButtonIcon
-          tag="a"
-          href={this.props.goToPageHref(number)}
-          key={`link-${direction}`}
-          aria-label={buttonLabel}
-          aria-disabled={isDisabled}
-          title={buttonLabel}
-          isDisabled={isDisabled}
-          tabIndex={isDisabled ? -1 : null}
-          onClick={
-            isDisabled
-              ? this.preventClickDefault
-              : this.pageClickHandler(number)
-          }
-        >
-          <StyledSvg
-            direction={direction}
-            disabled={isDisabled}
-            aria-hidden="true"
-          />
-        </StyledButtonIcon>
-      </StyledArrowIconDirection>
+      <nav role="navigation" aria-label={ariaLabelProp}>
+        <StyledGroup>
+          {renderArrowButton('left')}
+          {pageNumbers.map(renderPage)}
+          {renderArrowButton('right')}
+        </StyledGroup>
+      </nav>
     )
-  }
+  },
+)
+
+PaginationBase.propTypes = {
+  prevButtonLabel: PropTypes.string,
+  nextButtonLabel: PropTypes.string,
+  goToPageLabel: PropTypes.func,
+  goToPageHref: PropTypes.func,
+  onPageClick: PropTypes.func,
+  totalPages: PropTypes.number,
+  currentPage: PropTypes.number,
+  currentPageLabel: PropTypes.func,
+  'aria-label': PropTypes.string,
+}
+
+PaginationBase.defaultProps = {
+  prevButtonLabel: 'Previous page',
+  nextButtonLabel: 'Next page',
+  goToPageLabel: n => `Go to page ${n}`,
+  goToPageHref: n => `#${n}`,
+  onPageClick: () => {},
+  currentPageLabel: n => `Page ${n}, this is the current page`,
+  currentPage: 1,
+  totalPages: 1,
+  'aria-label': 'Pagination navigation',
 }
 
 export const Pagination = mediaQueries(PaginationBase, {
-  viewportIsTabletOrLess: true,
+  viewportIsMOrLess: true,
 })
