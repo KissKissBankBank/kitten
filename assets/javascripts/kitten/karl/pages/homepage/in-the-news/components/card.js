54b5ae937dd469df1f8664074c85767cf6fb87ef
Camille Delbove <camille.delbove@gmail.com>
2020-03-24 18:48:32 +0100

    image styles

--- assets/javascripts/kitten/karl/pages/homepage/in-the-news/components/card.js
+++ assets/javascripts/kitten/karl/pages/homepage/in-the-news/components/card.js
@@ -6,19 +6,28 @@ import { Container } from '../../../../../components/grid/container'
 import { Grid, GridCol } from '../../../../../components/grid/grid'
 import { Title } from '../../../../../components/typography/title'
 import { Text } from '../../../../../components/typography/text'
+import { ScreenConfig } from '../../../../../constants/screen-config'
+
+const StyledImage = styled.img`
+  margin-bottom: ${pxToRem(20)};
+
+  @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+    margin-bottom: ${pxToRem(30)};
+  }
+`
 
 const Card = () => {
   return (
     <Container>
       <Grid>
         <GridCol col="6">
-          <img
+          <StyledImage
             alt=""
-            style={{ width: '100%' }}
+            style={{ width: '100%', display: 'block' }}
             src="https://via.placeholder.com/150x75"
           />
           <Title modifier="senary">Keep It Silky</Title>
-          <Text>
+          <Text tag="p">
             Soutenez ma première participation au Salon de la Lingerie de Paris
             et aidez moi à développer mon réseau commercial ainsi que mon image.
           </Text>
