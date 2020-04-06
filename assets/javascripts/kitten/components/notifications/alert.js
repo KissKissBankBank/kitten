ddb201161af14ca5503cc2a1eb67684b76dd12ad
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2020-04-06 09:48:05 +0200

    [Alert] Fix onAfterClose error (#1984)

--- assets/javascripts/kitten/components/notifications/alert.js
+++ assets/javascripts/kitten/components/notifications/alert.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
+import PropTypes from 'prop-types'
 import styled, { css, keyframes } from 'styled-components'
 import { CloseButton } from '../../components/buttons/close-button'
 import COLORS from '../../constants/colors-config'
@@ -115,6 +116,17 @@ export const Alert = ({
   )
 }
 
+Alert.propTypes = {
+  className: PropTypes.string,
+  show: PropTypes.bool,
+  error: PropTypes.bool,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+  closeButton: PropTypes.bool,
+  closeButtonLabel: PropTypes.string,
+  onAfterClose: PropTypes.func,
+}
+
 Alert.defaultProps = {
   className: null,
   show: true,
@@ -123,4 +135,5 @@ Alert.defaultProps = {
   warning: false,
   closeButton: false,
   closeButtonLabel: 'Close',
+  onAfterClose: () => {},
 }
