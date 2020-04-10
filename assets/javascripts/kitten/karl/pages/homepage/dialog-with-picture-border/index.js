b78d0f9c903b2b88302c164c4579917abe68d8b4
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-04-10 18:18:10 +0200

    Finesse things

--- assets/javascripts/kitten/karl/pages/homepage/dialog-with-picture-border/index.js
+++ assets/javascripts/kitten/karl/pages/homepage/dialog-with-picture-border/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import styled from 'styled-components'
 import { Title } from '../../../../components/typography/title'
 import { Paragraph } from '../../../../components/typography/paragraph'
@@ -10,6 +10,10 @@ import {
   CONTAINER_PADDING_THIN,
   CONTAINER_PADDING,
 } from '../../../../constants/grid-config'
+import { HorizontalStroke } from '../../../../components/layout/horizontal-stroke'
+import { Button } from '../../../../components/buttons/button/button'
+import { cssPropertyDistributor } from '../../../../helpers/dom/css-property-distributor'
+import domElementHelper from '../../../../helpers/dom/element-helper'
 
 const oneGridCol = `calc((100vw - (2 * ${pxToRem(CONTAINER_PADDING)})) / 12)`
 const oneGridColThin = `calc((100vw - (2 * ${pxToRem(CONTAINER_PADDING_THIN)})) / 12)`
@@ -59,6 +63,8 @@ const StyledContent = styled.div`
   padding: 0 ${pxToRem(GUTTER / 2)};
   margin: 0 ${oneGridColThin};
 
+  text-align: center;
+
   @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
     margin: 0 ${pxToRem(oneGridCol)};
   }
@@ -68,21 +74,91 @@ const StyledContent = styled.div`
   @media (min-width: ${pxToRem(ScreenConfig.XL.min)}) {
     margin: 0 calc(2 * ${oneGridColXl});
   }
-
 `
 
+const StyledHorizontalStroke = styled(HorizontalStroke)`
+  width: ${pxToRem(30)};
+  height: ${pxToRem(4)};
 
-const DialogWithPictureBorder = () => (
-  <StyledSection>
-    <StyledCard>
-      <StyledContent>
-        <Title>L’idée, c’est de se lancer !</Title>
-        <Paragraph>
-          Citoyenneté, optimiste et indépendance. Créé en 2009, KissKissBankBank, pionnier du financement participatif en France, reste fidèle à ses trois valeurs fondamentales. Notre ambition ? Permettre à toutes et tous de financer ses projets créatifs, associatifs et entrepreneuriaux. Depuis 10 ans, la force du collectif libère la créativité et le crowdfunding est devenu une fabrique à optimisme.
-        </Paragraph>
-      </StyledContent>
-    </StyledCard>
-  </StyledSection>
-)
+  margin: ${pxToRem(15)} auto ${pxToRem(30)};
+`
+
+const StyledParagraph = styled(Paragraph)`
+  margin-bottom: ${pxToRem(40)};
+
+  @media (max-width: ${pxToRem(ScreenConfig.XS.max)}) {
+    margin-bottom: ${pxToRem(30)};
+    font-size: ${stepToRem(-2)};
+  }
+`
+
+const StyledButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: ${pxToRem(ScreenConfig.XS.max)}) {
+    flex-direction: column-reverse;
+  }
+
+  & > :last-child {
+    margin: 0 0 ${pxToRem(GUTTER)} 0;
+
+    @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+      margin: 0 0 0 ${pxToRem(GUTTER)};
+    }
+  }
+`
+
+const StyledButtonWidthSetter = styled.span`
+  @media (max-width: ${pxToRem(ScreenConfig.XS.max)}) {
+    width: 100% !important;
+  }
+`
+
+const StyledButton = styled(Button)`
+  @media (max-width: ${pxToRem(ScreenConfig.XS.max)}) {
+    width: 100%;
+  }
+`
+
+const DialogWithPictureBorder = () => {
+  const button_1 = useRef(null)
+  const button_2 = useRef(null)
+
+  useEffect(() => {
+    if (domElementHelper.canUseDom() && typeof document !== 'undefined') {
+      document.fonts.ready.then(() => cssPropertyDistributor({
+        elements: [button_1.current, button_2.current],
+        property: 'width',
+        direction: 'max',
+      })
+    )}
+  }, [])
+
+  return (
+    <StyledSection>
+      <StyledCard>
+        <StyledContent>
+          <Title modifier="quaternary" tag="h2" margin={false}>
+            L’idée, c’est de se lancer !
+          </Title>
+          <StyledHorizontalStroke />
+          <StyledParagraph className="k-u-margin-none" modifier="quaternary">
+            Citoyenneté, optimiste et indépendance. Créé en 2009, KissKissBankBank, pionnier du financement participatif en France, reste fidèle à ses trois valeurs fondamentales. Notre ambition ? Permettre à toutes et tous de financer ses projets créatifs, associatifs et entrepreneuriaux. Depuis 10 ans, la force du collectif libère la créativité et le crowdfunding est devenu une fabrique à optimisme.
+          </StyledParagraph>
+          <StyledButtonContainer>
+            <StyledButtonWidthSetter ref={button_1}>
+              <Button modifier="hydrogen" big fluid>
+                Lancez votre projet
+              </Button></StyledButtonWidthSetter>
+            <StyledButtonWidthSetter ref={button_2}>
+              <Button modifier="helium" big fluid>
+                Découvrez nos valeurs
+              </Button></StyledButtonWidthSetter>
+          </StyledButtonContainer>
+        </StyledContent>
+      </StyledCard>
+    </StyledSection>
+)}
 
 export default DialogWithPictureBorder
