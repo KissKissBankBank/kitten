37d3f89154ae3fc5cb132eb27d2c878c9e09aec3
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-04-14 18:05:18 +0200

    [RemoveRadium] Remove from EmbedPlayer

--- assets/javascripts/kitten/components/media/embed-player.js
+++ assets/javascripts/kitten/components/media/embed-player.js
@@ -1,117 +1,145 @@
-import React, { Component } from 'react'
+import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import COLORS from '../../constants/colors-config'
-import Radium, { StyleRoot } from 'radium'
 import { Text } from '../../components/typography/text'
 import { ResponsiveIframeContainer } from '../../components/layout/responsive-iframe-container'
 import { parseHtml } from '../../helpers/utils/parser'
 import { ScreenConfig } from '../../constants/screen-config'
+import { pxToRem } from '../../helpers/utils/typography'
 
-const PlayerButton = props => (
-  <StyleRoot>
-    <div style={styles.playerButton}>
-      <Text size="default" weight="regular" aria-label={props.playButtonLabel}>
-        ►
-      </Text>
-    </div>
-  </StyleRoot>
-)
+import styled, {css} from 'styled-components'
 
-PlayerButton.propTypes = {
-  playButtonLabel: PropTypes.string.isRequired,
-}
+const playerButtonSize = 90
+const playerButtonXSSize = 70
 
-class EmbedPlayerBase extends Component {
-  constructor(props) {
-    super(props)
+const StyledEmbedPlayer = styled.div`
+    position: relative;
+    display: block;
+    width: 100%;
 
-    this.state = { showPlayer: false }
+  .EmbedPlayer__thumbnail {
+      display: block;
+      width: 100%;
   }
 
-  handleClick = () => {
-    this.setState({ showPlayer: true })
-    this.previewVideo.blur()
+  .EmbedPlayer__embededPlayer {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
   }
 
-  handleKeyPress = event => {
+  .EmbedPlayer__playerButton {
+    width: ${pxToRem(playerButtonXSSize)};
+    height: ${pxToRem(playerButtonXSSize)};
+    top: calc(50% - ${pxToRem(playerButtonXSSize / 2)});
+    left: calc(50% - ${pxToRem(playerButtonXSSize / 2)});
+    background: ${COLORS.background1};
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+      width: ${pxToRem(playerButtonSize)};
+      height: ${pxToRem(playerButtonSize)};
+      top: calc(50% - ${pxToRem(playerButtonSize / 2)});
+      left: calc(50% - ${pxToRem(playerButtonSize / 2)});
+    }
+  }
+
+  .EmbedPlayer__playerPreview {
+    position: relative;
+    transition: opacity ease 600ms;
+    z-index: 1;
+    opacity: 1;
+
+    ${({isVideoPlaying}) => isVideoPlaying && css`
+      opacity: 0;
+      z-index: 0;
+    `}
+    ${({isCursorPointer}) => isCursorPointer && css`
+      cursor: pointer;
+    `}
+  }
+`
+
+export const EmbedPlayer = ({
+    ratio,
+    previewProps: { thumbnail, badgeComponent },
+    iframeHtml,
+    playButtonLabel,
+    style: mainStyle = void 0,
+    className,
+    ...others
+  }) => {
+  const [isPlayerVisible, setPlayerVisibility] = useState(false)
+  const previewVideo = useRef(null)
+
+  const handleClick = () => {
+    setPlayerVisibility(true)
+    previewVideo.current.blur()
+  }
+
+  const handleKeyPress = event => {
     event.preventDefault()
     const enterKey = event.key === 'Enter'
     const spaceKey = event.key === ' '
 
-    if (enterKey || spaceKey) this.handleClick()
+    if (enterKey || spaceKey) handleClick()
   }
 
-  handleFocus = event => {
+  const handleFocus = event => {
     event.preventDefault()
-    this.previewVideo.focus()
-    this.handleKeyPress(event)
+    previewVideo.current.focus()
+    handleKeyPress(event)
   }
 
-  a11yOnClickProps = () => {
-    if (!this.props.iframeHtml) return
+  const validRatio = parseInt(ratio, 10)
+  const hasIframeHtml = !!iframeHtml
 
-    return {
-      onClick: this.handleClick,
-      onKeyPress: this.handleKeyPress,
-      onFocus: this.handleFocus,
-      role: 'button',
-      tabIndex: 0,
-    }
-  }
-
-  render() {
-    const {
-      ratio,
-      previewProps,
-      iframeHtml,
-      playButtonLabel,
-      style: mainStyle = void 0,
-    } = this.props
-
-    const validRatio = parseInt(ratio, 10)
-    const { thumbnail, badgeComponent } = previewProps
-    const isVideoPlaying = iframeHtml && this.state.showPlayer
-    const playerPreviewStyle = [
-      styles.player.base,
-      iframeHtml ? { cursor: 'pointer' } : null,
-      isVideoPlaying ? styles.player.hide : styles.player.show,
-    ]
-
-    return (
-      <StyleRoot>
-        <div
-          style={{ ...mainStyle, ...styles.container }}
-          ref={node => {
-            this.previewVideo = node
-          }}
-          {...this.a11yOnClickProps()}
-        >
-          <div style={playerPreviewStyle}>
-            {iframeHtml && <PlayerButton playButtonLabel={playButtonLabel} />}
-
-            <img
-              style={styles.thumbnail}
-              src={thumbnail.src}
-              alt={thumbnail.alt}
-            />
-
-            {badgeComponent}
+  return (
+    <StyledEmbedPlayer
+      ref={previewVideo}
+      style={{ ...mainStyle }}
+      onClick={hasIframeHtml ? handleClick : null}
+      onKeyPress={hasIframeHtml ? handleKeyPress : null}
+      onFocus={hasIframeHtml ? handleFocus : null}
+      role={hasIframeHtml ? 'button' : null}
+      tabIndex={hasIframeHtml ? 0 : null}
+      isCursorPointer={hasIframeHtml}
+      isVideoPlaying={hasIframeHtml && isPlayerVisible}
+      {...others}
+    >
+      <div className="EmbedPlayer__playerPreview">
+        {hasIframeHtml && (
+          <div className="EmbedPlayer__playerButton">
+            <Text size="default" weight="regular" aria-label={playButtonLabel}>
+              ►
+            </Text>
           </div>
+        )}
 
-          {iframeHtml && (
-            <div style={styles.embedPlayer}>
-              <ResponsiveIframeContainer ratio={validRatio}>
-                {parseHtml(iframeHtml)}
-              </ResponsiveIframeContainer>
-            </div>
-          )}
+        <img
+          {...thumbnail}
+          className={`EmbedPlayer__thumbnail ${thumbnail.className || ''}`}
+        />
+
+        {badgeComponent}
+      </div>
+
+      {hasIframeHtml && isPlayerVisible && (
+        <div className="EmbedPlayer__embededPlayer">
+          <ResponsiveIframeContainer ratio={validRatio}>
+            {parseHtml(iframeHtml)}
+          </ResponsiveIframeContainer>
         </div>
-      </StyleRoot>
-    )
-  }
+      )}
+    </StyledEmbedPlayer>
+  )
 }
 
-EmbedPlayerBase.propTypes = {
+EmbedPlayer.propTypes = {
   previewProps: PropTypes.shape({
     thumbnail: PropTypes.shape({
       src: PropTypes.string.isRequired,
@@ -125,7 +153,7 @@ EmbedPlayerBase.propTypes = {
   iframeHtml: PropTypes.string,
 }
 
-EmbedPlayerBase.defaultProps = {
+EmbedPlayer.defaultProps = {
   previewProps: {
     thumbnail: {
       style: {},
@@ -134,62 +162,3 @@ EmbedPlayerBase.defaultProps = {
   badgeComponent: null,
   iframeHtml: null,
 }
-
-const playerButtonSize = 90
-const playerButtonXSSize = 50
-
-const styles = {
-  container: {
-    position: 'relative',
-    display: 'block',
-    width: '100%',
-  },
-
-  thumbnail: {
-    display: 'block',
-    width: '100%',
-  },
-
-  embedPlayer: {
-    position: 'absolute',
-    top: 0,
-    width: '100%',
-    height: '100%',
-  },
-
-  playerButton: {
-    width: `${playerButtonSize}px`,
-    height: `${playerButtonSize}px`,
-    background: COLORS.background1,
-    position: 'absolute',
-    top: `calc(50% - ${playerButtonSize / 2}px)`,
-    left: `calc(50% - ${playerButtonSize / 2}px)`,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-    [`@media (max-width: ${ScreenConfig.XS.max}px)`]: {
-      width: `${playerButtonXSSize}px`,
-      height: `${playerButtonXSSize}px`,
-      top: `calc(50% - ${playerButtonXSSize / 2}px)`,
-      left: `calc(50% - ${playerButtonXSSize / 2}px)`,
-    },
-  },
-
-  player: {
-    base: {
-      position: 'relative',
-      transition: 'opacity ease 600ms, z-index ease 600ms',
-      zIndex: 1,
-    },
-    show: {
-      opacity: 1,
-    },
-    hide: {
-      opacity: 0,
-      zIndex: 0,
-    },
-  },
-}
-
-export const EmbedPlayer = Radium(EmbedPlayerBase)
