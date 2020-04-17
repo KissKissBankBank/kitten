29a2f11aa33315c38449aafc1e93e896bc185d8f
Joachim <joachim.robert@kisskissbankbank.com>
2020-04-17 11:38:38 +0200

    [Homepage] Video section (#2005)

---
+++ assets/javascripts/kitten/karl/pages/homepage/video/stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Video from './index'
+
+storiesOf('Pages/Homepage', module).add('Video', () => {
+  return <Video />
+})
