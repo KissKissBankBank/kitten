685459341dc13fea6ca276f964d761ffafb8cba7
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-04-23 13:31:31 +0200

    Fix FeaturedProjects

--- assets/javascripts/kitten/karl/pages/homepage/featured-projects/stories.js
+++ assets/javascripts/kitten/karl/pages/homepage/featured-projects/stories.js
@@ -2,6 +2,21 @@ import React from 'react'
 import { withKnobs } from '@storybook/addon-knobs'
 import { FeaturedProjects } from './index'
 import { Container } from '../../../../components/grid/container'
+import styled from 'styled-components'
+import { stepToRem, pxToRem } from '../../../../helpers/utils/typography'
+import { ScreenConfig } from '../../../../constants/screen-config'
+
+const StyledContainer = styled(Container)`
+  margin-top: 1rem;
+
+  .Homepage__section__title {
+    font-size: ${stepToRem(-1)};
+
+    @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+      font-size: ${stepToRem(0)};
+    }
+  }
+`
 
 export default {
   component: FeaturedProjects,
@@ -13,7 +28,7 @@ export default {
 }
 
 export const DefaultProps = () => (
-  <Container>
+  <StyledContainer>
     <FeaturedProjects />
-  </Container>
+  </StyledContainer>
 )
