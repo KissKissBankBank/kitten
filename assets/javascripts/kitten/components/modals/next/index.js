ed589477ca99daf83dadbf5cdb3a592a1deef121
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-04-28 21:02:39 +0200

    init next modal

---
+++ assets/javascripts/kitten/components/modals/next/index.js
@@ -0,0 +1,298 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import ReactModal from 'react-modal'
+import { CloseButton } from '../../../components/buttons/close-button'
+import { Button } from '../../../components/buttons/button/button'
+import { Paragraph } from '../../../components/typography/paragraph'
+import { Grid, GridCol } from '../../../components/grid/grid'
+import { Container } from '../../../components/grid/container'
+import styled, { createGlobalStyle } from 'styled-components'
+import { pxToRem } from '../../../helpers/utils/typography'
+import { ScreenConfig } from '../../../constants/screen-config'
+import { Title } from '../../typography/title'
+import COLORS from '../../../constants/colors-config'
+import {
+  CONTAINER_PADDING,
+  GUTTER,
+  CONTAINER_MAX_WIDTH,
+} from '../../../constants/grid-config'
+
+const paddingPlusGutters = 2 * CONTAINER_PADDING + 11 * GUTTER
+const oneGridCol = `calc((100vw - ${pxToRem(
+  paddingPlusGutters,
+)}) / 12 + ${pxToRem(GUTTER)})`
+const oneGridColXl = `${pxToRem(
+  (CONTAINER_MAX_WIDTH - paddingPlusGutters) / 12 + GUTTER,
+)}`
+
+const ModalLayout = styled.div`
+  position: relative;
+  background-color: ${COLORS.background1};
+  box-sizing: border-box;
+  margin: ${pxToRem(50)} ${pxToRem(20)};
+  padding: ${pxToRem(50)} ${pxToRem(30)};
+
+  @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+    margin: ${pxToRem(100)} auto;
+    padding: ${pxToRem(80)} ${oneGridCol};
+  }
+
+  @media (min-width: ${pxToRem(ScreenConfig.XL.min)}) {
+    margin: ${pxToRem(100)} auto;
+    padding: ${pxToRem(80)} ${oneGridColXl};
+  }
+`
+
+const StyledParagraph = styled(Paragraph)`
+  font-size: ${pxToRem(12)};
+  @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+    font-size: ${pxToRem(14)};
+  }
+`
+
+const GlobalStyle = createGlobalStyle`
+  body.k-Modal__body--open {
+    overflow: hidden;
+  }
+
+  .k-Modal__close {
+    position: absolute;
+    top: 0;
+    right: ${pxToRem(40)};
+    @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+     right: ${pxToRem(50)};
+  }
+
+    button {
+      margin: 0;
+    }
+  }
+  
+  .k-Modal-content {
+    transform: scale(0.94);
+  }
+
+  .k-Modal__overlay {
+    position: fixed;
+    z-index: 110;
+    overflow: scroll;
+    display: block;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(34, 34, 34, .8);
+  }
+  
+  .k-Modal__overlay--afterOpen {
+    transition: opacity .3s ease;
+    opacity: 1;
+  }
+  .k-Modal--afterOpen {
+    transition: opacity .3s ease, transform .3s ease;
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  .k-Modal__overlay--beforeClose {
+    opacity: 0;
+  }
+  .k-Modal--beforeClose {
+    transition: opacity .3s ease, transform .5s ease;
+    transform: scale(1.06);
+    opacity: 0;
+  }
+`
+
+const ModalTitle = ({ children }) => (
+  <Title
+    modifier="quaternary"
+    margin={false}
+    tag="p"
+    className="k-u-margin-bottom-singleHalf--important k-u-align-center"
+  >
+    {children}
+  </Title>
+)
+
+const ModalParagraph = ({ children }) => (
+  <StyledParagraph
+    modifier="quaternary"
+    margin={false}
+    tag="p"
+    className="k-u-margin-bottom-triple k-u-margin-bottom-quadruple@s-up"
+  >
+    {children}
+  </StyledParagraph>
+)
+
+const Actions = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+    flex-direction: row;
+  }
+`
+
+const ModalButton = styled(props => <Button big fluid {...props} />)`
+  :not(:last-child) {
+    margin-bottom: ${pxToRem(20)};
+  }
+  @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
+    :not(:last-child) {
+      margin-bottom: 0;
+    }
+    &:not(:first-child) {
+      margin-left: ${pxToRem(GUTTER)};
+    }
+  }
+`
+
+export const Modal = ({
+  trigger,
+  children,
+  label,
+  labelledby,
+  describedby,
+  className,
+  closeButtonLabel,
+  onClose,
+  modalProps,
+  hasCloseButton,
+  maxWidth,
+  big,
+  huge,
+  ...others
+}) => {
+  const [showModal, setShowModal] = useState(false)
+  const colsOnDesktop = huge ? 10 : big ? 8 : 6
+  const offsetOnDesktop = huge ? 1 : big ? 2 : 3
+  const close = () => {
+    setShowModal(false)
+    if (onClose) {
+      onClose()
+    }
+  }
+  useEffect(() => {
+    if (!trigger) {
+      setShowModal(true)
+    }
+  }, [])
+  const Layout = (
+    <ModalLayout>
+      {children}
+      {hasCloseButton && (
+        <div className="k-Modal__close">
+          <CloseButton
+            style={{ position: 'fixed' }}
+            className="k-u-hidden@s-up"
+            modifier="beryllium"
+            onClick={close}
+            size="tiny"
+            closeButtonLabel={closeButtonLabel}
+          />
+          <CloseButton
+            style={{ position: 'fixed' }}
+            className="k-u-hidden@xs-down"
+            modifier="beryllium"
+            onClick={close}
+            closeButtonLabel={closeButtonLabel}
+          />
+        </div>
+      )}
+    </ModalLayout>
+  )
+  const ModalPortal = ReactDOM.createPortal(
+    <div
+      id="modal-overlay"
+      onClick={e => {
+        if (
+          e.target.dataset.overlay ||
+          (e.target.className.indexOf &&
+            e.target.className.indexOf('k-Modal-content') !== -1)
+        ) {
+          close()
+        }
+      }}
+    >
+      <GlobalStyle />
+      <ReactModal
+        closeTimeoutMS={500}
+        role="dialog"
+        className={{
+          base: 'k-Modal-content',
+          afterOpen: 'k-Modal--afterOpen',
+          beforeClose: 'k-Modal--beforeClose',
+        }}
+        overlayClassName={{
+          base: 'k-Modal__overlay',
+          afterOpen: 'k-Modal__overlay--afterOpen',
+          beforeClose: 'k-Modal__overlay--beforeClose',
+        }}
+        isOpen={showModal}
+        aria={{
+          labelledby,
+          describedby,
+        }}
+        ariaHideApp={false}
+        onRequestClose={close}
+        contentLabel={label}
+        bodyOpenClassName="k-Modal__body--open"
+        {...modalProps}
+      >
+        <Container fullWidthBelowScreenSize="XS" data-overlay="container">
+          <Grid className="k-u-hidden@xs-down" data-overlay="grid">
+            <GridCol
+              col={12}
+              col-l={colsOnDesktop}
+              offset-l={offsetOnDesktop}
+              data-overlay="grid-col"
+            >
+              {Layout}
+            </GridCol>
+          </Grid>
+          <div className="k-u-hidden@s-up">{Layout}</div>
+        </Container>
+      </ReactModal>
+    </div>,
+    document.body,
+  )
+  return (
+    <div className={classNames('k-Modal', className)} {...others}>
+      {trigger && <span onClick={() => setShowModal(true)}>{trigger}</span>}
+      {ModalPortal}
+    </div>
+  )
+}
+
+Modal.propTypes = {
+  label: PropTypes.string,
+  labelledby: PropTypes.string,
+  describedby: PropTypes.string,
+  closeButtonLabel: PropTypes.string,
+  modalProps: PropTypes.object,
+  hasCloseButton: PropTypes.bool,
+  big: PropTypes.boolean,
+  huge: PropTypes.boolean,
+}
+
+Modal.defaultProps = {
+  label: 'Modal',
+  labelledby: '',
+  describedby: '',
+  closeButtonLabel: 'Fermer',
+  modalProps: {},
+  hasCloseButton: true,
+  big: false,
+  huge: false,
+}
+
+Modal.Title = ModalTitle
+Modal.Paragraph = ModalParagraph
+Modal.Actions = Actions
+Modal.Button = ModalButton
---
+++ assets/javascripts/kitten/components/modals/next/stories.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+import { Modal } from './index'
+import { Button } from '../../../components/buttons/button/button'
+
+const paragraphContainer = `
+  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+  quae ab illo inventore veritatis et quasi architecto beatae vitae
+  dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+  aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+  eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+  qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+  sed quia non numquam eius modi tempora incidunt ut labore et dolore
+  magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+  nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+  aliquid ex ea commodi consequatur? Quis autem vel eum iure
+  reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+  consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+  pariatur? Sed ut perspiciatis unde omnis iste natus error sit
+  voluptatem accusantium doloremque laudantium, totam rem aperiam,
+  eaque ipsa quae ab illo inventore veritatis et quasi architecto
+  beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+  voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+  magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
+  quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+  adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+  labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
+  minima veniam, quis nostrum exercitationem ullam corporis suscipit
+  laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+  vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+  molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+  nulla pariatur?
+`
+
+export default {
+  title: 'NEXT/Modal',
+  decorators: [withKnobs],
+  component: Modal,
+}
+
+export const OneButton = () => (
+  <Modal
+    trigger={<Button modifier="helium">Open</Button>}
+    hasCloseButton={boolean('Has close button', true)}
+    big={boolean('Big size', false)}
+    huge={boolean('Huge size', false)}
+  >
+    <Modal.Title>Lorem ipsum dolor sit consectetuer</Modal.Title>
+    <Modal.Paragraph>{text('content', paragraphContainer)}</Modal.Paragraph>
+    <Modal.Actions>
+      <Modal.Button modifier="helium">Action 1 Button</Modal.Button>
+    </Modal.Actions>
+  </Modal>
+)
+
+export const TwoButton = () => (
+  <Modal
+    trigger={<Button modifier="helium">Open</Button>}
+    hasCloseButton={boolean('Has close button', true)}
+    big={boolean('Big size', true)}
+    huge={boolean('Huge size', false)}
+  >
+    <Modal.Title>Lorem ipsum dolor sit consectetuer</Modal.Title>
+    <Modal.Paragraph>{text('content', paragraphContainer)}</Modal.Paragraph>
+    <Modal.Actions>
+      <Modal.Button modifier="helium">Action 1 Button</Modal.Button>
+      <Modal.Button modifier="oxygen">Action 2 Button</Modal.Button>
+    </Modal.Actions>
+  </Modal>
+)
+
+export const LaunchOnMount = () => (
+  <Modal
+    hasCloseButton={boolean('Has close button', true)}
+    big={boolean('Big size', false)}
+    huge={boolean('Huge size', false)}
+  >
+    <Modal.Title>Lorem ipsum dolor sit consectetuer</Modal.Title>
+    <Modal.Paragraph>{text('content', paragraphContainer)}</Modal.Paragraph>
+    <Modal.Actions>
+      <Modal.Button modifier="helium">Action 1 Button</Modal.Button>
+    </Modal.Actions>
+  </Modal>
+)
