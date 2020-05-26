ec264aff47f35f5d7c2e6f82a5162fb2830df8ea
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-05-26 11:41:53 +0200

    [ProgressRing] Fix bug on Chrome

--- assets/javascripts/kitten/components/meters/progress-ring/index.js
+++ assets/javascripts/kitten/components/meters/progress-ring/index.js
@@ -18,23 +18,14 @@ const rotateAnimate = ({ r, progressValue }) => keyframes`
   }
 `
 
-const StyledCircleBackground = styled.circle`
-  fill: transparent;
-  stroke: ${COLORS.line1};
-  stroke-width: ${({ strokeWidth }) => pxToRem(strokeWidth)};
-`
-
-const StyledCircle = styled.circle`
-  fill: transparent;
-  stroke-width: ${({ strokeWidth }) => pxToRem(strokeWidth)};
+const StyledMeterCircle = styled.circle`
   stroke-linecap: butt;
   stroke-dasharray: ${({ r }) => getDashLength(r)};
   stroke-dashoffset: ${({ r, progressValue }) =>
     getDashOffset({ r, progressValue })};
-  stroke: ${({ progressColor }) => progressColor};
-  transform: rotate(-90deg);
-  transform-origin: 50% 50%;
   animation: ${rotateAnimate} 1.4s ease-out;
+  transform: rotate(-90deg);
+  transform-origin: ${({ cx, cy }) => `${pxToRem(cx)} ${pxToRem(cy)}`};
 `
 
 export const ProgressRing = ({
@@ -61,19 +52,24 @@ export const ProgressRing = ({
       viewBox={viewBox}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <StyledCircleBackground
+      <circle
+        className="ProgressRing__backgroundCircle"
         cx={circleX}
         cy={circleY}
         r={radius}
-        strokeWidth={strokeWidth}
+        stroke-width={strokeWidth}
+        fill="transparent"
+        stroke={COLORS.line1}
       />
-      <StyledCircle
-        progressColor={color}
-        progressValue={progressValue}
+      <StyledMeterCircle
+        className="ProgressRing__meterCircle"
         cx={circleX}
         cy={circleY}
         r={radius}
         strokeWidth={strokeWidth}
+        fill="transparent"
+        stroke={color}
+        progressValue={progressValue}
       />
     </svg>
   )
