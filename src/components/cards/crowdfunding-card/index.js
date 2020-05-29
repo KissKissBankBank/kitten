a29543b9739bb44b5d9cb90dcb4e7dee0ef335e6
Morgan JARRY <morgan.jarry@gmail.com>
2020-05-29 17:57:21 +0200

    add built js files for 2.73.0

--- src/components/cards/crowdfunding-card/index.js
+++ src/components/cards/crowdfunding-card/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
@@ -13,17 +11,7 @@ var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends")
 
 var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
 
-var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
-
-var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
-
-var _inherits2 = _interopRequireDefault(require("@babel/runtime/helpers/inherits"));
-
-var _possibleConstructorReturn2 = _interopRequireDefault(require("@babel/runtime/helpers/possibleConstructorReturn"));
-
-var _getPrototypeOf2 = _interopRequireDefault(require("@babel/runtime/helpers/getPrototypeOf"));
-
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
@@ -45,122 +33,98 @@ var _styles = require("./styles");
 
 var _text = require("../../../components/typography/text");
 
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = (0, _getPrototypeOf2.default)(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = (0, _getPrototypeOf2.default)(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return (0, _possibleConstructorReturn2.default)(this, result); }; }
+var CrowdfundingCard = function CrowdfundingCard(_ref) {
+  var additionalInfo = _ref.additionalInfo,
+      avatarProps = _ref.avatarProps,
+      cardSubTitle = _ref.cardSubTitle,
+      cardTitle = _ref.cardTitle,
+      className = _ref.className,
+      href = _ref.href,
+      imageContainerBackground = _ref.imageContainerBackground,
+      imageProps = _ref.imageProps,
+      info1 = _ref.info1,
+      info2 = _ref.info2,
+      info3 = _ref.info3,
+      loading = _ref.loading,
+      ownerDescription = _ref.ownerDescription,
+      ownerTitle = _ref.ownerTitle,
+      progress = _ref.progress,
+      progressColor = _ref.progressColor,
+      imageContainerRatio = _ref.imageContainerRatio,
+      state = _ref.state,
+      stretch = _ref.stretch,
+      subTitleTruncate = _ref.subTitleTruncate,
+      titleProps = _ref.titleProps,
+      titlesMinHeight = _ref.titlesMinHeight,
+      titleTruncate = _ref.titleTruncate,
+      widgetState = _ref.widgetState,
+      others = (0, _objectWithoutProperties2.default)(_ref, ["additionalInfo", "avatarProps", "cardSubTitle", "cardTitle", "className", "href", "imageContainerBackground", "imageProps", "info1", "info2", "info3", "loading", "ownerDescription", "ownerTitle", "progress", "progressColor", "imageContainerRatio", "state", "stretch", "subTitleTruncate", "titleProps", "titlesMinHeight", "titleTruncate", "widgetState"]);
 
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+  var removeCurrentFocus = function removeCurrentFocus() {
+    document.activeElement.blur();
+  };
 
-var CrowdfundingCard = /*#__PURE__*/function (_PureComponent) {
-  (0, _inherits2.default)(CrowdfundingCard, _PureComponent);
-
-  var _super = _createSuper(CrowdfundingCard);
-
-  function CrowdfundingCard() {
-    var _this;
-
-    (0, _classCallCheck2.default)(this, CrowdfundingCard);
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _super.call.apply(_super, [this].concat(args));
-
-    _this.removeCurrentFocus = function () {
-      document.activeElement.blur();
-    };
-
-    return _this;
-  }
-
-  (0, _createClass2.default)(CrowdfundingCard, [{
-    key: "render",
-    value: function render() {
-      var _this$props = this.props,
-          additionalInfo = _this$props.additionalInfo,
-          avatarProps = _this$props.avatarProps,
-          cardSubTitle = _this$props.cardSubTitle,
-          cardTitle = _this$props.cardTitle,
-          className = _this$props.className,
-          href = _this$props.href,
-          imageContainerBackground = _this$props.imageContainerBackground,
-          imageProps = _this$props.imageProps,
-          info1 = _this$props.info1,
-          info2 = _this$props.info2,
-          info3 = _this$props.info3,
-          loading = _this$props.loading,
-          ownerDescription = _this$props.ownerDescription,
-          ownerTitle = _this$props.ownerTitle,
-          progress = _this$props.progress,
-          progressColor = _this$props.progressColor,
-          state = _this$props.state,
-          stretch = _this$props.stretch,
-          subTitleTruncate = _this$props.subTitleTruncate,
-          titleProps = _this$props.titleProps,
-          titlesMinHeight = _this$props.titlesMinHeight,
-          titleTruncate = _this$props.titleTruncate,
-          widgetState = _this$props.widgetState,
-          others = (0, _objectWithoutProperties2.default)(_this$props, ["additionalInfo", "avatarProps", "cardSubTitle", "cardTitle", "className", "href", "imageContainerBackground", "imageProps", "info1", "info2", "info3", "loading", "ownerDescription", "ownerTitle", "progress", "progressColor", "state", "stretch", "subTitleTruncate", "titleProps", "titlesMinHeight", "titleTruncate", "widgetState"]);
-      return /*#__PURE__*/_react.default.createElement(_styles.StyledCrowdfundingCard, (0, _extends2.default)({}, others, {
-        as: href ? 'a' : 'div',
-        onClick: this.removeCurrentFocus,
-        className: (0, _classnames.default)('k-CrowdfundingCard', 'k-Card k-Card--light k-Card--withoutBoxShadowOnHover', className, {
-          'k-CrowdfundingCard--titlesMinHeight': titlesMinHeight,
-          'k-CrowdfundingCard--isLoading': loading,
-          'k-CrowdfundingCard--isStretched': stretch
-        }),
-        href: href
-      }), /*#__PURE__*/_react.default.createElement(_image.default, {
-        ownerTitle: ownerTitle,
-        ownerDescription: ownerDescription,
-        imageContainerBackground: imageContainerBackground,
-        imageProps: imageProps,
-        avatarProps: avatarProps,
-        loading: loading
-      }), /*#__PURE__*/_react.default.createElement("div", {
-        className: "k-CrowdfundingCard__titleAndDesc"
-      }, /*#__PURE__*/_react.default.createElement(_title.default, {
-        title: cardTitle,
-        loading: loading,
-        titleTruncate: titleTruncate
-      }), /*#__PURE__*/_react.default.createElement(_subtitle.default, {
-        subTitle: cardSubTitle,
-        subTitleTruncate: subTitleTruncate,
-        loading: loading
-      }), additionalInfo && /*#__PURE__*/_react.default.createElement("span", {
-        className: "k-CrowdfundingCard__additionalInfo"
-      }, /*#__PURE__*/_react.default.createElement(_text.Text, {
-        size: "micro",
-        lineHeight: "normal",
-        title: additionalInfo
-      }, additionalInfo))), /*#__PURE__*/_react.default.createElement(_informations.default, {
-        info1: info1,
-        info2: info2,
-        info3: info3,
-        loading: loading
-      }), /*#__PURE__*/_react.default.createElement(_progress.default, {
-        progress: progress,
-        progressColor: progressColor,
-        loading: loading
-      }), /*#__PURE__*/_react.default.createElement(_state.default, {
-        state: state,
-        widgetState: widgetState,
-        loading: loading
-      }), loading && /*#__PURE__*/_react.default.createElement("span", {
-        className: "k-CrowdfundingCard__loading"
-      }));
-    }
-  }]);
-  return CrowdfundingCard;
-}(_react.PureComponent);
+  return /*#__PURE__*/_react.default.createElement(_styles.StyledCrowdfundingCard, (0, _extends2.default)({}, others, {
+    as: href ? 'a' : 'div',
+    imageContainerRatio: imageContainerRatio,
+    onClick: removeCurrentFocus,
+    className: (0, _classnames.default)('k-CrowdfundingCard', 'k-Card k-Card--light k-Card--withoutBoxShadowOnHover', className, {
+      'k-CrowdfundingCard--titlesMinHeight': titlesMinHeight,
+      'k-CrowdfundingCard--isLoading': loading,
+      'k-CrowdfundingCard--isStretched': stretch
+    }),
+    href: href
+  }), /*#__PURE__*/_react.default.createElement(_image.default, {
+    ownerTitle: ownerTitle,
+    ownerDescription: ownerDescription,
+    imageContainerBackground: imageContainerBackground,
+    imageProps: imageProps,
+    avatarProps: avatarProps,
+    loading: loading
+  }), /*#__PURE__*/_react.default.createElement("div", {
+    className: "k-CrowdfundingCard__titleAndDesc"
+  }, /*#__PURE__*/_react.default.createElement(_title.default, {
+    title: cardTitle,
+    loading: loading,
+    titleTruncate: titleTruncate
+  }), /*#__PURE__*/_react.default.createElement(_subtitle.default, {
+    subTitle: cardSubTitle,
+    subTitleTruncate: subTitleTruncate,
+    loading: loading
+  }), !loading && additionalInfo && /*#__PURE__*/_react.default.createElement("span", {
+    className: "k-CrowdfundingCard__additionalInfo"
+  }, /*#__PURE__*/_react.default.createElement(_text.Text, {
+    size: "micro",
+    lineHeight: "normal",
+    weight: "light"
+  }, additionalInfo))), /*#__PURE__*/_react.default.createElement(_informations.default, {
+    info1: info1,
+    info2: info2,
+    info3: info3,
+    loading: loading
+  }), /*#__PURE__*/_react.default.createElement(_progress.default, {
+    progress: progress,
+    progressColor: progressColor,
+    loading: loading
+  }), /*#__PURE__*/_react.default.createElement(_state.default, {
+    state: state,
+    widgetState: widgetState,
+    loading: loading
+  }), loading && /*#__PURE__*/_react.default.createElement("span", {
+    className: "k-CrowdfundingCard__loading"
+  }));
+};
 
 exports.CrowdfundingCard = CrowdfundingCard;
 CrowdfundingCard.propTypes = {
   href: _propTypes.default.string,
   titlesMinHeight: _propTypes.default.bool,
-  stretch: _propTypes.default.bool
+  stretch: _propTypes.default.bool,
+  imageContainerRatio: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.number])
 };
 CrowdfundingCard.defaultProps = {
   href: null,
   titlesMinHeight: true,
-  stretch: false
+  stretch: false,
+  imageContainerRatio: 10 / 16
 };
