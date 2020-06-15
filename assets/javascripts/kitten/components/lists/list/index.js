02bc5d30f594d77b0a8e9dd3f6da2a90c4bacaf1
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-06-15 13:19:04 +0200

    Apply review suggestions

--- assets/javascripts/kitten/components/lists/list/index.js
+++ assets/javascripts/kitten/components/lists/list/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
+import deprecated from 'prop-types-extra/lib/deprecated'
+import styled from 'styled-components'
 import { pxToRem } from '../../../helpers/utils/typography'
 import { ButtonItem } from './components/button-item'
 import classNames from 'classnames'
@@ -10,39 +11,52 @@ const StyledList = styled.ul`
   margin: 0;
   padding: 0;
 
-  ${({ withBottomBorderRadius }) =>
-    !!withBottomBorderRadius &&
-    css`
-      & li:last-child .k-List__button {
-        border-bottom-left-radius: ${pxToRem(withBottomBorderRadius)};
-        border-bottom-right-radius: ${pxToRem(withBottomBorderRadius)};
-      }
-    `}
   ${({ styles }) => styles}
 `
 
-export const List = ({ children, style, withBottomBorderRadius }) => (
-  <StyledList
-    styles={style}
-    className={classNames('k-List', {
-      'k-List--withBottomBorderRadius': !!withBottomBorderRadius,
-    })}
-    withBottomBorderRadius={withBottomBorderRadius}
-  >
-    {React.Children.map(children, child => {
-      if (!React.isValidElement(child)) return null
+export const List = ({
+  children,
+  style,
+  className,
+  bottomBorderRadiusValue,
+  withBottomBorderRadius,
+  ...props
+}) => {
+  const radiusValue =
+    bottomBorderRadiusValue > 0
+      ? bottomBorderRadiusValue
+      : withBottomBorderRadius
 
-      return <li>{child}</li>
-    })}
-  </StyledList>
-)
+  return (
+    <StyledList
+      className={classNames('k-List', className)}
+      styles={{
+        ...style,
+        borderBottomLeftRadius: pxToRem(radiusValue),
+        borderBottomRightRadius: pxToRem(radiusValue),
+      }}
+      {...props}
+    >
+      {React.Children.map(children, child => {
+        if (!React.isValidElement(child)) return null
+
+        return <li>{child}</li>
+      })}
+    </StyledList>
+  )
+}
 
 List.ButtonItem = ButtonItem
 
 List.defaultProps = {
+  bottomBorderRadiusValue: 0,
   withBottomBorderRadius: 0,
 }
 
 List.propTypes = {
-  withBottomBorderRadius: PropTypes.number,
+  bottomBorderRadiusValue: PropTypes.number,
+  withBottomBorderRadius: deprecated(
+    PropTypes.number,
+    'This prop is deprecated, please use bottomBorderRadiusValue instead.',
+  ),
 }
