54f876038c1fb96d3015a61d34694e70662f54fc
Camille Delbove <camille.delbove@gmail.com>
2020-07-07 13:03:39 +0200

    update docs

--- assets/javascripts/kitten/components/typography/text/stories.js
+++ assets/javascripts/kitten/components/typography/text/stories.js
@@ -50,13 +50,27 @@ export const DefaultProp = () => (
 
 export const Size = () => (
   <>
+    <Text size="micro">• size="huge"</Text>
+    <br />
     <Text size="huge">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <br />
+    <Text size="micro">• size="big"</Text>
     <br />
     <Text size="big">Lorem ipsum dolor sit amet…</Text>
     <br />
+    <br />
+    <Text size="micro">• size="default"</Text>
+    <br />
     <Text size="default">Lorem ipsum dolor sit amet…</Text>
     <br />
+    <br />
+    <Text size="micro">• size="tiny"</Text>
+    <br />
     <Text size="tiny">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <br />
+    <Text size="micro">• size="micro"</Text>
     <br />
     <Text size="micro">Lorem ipsum dolor sit amet…</Text>
   </>
@@ -64,15 +78,32 @@ export const Size = () => (
 
 export const Color = () => (
   <>
+    <Text size="micro">• color="font1"</Text>
+    <br />
     <Text color="font1">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <br />
+    <Text size="micro">• color="font2"</Text>
     <br />
     <Text color="font2">Lorem ipsum dolor sit amet…</Text>
     <br />
+    <br />
+    <Text size="micro">• color="primary1"</Text>
+    <br />
     <Text color="primary1">Lorem ipsum dolor sit amet…</Text>
     <br />
-    <Text color="background1">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <Text size="micro">• color="background1"</Text>
+    <br />
+    <Text style={{ backgroundColor: "#222" }} color="background1">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <br />
+    <Text size="micro">• color="error"</Text>
     <br />
     <Text color="error">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <br />
+    <Text size="micro">• color="valid"</Text>
     <br />
     <Text color="valid">Lorem ipsum dolor sit amet…</Text>
   </>
@@ -80,7 +111,12 @@ export const Color = () => (
 
 export const style = () => (
   <>
+    <Text size="micro">• fontStyle="normal"</Text>
+    <br />
     <Text fontStyle="normal">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <br />
+    <Text size="micro">• fontStyle="italic"</Text>
     <br />
     <Text fontStyle="italic">Lorem ipsum dolor sit amet…</Text>
 </>
@@ -88,10 +124,34 @@ export const style = () => (
 
 export const weight = () => (
   <>
+    <Text size="micro">• weight="light" - font-weight: 400</Text>
+    <br />
     <Text weight="light">Lorem ipsum dolor sit amet…</Text>
     <br />
-    <Text weight="regulat">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <Text size="micro">• weight="regular" - font-weight: 500</Text>
+    <br />
+    <Text weight="regular">Lorem ipsum dolor sit amet…</Text>
+    <br />
+    <br />
+    <Text size="micro">• weight="bold" - font-weight: 600</Text>
     <br />
     <Text weight="bold">Lorem ipsum dolor sit amet…</Text>
+  </>
+)
+
+export const decoration = () => (
+  <>  
+    <Text size="micro">• decoration="underline"</Text>
+    <br />
+    <Text decoration="underline">Lorem ipsum dolor sit amet…</Text>
+  </>
+)
+
+export const transform = () => (
+  <>
+    <Text size="micro">• transform="uppercase"</Text>
+    <br />
+    <Text transform="uppercase">Lorem ipsum dolor sit amet…</Text>
   </>
 )
