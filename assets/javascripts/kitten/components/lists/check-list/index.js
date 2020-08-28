8dbb85ac3f332a4580daa739f0bbede7ee63aac4
Morgan JARRY <morgan.jarry@gmail.com>
2020-08-28 14:04:58 +0400

    Add a11yText props

--- assets/javascripts/kitten/components/lists/check-list/index.js
+++ assets/javascripts/kitten/components/lists/check-list/index.js
@@ -42,9 +42,9 @@ const List = styled.ul`
   }
 `
 
-const a11yProps = done => {
+const a11yProps = (a11yText, done) => {
   return {
-    'aria-label': done ? 'Élément validé' : 'Élément non validé',
+    'aria-label': done ? a11yText.done : a11yText.default,
   }
 }
 
@@ -52,14 +52,14 @@ export const CheckList = ({ children, ...props }) => {
   return <List {...props}>{children}</List>
 }
 
-CheckList.Item = ({ done, children, className, ...props }) => {
+CheckList.Item = ({ done, children, a11yText, className, ...props }) => {
   return (
     <li
       className={classNames('k-CheckList__item', className, {
         'k-CheckList__item--done': done,
       })}
       {...props}
-      {...a11yProps(done)}
+      {...a11yProps(a11yText, done)}
     >
       {done ? <span className="strike">{children}</span> : children}
     </li>
@@ -68,8 +68,16 @@ CheckList.Item = ({ done, children, className, ...props }) => {
 
 CheckList.Item.PropTypes = {
   done: PropTypes.bool,
+  a11yText: PropTypes.shape({
+    default: PropTypes.string,
+    done: PropTypes.string,
+  }),
 }
 
 CheckList.defaultProps = {
   done: false,
+  a11yText: {
+    default: 'Élément non validé',
+    done: 'Élément validé',
+  },
 }
