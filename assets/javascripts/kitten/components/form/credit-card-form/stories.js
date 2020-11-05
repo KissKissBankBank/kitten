cc7ed06fee9a609bb5e3a469278d0bd0c94bac35
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2020-11-05 18:05:42 +0100

    Add example with custom components

--- assets/javascripts/kitten/components/form/credit-card-form/stories.js
+++ assets/javascripts/kitten/components/form/credit-card-form/stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { CreditCardForm } from './index'
 import { Marger } from '../../layout/marger'
 import { Container } from '../../grid/container'
+import { Field } from '../field'
 
 const StoryContainer = ({ children }) => (
   <Container>
@@ -19,9 +20,23 @@ export default {
 export const Default = () => {
   return (
     <StoryContainer>
+      <CreditCardForm />
+    </StoryContainer>
+  )
+}
+
+export const withCustomComponents = () => {
+  return (
+    <StoryContainer>
       <CreditCardForm
         customComponents={{
-          field: props => <span {...props} />,
+          field: props => (
+            <div {...props} className="k-u-margin-bottom-triple" />
+          ),
+          label: ({ children, ...props }) => (
+            <Field.Label labelProps={props} children={children} />
+          ),
+          input: Field.Input,
         }}
       />
     </StoryContainer>
