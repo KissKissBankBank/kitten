edc31da9fc8841aaabbfacf5ff1c5a7b712ca4cf
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-11-09 19:06:38 +0100

    Update RadioButtonSet tests

--- assets/javascripts/kitten/components/form/radio-button-set/index.js
+++ assets/javascripts/kitten/components/form/radio-button-set/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
-import { RadioButton } from '../../components/form/radio-button'
+import { RadioButton } from '../../../components/form/radio-button'
 
 export class RadioButtonSet extends React.Component {
   constructor(props) {
