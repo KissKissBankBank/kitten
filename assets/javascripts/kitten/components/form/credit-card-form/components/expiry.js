283b46fb48c2e3e45d139b734fe50e617001ca5d
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2020-11-16 12:36:01 +0100

    Add full year case

--- assets/javascripts/kitten/components/form/credit-card-form/components/expiry.js
+++ assets/javascripts/kitten/components/form/credit-card-form/components/expiry.js
@@ -3,6 +3,31 @@ import PropTypes from 'prop-types'
 import NumberFormat from 'react-number-format'
 import { useFormContext } from '../'
 
+const limit = (val, max) => {
+  if (val.length === 1 && val[0] > max[0]) {
+    val = '0' + val
+  }
+
+  if (val.length === 2) {
+    if (Number(val) === 0) {
+      val = '01'
+    } else if (val > max) {
+      val = max
+    }
+  }
+
+  return val
+}
+
+const cardExpiry = val => {
+  const month = limit(val.substring(0, 2), '12')
+  const fullYear = val.substring(2, 6)
+  const year =
+    fullYear.length === 4 ? fullYear.substring(2, 4) : fullYear.substring(0, 2)
+
+  return val ? month + '/' + year : ''
+}
+
 export const Expiry = ({
   label,
   fieldComponent: Field,
@@ -31,7 +56,7 @@ export const Expiry = ({
         name="cc-exp"
         id="frmCCExp"
         autoComplete="cc-exp"
-        format="##/##"
+        format={cardExpiry}
         inputMode="numeric"
         placeholder="MM/YY"
         customInput={Input}
