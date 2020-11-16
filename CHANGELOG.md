d73f9b37bdee9ffd6634c641e0499ec6c23f5f50
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-11-16 12:54:11 +0100

    [Form] Add new `DropdownSelectWithInput`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,8 +4,12 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [unreleased]
 
+Feature:
+- Add `DropdownSelectWithInput` component.
+- Add `GlobeIcon` icon.
+
 ## [2.96.0] - 2020-11-12
-Feature: 
+Feature:
 - Update `RewardCard` component.
 
 ## [2.95.3] - 2020-11-05
---
+++ assets/javascripts/kitten/components/form/dropdown-select-with-input/test.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { DropdownSelectWithInput } from './index'
+
+import { FacebookIcon } from '../../../components/icons/facebook-icon'
+import { TwitterIcon } from '../../../components/icons/twitter-icon'
+import { LinkedinIcon } from '../../../components/icons/linkedin-icon'
+import { InstagramIcon } from '../../../components/icons/instagram-icon'
+import { YoutubeIcon } from '../../../components/icons/youtube-icon'
+import { GlobeIcon } from '../../../components/icons/globe-icon'
+
+describe('<DropdownSelectWithInput />', () => {
+  it('should match its snapshot with props', () => {
+    const tree = renderer
+      .create(
+        <DropdownSelectWithInput
+          id="dropdown-select-with-input-test"
+          labelText="DropdownSelectWithInput test"
+          items={[
+            {
+              value: 'facebook',
+              label: 'https://www.facebook.com/',
+              icon: <FacebookIcon width="8" height="15" />,
+            },
+            {
+              value: 'twitter',
+              label: 'https://www.twitter.com/',
+              icon: <TwitterIcon width="15" height="14" />,
+            },
+            {
+              value: 'linkedin',
+              label: 'https://www.linkedin.com/',
+              icon: <LinkedinIcon width="14" height="14" />,
+            },
+            {
+              value: 'instagram',
+              label: 'https://www.instagram.com/',
+              icon: <InstagramIcon width="17" height="17" />,
+            },
+            {
+              value: 'youtube',
+              label: 'https://www.youtube.com/',
+              icon: <YoutubeIcon width="17" height="12" />,
+            },
+            {
+              value: 'website',
+              label: 'https://www.…',
+              icon: <GlobeIcon width="16" height="16" />,
+            },
+          ]}
+        />,
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
