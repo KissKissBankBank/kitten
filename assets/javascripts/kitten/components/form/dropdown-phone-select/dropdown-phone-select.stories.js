ec363b9d24008038b52eeaa1efc29daa1b0f15c7
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-11-19 15:00:40 +0100

    Update SelectWithDropdown

--- assets/javascripts/kitten/components/form/dropdown-phone-select/dropdown-phone-select.stories.js
+++ assets/javascripts/kitten/components/form/dropdown-phone-select/dropdown-phone-select.stories.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { text, boolean } from '@storybook/addon-knobs'
+import { text, boolean, select } from '@storybook/addon-knobs'
 import { DropdownPhoneSelect } from './index'
 import { Grid, GridCol } from '../../../components/grid/grid'
+import flagFile from 'icons/flags.png'
 
 export default {
   component: DropdownPhoneSelect,
@@ -22,9 +23,26 @@ export const Default = () => {
           disabled={boolean('disabled', false)}
           hideLabel={boolean('hide label?', false)}
           labelText={text('LabelText', 'label')}
-          resetOnBackspace={boolean('resetOnBackspace', false)}
-          highlightOptionBox={boolean('highlightOptionBox', true)}
-          preferredCountries={['fr', 'lu', 'be']}
+          resetOnBackspace={true}
+          highlightOptionBox={true}
+          placeholder={text('placeholder', 'Téléphone')}
+          phoneProps={{
+            preferredCountries: [
+              'fr',
+              'be',
+              'lu',
+              're',
+              'gp',
+              'mq',
+              'pf',
+              'nc',
+              'gf',
+              'yt',
+            ],
+          }}
+          country="fr"
+          locale={select('locale', ['fr', 'en', 'nl'], 'fr')}
+          flagsUrl={flagFile}
         />
         <p>
           Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
@@ -32,7 +50,6 @@ export const Default = () => {
           penatibus et magnis dis parturient montes, nascetur ridiculus mus.
           Etiam porta sem malesuada magna mollis euismod.
         </p>
-        <p>labelPropsGetter props:</p>
       </GridCol>
     </Grid>
   )
--- assets/javascripts/kitten/components/form/dropdown-phone-select/index.js
+++ assets/javascripts/kitten/components/form/dropdown-phone-select/index.js
@@ -3,10 +3,10 @@ import { DropdownSelectWithInput } from '../../../components/form/dropdown-selec
 import CountryData from './data/CountryData.js'
 import reduce from 'lodash/reduce'
 import { usePrevious } from '../../../helpers/utils/use-previous-hook'
+import lang_fr from './data/lang/fr.json'
+import { FlagIcon } from '../../../components/icons/flag-icon'
 
-const FlagIcon = ({ country }) => {
-  return <span>{country}</span>
-}
+const locale_fr = JSON.parse(JSON.stringify(lang_fr))
 
 const removeCountryCodeFromFormat = format => {
   const patternArray = format.split(' ')
@@ -14,89 +14,219 @@ const removeCountryCodeFromFormat = format => {
   return patternArray.join(' ')
 }
 
-const processCountries = (countries, prefix) => {
+const processCountries = ({ countries, prefix, locale, flagsUrl }) => {
   return countries.map(country => {
+    const localizedName =
+      locale === 'fr' ? locale_fr[country.iso2] : country.name
+
     return {
-      value: country,
-      label: country.name,
-      labelInList: `${country.name} ${prefix}${country.countryCode}`,
+      value: country.iso2,
+      label: localizedName,
+      labelInList: (
+        <>
+          {localizedName}
+          <span className="k-u-color-font2 k-u-margin-left-noneHalf">
+            {prefix}
+            {country.countryCode}
+          </span>
+        </>
+      ),
       labelSelected: `${prefix}${country.countryCode}`,
-      icon: <FlagIcon country={country.iso2} />,
+      icon: (
+        <FlagIcon
+          country={country.iso2}
+          countryName={localizedName}
+          flagsUrl={flagsUrl}
+        />
+      ),
     }
   })
 }
 
-export const DropdownPhoneSelect = ({ onChange, ...props }) => {
+const formatNumber = (text, country) => {
+  if (!country) return text
+  if (!format) return text
+
+  const { format } = country
+
+  const pattern = removeCountryCodeFromFormat(format)
+
+  if (!text || text.length === 0) {
+    return ''
+  }
+
+  if ((text && text.length < 2) || !pattern) {
+    return text
+  }
+
+  const formattedObject = reduce(
+    pattern,
+    (acc, character) => {
+      if (acc.remainingText.length === 0) {
+        return acc
+      }
+
+      if (character !== '.') {
+        return {
+          formattedText: acc.formattedText + character,
+          remainingText: acc.remainingText,
+        }
+      }
+
+      const [head, ...tail] = acc.remainingText
+
+      return {
+        formattedText: acc.formattedText + head,
+        remainingText: tail,
+      }
+    },
+    {
+      formattedText: '',
+      remainingText: text.split(''),
+    },
+  )
+
+  const formattedNumber = formattedObject.formattedText
+
+  return formattedNumber
+}
+
+const getOptions = ({
+  onlyCountries,
+  preferredCountries,
+  prefix,
+  locale,
+  flagsUrl,
+}) => {
+  const options = []
+  if (preferredCountries) {
+    options.push(
+      ...processCountries({
+        countries: preferredCountries,
+        prefix,
+        locale,
+        flagsUrl,
+      }),
+    )
+    options.push({
+      separator: true,
+      disabled: true,
+    })
+  }
+
+  onlyCountries &&
+    options.push(
+      ...processCountries({
+        countries: onlyCountries,
+        prefix,
+        locale,
+        flagsUrl,
+      }),
+    )
+
+  return options
+}
+
+// const guessSelectedCountry = (inputNumber, country, onlyCountries) => {
+//   const secondBestGuess = onlyCountries.find(o => o.iso2 == country)
+//   if (inputNumber.trim() === '') return secondBestGuess
+
+//   const bestGuess = onlyCountries.reduce((selectedCountry, country) => {
+//     if (startsWith(inputNumber, country.dialCode)) {
+//       if (country.dialCode.length > selectedCountry.dialCode.length) {
+//         return country
+//       }
+//       if (country.dialCode.length === selectedCountry.dialCode.length && country.priority < selectedCountry.priority) {
+//         return country
+//       }
+//     }
+//     return selectedCountry
+//   }, {dialCode: '', priority: 10001}, this)
+
+//   if (!bestGuess.name) return secondBestGuess
+//   return bestGuess
+// }
+
+const getCountryObjectFromIso = (country, onlyCountries) => {
+  let newSelectedCountry
+  if (country.indexOf(0) >= '0' && country.indexOf(0) <= '9') {
+    // digit
+    newSelectedCountry = onlyCountries.find(o => o.dialCode == +country)
+  } else {
+    newSelectedCountry = onlyCountries.find(o => o.iso2 == country)
+  }
+  return newSelectedCountry
+}
+
+export const DropdownPhoneSelect = ({
+  id,
+  value,
+  country,
+  locale,
+  placeholder,
+  onChange,
+  flagsUrl,
+  ...props
+}) => {
+  // Consts
+
   const [getCountry, setCountry] = useState(null)
   const [getFormattedNumber, setFormattedNumber] = useState('')
   const [getCaretPosition, setCaretPosition] = useState(0)
+  const [getInputPlaceholder, setInputPlaceholder] = useState('0 00 00 00 00')
+  const [getDefaultSelectedValue, setDefaultSelectedValue] = useState(null)
   const getPreviousFormattedNumber = usePrevious(getFormattedNumber)
 
-  const { onlyCountries, preferredCountries } = new CountryData(
-    props.enableAreaCodes,
-    props.enableTerritories,
-    props.regions,
-    props.onlyCountries,
-    props.preferredCountries,
-    props.excludeCountries,
-    props.preserveOrder,
-    props.masks,
-    props.priority,
-    props.areaCodes,
-    props.localization,
-    props.prefix,
-    props.defaultMask,
-    props.alwaysDefaultMask,
-  )
-
-  const formatNumber = (text, country) => {
-    if (!country) return text
-
-    const { format } = country
-
-    const pattern = removeCountryCodeFromFormat(format)
-
-    if (!text || text.length === 0) {
-      return ''
-    }
-
-    if ((text && text.length < 2) || !pattern) {
-      return text
-    }
-
-    const formattedObject = reduce(
-      pattern,
-      (acc, character) => {
-        if (acc.remainingText.length === 0) {
-          return acc
-        }
-
-        if (character !== '.') {
-          return {
-            formattedText: acc.formattedText + character,
-            remainingText: acc.remainingText,
-          }
-        }
-
-        const [head, ...tail] = acc.remainingText
-
-        return {
-          formattedText: acc.formattedText + head,
-          remainingText: tail,
-        }
-      },
-      {
-        formattedText: '',
-        remainingText: text.split(''),
-      },
-    )
-
-    const formattedNumber = formattedObject.formattedText
-
-    return formattedNumber
+  const phoneProps = {
+    ...DropdownPhoneSelect.defaultProps.phoneProps,
+    ...props.phoneProps,
   }
 
+  const localization = locale === 'fr' ? locale_fr : []
+
+  const { onlyCountries, preferredCountries } = new CountryData(
+    phoneProps.enableAreaCodes,
+    phoneProps.enableTerritories,
+    phoneProps.regions,
+    phoneProps.onlyCountries,
+    phoneProps.preferredCountries,
+    phoneProps.excludeCountries,
+    phoneProps.preserveOrder,
+    phoneProps.masks,
+    phoneProps.priority,
+    phoneProps.areaCodes,
+    localization,
+    phoneProps.prefix,
+    phoneProps.defaultMask,
+    phoneProps.alwaysDefaultMask,
+  )
+
+  const options = getOptions({
+    onlyCountries,
+    preferredCountries,
+    prefix: phoneProps.prefix,
+    locale,
+    flagsUrl,
+  })
+
+  // const inputNumber = value ? value.replace(/\D/g, '') : ''
+
+  // let countryGuess
+  // if (inputNumber.length > 1) {
+  //   // Country detect by phone
+  //   countryGuess = guessSelectedCountry(inputNumber.substring(0, 6), country, onlyCountries) || 0
+  // } else if (country) {
+  //   // Default country
+  //   countryGuess = onlyCountries.find(o => o.iso2 == country) || 0
+  // } else {
+  //   // Empty params
+  //   countryGuess = 0
+  // }
+
+  // Handlers
+
   const handleInput = event => {
+    const currentCountry = getCountryObjectFromIso(getCountry, onlyCountries)
     const { value } = event.target
     const innerValue = value.replace(/\D/g, '')
 
@@ -105,12 +235,12 @@ export const DropdownPhoneSelect = ({ onChange, ...props }) => {
 
     let innerFormattedNumber = ''
     if (innerValue.length > 0) {
-      innerFormattedNumber = formatNumber(innerValue, getCountry)
+      innerFormattedNumber = formatNumber(innerValue, currentCountry)
     }
 
     const caretPosition = event.target.selectionStart
 
-    const { format } = getCountry
+    const { format } = currentCountry
     const pattern = removeCountryCodeFromFormat(format)
     const limit = pattern.length
 
@@ -126,17 +256,42 @@ export const DropdownPhoneSelect = ({ onChange, ...props }) => {
     if (!!onChange) {
       onChange(
         innerFormattedNumber.replace(/[^0-9]+/g, ''),
-        getCountry,
+        currentCountry,
         event,
         innerFormattedNumber,
       )
     }
   }
 
+  const handleSelect = country => {
+    if (!country) {
+      return setCountry(null)
+    }
+    const newCountry = getCountryObjectFromIso(country.value, onlyCountries)
+
+    setCountry(newCountry.iso2)
+
+    const placeholderFormat = removeCountryCodeFromFormat(newCountry.format)
+    setInputPlaceholder(placeholderFormat.replace(/\./g, '0'))
+  }
+
+  // Effects
   useEffect(() => {
+    if (country) {
+      const newCountry = getCountryObjectFromIso(country, onlyCountries)
+
+      if (newCountry && newCountry.iso2) {
+        setCountry(newCountry.iso2)
+        setDefaultSelectedValue(newCountry.iso2)
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    // Adjust caret position depending on the edit
     if (!getFormattedNumber || !getPreviousFormattedNumber) return
 
-    const inputElement = document.getElementById(`${props.id}_element-input`)
+    const inputElement = document.getElementById(`${id}_element-input`)
 
     const diff = getFormattedNumber.length - getPreviousFormattedNumber.length
 
@@ -145,29 +300,17 @@ export const DropdownPhoneSelect = ({ onChange, ...props }) => {
     }
   }, [getFormattedNumber])
 
-  const handleSelect = country => {
-    setCountry(country ? country.value : null)
-  }
-
-  const options = []
-
-  preferredCountries &&
-    options.push(...processCountries(preferredCountries, props.prefix))
-  options.push({
-    separator: true,
-    disabled: true,
-  })
-  onlyCountries &&
-    options.push(...processCountries(onlyCountries, props.prefix))
-
   return (
     <DropdownSelectWithInput
       {...props}
+      id={id}
       resetOnBackspace
       highlightOptionBox={false}
-      inputPlaceholder={props.placeholder}
+      placeholder={placeholder}
       options={options}
+      inputPlaceholder={getInputPlaceholder}
       onChange={handleSelect}
+      defaultSelectedValue={getDefaultSelectedValue}
       inputProps={{
         onChange: handleInput,
         value: getFormattedNumber,
@@ -179,56 +322,28 @@ export const DropdownPhoneSelect = ({ onChange, ...props }) => {
 DropdownPhoneSelect.defaultProps = {
   country: '',
   value: '',
-
-  onlyCountries: [],
-  preferredCountries: [],
-  excludeCountries: [],
-
-  placeholder: '01 23 45 67 89',
-  flagsImagePath: './flags.png',
-  disabled: false,
-
-  autoFormat: true,
-  enableAreaCodes: false,
-  enableTerritories: false,
-  disableCountryCode: false,
-  disableDropdown: false,
-  enableLongNumbers: false,
-  countryCodeEditable: true,
-  enableSearch: false,
-  disableSearchIcon: false,
-  disableInitialCountryGuess: false,
-  disableCountryGuess: false,
-
-  regions: '',
-
-  inputProps: {},
-  localization: {},
-
-  masks: null,
-  priority: null,
-  areaCodes: null,
-
-  preserveOrder: [],
-
-  defaultMask: '... ... ... ... ..', // prefix+dialCode+' '+defaultMask
-  alwaysDefaultMask: false,
-  prefix: '+',
-  copyNumbersOnly: true,
-  renderStringAsFlag: '',
-  autocompleteSearch: false,
-  jumpCursorToEnd: true,
-  enableAreaCodeStretch: false,
-  enableClickOutside: true,
-  showDropdown: false,
-
-  isValid: true, // (value, selectedCountry, onlyCountries, hiddenAreaCodes) => true | false | 'Message'
-  defaultErrorMessage: '',
-  specialLabel: 'Phone',
-
-  onEnterKeyPress: null, // null or function
-
-  // OWN FUNCIONS
+  phoneProps: {
+    onlyCountries: [],
+    preferredCountries: [],
+    excludeCountries: [],
+    disabled: false,
+    enableAreaCodes: false,
+    enableTerritories: true,
+    regions: '',
+    masks: null,
+    priority: null,
+    areaCodes: null,
+    preserveOrder: ['preferredCountries'],
+    defaultMask: '. .. .. .. ..',
+    alwaysDefaultMask: false,
+    prefix: '+',
+  },
 
   id: 'dropdown-phone-select',
+  inputProps: {},
+  locale: 'en',
+  placeholder: 'Telephone',
+  flagsUrl: './flags.png',
+
+  onChange: () => {},
 }
