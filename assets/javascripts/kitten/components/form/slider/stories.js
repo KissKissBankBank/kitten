02e07135a44fb01c39f45179aa34fe9352bab87a
Camille Delbove <camille.delbove@gmail.com>
2020-11-19 17:09:45 +0100

    nothing

--- assets/javascripts/kitten/components/form/slider/stories.js
+++ assets/javascripts/kitten/components/form/slider/stories.js
@@ -1,16 +1,8 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Slider } from './index'
 import { Marger } from '../../layout/marger'
 import { Container } from '../../grid/container'
 import { Text } from '../../typography/text'
-import merge from 'lodash/fp/merge'
-
-const defaultProps = {
-  values: {
-    day: '',
-  },
-  onChange: _values => {},
-}
 
 const StoryContainer = ({ children }) => (
   <Container>
@@ -20,41 +12,27 @@ const StoryContainer = ({ children }) => (
   </Container>
 )
 
-const SliderContext = createContext(defaultProps)
-export const useSliderContext = () => useContext(SliderContext)
-
 export default {
   title: 'Sliders/Slider',
   component: Slider,
 }
 
-export const SliderDefault = ({ ...props }) => {
-  const [sliderMin, setSliderMin] = useState(0)
-  const [sliderMax, setSliderMax] = useState(100)
-
-  const [sliderValues, updateSliderValues] = useState(
-    merge(defaultProps.values)(props.values),
-  )
-  const contextValues = merge(defaultProps)({
-    ...props,
-    values: sliderValues,
-    setSliderValues: v => {
-      updateSliderValues(merge(sliderValues)(v))
-    },
-  })
-
-  useEffect(() => {
-    contextValues.onChange(sliderValues)
-  }, [sliderValues])
-
+export const SliderDefault = () => {
+  const sliderMin = useState(0)
+  const sliderMax = useState(100)
+  const [sliderValue, setSliderValue] = useState(0)
+  
   return (
-    <StoryContainer>
+    <StoryContainer>   
       <Slider
         name="nombre de jours"
         min={sliderMin}
         max={sliderMax}
-        value={contextValues}
-        // onChange={ _values => {}}
+        value={sliderValue}
+        // onChange={value => {
+        //   setSliderValue(value)
+        //   cropperInstance.zoomTo(value / 100)
+        // }}
     />
     </StoryContainer>
   )
