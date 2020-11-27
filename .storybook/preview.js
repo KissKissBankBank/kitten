b7358951ccc07ffa6c9a3886d69dd9b445e6f1af
Joachim Robert <joachim.robert@kisskissbankbank.com>
2020-11-27 15:23:08 +0100

    New order

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,11 +6,29 @@ import { addDecorator } from '@storybook/react'
 import GridDecorator from './decorators/grid'
 import { addParameters } from '@storybook/react'
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks'
+import anysort from 'anysort'
 
 addParameters({
   docs: {
     container: DocsContainer,
     page: DocsPage,
+  },
+  options: {
+    /**
+     * https://github.com/storybookjs/storybook/issues/6327#issuecomment-664900197
+     * display the top-level grouping as a "root" in the sidebar
+     * @type {Boolean}
+     */
+    storySort: (previous, next) => {
+      const [previousStory, previousMeta] = previous
+      const [nextStory, nextMeta] = next
+
+      return anysort(previousMeta.kind, nextMeta.kind, [
+        'Documentation/Introduction',
+        'Documentation/**',
+        '**',
+      ])
+    }
   },
 })
 
