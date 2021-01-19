7efc18cb21e32a0ffe2069039ecd3f880fd7e1d5
Joachim Robert <joachim.robert@kisskissbankbank.com>
2021-01-19 09:26:15 +0100

    Fix deploy bug

--- assets/javascripts/kitten/karl/widget/stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import WidgetCard from './components/widget-card'
-
-export default {
-  title: 'Modals/WidgetCard Modal',
-  component: WidgetCard,
-}
-
-export const Default = () => <WidgetCard />
