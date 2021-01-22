cab497890d607dc913f032fab8c2733062b8783b
Camille Delbove <camilledel@users.noreply.github.com>
2021-01-22 17:12:08 +0100

    [KittenV3] Big Karl cleaning (#2175)

--- assets/javascripts/kitten/karl/cards/project-similar-card.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { ProjectSimilarCard } from '../../components/cards/project-similar-card'
-
-export const KarlProjectSimilarLoader = () => (
-  <ProjectSimilarCard step="1/5" loading />
-)
--- styleguide/component/layout/horizontal-stroke.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { KarlHorizontalStrokes } from '../../../assets/javascripts/kitten/karl/layout/horizontal-stroke'
-
-export default () => {
-  return <KarlHorizontalStrokes />
-}
