277c52ddabb432f4925f9e021e9424c44e857cfa
Joachim Robert <joachim.robert@kisskissbankbank.com>
2021-02-05 12:54:21 +0100

    Add `fullWidth` prop on Info

--- assets/javascripts/kitten/components/cards/backing-card/components/info.js
+++ assets/javascripts/kitten/components/cards/backing-card/components/info.js
@@ -2,16 +2,19 @@ import React from 'react'
 import classNames from 'classnames'
 import { Text } from '../../../typography/text'
 
-export const Info = ({ legend, value, ...props }) => {
+export const Info = ({ legend, value, fullWidth = false, ...props }) => {
   return (
     <p
       {...props}
       className={classNames(
         'k-BackingCard__info',
-        'k-BackingCard__halfDrawer',
         'k-u-size-tiny',
         'k-u-line-height-normal',
         props.className,
+        {
+          'k-BackingCard__halfDrawer': !fullWidth,
+          'k-BackingCard__drawer': fullWidth,
+        },
       )}
     >
       <Text
