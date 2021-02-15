417d639499391fcbe52dc9cbe26bf38fb6667ecc
Bastien Maillard <bastien.maillard@kisskissbankbank.com>
2021-02-15 11:23:44 +0100

    Remove k-Separator mixin

--- src/components/layout/separator/index.js
+++ src/components/layout/separator/index.js
@@ -15,7 +15,15 @@ var _react = _interopRequireDefault(require("react"));
 
 var _classnames = _interopRequireDefault(require("classnames"));
 
-var Separator = function Separator(_ref) {
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+var _colorsConfig = _interopRequireDefault(require("../../../constants/colors-config"));
+
+var _typography = require("../../../helpers/utils/typography");
+
+var Separator = (0, _styledComponents.default)(function (_ref) {
   var darker = _ref.darker,
       className = _ref.className,
       props = (0, _objectWithoutProperties2.default)(_ref, ["darker", "className"]);
@@ -25,9 +33,14 @@ var Separator = function Separator(_ref) {
   return /*#__PURE__*/_react.default.createElement("hr", (0, _extends2.default)({
     className: separatorClassName
   }, props));
-};
-
+}).withConfig({
+  displayName: "separator__Separator",
+  componentId: "sc-1dgi38g-0"
+})(["&.k-Separator{margin:0;border:none;background:", ";height:", ";}&.k-Separator--darker{background:", ";}"], _colorsConfig.default.line1, (0, _typography.pxToRem)(1), _colorsConfig.default.line2);
 exports.Separator = Separator;
+Separator.propTypes = {
+  darker: _propTypes.default.bool
+};
 Separator.defaultProps = {
   className: null,
   darker: false
