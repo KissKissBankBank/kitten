b905ab203e2ee9eacae64b3ef8c2363bb41e6d5d
Joachim Robert <joachim.robert@kisskissbankbank.com>
2021-02-22 11:07:22 +0100

    Add tests

--- assets/javascripts/kitten/components/layout/dashboard-layout/test.js
+++ assets/javascripts/kitten/components/layout/dashboard-layout/test.js
@@ -38,4 +38,70 @@ describe('<DashboardLayout />', () => {
       expect(component).toMatchSnapshot()
     })
   })
+
+  describe('<DefaultLayout.Flow />', () => {
+    beforeEach(() => {
+      component = renderer
+        .create(
+          <DashboardLayout.Flow>
+            <DashboardLayout.Flow.Content>
+              Main flow content
+            </DashboardLayout.Flow.Content>
+            <DashboardLayout.Flow.Aside
+              mobileAsideProps={{
+                openLabel: 'Open help',
+                closeLabel: 'Close help',
+              }}
+            >
+              <DashboardLayout.Flow.AsideCard>
+                Side content in a card
+              </DashboardLayout.Flow.AsideCard>
+            </DashboardLayout.Flow.Aside>
+            <DashboardLayout.Flow.Nav>
+              <button>Back</button>
+
+              <button>Save</button>
+            </DashboardLayout.Flow.Nav>
+          </DashboardLayout.Flow>,
+        )
+        .toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('<DefaultLayout.Flow loading />', () => {
+    beforeEach(() => {
+      component = renderer
+        .create(
+          <DashboardLayout.Flow>
+            <DashboardLayout.Flow.Content>
+              Main flow content
+            </DashboardLayout.Flow.Content>
+            <DashboardLayout.Flow.Aside
+              mobileAsideProps={{
+                openLabel: 'Open help',
+                closeLabel: 'Close help',
+              }}
+            >
+              <DashboardLayout.Flow.AsideCard>
+                Side content in a card
+              </DashboardLayout.Flow.AsideCard>
+            </DashboardLayout.Flow.Aside>
+            <DashboardLayout.Flow.Nav>
+              <button>Back</button>
+
+              <button>Save</button>
+            </DashboardLayout.Flow.Nav>
+          </DashboardLayout.Flow>,
+        )
+        .toJSON()
+    })
+
+    it('matches with snapshot', () => {
+      expect(component).toMatchSnapshot()
+    })
+  })
 })
