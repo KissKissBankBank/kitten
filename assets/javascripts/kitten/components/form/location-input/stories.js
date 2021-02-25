8f0fd427a097dfb06b0bdd50fe580aa32ef4a168
Joachim Robert <joachim.robert@kisskissbankbank.com>
2021-02-25 16:59:15 +0100

    Add story

---
+++ assets/javascripts/kitten/components/form/location-input/stories.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import { LocationInput } from './index'
+
+export default {
+  title: 'Form/LocationInput',
+  component: LocationInput,
+}
+
+export const Default = () => {
+  const [googleMapsReady, setGoogleMapsReadiness] = useState(false)
+
+  useEffect(() => {
+    loadGoogleMaps(() => setGoogleMapsReadiness(true))
+
+    return () => unloadGoogleMaps()
+  }, [])
+
+  const loadGoogleMaps = callback => {
+    const existingScript = document.getElementById('googlePlacesScript')
+    if (!existingScript) {
+      const script = document.createElement('script')
+      script.id = 'googleMaps'
+      document.body.appendChild(script)
+
+      script.onload = () => {
+        if (callback) callback()
+      }
+    }
+    if (existingScript && callback) callback()
+  }
+
+  const unloadGoogleMaps = () => {
+    let googlePlacesScript = document.getElementById('googlePlacesScript')
+    if (googlePlacesScript) {
+      googlePlacesScript.remove()
+    }
+  }
+
+  if (!googleMapsReady) return <p>Loading</p>
+
+  return (
+    <LocationInput
+      onChange={e => console.warn(e)}
+      onSelect={e => console.warn(e)}
+    />
+  )
+}
