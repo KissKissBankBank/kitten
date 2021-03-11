21b4b12975401c1af3850f23c23a0babfc69250f
Joachim Robert <joachim.robert@kisskissbankbank.com>
2021-03-11 11:47:46 +0100

    Update index.js

--- assets/javascripts/kitten/components/form/form-actions/index.js
+++ assets/javascripts/kitten/components/form/form-actions/index.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { pxToRem } from '../../../helpers/utils/typography'
 import { ScreenConfig } from '../../../constants/screen-config'
-import { useFlexGapCheck } from '../../../helpers/dom/use-flex-gap-check'
 
 const StyledFormActions = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  gap: ${pxToRem(10)};
+
+  .k-FormActions__item {
+    margin-left: ${pxToRem(5)};
+    margin-right: ${pxToRem(5)};
+  }
 
   &.k-FormActions--spreadOut-s-up {
     @media (min-width: ${pxToRem(ScreenConfig.S.min)}) {
       justify-content: space-between;
 
-      &.k-FormActions--noGap .k-FormActions__item {
+      .k-FormActions__item {
         &:first-child {
           margin-left: 0;
         }
@@ -27,19 +30,11 @@ const StyledFormActions = styled.div`
       }
     }
   }
-
-  &.k-FormActions--noGap .k-FormActions__item {
-    margin-left: ${pxToRem(5)};
-    margin-right: ${pxToRem(5)};
-  }
 `
 
 export const FormActions = ({ className, spreadOutAt, ...others }) => {
-  const canUseGap = useFlexGapCheck()
-
   const formActionsClassName = classNames('k-FormActions', className, {
     [`k-FormActions--spreadOut-${spreadOutAt}`]: spreadOutAt,
-    'k-FormActions--noGap': !canUseGap,
   })
 
   return <StyledFormActions className={formActionsClassName} {...others} />
