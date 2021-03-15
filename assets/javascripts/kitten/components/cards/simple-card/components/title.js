cc81b858be9328596a61968ad6a79eceacf5f3e5
Camille Delbove <camille.delbove@gmail.com>
2021-03-15 15:24:52 +0100

    Clean fixe

--- assets/javascripts/kitten/components/cards/simple-card/components/title.js
+++ assets/javascripts/kitten/components/cards/simple-card/components/title.js
@@ -9,22 +9,21 @@ export const Title = ({
   title,
   titleProps
 }) => {
-    const titleClassName = classNames('k-Card__title', titleProps.className)
+  const titleClassName = classNames('k-Card__title', titleProps.className)
 
-    return (
-      <Marger top="2" bottom=".3">
-        <Title
-          tag="p"
-          {...titleProps}
-          className={titleClassName}
-          noMargin
-          modifier="senary"
-        >
-          {parseHtml(title)}
-        </Title>
-      </Marger>
-    )
-  }
+  return (
+    <Marger top="2" bottom=".3">
+      <TitleComponent
+        tag="p"
+        {...titleProps}
+        className={titleClassName}
+        noMargin
+        modifier="senary"
+      >
+        {parseHtml(title)}
+      </TitleComponent>
+    </Marger>
+  )
 }
 
 Title.propTypes = {
