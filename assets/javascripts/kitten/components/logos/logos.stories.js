030ad4daf707b09f4eda5616fa81f07bab725d17
Camille Delbove <camille.delbove@gmail.com>
2021-03-18 10:54:42 +0100

    Update styles

--- assets/javascripts/kitten/components/logos/logos.stories.js
+++ assets/javascripts/kitten/components/logos/logos.stories.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { pxToRem } from '../../helpers/utils/typography'
 import styled from 'styled-components'
+import { Text } from '../..'
 
 import { KissKissBankBankLogo } from './kisskissbankbanklogo'
 import { GoodeedLogo } from './goodeedlogo'
 import { MaisonDeCrowdfundingLogo } from './mdc'
 import { MicrodonLogo } from './microdonlogo'
-
-import { Text } from '../..'
+import { LendopolisLogo } from './lendopolis-logo'
 
 const Container = styled.div`
   padding: ${pxToRem(40)};
@@ -21,24 +21,30 @@ const Group = styled(({ className, children, title }) => {
     </>
   )
 })`
-  margin-bottom: ${pxToRem(40)};
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(${pxToRem(140)}, 1fr));
+  grid-gap: ${pxToRem(80)} ${pxToRem(40)};
+  margin-bottom: ${pxToRem(50)};
 `
 
-const LogoContainer = styled(({ className, children }) => {
+const LogoContainer = styled(({ className, children, name }) => {
   return (
     <div className={className}>
       <div className="svgWrapper">{children}</div>
       <Text size="micro">
-        {children.type.name}
+        {name}
       </Text>
     </div>
   )
 })`
-  text-align: left;
-  margin-bottom: ${pxToRem(40)};
+  text-align: center;
 
   .svgWrapper {
+    margin: auto;
     height: ${pxToRem(50)};
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   svg {
@@ -47,15 +53,45 @@ const LogoContainer = styled(({ className, children }) => {
     width: ${pxToRem(150)};
     max-height: ${pxToRem(100)};
   }
+
+  &.noMaxDimensions svg {
+    max-height: ${pxToRem(50)};
+    max-width: ${pxToRem(50)};
+  }
 `
 
 export const AllLogos = () => {
   return (
     <Container>
-      <Group>
-        <LogoContainer children={<KissKissBankBankLogo />} />
+      <Group title="KissKissBankBank">
+        <LogoContainer
+          children={<KissKissBankBankLogo />}
+        />
+        <LogoContainer
+          className="noMaxDimensions"
+          name="version tiny"
+          children={<KissKissBankBankLogo tiny />}
+        />
+      </Group>
+
+      <Group title="Lendopolis">
+        <LogoContainer children={<LendopolisLogo />} />
+        <LogoContainer
+          className="noMaxDimensions"
+          name="version tiny"
+          children={<LendopolisLogo tiny />}
+        />
+      </Group>
+
+      <Group title="Goodeed">
         <LogoContainer children={<GoodeedLogo />} />
+      </Group>
+
+      <Group title="Maison de Crowdfunding">
         <LogoContainer children={<MaisonDeCrowdfundingLogo />} />
+      </Group>
+
+      <Group title="MicroDon">
         <LogoContainer children={ <MicrodonLogo />} />
       </Group>
     </Container>
