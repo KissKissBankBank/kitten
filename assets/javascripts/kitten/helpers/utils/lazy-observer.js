ebb1802ce12361e7fb281d94c66ba9ddc55b273a
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2021-03-24 18:11:21 +0100

    add threshold for safari

--- assets/javascripts/kitten/helpers/utils/lazy-observer.js
+++ assets/javascripts/kitten/helpers/utils/lazy-observer.js
@@ -2,6 +2,7 @@ import { IntersectionObserverClass } from './intersection-observer'
 
 const LazyObserver = new IntersectionObserverClass({
   rootMargin: '100px 0px',
+  threshold: 0.5,
 })
 
 export default LazyObserver
