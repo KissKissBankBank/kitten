a230fb6087fde9245a232a1fd96aa22a30ba7eea
Camille Delbove <camille.delbove@gmail.com>
2021-05-28 16:07:33 +0200

    [RadioButton] Fix status style

--- assets/javascripts/kitten/components/form/radio-button/index.js
+++ assets/javascripts/kitten/components/form/radio-button/index.js
@@ -88,8 +88,10 @@ const StyledRadioButton = styled.div`
   /* Disabled */
 
   .k-Form-RadioButton__input:disabled {
+    cursor: not-allowed;
+
     & + .k-Form-RadioButton__label {
-      border-color: ${COLORS.line2};
+      border-color: ${COLORS.line2};      
     }
 
     & + .k-Form-RadioButton__label::before {
