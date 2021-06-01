ffc858e48da0fa8adc3bf179f55cabcc1a3f98ca
Camille Delbove <camille.delbove@gmail.com>
2021-06-01 18:07:32 +0200

    Responsive

--- assets/javascripts/kitten/karl/pages/contribution-card/index.js
+++ assets/javascripts/kitten/karl/pages/contribution-card/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { pxToRem } from '../../../helpers/utils/typography'
+import { pxToRem, stepToRem } from '../../../helpers/utils/typography'
 import { Text } from '../../../components/typography/text'
 import { CloseButton } from '../../../components/buttons/close-button'
 import COLORS from '../../../constants/colors-config'
@@ -26,26 +26,52 @@ const StyledSummary = styled.div`
   
   .kiss-Summary__img {
     position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: ${pxToRem(140)};
-    object-fit: cover;
+    top: ${pxToRem(20)};
+    left: ${pxToRem(20)};
+    width: ${pxToRem(80)};
+  
+    @media (min-width: ${ScreenConfig.S.min}px) {
+      top: 0;
+      left: 0;
+      object-fit: cover;
+      width: ${pxToRem(140)};
+      height: 100%;
+    }
   }
 
   .kiss-Summary__container {
-    display: flex;
+    display: inline-block;
     width: 100%;
-    padding: ${pxToRem(20)} ${pxToRem(60)} ${pxToRem(20)} ${pxToRem(30)};
+    padding: ${pxToRem(20)} ${pxToRem(30)} ${pxToRem(20)} ${pxToRem(110)};
     align-items: center;
     justify-content: space-between;
+
+    @media (min-width: ${ScreenConfig.S.min}px) {
+      display: flex;
+      padding: ${pxToRem(20)} ${pxToRem(60)} ${pxToRem(20)} ${pxToRem(160)};
+    }
   }
 
   .kiss-Summary__subSummary {
-    padding-left: ${pxToRem(140)};
-    margin-right: ${pxToRem(5)};
-    max-width: ${pxToRem(240)};
+    font-size: ${stepToRem(-1)};
+    margin-top: ${pxToRem(10)};
+    margin-bottom: ${pxToRem(25)};
+  
+    @media (min-width: ${ScreenConfig.XS.min}px) {
+      font-size: ${stepToRem(0)};
+      margin-right: ${pxToRem(5)};
+      max-width: ${pxToRem(240)};
+      margin-top: 0;
+      margin-bottom: 0;
+    }
   }
+
+  ${'' /* .kiss-Summary__infosPrice {
+    display: inline-flex;
+    vertical-align: middle;
+    text-align: right;
+    margin: 0;
+  } */}
 `
 
 const Summary = () => {
@@ -57,13 +83,33 @@ const Summary = () => {
         className="kiss-Summary__img"
       />
       <div className="kiss-Summary__container">
-        <Text color="font1" weight="bold" lineHeight="1" size="default" className="kiss-Summary__subSummary">
+        <Text
+          tag="p"
+          color="font1"
+          weight="bold"
+          lineHeight="1"
+          className="kiss-Summary__subSummary"
+        >
           Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor 
           eget dolor.eget dolor.eget dolor.
         </Text>
-        <PillNumberInput value={0} min={0} max={100} className="k-u-margin-right-triple" />
-        <Text color="font1" weight="bold" size="big">135€</Text>
-      </div>
+        <PillNumberInput 
+          value={0}
+          min={0}
+          max={100}
+          className="k-u-margin-right@s-up"
+        />
+        <div>
+          <Text
+            color="font1"
+            weight="bold"
+            size="huge"
+            className="kiss-Summary__infosPrice"
+          >
+            135€
+          </Text>
+        </div>
+        </div>
     </StyledSummary>
   )
 }
