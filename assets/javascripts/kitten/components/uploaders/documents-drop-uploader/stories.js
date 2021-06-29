f9675ff132c92859fcbbdc4a83032c023cdf42be
Joachim Robert <joachim.robert@kisskissbankbank.com>
2021-06-29 19:19:44 +0200

    Update stories.js

--- assets/javascripts/kitten/components/uploaders/documents-drop-uploader/stories.js
+++ assets/javascripts/kitten/components/uploaders/documents-drop-uploader/stories.js
@@ -67,3 +67,29 @@ export const StatusManage = () => (
     removeActionMessage={e => `Click to remove ${e}`}
   />
 )
+
+export const StatusDenied = () => (
+  <DocumentsDropUploader
+    id="DocumentsDropUploader"
+    labelText="Add a document"
+    managerTitle="Upload your ID documents"
+    managerText="Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."
+    initialValue={[
+      { name: 'kitten_valid.jpg' },
+      { name: 'kitten_invalid.jpg' },
+    ]}
+    onChange={e => console.warn('onChange', e)}
+    disabled={boolean('Disabled?', false)}
+    status="deniad"
+    errorMessageMessage={text('ErrorMessage', null)}
+    typeErrorText={e => `File ${e} is not the right file type`}
+    sizeErrorText={e => `File ${e} is too large`}
+    acceptedTitle="Your document was accepted"
+    waitTitle="This operation can take up to 48 hours"
+    waitMessage="This operation can take up to 48 hours"
+    deniedTitle="Some documents are not valid"
+    deniedMessage="Your documents are not valid"
+    retryActionMessage="Click to load another document"
+    removeActionMessage={e => `Click to remove ${e}`}
+  />
+)
