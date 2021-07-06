758777581e6efbd759ea2f50a884d170d2f25fc8
Joachim Robert <joachim.robert@kisskissbankbank.com>
2021-07-06 18:44:57 +0200

    add built js files for 2.23.0

--- src/esm/components/steppers/vertical-progress/index.js
+++ src/esm/components/steppers/vertical-progress/index.js
@@ -1,7 +1,9 @@
 import _extends from "@babel/runtime/helpers/esm/extends";
 import _objectWithoutProperties from "@babel/runtime/helpers/esm/objectWithoutProperties";
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import classNames from 'classnames';
 import { Step } from './components/step';
 import { pxToRem } from '../../../helpers/utils/typography';
 import { ScreenConfig } from '../../../constants/screen-config';
@@ -9,16 +11,27 @@ import COLORS from '../../../constants/colors-config';
 var StyledContainer = styled.div.withConfig({
   displayName: "vertical-progress__StyledContainer",
   componentId: "sc-4vkdj5-0"
-})(["border:", " solid ", ";padding:", ";border-radius:", ";@media (min-width:", "px){padding:", ";}.k-VerticalProgress{position:relative;padding:0;}.k-VerticalProgress__list{margin:0;padding:0;list-style:none;:before{position:absolute;top:0;bottom:0;left:", ";content:'';border-left:", " dotted ", ";}}"], pxToRem(2), COLORS.line1, pxToRem(15), pxToRem(8), ScreenConfig.S.min, pxToRem(30), pxToRem(14), pxToRem(2), COLORS.line1);
+})(["&:not(.k-VerticalProgress__wrapper--withoutBorder){border:", " solid ", ";padding:", ";border-radius:", ";@media (min-width:", "px){padding:", ";}}.k-VerticalProgress{position:relative;padding:0;}.k-VerticalProgress__list{margin:0;padding:0;list-style:none;:before{position:absolute;top:0;bottom:0;left:", ";content:'';border-left:", " dotted ", ";}}"], pxToRem(2), COLORS.line1, pxToRem(15), pxToRem(8), ScreenConfig.S.min, pxToRem(30), pxToRem(14), pxToRem(2), COLORS.line1);
 export var VerticalProgress = function VerticalProgress(_ref) {
   var children = _ref.children,
-      props = _objectWithoutProperties(_ref, ["children"]);
+      withoutBorder = _ref.withoutBorder,
+      props = _objectWithoutProperties(_ref, ["children", "withoutBorder"]);
 
-  return /*#__PURE__*/React.createElement(StyledContainer, null, /*#__PURE__*/React.createElement("nav", {
+  return /*#__PURE__*/React.createElement(StyledContainer, {
+    className: classNames("k-VerticalProgress__wrapper", {
+      'k-VerticalProgress__wrapper--withoutBorder': withoutBorder
+    })
+  }, /*#__PURE__*/React.createElement("nav", {
     className: "k-VerticalProgress"
   }, /*#__PURE__*/React.createElement("ul", _extends({
     role: "tablist",
     className: "k-VerticalProgress__list"
   }, props), children)));
+};
+VerticalProgress.propTypes = {
+  withoutBorder: PropTypes.bool
+};
+VerticalProgress.defaultProps = {
+  withoutBorder: false
 };
 VerticalProgress.Step = Step;
---
+++ src/esm/components/uploaders/image-drop-uploader/components/image-cropper.js
@@ -0,0 +1,119 @@
+import _extends from "@babel/runtime/helpers/esm/extends";
+import _slicedToArray from "@babel/runtime/helpers/esm/slicedToArray";
+import _objectWithoutProperties from "@babel/runtime/helpers/esm/objectWithoutProperties";
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+import { useDrag } from '../hooks/use-drag';
+import { usePrevious } from '../../../../helpers/utils/use-previous-hook';
+export var ImageCropper = function ImageCropper(_ref) {
+  var src = _ref.src,
+      onChange = _ref.onChange,
+      className = _ref.className,
+      initialCrop = _ref.initialCrop,
+      disabled = _ref.disabled,
+      props = _objectWithoutProperties(_ref, ["src", "onChange", "className", "initialCrop", "disabled"]);
+
+  var _useState = useState(null),
+      _useState2 = _slicedToArray(_useState, 2),
+      imageDimensions = _useState2[0],
+      setImageDimensions = _useState2[1];
+
+  var _useState3 = useState(null),
+      _useState4 = _slicedToArray(_useState3, 2),
+      scaledInitialCrop = _useState4[0],
+      setScaledInitialCrop = _useState4[1];
+
+  useEffect(function () {
+    if (!initialCrop || !imageDimensions) return;
+    setScaledInitialCrop({
+      x: -1 * Math.round(initialCrop.x / imageDimensions.scaleRatio),
+      y: -1 * Math.round(initialCrop.y / imageDimensions.scaleRatio)
+    });
+  }, [imageDimensions, initialCrop]);
+
+  var _useDrag = useDrag({
+    startingPosition: scaledInitialCrop,
+    imageDimensions: imageDimensions,
+    disabled: disabled
+  }),
+      cropZoneProps = _useDrag.cropZoneProps,
+      imagePosition = _useDrag.imagePosition,
+      liveImagePosition = _useDrag.liveImagePosition,
+      isDragging = _useDrag.isDragging;
+
+  var previousImagePosition = usePrevious(imagePosition);
+  useEffect(function () {
+    if (!imageDimensions) return;
+    if ((previousImagePosition === null || previousImagePosition === void 0 ? void 0 : previousImagePosition.x) === imagePosition.x && (previousImagePosition === null || previousImagePosition === void 0 ? void 0 : previousImagePosition.y) === imagePosition.y) return;
+    var cropValue = {
+      x: Math.round(Math.abs(imagePosition.x * imageDimensions.scaleRatio)),
+      y: Math.round(Math.abs(imagePosition.y * imageDimensions.scaleRatio)),
+      width: Math.round(imageDimensions.containedSize.width * imageDimensions.scaleRatio),
+      height: Math.round(imageDimensions.containedSize.height * imageDimensions.scaleRatio)
+    };
+    var cropPercent = {
+      x: Math.abs(imagePosition.x) * 100 / Math.abs(imageDimensions.containedSize.width - imageDimensions.scaledSize.width || 1),
+      y: Math.abs(imagePosition.y) * 100 / Math.abs(imageDimensions.containedSize.height - imageDimensions.scaledSize.height || 1)
+    };
+    onChange({
+      cropValue: cropValue,
+      imagePosition: imagePosition,
+      cropPercent: cropPercent
+    });
+  }, [imagePosition]);
+
+  var getRatio = function getRatio(size) {
+    return size.height / size.width;
+  };
+
+  var handleImageLoad = function handleImageLoad(e) {
+    var naturalSize = {
+      width: e.target.naturalWidth,
+      height: e.target.naturalHeight
+    };
+    var containedSize = {
+      width: e.target.width,
+      height: e.target.height
+    };
+    var scaledSize = {
+      width: 0,
+      height: 0
+    };
+
+    if (getRatio(naturalSize) === getRatio(containedSize)) {
+      scaledSize = _extends({}, containedSize);
+    } else if (getRatio(naturalSize) > getRatio(containedSize)) {
+      scaledSize = {
+        width: e.target.width,
+        height: Math.round(e.target.width * getRatio(naturalSize))
+      };
+    } else {
+      scaledSize = {
+        width: Math.round(e.target.height / getRatio(naturalSize)),
+        height: e.target.height
+      };
+    }
+
+    var scaleRatio = naturalSize.width / scaledSize.width;
+    setImageDimensions({
+      naturalSize: naturalSize,
+      containedSize: containedSize,
+      scaledSize: scaledSize,
+      scaleRatio: scaleRatio
+    });
+  };
+
+  return /*#__PURE__*/React.createElement("div", _extends({}, props, cropZoneProps, {
+    className: classNames('k-ImageDropUploader-imageCropper', className, {
+      'k-ImageDropUploader-imageCropper--isDragging': isDragging
+    })
+  }), /*#__PURE__*/React.createElement("img", {
+    alt: "",
+    src: src,
+    style: {
+      '--ImageDropUploader-cropX': "".concat(liveImagePosition.x, "px"),
+      '--ImageDropUploader-cropY': "".concat(liveImagePosition.y, "px")
+    },
+    onLoad: handleImageLoad
+  }));
+};
