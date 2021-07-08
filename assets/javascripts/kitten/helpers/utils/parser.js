cd159656e3ad5cb39484bd95bafa33ca8d380670
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2021-07-08 17:57:11 +0200

    sanitize html by default

--- assets/javascripts/kitten/helpers/utils/parser.js
+++ assets/javascripts/kitten/helpers/utils/parser.js
@@ -1,13 +1,16 @@
 import HtmlToReact from 'html-to-react'
+import DOMPurify from 'dompurify'
 
 // We add a span to make parseHtml works with strings.
-export const parseHtml = value => {
+export const parseHtml = (value, options = { sanitize: true }) => {
   if (!value) return
 
   // We need to escape "<3" common emoji
-  const encodedValue =
-    typeof value === 'string' ? value.replace('<3', '&lt;3') : value
+  let clean = typeof value === 'string' ? value.replace('<3', '&lt;3') : value
 
-  return new HtmlToReact.Parser().parse(`<span>${encodedValue}</span>`).props
-    .children
+  if (options.sanitize) {
+    clean = DOMPurify.sanitize(clean)
+  }
+
+  return new HtmlToReact.Parser().parse(`<span>${clean}</span>`).props.children
 }
