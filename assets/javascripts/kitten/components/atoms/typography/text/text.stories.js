f1b8bbdd900fdcd0476e1c487c6ad35cb42f218c
Camille Delbove <camille.delbove@gmail.com>
2021-08-05 12:04:33 +0200

    Update story

--- assets/javascripts/kitten/components/atoms/typography/text/text.stories.js
+++ assets/javascripts/kitten/components/atoms/typography/text/text.stories.js
@@ -5,91 +5,59 @@ const argTypes = {
   color: {
     name: 'color',
     description: ' Available colors:',
-    options: [
-      'font1',
-      'font2',
-      'primary1',
-      'background1',
-      'error',
-      'valid',
-    ],
-    control: {type: 'select'},
+    options: ['font1', 'font2', 'primary1', 'background1', 'error', 'valid'],
+    control: { type: 'select' },
   },
   cssColor: {
     name: 'cssColor',
     description: 'Specify a color (use a CSS color string).',
-    control: {type: 'color'},
+    control: { type: 'color' },
   },
   decoration: {
     name: 'decoration',
     description: 'If `tag="a"`, show underline.',
-    options: [
-      'underline',
-      'none',
-    ],
-    control: {type: 'select'},
+    options: ['underline', 'none'],
+    control: { type: 'select' },
   },
   fontStyle: {
     name: 'fontStyle',
     description: 'Available font styles (`normal` or `italic`):',
-    options: [
-      'normal',
-      'italic',
-    ],
-    control: {type: 'select'},
+    options: ['normal', 'italic'],
+    control: { type: 'select' },
   },
   transform: {
     name: 'transform',
     description: '`text-transform: uppercase`',
-    options: [
-      'uppercase',
-    ],
-    control: {type: 'select'},
+    options: ['uppercase'],
+    control: { type: 'select' },
   },
   lineHeight: {
     name: 'lineHeight',
     description: '`line-height: normal` correspond approximately to 1.2.',
-    options: [
-      'normal',
-      '1',
-      '1.3',
-    ],
-    control: {type: 'select'},
+    options: ['normal', '1', '1.3'],
+    control: { type: 'select' },
   },
   size: {
     name: 'size',
     description: 'Available sizes:',
-    options: [
-      'giant',
-      'huge',
-      'big',
-      'default',
-      'tiny',
-      'micro',
-      'nano',
-    ],
-    control: {type: 'select'},
+    options: ['giant', 'huge', 'big', 'default', 'tiny', 'micro', 'nano'],
+    control: { type: 'select' },
   },
   weight: {
     name: 'weight',
     description: 'Available font weights (`light`, `regular` or `italic`):',
-    options: [
-      'light',
-      'regular',
-      'bold',
-    ],
-    control: {type: 'select'},
+    options: ['light', 'regular', 'bold'],
+    control: { type: 'select' },
   },
   text: {
-    control: {type: 'text'},
+    control: { type: 'text' },
   },
   setting: {
     name: 'setting',
-    description: '`font-feature-settings: "tnum"` enables tabular (monospace) numerals.',
-    options: [
-      'tnum',
-    ],
-    control: {type: 'select'},
+    description:
+      '`font-feature-settings: "tnum"` enables tabular (monospace) numerals.',
+    options: ['tnum'],
+    control: { type: 'select' },
   },
 }
 
@@ -108,38 +76,10 @@ const args = {
   text: 'Lorem ipsum dolor sit amet…',
 }
 
-export const Default = ({
-  color,
-  cssColor,
-  decoration,
-  lineHeight,
-  size,
-  fontStyle,
-  transform,
-  weight,
-  setting,
-  text,
-  ...props
-}) => (
-  <Text
-    color={color}
-    cssColor={cssColor}
-    decoration={decoration}
-    lineHeight={lineHeight}
-    size={size}
-    fontStyle={fontStyle}
-    transform={transform}
-    weight={weight}
-    setting={setting}
-    
-    {...props}
-  >
-    {text}
-  </Text>
-)
+export const Default = ({ text, ...props }) => <Text {...props}>{text}</Text>
 
-Default.args = args;
-Default.argTypes = argTypes;
+Default.args = args
+Default.argTypes = argTypes
 
 export default {
   component: Text,
