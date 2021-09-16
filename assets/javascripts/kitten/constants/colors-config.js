e5fdc5bab4d1d8445d227f6697e87cf46b85d738
Joachim <joachim.robert@protonmail.com>
2021-09-16 18:08:59 +0200

    Use CSS vars to define colors

--- assets/javascripts/kitten/constants/colors-config.js
+++ assets/javascripts/kitten/constants/colors-config.js
@@ -1,44 +1,44 @@
 export default {
-  font1: '#222', // Dark Grey
-  font2: '#b8b8b8', // Semi Dark Grey
-  font3: '#949494', // Extra semi dark grey, used for text
+  font1: 'var(--color-font1, #222)', // Dark Grey
+  font2: 'var(--color-font2, #b8b8b8)', // Semi Dark Grey
+  font3: 'var(--color-font3, #949494)', // Extra semi dark grey, used for text
 
-  background1: '#fff', // White
-  background2: '#fbfbfb', // Extra Light Grey
-  background3: '#f6f6f6', // Light Grey
+  background1: 'var(--color-background1, #fff)', // White
+  background2: 'var(--color-background2, #fbfbfb)', // Extra Light Grey
+  background3: 'var(--color-background3, #f6f6f6)', // Light Grey
 
-  lightRed: '#ffebe0', // Light Red
+  lightRed: 'var(--color-lightRed, #ffebe0)', // Light Red
 
-  line1: '#eee', // Semi Light Grey
-  line2: '#d8d8d8', // Grey
-  line3: '#2d2d2d', // Extra semi dark grey, not used for text
+  line1: 'var(--color-line1, #eee)', // Semi Light Grey
+  line2: 'var(--color-line2, #d8d8d8)', // Grey
+  line3: 'var(--color-line3, #2d2d2d)', // Extra semi dark grey, not used for text
 
-  grey1: '#636363', // Medium grey used for text or not
+  grey1: 'var(--color-grey1, #636363)', // Medium grey used for text or not
 
-  primary1: '#19b4fa', // Primary
-  primary2: '#05a8e6', // Semi Dark Primary
-  primary3: '#0496cc', // Dark Primary
-  primary4: '#bae8fd', // Light Primary
-  primary5: '#e8f7fe', // Extra Light Primary
-  primary6: '#f3fbff', // Thin Primary
+  primary1: 'var(--color-primary1, #19b4fa)', // Primary
+  primary2: 'var(--color-primary2, #05a8e6)', // Semi Dark Primary
+  primary3: 'var(--color-primary3, #0496cc)', // Dark Primary
+  primary4: 'var(--color-primary4, #bae8fd)', // Light Primary
+  primary5: 'var(--color-primary5, #e8f7fe)', // Extra Light Primary
+  primary6: 'var(--color-primary6, #f3fbff)', // Thin Primary
 
-  tertiary1: '#effaf1', // Light Green
-  tertiary2: '#cff0d6', // Semi Light Green
+  tertiary1: 'var(--color-tertiary1, #effaf1)', // Light Green
+  tertiary2: 'var(--color-tertiary2, #cff0d6)', // Semi Light Green
 
-  valid: '#61d079', // Green
-  valid1: 'hsl(133, 54%, 94%)', // Green Light
+  valid: 'var(--color-valid, #61d079)', // Green
+  valid1: 'var(--color-valid1, hsl(133, 54%, 94%))', // Green Light
 
-  error: '#ff0046', // Red
-  error2: '#ffe5ec', // Light Red
-  error3: '#ffb2c7', // Semi Light Red
-  error4: '#d8043e', // Semi Dark Red
-  error5: '#a8002e', // Dark Red
+  error: 'var(--color-error, #ff0046)', // Red
+  error2: 'var(--color-error2, #ffe5ec)', // Light Red
+  error3: 'var(--color-error3, #ffb2c7)', // Semi Light Red
+  error4: 'var(--color-error4, #d8043e)', // Semi Dark Red
+  error5: 'var(--color-error5, #a8002e)', // Dark Red
 
-  orange: '#ff7800',
-  orange1: '#fff2e7', // Orange light
-  orange2: '#f57905', // Orange semi dark
-  orange3: '#eb6e00', // Orange dark
+  orange: 'var(--color-orange, #ff7800)',
+  orange1: 'var(--color-orange1, #fff2e7)', // Orange light
+  orange2: 'var(--color-orange2, #f57905)', // Orange semi dark
+  orange3: 'var(--color-orange3, #eb6e00)', // Orange dark
 
-  warning: '#8a6d3b',
-  warning2: '#fcf8e3',
+  warning: 'var(--color-warning, #8a6d3b)',
+  warning2: 'var(--color-warning2, #fcf8e3)',
 }
