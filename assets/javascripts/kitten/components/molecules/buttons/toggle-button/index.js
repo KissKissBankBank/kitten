eae6328c8c2c1f464601b9ffb6507d2f02cf671a
Camille Delbove <camille.delbove@gmail.com>
2021-09-22 16:52:21 +0200

    Button styles

--- assets/javascripts/kitten/components/molecules/buttons/toggle-button/index.js
+++ assets/javascripts/kitten/components/molecules/buttons/toggle-button/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
 import PropTypes from 'prop-types'
-import { Button, ArrowContainer } from '../../../../'
-import styled from 'styled-components'
+import { Button, ArrowContainer, GiftIcon } from '../../../../'
 import classNames from 'classnames'
+
+const zoomInAndOpacity = keyframes`
+  0% {
+    transform: translateX(calc(-1 * var(--Dropdown-transform))) scale(.66);
+    opacity: 0;
+  }
+  to
+  {
+    transform: translateX(calc(-1 * var(--Dropdown-transform))) scale(1);
+    opacity: 1;
+  }
+`
 
 const StyledToggleButton = styled(Button)`
 
@@ -19,8 +31,10 @@ export const ToggleButton = ({
         className,
         {...props}
       )}
+      icon
+      borderRadius={100}
     >
-
+      <GiftIcon width={19} height={16} title="Carte cadeau" />
     </StyledToggleButton>
   )
 }
