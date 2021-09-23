c76ed11dd585677b469e17870d8e911282093e16
Camille Delbove <camille.delbove@gmail.com>
2021-09-23 18:58:28 +0200

    Add styles

--- assets/javascripts/kitten/components/molecules/buttons/toggle-button/index.js
+++ assets/javascripts/kitten/components/molecules/buttons/toggle-button/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import PropTypes from 'prop-types'
-import { Button, ArrowContainer, GiftIcon, COLORS } from '../../../../'
+import {
+  Button,
+  ArrowContainer,
+  GiftIcon,
+  COLORS,
+  pxToRem,
+  Text
+} from '../../../../'
 import classNames from 'classnames'
 
 const zoomInAndOpacity = keyframes`
@@ -16,43 +23,66 @@ const zoomInAndOpacity = keyframes`
   }
 `
 
-const StyledToggleButton = styled.details`
-
+const StyledToggleButton = styled.div`
+  .k-ToggleButton__button {
+    &:hover + .k-ToggleButton__container {
+      display: block;
+      animation: 0.16s ease ${zoomInAndOpacity};
+      margin-top: ${pxToRem(7)};
+      width: max-content;
+    }
+  }
+  
+  .k-ToggleButton__container {
+    display: none;
+  }
 `
 
 export const ToggleButton = ({
   className,
+  iconTitle,
   ...props
 }) => {
+
   return (
-    <StyledToggleButton
-      className={classNames(
-        'k-ToggleButton',
-        className,
-        {...props}
-      )}
-    >
+    <StyledToggleButton>
       <Button
         className={classNames(
           'k-ToggleButton__button',
           className,
           {...props}
         )}
+        aria-label={iconTitle}
         icon
         borderRadius={100}
       >
-        <GiftIcon width={19} height={16} title="Carte cadeau" />
+        <GiftIcon width={19} height={16} title={iconTitle} />
       </Button>
+
       <ArrowContainer
+        className={classNames(
+          'k-ToggleButton__container'
+        )}
         color={COLORS.line1}
         size={7}
-        padding={0}
+        padding={8}
         borderRadius={4}
         position="top"
-        {...arrowDistanceProps}
+        aria-hidden
+        centered
       >
-        {children}
+        <Text size="tiny" color="font1" weight="regular">
+          {iconTitle}
+        </Text>
       </ArrowContainer>
     </StyledToggleButton>
   )
+}
+
+ToggleButton.protoTypes = {
+  iconTitle: PropTypes.string,
+}
+
+ToggleButton.defaultProps = {
+  iconTitle: "Carte cadeau",
 }
