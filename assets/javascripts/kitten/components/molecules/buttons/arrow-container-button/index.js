3396db4d8874650de18c66647389cb8d73652ec5
Joachim <joachim.robert@kisskissbankbank.com>
2021-09-24 15:59:20 +0200

    [ButtonWithTooltip] Fixes (#2770)
    * Update index.js
    
    * Update index.js

--- assets/javascripts/kitten/components/molecules/buttons/arrow-container-button/index.js
+++ assets/javascripts/kitten/components/molecules/buttons/arrow-container-button/index.js
@@ -12,39 +12,56 @@ import {
 import classNames from 'classnames'
 
 const zoomInAndOpacity = keyframes`
-  0% {
-    transform: translateY(calc(-1 * 100% scale(.66);
+  from {
+    transform: translateX(-50%) translateY(0) scale(.66);
     opacity: 0;
   }
   to
   {
-    transform: translateY(calc(-1 * 50% scale(1);
+    transform: translateX(-50%) translateY(${pxToRem(7)}) scale(1);
     opacity: 1;
   }
 `
 
-const StyledArrowContainerButton = styled.div`
+const zoomOutAndOpacity = keyframes`
+  from {
+    transform: translateX(-50%) translateY(${pxToRem(7)}) scale(1);
+    opacity: 1;
+  }
+  to
+  {
+    transform: translateX(-50%) translateY(0) scale(.66);
+    opacity: 0;
+  }
+`
 
-  --Button-tiny-size: ${pxToRem(40)};
-  --Button-regular-size: ${pxToRem(50)};
+const StyledArrowContainerButton = styled.div`
+  display: inline-block;
+  position: relative;
+
+  .k-ArrowContainerButton__container {
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      transform: translateX(-50%) translateY(0) scale(0.66);
+      transform-origin: 50% ${pxToRem(7)};
+
+      width: max-content;
+      max-width: ${pxToRem(250)};
+
+      display: block;
+
+      animation: 0.16s ease ${zoomOutAndOpacity};
+      opacity: 0;
+  }
 
   .k-ArrowContainerButton__button {
-    display: inline-flex;
-  
-    &:hover + .k-ArrowContainerButton__container {
-      position: absolute;
-      flex-shrink: 0;
-      display: block;
+    &:hover + .k-ArrowContainerButton__container,
+    &:focus + .k-ArrowContainerButton__container {
       animation: 0.16s ease ${zoomInAndOpacity};
-      margin-top: ${pxToRem(7)};
-      width: max-content;
-      height: auto;
-      transform: translateX(calc(-50% + var(--Button-tiny-size) / 2));
+      transform: translateX(-50%) translateY(${pxToRem(7)}) scale(1);
+      opacity: 1;
     }
-  }
-  
-  .k-ArrowContainerButton__container {
-    display: none;
   }
 `
 
@@ -60,12 +77,12 @@ export const ArrowContainerButton = ({
         className={classNames(
           'k-ArrowContainerButton__button',
           className,
-          {...props}
         )}
         aria-label={iconTitle}
         icon
         borderRadius={100}
         size="tiny"
+        {...props}
       >
         <GiftIcon width={19} height={16} title={iconTitle} />
       </Button>
