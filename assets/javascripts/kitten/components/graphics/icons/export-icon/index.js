3bfb2a94d458839130a8ef13e9650577cd6951eb
florentduveau <florent.duveau@gmail.com>
2021-09-30 17:34:48 +0200

    update export icon

--- assets/javascripts/kitten/components/graphics/icons/export-icon/index.js
+++ assets/javascripts/kitten/components/graphics/icons/export-icon/index.js
@@ -3,15 +3,25 @@ import PropTypes from 'prop-types'
 
 export const ExportIcon = ({ color, title, ...props }) => (
   <svg
-    width="14"
-    height="16"
-    viewBox="0 0 14 16"
-    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
     fill={color}
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     {title && <title>{title}</title>}
-    <path d="M8 4.207v5.414H6V4.207L4.172 6.036 2.757 4.62 7 .38l1.414 1.414 2.829 2.828-1.415 1.415L8 4.207zm4 11.414H0v-6h2v4h10v-4h2v6h-2z" />
+    <path
+      d="M3 15V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V15"
+      stroke="#222222"
+      stroke-width="2"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12 15L11.2929 15.7071L12 16.4142L12.7071 15.7071L12 15ZM7.29289 11.7071L11.2929 15.7071L12.7071 14.2929L8.70711 10.2929L7.29289 11.7071ZM12.7071 15.7071L16.7071 11.7071L15.2929 10.2929L11.2929 14.2929L12.7071 15.7071Z"
+      fill="#222222"
+    />
+    <path d="M12 15L12 4" stroke="#222222" stroke-width="2" />
   </svg>
 )
 
