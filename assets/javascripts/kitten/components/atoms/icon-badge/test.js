08f7577c1d05afdd2f564a72cb77e89ba9132be7
Camille Delbove <camille.delbove@gmail.com>
2021-10-11 16:57:23 +0200

    Remove import

--- assets/javascripts/kitten/components/atoms/icon-badge/test.js
+++ assets/javascripts/kitten/components/atoms/icon-badge/test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { IconBadge } from './index'
-import { pxToRem } from '../../../helpers/utils/typography'
 
 
 describe('<IconBadge />', () => {
