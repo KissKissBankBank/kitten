c7f62af08d55a7667740d463aa6a8f834b335dbd
Camille Delbove <camille.delbove@gmail.com>
2021-10-13 18:26:27 +0200

    fix

--- assets/javascripts/kitten/components/graphics/icons/cross-icon/index.js
+++ assets/javascripts/kitten/components/graphics/icons/cross-icon/index.js
@@ -22,7 +22,6 @@ export const CrossIcon = ({ color, title, width, height, size, ...props }) => {
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        {...props}
       >
         <path
           d="M7 7L17 17"
@@ -86,5 +85,3 @@ CrossIcon.defaultProps = {
   color: COLORS.font1,
   size: 'normal',
 }
-
-
