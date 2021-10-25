597803b7bcaa58431a1c886ea53c5116012b9ef0
Joachim <joachim.robert@protonmail.com>
2021-10-25 15:12:44 +0200

    Better stories

--- assets/javascripts/kitten/components/layout/dashboard-layout/stories/with-reward.js
+++ assets/javascripts/kitten/components/layout/dashboard-layout/stories/with-reward.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
+import styled from 'styled-components'
 import {
   RewardSummaryCard,
   useRewardSummaryCardResizeObserver,
@@ -7,7 +9,71 @@ import {
   DropdownMenu,
   COLORS,
   EllipsisIcon,
+  DragAndDropList,
+  DRAG_AND_DROP_LIST_BUTTON_SHIFT,
+  mq,
 } from 'kitten'
+
+const StyledDragAndDropList = styled(DragAndDropList)`
+  @media ${mq.tabletAndDesktop} {
+    margin-left: -${DRAG_AND_DROP_LIST_BUTTON_SHIFT};
+  }
+
+  @media ${mq.mobile} {
+    .k-DragAndDropList__item__button {
+      display: none;
+    }
+    .k-DragAndDropList__item__child {
+      flex-basis: 100%;
+      max-width: 100%;
+    }
+  }
+`
+
+const RewardCardComponent = ({
+  imageProps,
+  title,
+  amount,
+  contribution,
+  availability,
+  star,
+  id,
+  ...props
+}) => (
+  <RewardSummaryCard id={id} {...props}>
+    <RewardSummaryCard.Image>
+      <img alt="" {...imageProps} />
+    </RewardSummaryCard.Image>
+    <div style={{ display: 'grid' }}>
+      <RewardSummaryCard.Title aria-describedby="RewardSummaryList-title">
+        {title}
+      </RewardSummaryCard.Title>
+      {!!star &&
+        <RewardSummaryCard.TitleTag text="Contrepartie star" />
+      }
+    </div>
+    <RewardSummaryCard.Amount aria-describedby="RewardSummaryList-amount">
+      {amount}
+    </RewardSummaryCard.Amount>
+    <RewardSummaryCard.Contribution aria-describedby="RewardSummaryList-contribution">
+      {contribution}
+    </RewardSummaryCard.Contribution>
+    <RewardSummaryCard.Availability aria-describedby="RewardSummaryList-availability">
+      {availability}
+    </RewardSummaryCard.Availability>
+    <RewardSummaryCard.Options>
+      <DropdownMenu
+        button={() => <EllipsisIcon color={COLORS.font1} />}
+        menuPosition="left"
+      >
+        <DropdownMenu.Link href="#">Editer</DropdownMenu.Link>
+        <DropdownMenu.Link href="#">Dupliquer</DropdownMenu.Link>
+        <DropdownMenu.Link href="#">Désactiver</DropdownMenu.Link>
+        <DropdownMenu.Link>Supprimer</DropdownMenu.Link>
+      </DropdownMenu>
+    </RewardSummaryCard.Options>
+  </RewardSummaryCard>
+)
 
 export const StoryWithReward = () => {
   const { ref, size } = useRewardSummaryCardResizeObserver()
@@ -21,141 +87,77 @@ export const StoryWithReward = () => {
         Curabitur blandit tempus porttitor.
       </Title>
 
-      <FlexWrapper gap={15}>
-        <RewardSummaryCard.TitleBar
-          values={{
-            image: 'Visuel de la contrepartie',
-            title: 'Titre de la contrepartie',
-            amount: 'Montant',
-            contributions: 'Contributions',
-            availability: 'Disponibilités',
-          }}
-          className="k-u-hidden@xs-down k-u-margin-bottom-double"
-          id="RewardSummaryList"
+      <p id="container_label_element" className="k-u-hidden@xs-down k-u-a11y-visuallyHidden">
+        Cette liste peut être réorganisée à l'aide des boutons.
+      </p>
+
+      <p id="description_element" className="k-u-hidden@xs-down k-u-a11y-visuallyHidden">
+        Cliquez pour attraper/lâcher l'élément. Une fois attrapé, l'élément peut
+        être déplacé à l'aide des flèches haut/bas du clavier.
+      </p>
+
+      <RewardSummaryCard.TitleBar
+        values={{
+          image: 'Visuel de la contrepartie',
+          title: 'Titre de la contrepartie',
+          amount: 'Montant',
+          contributions: 'Contributions',
+          availability: 'Disponibilités',
+        }}
+        className="k-u-hidden@xs-down k-u-margin-bottom-double"
+        id="RewardSummaryList"
+        size={size}
+      />
+
+      <StyledDragAndDropList
+        onChange={action('onChange')}
+        a11yButtonDescElement="description_element"
+        a11yContainerLabelElement="container_label_element"
+        a11yButtonLabel="Réorganiser"
+        a11yAnnouncement={{
+          grabbed: name => `${name} attrapé`,
+          dropped: name => `${name} relâché`,
+          reorder: (name, position, length) => {
+            return `L’ordre a été changé, ${name} est maintenant en position ${position} sur ${length}`
+          },
+          cancel: 'Réorganisation annulée.'
+        }}
+      >
+        <RewardCardComponent
           size={size}
+          href='#'
+          imageProps={{ src: '/kitten.jpg' }}
+          title='Aliquip aliqua excepteur quis eu laborum do dolore veniam ullamco anim fugiat aliquip consequat aliquip voluptate cupidatat consectetur.'
+          amount="10 000€"
+          contribution="1 468 000"
+          star
+          availability="Illimitée"
+          id="911c90cc-311e-478f-9730-872c468bb9e0"
+          simpleName="Aliquip aliqua excepteur quis"
         />
-
-        <RewardSummaryCard size={size}>
-          <RewardSummaryCard.Image>
-            <img alt="" src="/kitten.jpg" />
-          </RewardSummaryCard.Image>
-          <div style={{ display: 'grid' }}>
-            <RewardSummaryCard.Title aria-describedby="RewardSummaryList-title">
-              Stickers Free Boobs Club Stickers Free Boobs Club Stickers Free
-              Boobs Club.
-            </RewardSummaryCard.Title>
-            <RewardSummaryCard.TitleTag text="Contrepartie star" />
-          </div>
-          <RewardSummaryCard.Amount aria-describedby="RewardSummaryList-amount">
-            10 000€
-          </RewardSummaryCard.Amount>
-          <RewardSummaryCard.Contribution aria-describedby="RewardSummaryList-contribution">
-            1 468 000
-          </RewardSummaryCard.Contribution>
-          <RewardSummaryCard.Availability aria-describedby="RewardSummaryList-availability">
-            Illimitée
-          </RewardSummaryCard.Availability>
-          <RewardSummaryCard.Options>
-            <DropdownMenu
-              button={() => <EllipsisIcon color={COLORS.font1} />}
-              menuPosition="left"
-            >
-              <DropdownMenu.Link href="#">Editer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Dupliquer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Désactiver</DropdownMenu.Link>
-              <DropdownMenu.Link>Supprimer</DropdownMenu.Link>
-            </DropdownMenu>
-          </RewardSummaryCard.Options>
-        </RewardSummaryCard>
-
-        <RewardSummaryCard size={size}>
-          <div style={{ display: 'grid' }}>
-            <RewardSummaryCard.Title aria-describedby="RewardSummaryList-title">
-              Stickers Free Boobs Club
-            </RewardSummaryCard.Title>
-            <RewardSummaryCard.TitleTag text="Contrepartie" />
-          </div>
-          <RewardSummaryCard.Amount aria-describedby="RewardSummaryList-amount">
-            10 000€
-          </RewardSummaryCard.Amount>
-          <RewardSummaryCard.Contribution aria-describedby="RewardSummaryList-contribution">
-            1 468 000
-          </RewardSummaryCard.Contribution>
-          <RewardSummaryCard.Availability aria-describedby="RewardSummaryList-availability">
-            Illimitée
-          </RewardSummaryCard.Availability>
-          <RewardSummaryCard.Options>
-            <DropdownMenu
-              button={() => <EllipsisIcon color={COLORS.font1} />}
-              menuPosition="left"
-            >
-              <DropdownMenu.Link href="#">Editer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Dupliquer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Désactiver</DropdownMenu.Link>
-              <DropdownMenu.Link>Supprimer</DropdownMenu.Link>
-            </DropdownMenu>
-          </RewardSummaryCard.Options>
-        </RewardSummaryCard>
-
-        <RewardSummaryCard size={size}>
-          <RewardSummaryCard.Image>
-            <img alt="" src="/kitten.jpg" />
-          </RewardSummaryCard.Image>
-          <div style={{ display: 'grid' }}>
-            <RewardSummaryCard.Title aria-describedby="RewardSummaryList-title">
-              Stickers Free Boobs Club
-            </RewardSummaryCard.Title>
-            <RewardSummaryCard.TitleTag text="Contrepartie star" />
-          </div>
-          <RewardSummaryCard.Amount aria-describedby="RewardSummaryList-amount">
-            10 000€
-          </RewardSummaryCard.Amount>
-          <RewardSummaryCard.Contribution aria-describedby="RewardSummaryList-contribution">
-            1 468 000
-          </RewardSummaryCard.Contribution>
-          <RewardSummaryCard.Availability aria-describedby="RewardSummaryList-availability">
-            Illimitée
-          </RewardSummaryCard.Availability>
-          <RewardSummaryCard.Options>
-            <DropdownMenu
-              button={() => <EllipsisIcon color={COLORS.font1} />}
-              menuPosition="left"
-            >
-              <DropdownMenu.Link href="#">Editer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Dupliquer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Désactiver</DropdownMenu.Link>
-              <DropdownMenu.Link>Supprimer</DropdownMenu.Link>
-            </DropdownMenu>
-          </RewardSummaryCard.Options>
-        </RewardSummaryCard>
-        <RewardSummaryCard size={size}>
-          <div style={{ display: 'grid' }}>
-            <RewardSummaryCard.Title aria-describedby="RewardSummaryList-title">
-              Stickers Free Boobs Club
-            </RewardSummaryCard.Title>
-          </div>
-          <RewardSummaryCard.Amount aria-describedby="RewardSummaryList-amount">
-            10 000€
-          </RewardSummaryCard.Amount>
-          <RewardSummaryCard.Contribution aria-describedby="RewardSummaryList-contribution">
-            1 468 000
-          </RewardSummaryCard.Contribution>
-          <RewardSummaryCard.Availability aria-describedby="RewardSummaryList-availability">
-            Illimitée
-          </RewardSummaryCard.Availability>
-          <RewardSummaryCard.Options>
-            <DropdownMenu
-              button={() => <EllipsisIcon color={COLORS.font1} />}
-              menuPosition="left"
-            >
-              <DropdownMenu.Link href="#">Editer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Dupliquer</DropdownMenu.Link>
-              <DropdownMenu.Link href="#">Désactiver</DropdownMenu.Link>
-              <DropdownMenu.Link>Supprimer</DropdownMenu.Link>
-            </DropdownMenu>
-          </RewardSummaryCard.Options>
-        </RewardSummaryCard>
-      </FlexWrapper>
+        <RewardCardComponent
+          size={size}
+          href='#'
+          imageProps={{ src: '/kitten.jpg' }}
+          title='Eiusmod enim officia sed ut dolor elit ea aute.'
+          amount="10 000€"
+          contribution="1 468 000"
+          availability="Illimitée"
+          id="ad9eb082-1d78-47cb-af56-8a7e9659cf03"
+          simpleName="Eiusmod enim officia sed"
+        />
+        <RewardCardComponent
+          size={size}
+          href='#'
+          imageProps={{ src: '/kitten.jpg' }}
+          title='Anim enim deserunt ut mollit cupidatat culpa id consequat ut sit sint culpa qui anim fugiat deserunt laborum.'
+          amount="10 000€"
+          contribution="1 468 000"
+          availability="5"
+          id="72ce89ff-6592-4b9a-9e5f-7f7b3ddd7b26"
+          simpleName="Anim enim deserunt ut mollit"
+        />
+      </StyledDragAndDropList>
     </div>
   )
 }
--- assets/javascripts/kitten/components/molecules/drag-and-drop-list/stories.js
+++ assets/javascripts/kitten/components/molecules/drag-and-drop-list/stories.js
@@ -1,43 +1,138 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
+import { DocsPage } from 'storybook/docs-page'
 import { DragAndDropList } from './index'
 import {
-  HorizontalCrowdfundingCard
-} from '../../..'
+  RewardSummaryCard,
+  useRewardSummaryCardResizeObserver,
+  DropdownMenu,
+  EllipsisIcon,
+  COLORS,
+} from 'kitten'
 
 export default {
   title: 'Molecules/DragAndDropList',
   component: DragAndDropList,
+  decorators: [
+    story => (
+      <div className="story-Container">
+        {story()}
+      </div>
+    ),
+  ],
+  parameters: {
+    docs: {
+      page: () => <DocsPage filepath={__filename} importString="DragAndDropList, DRAG_AND_DROP_LIST_BUTTON_SHIFT" />,
+    },
+  },
 }
 
-export const Default = () => (
-  <DragAndDropList onChange={action('onChange')}>
-    <HorizontalCrowdfundingCard
-      href='#'
-      imageProps={{ src: '/kitten.jpg' }}
-      title='Aliquip aliqua excepteur quis eu laborum do dolore veniam ullamco anim fugiat aliquip consequat aliquip voluptate cupidatat consectetur.'
-      description='Lorem ipsum mollit nostrud in dolor aliqua exercitation deserunt commodo nostrud quis dolor laboris tempor eu ullamco aliquip deserunt sint ut.'
-      progress={15}
-      data-simple-name="Aliquip aliqua excepteur quis eu"
-      data-id="911c90cc-311e-478f-9730-872c468bb9e0"
-    />
-    <HorizontalCrowdfundingCard
-      href='#'
-      imageProps={{ src: '/kitten.jpg' }}
-      title='Eiusmod enim officia sed ut dolor elit ea aute.'
-      description='Lorem ipsum mollit veniam dolor ex dolore minim sit velit in est aliquip dolore.'
-      progress={87}
-      data-simple-name="Eiusmod enim officia sed ut"
-      data-id="ad9eb082-1d78-47cb-af56-8a7e9659cf03"
-    />
-    <HorizontalCrowdfundingCard
-      href='#'
-      imageProps={{ src: '/kitten.jpg' }}
-      title='Anim enim deserunt ut mollit cupidatat culpa id consequat ut sit sint culpa qui anim fugiat deserunt laborum.'
-      description='Dolor anim ullamco veniam non dolore cillum in mollit.'
-      progress={31}
-      data-simple-name="Anim enim deserunt ut mollit"
-      data-id="72ce89ff-6592-4b9a-9e5f-7f7b3ddd7b26"
-    />
-  </DragAndDropList>
+const RewardCardComponent = ({
+  imageProps,
+  title,
+  amount,
+  contribution,
+  availability,
+  star,
+  id,
+  ...props
+}) => (
+  <RewardSummaryCard id={id} {...props}>
+    <RewardSummaryCard.Image>
+      <img alt="" {...imageProps} />
+    </RewardSummaryCard.Image>
+    <div style={{ display: 'grid' }}>
+      <RewardSummaryCard.Title aria-describedby="RewardSummaryList-title">
+        {title}
+      </RewardSummaryCard.Title>
+      {!!star && <RewardSummaryCard.TitleTag text="Contrepartie star" />}
+    </div>
+    <RewardSummaryCard.Amount aria-describedby="RewardSummaryList-amount">
+      {amount}
+    </RewardSummaryCard.Amount>
+    <RewardSummaryCard.Contribution aria-describedby="RewardSummaryList-contribution">
+      {contribution}
+    </RewardSummaryCard.Contribution>
+    <RewardSummaryCard.Availability aria-describedby="RewardSummaryList-availability">
+      {availability}
+    </RewardSummaryCard.Availability>
+    <RewardSummaryCard.Options>
+      <DropdownMenu
+        button={() => <EllipsisIcon color={COLORS.font1} />}
+        menuPosition="left"
+      >
+        <DropdownMenu.Link href="#">Editer</DropdownMenu.Link>
+        <DropdownMenu.Link href="#">Dupliquer</DropdownMenu.Link>
+        <DropdownMenu.Link href="#">Désactiver</DropdownMenu.Link>
+        <DropdownMenu.Link>Supprimer</DropdownMenu.Link>
+      </DropdownMenu>
+    </RewardSummaryCard.Options>
+  </RewardSummaryCard>
 )
+
+export const Default = () => {
+  const { ref, size } = useRewardSummaryCardResizeObserver()
+
+  return (
+    <div ref={ref}>
+      <p id="container_label_element" className="k-u-weight-light k-u-margin-bottom-double">
+        Cette liste peut être réorganisée à l'aide des boutons.
+      </p>
+
+      <p id="description_element" className="k-u-weight-light k-u-margin-bottom-double">
+        Cliquez pour attraper/lâcher l'élément. Une fois attrapé, l'élément peut
+        être déplacé à l'aide des flèches haut/bas du clavier.
+      </p>
+
+      <DragAndDropList
+        onChange={action('onChange')}
+        a11yButtonDescElement="description_element"
+        a11yContainerLabelElement="container_label_element"
+        a11yButtonLabel="Réorganiser"
+        a11yAnnouncement={{
+          grabbed: name => `${name} attrapé`,
+          dropped: name => `${name} relâché`,
+          reorder: (name, position, length) => {
+            return `L’ordre a été changé, ${name} est maintenant en position ${position} sur ${length}`
+          },
+          cancel: 'Réorganisation annulée.',
+        }}
+      >
+        <RewardCardComponent
+          size={size}
+          href="#"
+          imageProps={{ src: '/kitten.jpg' }}
+          title="Aliquip aliqua excepteur quis eu laborum do dolore veniam ullamco anim fugiat aliquip consequat aliquip voluptate cupidatat consectetur."
+          amount="10 000€"
+          contribution="1 468 000"
+          star
+          availability="Illimitée"
+          id="911c90cc-311e-478f-9730-872c468bb9e0"
+          simpleName="Aliquip aliqua excepteur quis"
+        />
+        <RewardCardComponent
+          size={size}
+          href="#"
+          imageProps={{ src: '/kitten.jpg' }}
+          title="Eiusmod enim officia sed ut dolor elit ea aute."
+          amount="10 000€"
+          contribution="1 468 000"
+          availability="Illimitée"
+          id="ad9eb082-1d78-47cb-af56-8a7e9659cf03"
+          simpleName="Eiusmod enim officia sed"
+        />
+        <RewardCardComponent
+          size={size}
+          href="#"
+          imageProps={{ src: '/kitten.jpg' }}
+          title="Anim enim deserunt ut mollit cupidatat culpa id consequat ut sit sint culpa qui anim fugiat deserunt laborum."
+          amount="10 000€"
+          contribution="1 468 000"
+          availability="5"
+          id="72ce89ff-6592-4b9a-9e5f-7f7b3ddd7b26"
+          simpleName="Anim enim deserunt ut mollit"
+        />
+      </DragAndDropList>
+    </div>
+  )
+}
