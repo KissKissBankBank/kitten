ce882c7bfd3214d149a43e84913066edf27574a7
Joachim <joachim.robert@protonmail.com>
2021-10-26 14:38:21 +0200

    add built js files for 5.5.0

--- src/components/molecules/expand/accordeon/components/item.js
+++ src/components/molecules/expand/accordeon/components/item.js
@@ -7,6 +7,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Item = void 0;
 
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
+var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
+
 var _react = _interopRequireDefault(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
@@ -17,10 +21,14 @@ var _content = require("./content");
 
 var _reactElements = require("../../../../../helpers/react/react-elements");
 
+var _classnames = _interopRequireDefault(require("classnames"));
+
 var Item = function Item(_ref) {
   var children = _ref.children,
       id = _ref.id,
-      index = _ref.index;
+      index = _ref.index,
+      className = _ref.className,
+      props = (0, _objectWithoutProperties2.default)(_ref, ["children", "id", "index", "className"]);
   var header = (0, _reactElements.getReactElementsByType)({
     children: children,
     type: _header.Header
@@ -29,9 +37,9 @@ var Item = function Item(_ref) {
     children: children,
     type: _content.Content
   });
-  return /*#__PURE__*/_react.default.createElement("div", {
-    className: "k-Accordeon__item"
-  }, header[0] && _react.default.cloneElement(header[0], {
+  return /*#__PURE__*/_react.default.createElement("div", (0, _extends2.default)({
+    className: (0, _classnames.default)('k-Accordeon__item', className)
+  }, props), header[0] && _react.default.cloneElement(header[0], {
     id: id,
     index: index
   }), content[0] && _react.default.cloneElement(content[0], {
