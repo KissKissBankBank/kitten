868317f5099cd4685c44bdf084f5a14e09fc77e2
Camille Delbove <camille.delbove@gmail.com>
2022-01-05 20:23:41 +0100

    Styles

--- assets/javascripts/kitten/karl/pages/subscription-details/stories.js
+++ assets/javascripts/kitten/karl/pages/subscription-details/stories.js
@@ -1,9 +1,204 @@
 import React from 'react'
-import SubscriptionDetails from './index'
+import { StyledWrapper } from './styles'
+import {
+  Tag,
+  HorizontalStroke,
+  Text,
+  StatusIconNext,
+  Grid,
+  GridCol,
+} from 'kitten'
+import classNames from 'classnames'
 
 export default {
   title: 'pages/SubscriptionDetails',
-  component: SubscriptionDetails,
+}
+const Stroke = ({ className, ...props }) => (
+  <HorizontalStroke
+    {...props}
+    color="var(--color-grey-400)"
+    className={classNames(
+      'k-u-margin-bottom-singleHalf',
+      'k-u-margin-top-singleHalf',
+    )}
+    customSize={{ width: '100%', height: 1 }}
+  />
+)
+
+const List = ({
+  className,
+  subtitleWeight,
+  subtitle,
+  subtitleSize,
+  amount,
+  tag,
+  typeTag,
+  date,
+  ...props
+}) => (
+  <div className="k-SubscriptionDetails__listWrapper" {...props}>
+    <Text weight={subtitleWeight} size={subtitleSize} className="k-SubscriptionDetails__subtitle">{subtitle}</Text>
+    <Text size="tiny" className="k-SubscriptionDetails__amount">{amount}</Text>
+    <Tag type={typeTag} size="small" className="k-SubscriptionDetails__tag">{tag}</Tag>
+    <Text size="tiny" className="k-SubscriptionDetails__date">{date}</Text>
+  </div>
+)
+
+export const ContributorPayment = () => {
+  return (
+    <Grid>
+      <GridCol col-m="8">
+        <StyledWrapper>
+          <div className="k-SubscriptionDetails__title">
+            <div>
+              <Text weight="bold">Abonnement</Text>
+              <Text
+                tag="p"
+                size="micro"
+                weight="light"
+                className="k-u-margin-none"
+                cssColor="var(--color-grey-700"
+              >
+                Souscrit le 22/12/20
+              </Text>
+            </div>
+            <Tag type="success">Actif</Tag>
+          </div>
+          <Stroke />
+
+          <List
+            subtitle="Avril"
+            amount="12 €"
+            subtitleSize="tiny"
+            tag="Validé"
+            typeTag="success"
+            date="le 01/03/21"
+          />
+          <List
+            subtitle="Mars"
+            amount="12 €"
+            subtitleSize="tiny"
+            tag="Validé"
+            typeTag="success"
+            date="le 01/03/21"
+          />
+          <List
+            subtitle="Février"
+            amount="12 €"
+            subtitleSize="tiny"
+            tag="Validé"
+            typeTag="success"
+            date="le 01/03/21"
+          />
+          <List
+            subtitle="Janvier"
+            amount="12 €"
+            subtitleSize="tiny"
+            tag="Validé"
+            typeTag="success"
+            date="le 01/03/21"
+          />
+
+          <Stroke />
+
+          <List
+            subtitleWeight="bold"
+            subtitle="Petit Plus"
+            amount="2 €"
+            tag="Validé"
+            typeTag="success"
+            date="le 01/03/21"
+          />
+        </StyledWrapper>
+      </GridCol>
+    </Grid>
+
+  )
 }
 
-export const Default = () => <SubscriptionDetails />
+export const ContributorSubscription = () => {
+  return (
+    <Grid>
+      <GridCol col-m="8">
+        <StyledWrapper>
+          <div className="k-SubscriptionDetails__gridWrapper">
+            <div className="k-SubscriptionDetails__imageWrapper">
+              <img
+                src={`/kitten-${Math.floor(Math.random() * 10)}.jpg`}
+                alt=""
+              />
+            </div>
+            <div>
+              <Text weight="regular" size="tiny">Club des lecteurs</Text>
+              <Text size="tiny" weight="light"><br />
+                par{' '}
+                <Text weight="bold" size="regular">
+                  Sick Magazine
+                </Text>
+              </Text>
+              <ul className="k-SubscriptionDetails__list">
+                <li className="k-SubscriptionDetails__listItem">
+                  <StatusIconNext status="success" color="var(--color-primary-500)" />
+                  <Text size="tiny">Le nouveau numéro</Text>
+                </li>
+                <li className="k-SubscriptionDetails__listItem">
+                  <StatusIconNext status="success" color="var(--color-primary-500)" />
+                  <Text size="tiny">Les coulisses de l'édition</Text>
+                </li>
+                <li className="k-SubscriptionDetails__listItem">
+                  <StatusIconNext status="success" color="var(--color-primary-500)" />
+                  <Text size="tiny">Un poster édition limitée</Text>
+                </li>
+                <li className="k-SubscriptionDetails__listItem">
+                  <StatusIconNext status="success" color="var(--color-primary-500)" />
+                  <Text size="tiny">Une surprise collector</Text>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+
+          <Stroke />
+          <div className="k-SubscriptionDetails__details">
+            <div>
+              <Text size="tiny">Fréquence</Text>
+              <Text
+                tag="p"
+                weight="bold"
+                size="tiny"
+                className="k-u-margin-none"
+              >
+                Mensuel
+              </Text>
+            </div>
+            <div>
+              <Text size="tiny">Mode de livraison</Text>
+              <Text
+                tag="p"
+                weight="bold"
+                size="tiny"
+                className="k-u-margin-none"
+              >
+                Livraison
+              </Text>
+            </div>
+          </div>
+        
+          <Stroke />
+          <div className="k-SubscriptionDetails__option">
+            <Text size="tiny">Option</Text>
+            <Text
+              tag="p"
+              weight="bold"
+              size="tiny"
+              className="k-u-margin-none"
+            >
+              Végétarien
+            </Text>
+          </div>
+        </StyledWrapper>
+  
+      </GridCol>
+    </Grid>
+  )
+}
--- assets/javascripts/kitten/karl/pages/subscription-details/styles.js
+++ assets/javascripts/kitten/karl/pages/subscription-details/styles.js
@@ -6,12 +6,79 @@ export const StyledWrapper = styled.div`
   border-radius: var(--border-radius-m);
   padding: ${pxToRem(30)};
 
-
   .k-SubscriptionDetails__title {
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
 
+  .k-SubscriptionDetails__listWrapper {
+    display: flex;
+    align-items: center;
+    align-content: flex-start;
+    width: 100%;
+    gap: ${pxToRem(20)};
+    margin-bottom: ${pxToRem(5)};
+  }
 
+  .k-SubscriptionDetails__subtitle {
+    flex: 1;
+  }
+
+  .k-SubscriptionDetails__gridWrapper {
+    display: flex;
+    gap: ${pxToRem(30)};
+
+    @media ${mq.mobile} {
+      display: block;
+    }
+  }
+
+  .k-SubscriptionDetails__imageWrapper {
+    position: relative;
+    overflow: hidden;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-self: stretch;
+    width: ${pxToRem(231)};
+    height: ${pxToRem(137)};
+    
+    @media ${mq.mobile} {
+      height: ${pxToRem(186)};
+      width: 100%;
+      margin-bottom: ${pxToRem(15)};
+    }
+    
+    img,
+    figure,
+    video {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center center;
+      transition: transform 0.2s ease-in-out;
+    }
+  }
+
+  .k-SubscriptionDetails__list {
+    list-style-type: none;
+    padding-inline-start: 0;
+  }
+
+  .k-SubscriptionDetails__listItem {
+    svg {
+      margin-right: ${pxToRem(10)};
+    }
+  }
+
+  .k-SubscriptionDetails__details {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: start;
+  }
 `
