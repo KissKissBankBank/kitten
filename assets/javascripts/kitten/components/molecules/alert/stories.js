3606375c33baad3120a030a40550e83ca7a0e890
Camille Delbove <camille.delbove@gmail.com>
2022-01-18 17:05:09 +0100

    [Storybook] Add `design` addon

--- assets/javascripts/kitten/components/molecules/alert/stories.js
+++ assets/javascripts/kitten/components/molecules/alert/stories.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Alert } from './'
 import { action } from '@storybook/addon-actions'
+import { withDesign } from 'storybook-addon-designs'
 import { DocsPage } from 'storybook/docs-page'
 import { CrossIconNext, COLORS } from 'kitten'
 
 export default {
   title: 'Molecules/Alert',
   component: Alert,
+  decorators: [withDesign],
   parameters: {
     docs: {
       page: () => <DocsPage filepath={__filename} importString="Alert" />,
+    },
+    design: {
+      type: 'Alert',
+      url: 'https://www.figma.com/file/U2KVyc2tmJXkZObaSDOj71/Design-System-Kitten?node-id=1463%3A8374',
     },
   },
   args: {
