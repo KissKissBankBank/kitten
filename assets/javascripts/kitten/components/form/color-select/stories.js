958d1aee409d62ac5fc37c59a8fa693ccd5f8f60
Joachim <joachim.robert@protonmail.com>
2022-01-25 17:59:14 +0100

    Fix story

--- assets/javascripts/kitten/components/form/color-select/stories.js
+++ assets/javascripts/kitten/components/form/color-select/stories.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ColorSelect } from './index'
 import { action } from '@storybook/addon-actions'
 import { DocsPage } from 'storybook/docs-page'
-import { Text } from 'kitten'
+import { Text, Button } from 'kitten'
+import { colord } from 'colord'
 
 export default {
   title: 'Form/ColorSelect',
@@ -46,6 +47,18 @@ export default {
 export const Default = ({ value, ...args }) => {
   const [currentValue, setCurrentValue] = useState(value)
 
+  const [colorsObject, setColorsObject] = useState({})
+
+  useEffect(() => {
+    setColorsObject({
+      '--color-primary-100': colord(currentValue).lighten(.24).toHex(),
+      '--color-primary-300': colord(currentValue).lighten(.12).toHex(),
+      '--color-primary-500': currentValue,
+      '--color-primary-700': colord(currentValue).darken(.12).toHex(),
+      '--color-primary-900': colord(currentValue).darken(.24).toHex(),
+    })
+  }, [currentValue])
+
   const handleChange = color => {
     action('onChange')(color)
     setCurrentValue(color)
@@ -53,7 +66,15 @@ export const Default = ({ value, ...args }) => {
 
   return (
     <>
-      <ColorSelect {...args} value={value} onChange={handleChange} />
+      <ColorSelect {...args} value={value} onChange={handleChange}>
+        <div
+          className="k-u-flex k-u-flex-alignItems-center k-u-flex-justifyContent-end k-u-flex-gap-single"
+          style={colorsObject}
+        >
+          <Text size="tiny">Rendu&nbsp;:</Text>
+          <Button modifier="helium" rounded size="tiny" fit="content">S’abonner</Button>
+        </div>
+      </ColorSelect>
       <div>
         <div
           style={{ width: 100, height: 80, backgroundColor: currentValue }}
@@ -61,6 +82,24 @@ export const Default = ({ value, ...args }) => {
         <Text cssColor={currentValue}>
           <strong className="k-u-strong">{currentValue}</strong>
         </Text>
+        <div className="k-u-flex" style={colorsObject}>
+          <div
+            style={{ width: '20%', flexBasis: '1 0 20%', height: 40, backgroundColor: colorsObject['--color-primary-100'] }}
+          />
+          <div
+            style={{ width: '20%', flexBasis: '1 0 20%', height: 40, backgroundColor: colorsObject['--color-primary-300'] }}
+          />
+          <div
+            style={{ width: '20%', flexBasis: '1 0 20%', height: 40, backgroundColor: colorsObject['--color-primary-500'] }}
+          />
+          <div
+            style={{ width: '20%', flexBasis: '1 0 20%', height: 40, backgroundColor: colorsObject['--color-primary-700'] }}
+          />
+          <div
+            style={{ width: '20%', flexBasis: '1 0 20%', height: 40, backgroundColor: colorsObject['--color-primary-900'] }}
+          />
+
+        </div>
       </div>
     </>
   )
