4043a201eaa1c70a3f9d2e5c9df329f570dc8fcb
Camille Delbove <camilledel@users.noreply.github.com>
2022-02-10 14:20:43 +0100

    [Storybook] Removed design addon (#3015)

--- assets/javascripts/kitten/components/information/alert/stories.js
+++ assets/javascripts/kitten/components/information/alert/stories.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import { Alert } from './'
 import { action } from '@storybook/addon-actions'
-import { withDesign } from 'storybook-addon-designs'
 import { DocsPage } from 'storybook/docs-page'
 import { CrossIconNext, COLORS } from 'kitten'
 
 export default {
   title: 'Information/Alert',
   component: Alert,
-  decorators: [withDesign],
   parameters: {
     docs: {
       page: () => <DocsPage filepath={__filename} importString="Alert" />,
-    },
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/U2KVyc2tmJXkZObaSDOj71/Design-System-Kitten?node-id=1463%3A8374',
     },
   },
   args: {
