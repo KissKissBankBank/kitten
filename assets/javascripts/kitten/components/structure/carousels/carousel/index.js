26daa9a7b0ab27a04c6e6d9f0ab23246755f08f8
Camille Delbove <camilledel@users.noreply.github.com>
2022-04-04 17:46:41 +0200

    Update assets/javascripts/kitten/components/structure/carousels/carousel/index.js
    Co-authored-by: Joachim <joachim.robert@protonmail.com>

--- assets/javascripts/kitten/components/structure/carousels/carousel/index.js
+++ assets/javascripts/kitten/components/structure/carousels/carousel/index.js
@@ -285,7 +285,7 @@ class CarouselBase extends Component {
             className="k-Carousel__pagination__button"
             fit="icon"
             modifier="beryllium"
-            size={smallButtons ? 'tiny' : null}
+            size={smallButtons ? 'small' : null}
             onClick={this.goNextPage}
             disabled={!loop && currentPageIndex >= numberOfPages - 1}
           >
