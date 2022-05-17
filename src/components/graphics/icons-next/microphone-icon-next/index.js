c65c176b5f44b9d059862f10c58e58287bd80c3e
Joachim <joachim.robert@protonmail.com>
2022-05-17 15:52:40 +0200

    v10.9.0 (#3185)
    * add built js files for 10.9.0
    
    * bump kitten version to 10.9.0

---
+++ src/components/graphics/icons-next/microphone-icon-next/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+exports.__esModule = true;
+exports.MicrophoneIconNext = void 0;
+
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
+var _objectWithoutPropertiesLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutPropertiesLoose"));
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _excluded = ["color", "title"];
+
+var MicrophoneIconNext = function MicrophoneIconNext(_ref) {
+  var color = _ref.color,
+      title = _ref.title,
+      props = (0, _objectWithoutPropertiesLoose2.default)(_ref, _excluded);
+  return /*#__PURE__*/_react.default.createElement("svg", (0, _extends2.default)({
+    width: "24",
+    height: "24",
+    viewBox: "0 0 24 24",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, props), title && /*#__PURE__*/_react.default.createElement("title", null, title), /*#__PURE__*/_react.default.createElement("path", {
+    d: "m15.38 16.23.5.02a8.13 8.13 0 1 0-8.11-7.63l-4.65 4.65a5.38 5.38 0 0 0-.93 6.35L.45 21.36a1.55 1.55 0 0 0 2.2 2.19l1.73-1.74a5.37 5.37 0 0 0 6.35-.93l4.65-4.65Zm.5-13.13a5.03 5.03 0 1 1 0 10.06 5.03 5.03 0 0 1 0-10.06ZM6.92 19.35a2.28 2.28 0 0 1-1.61-3.9l3.42-3.42a8.19 8.19 0 0 0 3.23 3.23l-3.42 3.42c-.43.43-1.01.67-1.62.67Z",
+    fill: color
+  }));
+};
+
+exports.MicrophoneIconNext = MicrophoneIconNext;
+MicrophoneIconNext.propTypes = {
+  color: _propTypes.default.string,
+  title: _propTypes.default.string
+};
+MicrophoneIconNext.defaultProps = {
+  color: '#222',
+  title: null
+};
---
+++ src/components/graphics/icons-next/question-bubble-icon-next/index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+exports.__esModule = true;
+exports.QuestionBubbleIconNext = void 0;
+
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
+var _objectWithoutPropertiesLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutPropertiesLoose"));
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _excluded = ["color", "title"];
+
+var QuestionBubbleIconNext = function QuestionBubbleIconNext(_ref) {
+  var color = _ref.color,
+      title = _ref.title,
+      props = (0, _objectWithoutPropertiesLoose2.default)(_ref, _excluded);
+  return /*#__PURE__*/_react.default.createElement("svg", (0, _extends2.default)({
+    width: "30",
+    height: "30",
+    viewBox: "0 0 30 30",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, props), title && /*#__PURE__*/_react.default.createElement("title", null, title), /*#__PURE__*/_react.default.createElement("path", {
+    fill: color,
+    fillRule: "evenodd",
+    clipRule: "evenodd",
+    d: "m10.82 24.71-.98-.4-1.01.3.85 2.88-.85-2.88h-.01l-.03.01-.12.04-.43.13a788.56 788.56 0 0 0-4.4 1.32 1015.56 1015.56 0 0 1 1.31-4.84l.13-.46.03-.12.01-.03v-.01l-2.89-.8 2.9.8.25-.94-.34-.91a12.97 12.97 0 0 1-.87-4.3 11.1 11.1 0 0 1 11.1-11.1A11.1 11.1 0 0 1 26.6 14.5a11.1 11.1 0 0 1-11.11 11.08c-1.66 0-3.28-.3-4.66-.87Zm-8.39-4.86s-1 3.64-1.95 7.22a2.02 2.02 0 0 0 2.55 2.43c3.22-1 6.65-2.01 6.65-2.01 1.77.73 3.8 1.1 5.8 1.1a14.1 14.1 0 1 0 0-28.17A14.1 14.1 0 0 0 1.37 14.5c0 1.72.42 3.66 1.06 5.35Zm12.48 1.97c-.76 0-1.35-.61-1.35-1.36 0-.76.59-1.37 1.35-1.37a1.37 1.37 0 1 1 0 2.73Zm4.65-9.85c0 2-1.4 3.5-3.5 3.92v1.67H13.8v-3.5h.63c1.47 0 2.86-.7 2.86-2.13 0-1.19-1-1.87-2.12-1.87-1.22 0-2.19.53-2.93 1.87l-1.45-1.59a5.22 5.22 0 0 1 4.36-2.37c2.54 0 4.4 1.63 4.4 4Z"
+  }));
+};
+
+exports.QuestionBubbleIconNext = QuestionBubbleIconNext;
+QuestionBubbleIconNext.propTypes = {
+  color: _propTypes.default.string,
+  title: _propTypes.default.string
+};
+QuestionBubbleIconNext.defaultProps = {
+  color: '#222',
+  title: null
+};
