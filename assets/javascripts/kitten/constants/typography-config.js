fbbe466f8c14774b5830bafe23a4ee2438783012
Camille Delbove <camille.delbove@gmail.com>
2022-06-03 16:15:56 +0200

    Update weight, values

--- assets/javascripts/kitten/constants/typography-config.js
+++ assets/javascripts/kitten/constants/typography-config.js
@@ -17,6 +17,19 @@ const TYPOGRAPHY = {
       letterSpacing: '.01rem',
       fontWeight: 700,
     },
+    400: {
+      fontFamily: 'Maax, Helvetica, Arial, sans-serif',
+      fontWeight: 400,
+    },
+    500: {
+      fontFamily: 'Maax, Helvetica, Arial, sans-serif',
+      fontWeight: 500,
+    },
+    700: {
+      fontFamily: 'Maax, Helvetica, Arial, sans-serif',
+      letterSpacing: '.01rem',
+      fontWeight: 700,
+    },
     regularUppercase: {
       fontFamily: 'Maax, Helvetica, Arial, sans-serif',
       fontWeight: 500,
