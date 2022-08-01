b1ee6ab0002def51039d80a10b095fb9ff883452
Joachim <joachim.robert@protonmail.com>
2022-08-01 11:49:06 +0200

    Update stories.js

--- assets/javascripts/kitten/components/interaction/editor-bar/stories.js
+++ assets/javascripts/kitten/components/interaction/editor-bar/stories.js
@@ -56,8 +56,9 @@ export const Default = ({ ...args }) => {
   return (
     <ScrollableContainer
       shadowColor={`var(--color-grey-${args.theme === 'grey' ? 2 : 0}00)`}
+      className={`k-u-background`}
     >
-      <EditorBar {...args}>
+      <EditorBar {...args} className="k-u-flex-grow-single">
         <EditorBar.List currentIcon={<Title3Icon />}>
           <EditorBar.Item onClick={action('Clicked Title1')}>
             <Title1Icon />
