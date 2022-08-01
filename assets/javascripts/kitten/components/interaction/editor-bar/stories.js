1f11746bc1902f2e2abd121757d6b92e9f23687e
Joachim <joachim.robert@protonmail.com>
2022-08-01 11:42:04 +0200

    Add ScrollableContainer in story

--- assets/javascripts/kitten/components/interaction/editor-bar/stories.js
+++ assets/javascripts/kitten/components/interaction/editor-bar/stories.js
@@ -16,6 +16,7 @@ import {
   Title2Icon,
   Title3Icon,
   Title4Icon,
+  ScrollableContainer,
 } from 'kitten'
 
 export default {
@@ -53,51 +54,53 @@ export default {
 
 export const Default = ({ ...args }) => {
   return (
-    <EditorBar {...args}>
-      <EditorBar.List currentIcon={<Title3Icon />}>
-        <EditorBar.Item onClick={action('Clicked Title1')}>
-          <Title1Icon />
-        </EditorBar.Item>
-        <EditorBar.Item onClick={action('Clicked Title2')}>
-          <Title2Icon />
-        </EditorBar.Item>
-        <EditorBar.Item onClick={action('Clicked Title3')} active>
-          <Title3Icon />
-        </EditorBar.Item>
-        <EditorBar.Item onClick={action('Clicked Title4')}>
-          <Title4Icon />
-        </EditorBar.Item>
-        <EditorBar.Item onClick={action('Clicked Paragraph')}>
-          <ParagraphIcon />
-        </EditorBar.Item>
-      </EditorBar.List>
-      <EditorBar.Group>
-        <EditorBar.Button onClick={action('Clicked AlignLeft')} active>
-          <AlignLeftIcon />
-        </EditorBar.Button>
-        <EditorBar.Button onClick={action('Clicked AlignCenter')}>
-          <AlignCenterIcon />
-        </EditorBar.Button>
-        <EditorBar.Button onClick={action('Clicked AlignRight')}>
-          <AlignRightIcon />
-        </EditorBar.Button>
-      </EditorBar.Group>
-      <EditorBar.Group>
-        <EditorBar.Button onClick={action('Clicked Bold')} active>
-          <BoldIcon />
-        </EditorBar.Button>
-        <EditorBar.Button onClick={action('Clicked Italic')}>
-          <ItalicIcon />
-        </EditorBar.Button>
-      </EditorBar.Group>
-      <EditorBar.Group>
-        <EditorBar.Button onClick={action('Clicked Link')} disabled>
-          <LinkIcon />
-        </EditorBar.Button>
-        <EditorBar.Button onClick={action('Clicked EditorButton')} wide>
-          <EditorButtonIcon />
-        </EditorBar.Button>
-      </EditorBar.Group>
-    </EditorBar>
+    <ScrollableContainer shadowColor={`var(--color-grey-${args.theme === "grey" ? 2 : 0 }00)`}>
+      <EditorBar {...args}>
+        <EditorBar.List currentIcon={<Title3Icon />}>
+          <EditorBar.Item onClick={action('Clicked Title1')}>
+            <Title1Icon />
+          </EditorBar.Item>
+          <EditorBar.Item onClick={action('Clicked Title2')}>
+            <Title2Icon />
+          </EditorBar.Item>
+          <EditorBar.Item onClick={action('Clicked Title3')} active>
+            <Title3Icon />
+          </EditorBar.Item>
+          <EditorBar.Item onClick={action('Clicked Title4')}>
+            <Title4Icon />
+          </EditorBar.Item>
+          <EditorBar.Item onClick={action('Clicked Paragraph')}>
+            <ParagraphIcon />
+          </EditorBar.Item>
+        </EditorBar.List>
+        <EditorBar.Group>
+          <EditorBar.Button onClick={action('Clicked AlignLeft')} active>
+            <AlignLeftIcon />
+          </EditorBar.Button>
+          <EditorBar.Button onClick={action('Clicked AlignCenter')}>
+            <AlignCenterIcon />
+          </EditorBar.Button>
+          <EditorBar.Button onClick={action('Clicked AlignRight')}>
+            <AlignRightIcon />
+          </EditorBar.Button>
+        </EditorBar.Group>
+        <EditorBar.Group>
+          <EditorBar.Button onClick={action('Clicked Bold')} active>
+            <BoldIcon />
+          </EditorBar.Button>
+          <EditorBar.Button onClick={action('Clicked Italic')}>
+            <ItalicIcon />
+          </EditorBar.Button>
+        </EditorBar.Group>
+        <EditorBar.Group>
+          <EditorBar.Button onClick={action('Clicked Link')} disabled>
+            <LinkIcon />
+          </EditorBar.Button>
+          <EditorBar.Button onClick={action('Clicked EditorButton')} wide>
+            <EditorButtonIcon />
+          </EditorBar.Button>
+        </EditorBar.Group>
+      </EditorBar>
+    </ScrollableContainer>
   )
 }
