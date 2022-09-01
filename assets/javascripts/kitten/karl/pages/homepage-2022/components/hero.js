9e40733eb10e6a324bec294a5ec3b8b170428a29
Joachim <joachim.robert@protonmail.com>
2022-09-01 12:00:01 +0200

    Update hero.js

--- assets/javascripts/kitten/karl/pages/homepage-2022/components/hero.js
+++ assets/javascripts/kitten/karl/pages/homepage-2022/components/hero.js
@@ -149,8 +149,8 @@ const HeroWrapper = styled.section`
 
     img,
     svg {
-      max-width: 100%;
-      max-height: 100%;
+      width: 100%;
+      height: 100%;
       aspect-ratio: 1 / 1;
       object-fit: cover;
       object-position: center;
