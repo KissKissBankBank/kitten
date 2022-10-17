6c8b20886fab7635fc225b10ae8b4d67743609fb
Camille Delbove <camille.delbove@gmail.com>
2022-10-17 17:13:01 +0200

    Update pricing

--- assets/javascripts/kitten/karl/pages/homepage-2022/components/pricing.js
+++ assets/javascripts/kitten/karl/pages/homepage-2022/components/pricing.js
@@ -86,6 +86,28 @@ const PricingWrapper = styled.section`
       letter-spacing: 0.1em;
       text-transform: uppercase;
     }
+
+    .kiss-Homepage__pricing__card__sticker {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      height: ${pxToRem(22)};
+      box-sizing: border-box;
+      top: ${pxToRem(-11)};
+      left: 46%;
+      max-width: calc(100% - ${pxToRem(2 * (10 + 15))});
+  
+      line-height: ${pxToRem(22)};
+      background-color: var(--color-primary-500);
+      border-radius: var(--border-radius-rounded);
+      padding: ${pxToRem(0)} ${pxToRem(10)};
+  
+      font: var(--font-weight-500) var(--font-size-2) / 1
+      var(--font-family-maax);
+      color: var(--color-grey-000);
+    }
+
     .kiss-Homepage__pricing__card__rate {
       margin: ${pxToRem(10)} 0 0;
       font: var(--font-weight-700) var(--font-size-10) / 1
@@ -97,6 +119,11 @@ const PricingWrapper = styled.section`
       font: var(--font-weight-400) var(--font-size-6) / 1
         var(--font-family-maax);
       letter-spacing: -0.01em;
+    }
+    .kiss-Homepage__New__card__rateComplement__bis {
+      font: var(--font-weight-400) var(--font-size-2) / 1
+        var(--font-family-maax);
+      color: var(--color-grey-900);
     }
     .kiss-Homepage__pricing__card__description {
       margin: 0;
@@ -128,8 +155,11 @@ const PricingWrapper = styled.section`
 
 const PricingCard = ({
   name,
+  sticker,
   rate,
-  rateComplement = 'HT',
+  rateComplement,
+  rateComplementBis,
+  rateDescription,
   description,
   actionButtonProps = {},
   listItems = [],
@@ -143,13 +173,24 @@ const PricingCard = ({
     })}
   >
     <div>
+      {sticker && (
+        <div className="kiss-Homepage__pricing__card__sticker k-u-ellipsis">
+          {sticker}
+        </div>
+      )}
       <h2 className="kiss-Homepage__pricing__card__name">{name}</h2>
       <p className="kiss-Homepage__pricing__card__rate">
         {rate}{' '}
         <small className="kiss-Homepage__pricing__card__rateComplement">
-          {rateComplement}
+          {rateComplement}{' '} 
+          <span className="kiss-Homepage__New__card__rateComplement__bis">
+            {rateComplementBis}
+          </span>
         </small>
       </p>
+      <Text size="micro" className="kiss-Homepage__pricing__card-rateDescription">
+        {rateDescription}
+      </Text>
     </div>
     <Paragraph
       modifier="quinary"
@@ -222,9 +263,8 @@ const Pricing = () => {
       <div className="kiss-Homepage__pricing__cards">
         <PricingCard
           name="Starter"
-          rate="0%"
-          rateComplement="HT soit x% TTC"
-          description="Petit texte descriptif Petit texte descriptif Petit texte descriptif Petit texte descriptif Petit texte descriptif "
+          rate="Gratuit"
+          description="Idéal si vous débutez et cherchez surtout à récolter des dons pour financer votre projet jusqu’à plusieurs milliers d’euros auprès de vos amis et amis d’amis."
           actionButtonProps={{
             as: 'a',
             href: '#button',
@@ -245,9 +285,12 @@ const Pricing = () => {
 
         <PricingCard
           main
+          sticker="Recommandé"
           name="Pro"
           rate="5%"
           rateComplement="HT"
+          rateComplementBis="SOIT 6% TTC"
+          rateDescription="du montant collecté sur KissKissBankBank"
           description="Idéal si vous avez déjà une solide communauté existante et cherchez à récolter plusieurs dizaines de milliers d’euros ou des précommandes."
           actionButtonProps={{
             as: 'a',
@@ -273,7 +316,9 @@ const Pricing = () => {
           name="Expert"
           rate="10%"
           rateComplement="HT"
-          description="Idéal si vous connaissez parfaitement les mécaniques de communauté et de financement participatif et souhaitez des fonctionnalités avancées pour optimiser "
+          rateComplementBis="SOIT 6% TTC"
+          rateDescription="du montant collecté sur KissKissBankBank"
+          description="Idéal si vous connaissez parfaitement les mécaniques de communauté et de financement participatif et souhaitez des fonctionnalités avancées pour optimiser votre campagne en continu."
           actionButtonProps={{
             as: 'a',
             href: '#button',
