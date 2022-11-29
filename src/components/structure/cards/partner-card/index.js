f7518734a361230473133f4c537477586b9bb844
Camille Delbove <camilledel@users.noreply.github.com>
2022-11-29 11:10:50 +0100

    add built js files for 13.3.0 (#3369)

--- src/components/structure/cards/partner-card/index.js
+++ src/components/structure/cards/partner-card/index.js
@@ -20,13 +20,14 @@ var _tag = require("../../../information/tag");
 const StyledCard = _styledComponents.default.div.withConfig({
   displayName: "partner-card__StyledCard",
   componentId: "sc-p435k7-0"
-})(["position:relative;display:flex;flex-direction:column;box-sizing:border-box;justify-content:center;border-radius:var(--border-radius-m);color:var(--color-grey-900);transition:box-shadow var(--transition);box-shadow:var(--box-shadow-m);background-color:var(--color-grey-000);&[href]:focus-visible{outline:var(--outline);}.k-PartnerCard__image{position:relative;overflow:hidden;flex:0 0 auto;border-top-left-radius:calc(var(--border-radius-m) - 1px);border-top-right-radius:calc(var(--border-radius-m) - 1px);.k-PartnerCard__image__image{display:block;width:100%;object-fit:cover;object-position:center;@supports (aspect-ratio:16 / 10){aspect-ratio:16 / 10;}}@supports not (aspect-ratio:16 / 10){height:0;padding-bottom:calc((10 / 16) * 100%);.k-PartnerCard__image__image{position:absolute;width:100%;height:100%;}}}.k-PartnerCard__content{flex:1 0 auto;position:relative;text-align:center;}.k-PartnerCard__avatar{position:absolute;width:", ";height:", ";padding-block:", ";box-sizing:border-box;left:calc(50% - ", " /2);top:-", ";background-color:var(--color-grey-000);border-radius:var(--border-radius-rounded);padding:", ";box-shadow:0px 0px ", " rgba(0,0,0,0.1);img{width:100%;height:100%;object-fit:contain;object-position:center;}}.k-PartnerCard__line{padding:", " ", " ", ";flex:0 0 auto;}"], (0, _typography.pxToRem)(130), (0, _typography.pxToRem)(75), (0, _typography.pxToRem)(24), (0, _typography.pxToRem)(130), (0, _typography.pxToRem)(37), (0, _typography.pxToRem)(10), (0, _typography.pxToRem)(20), (0, _typography.pxToRem)(60), (0, _typography.pxToRem)(20), (0, _typography.pxToRem)(20));
+})(["position:relative;display:flex;flex-direction:column;box-sizing:border-box;justify-content:center;border-radius:var(--border-radius-m);color:var(--color-grey-900);transition:box-shadow var(--transition);box-shadow:var(--box-shadow-m);background-color:var(--color-grey-000);&[href]:focus-visible{outline:var(--outline);}&.k-PartnerCard--isStretched{height:100%;}.k-PartnerCard__image{position:relative;overflow:hidden;flex:0 0 auto;border-top-left-radius:calc(var(--border-radius-m) - 1px);border-top-right-radius:calc(var(--border-radius-m) - 1px);.k-PartnerCard__image__image{display:block;width:100%;object-fit:cover;object-position:center;@supports (aspect-ratio:16 / 10){aspect-ratio:16 / 10;}}@supports not (aspect-ratio:16 / 10){height:0;padding-bottom:calc((10 / 16) * 100%);.k-PartnerCard__image__image{position:absolute;width:100%;height:100%;}}}.k-PartnerCard__content{flex:1 0 auto;position:relative;text-align:center;display:flex;flex-direction:column;gap:", ";justify-content:flex-end;}.k-PartnerCard__avatar{position:absolute;width:", ";height:", ";padding-block:", ";box-sizing:border-box;left:calc(50% - ", " /2);top:-", ";background-color:var(--color-grey-000);border-radius:var(--border-radius-rounded);padding:", ";box-shadow:0px 0px ", " rgba(0,0,0,0.1);img{width:100%;height:100%;object-fit:contain;object-position:center;}}.k-PartnerCard__line{padding:", " ", " ", ";flex:0 0 auto;}"], (0, _typography.pxToRem)(10), (0, _typography.pxToRem)(130), (0, _typography.pxToRem)(75), (0, _typography.pxToRem)(24), (0, _typography.pxToRem)(130), (0, _typography.pxToRem)(37), (0, _typography.pxToRem)(10), (0, _typography.pxToRem)(20), (0, _typography.pxToRem)(60), (0, _typography.pxToRem)(20), (0, _typography.pxToRem)(20));
 
 const PartnerCard = _ref => {
   let {
     children,
     className,
     href,
+    stretch,
     imageProps: {
       alt,
       imageClassName,
@@ -37,7 +38,11 @@ const PartnerCard = _ref => {
   return /*#__PURE__*/_react.default.createElement(StyledCard, (0, _extends2.default)({
     as: href ? 'a' : 'div',
     href: href
-  }, props), /*#__PURE__*/_react.default.createElement("div", {
+  }, props, {
+    className: (0, _classnames.default)('k-PartnerCard', className, {
+      'k-PartnerCard--isStretched': stretch
+    })
+  }), /*#__PURE__*/_react.default.createElement("div", {
     className: "k-PartnerCard__image"
   }, /*#__PURE__*/_react.default.createElement("img", (0, _extends2.default)({}, otherImageProps, {
     alt: alt || '',
--- src/esm/components/structure/cards/partner-card/index.js
+++ src/esm/components/structure/cards/partner-card/index.js
@@ -7,12 +7,13 @@ import { Tag } from '../../../information/tag';
 const StyledCard = styled.div.withConfig({
   displayName: "partner-card__StyledCard",
   componentId: "sc-p435k7-0"
-})(["position:relative;display:flex;flex-direction:column;box-sizing:border-box;justify-content:center;border-radius:var(--border-radius-m);color:var(--color-grey-900);transition:box-shadow var(--transition);box-shadow:var(--box-shadow-m);background-color:var(--color-grey-000);&[href]:focus-visible{outline:var(--outline);}.k-PartnerCard__image{position:relative;overflow:hidden;flex:0 0 auto;border-top-left-radius:calc(var(--border-radius-m) - 1px);border-top-right-radius:calc(var(--border-radius-m) - 1px);.k-PartnerCard__image__image{display:block;width:100%;object-fit:cover;object-position:center;@supports (aspect-ratio:16 / 10){aspect-ratio:16 / 10;}}@supports not (aspect-ratio:16 / 10){height:0;padding-bottom:calc((10 / 16) * 100%);.k-PartnerCard__image__image{position:absolute;width:100%;height:100%;}}}.k-PartnerCard__content{flex:1 0 auto;position:relative;text-align:center;}.k-PartnerCard__avatar{position:absolute;width:", ";height:", ";padding-block:", ";box-sizing:border-box;left:calc(50% - ", " /2);top:-", ";background-color:var(--color-grey-000);border-radius:var(--border-radius-rounded);padding:", ";box-shadow:0px 0px ", " rgba(0,0,0,0.1);img{width:100%;height:100%;object-fit:contain;object-position:center;}}.k-PartnerCard__line{padding:", " ", " ", ";flex:0 0 auto;}"], pxToRem(130), pxToRem(75), pxToRem(24), pxToRem(130), pxToRem(37), pxToRem(10), pxToRem(20), pxToRem(60), pxToRem(20), pxToRem(20));
+})(["position:relative;display:flex;flex-direction:column;box-sizing:border-box;justify-content:center;border-radius:var(--border-radius-m);color:var(--color-grey-900);transition:box-shadow var(--transition);box-shadow:var(--box-shadow-m);background-color:var(--color-grey-000);&[href]:focus-visible{outline:var(--outline);}&.k-PartnerCard--isStretched{height:100%;}.k-PartnerCard__image{position:relative;overflow:hidden;flex:0 0 auto;border-top-left-radius:calc(var(--border-radius-m) - 1px);border-top-right-radius:calc(var(--border-radius-m) - 1px);.k-PartnerCard__image__image{display:block;width:100%;object-fit:cover;object-position:center;@supports (aspect-ratio:16 / 10){aspect-ratio:16 / 10;}}@supports not (aspect-ratio:16 / 10){height:0;padding-bottom:calc((10 / 16) * 100%);.k-PartnerCard__image__image{position:absolute;width:100%;height:100%;}}}.k-PartnerCard__content{flex:1 0 auto;position:relative;text-align:center;display:flex;flex-direction:column;gap:", ";justify-content:flex-end;}.k-PartnerCard__avatar{position:absolute;width:", ";height:", ";padding-block:", ";box-sizing:border-box;left:calc(50% - ", " /2);top:-", ";background-color:var(--color-grey-000);border-radius:var(--border-radius-rounded);padding:", ";box-shadow:0px 0px ", " rgba(0,0,0,0.1);img{width:100%;height:100%;object-fit:contain;object-position:center;}}.k-PartnerCard__line{padding:", " ", " ", ";flex:0 0 auto;}"], pxToRem(10), pxToRem(130), pxToRem(75), pxToRem(24), pxToRem(130), pxToRem(37), pxToRem(10), pxToRem(20), pxToRem(60), pxToRem(20), pxToRem(20));
 export const PartnerCard = _ref => {
   let {
     children,
     className,
     href,
+    stretch,
     imageProps: {
       alt,
       imageClassName,
@@ -23,7 +24,11 @@ export const PartnerCard = _ref => {
   return /*#__PURE__*/React.createElement(StyledCard, _extends({
     as: href ? 'a' : 'div',
     href: href
-  }, props), /*#__PURE__*/React.createElement("div", {
+  }, props, {
+    className: classNames('k-PartnerCard', className, {
+      'k-PartnerCard--isStretched': stretch
+    })
+  }), /*#__PURE__*/React.createElement("div", {
     className: "k-PartnerCard__image"
   }, /*#__PURE__*/React.createElement("img", _extends({}, otherImageProps, {
     alt: alt || '',
